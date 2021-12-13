b72226cdfeb7b3a4a7b6facf42ffae3ed5c47426
taniaholst <tania.holst@nav.no>
2021-12-13 12:30:37 +0100

    tc-242 La til test før testene kjører om at det ikke finnes tiltaksvarianter ved navn Test tiltaksvariant for Cypress

--- frontend/cypress/integration/crud_spec.js
+++ frontend/cypress/integration/crud_spec.js
@@ -2,7 +2,31 @@ before('Start server', () => {
   cy.configure();
 });
 
-//TODO sjekk om det allerede eksisterer tiltaksvarianter ved navn "Test tiltaksvariant for Cypress", og slett de
+describe('Sjekk om det finnes test-tiltaksvariant fra før', () => {
+  const tittel = 'Test tiltaksvariant for Cypress';
+  it('Slett test-tiltaksvariant', () => {
+    cy.getByTestId('header-tiltaksvarianter').should('contain', 'Tiltaksvarianter');
+    cy.getByTestId('tabell_oversikt-tiltaksvarianter').children().children().should('have.length.at.least', 1);
+    cy.checkPageA11y();
+    cy.getByTestId('sokefelt_tiltaksvariant').focus().type(tittel);
+
+    cy.getByTestId('tabell_oversikt-tiltaksvarianter')
+      .find('tr')
+      .then($tiltak => {
+        if (Cypress.$($tiltak).length > 1) {
+          $tiltak.filter('.tabell__row').each(() => {
+            cy.getByTestId('tabell_oversikt-tiltaksvarianter').contains('td', tittel).click();
+            cy.getByTestId('tiltaksvariant_header').should('contain', tittel);
+            cy.getByTestId('knapp_rediger-tiltaksvariant').click();
+            cy.getByTestId('slett-knapp_rediger-tiltaksvariant').click();
+            cy.getByTestId('rediger-tiltaksvariant__slett-modal__knapperad').click();
+            cy.get(`.Toastify__toast-container`).should('contain', 'Sletting vellykket');
+          });
+        }
+        cy.getByTestId('sokefelt_tiltaksvariant').focus().clear();
+      });
+  });
+});
 
 describe('CRUD tiltaksvariant', () => {
   const tittel = 'Test tiltaksvariant for Cypress';
@@ -18,6 +42,7 @@ describe('CRUD tiltaksvariant', () => {
   beforeEach('Skal være på listevisninga', () => {
     cy.getByTestId('header-tiltaksvarianter').should('contain', 'Tiltaksvarianter');
     cy.getByTestId('tabell_oversikt-tiltaksvarianter').children().children().should('have.length.at.least', 1);
+    cy.checkPageA11y();
   });
 
   afterEach('Fjerner søketekst', () => {
@@ -28,8 +53,6 @@ describe('CRUD tiltaksvariant', () => {
     const borderWhite = 'rgb(106, 106, 106)';
     const borderRed = 'rgb(186, 58, 38)';
     const feilmeldingTomtFelt = 'Dette feltet kan ikke være tomt';
-
-    cy.checkPageA11y();
 
     cy.getByTestId('tabell_oversikt-tiltaksvarianter')
       .find('.tabell__row')
@@ -124,6 +147,9 @@ describe('CRUD tiltaksvariant', () => {
 
     cy.getByTestId('tabell_oversikt-tiltaksvarianter').contains('td', tittel).click();
 
+    cy.url().should('include', '/tiltaksvarianter/');
+    cy.checkPageA11y();
+
     cy.getByTestId('tiltaksvariant_header').should('contain', tittel);
     cy.getByTestId('tiltaksvariant_ingress').should('contain', nyIngress);
     cy.getByTestId('tiltaksvariant_beskrivelse').should('contain', nyBeskrivelse);
@@ -154,7 +180,7 @@ describe('CRUD tiltaksvariant', () => {
     cy.getByTestId('tabell_tiltaksgjennomforinger')
       .children()
       .children()
-      .should('have.length.at.least', 1)
+      .should('have.length.at.least', 2)
       .last()
       .click();
 
