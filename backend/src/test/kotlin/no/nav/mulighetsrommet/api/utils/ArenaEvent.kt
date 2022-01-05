c2751e564959f37311956d18a38282c0bd9f9cbc
Steffen Lien <steffen.lien@nav.no>
2022-01-05 09:13:30 +0100

    remove unused kafka test

--- backend/src/test/kotlin/no/nav/mulighetsrommet/api/utils/ArenaEvent.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.mulighetsrommet.api.utils
-
-import kotlinx.serialization.Serializable
-
-// Foreløpig syntetisk event fra Arena. Blir byttet ut med noe mer vettugt når vi vet mer hva Arena sender.
-@Serializable
-data class ArenaEvent(
-    val tiltaksNummer: Int
-)
