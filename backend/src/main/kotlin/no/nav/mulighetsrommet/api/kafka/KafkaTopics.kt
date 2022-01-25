0c1eb79541466b86f5739caf637bf42b35c15e87
Steffen Lien <steffen.lien@nav.no>
2022-01-25 19:11:25 +0100

    limit amount of topics for testing

--- backend/src/main/kotlin/no/nav/mulighetsrommet/api/kafka/KafkaTopics.kt
+++ backend/src/main/kotlin/no/nav/mulighetsrommet/api/kafka/KafkaTopics.kt
@@ -2,8 +2,8 @@ package no.nav.mulighetsrommet.api.kafka
 
 enum class KafkaTopics(val topic: String) {
     TiltaksgjennomforingEndret("teamarenanais.aapen-arena-tiltakgjennomforingendret-v1-q2"),
-    TiltaksdeltakerEndret("teamarenanais.aapen-arena-tiltakdeltakerendret-v1-q2"),
-    TiltaksgruppeEndret("teamarenanais.aapen-arena-tiltaksgruppeendret-v1-q2"),
-    TiltakEndret("teamarenanais.aapen-arena-tiltakendret-v1-q2"),
-    AvtaleinfoEndret("teamarenanais.aapen-arena-avtaleinfoendret-v1-q2")
+    // TiltaksdeltakerEndret("teamarenanais.aapen-arena-tiltakdeltakerendret-v1-q2"),
+    // TiltaksgruppeEndret("teamarenanais.aapen-arena-tiltaksgruppeendret-v1-q2"),
+    // TiltakEndret("teamarenanais.aapen-arena-tiltakendret-v1-q2"),
+    // AvtaleinfoEndret("teamarenanais.aapen-arena-avtaleinfoendret-v1-q2")
 }
