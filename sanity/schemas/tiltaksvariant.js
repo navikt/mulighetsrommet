44ffb3875873a129e00a2b597d94e524b946bd23
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-02-03 17:52:59 +0100

    setup and install a sanity content studio

---
+++ sanity/schemas/tiltaksvariant.js
@@ -0,0 +1,39 @@
+export default {
+  name: "tiltaksvariant",
+  title: "Tiltaksvariant",
+  type: "document",
+  fields: [
+    {
+      name: "tiltakskode",
+      title: "Tiltakskode",
+      type: "slug",
+      description: "Tiltakskode fra Arena, f.eks. 'INDOPPFAG'.",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "title",
+      title: "Tittel",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "promoImage",
+      title: "Promobilde",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "about",
+      title: "Om tiltaket",
+      type: "blockContent",
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      media: "promoImage",
+    },
+  },
+};
