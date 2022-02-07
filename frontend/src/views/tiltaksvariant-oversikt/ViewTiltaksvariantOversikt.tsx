b13b8107110f7f9c534441f7c8a4ea1e14060033
taniaholst <tania.holst@nav.no>
2022-02-07 15:32:51 +0100

    tc-10 la til filter for innsatsgrupper, endringer i oppsett av styling

--- frontend/src/views/tiltaksvariant-oversikt/ViewTiltaksvariantOversikt.tsx
+++ frontend/src/views/tiltaksvariant-oversikt/ViewTiltaksvariantOversikt.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Tiltaksvariantoversikt from '../../components/tiltaksvariantoversikt/Tiltaksvariantoversikt';
 import Sokefelt from '../../components/filtrering/Sokefelt';
 import './TiltaksvariantOversikt.less';
+import '../../layouts/MainView.less';
 import MainView from '../../layouts/MainView';
 import useTiltaksvarianter from '../../hooks/tiltaksvariant/useTiltaksvarianter';
-import { ReactComponent as AddCircle } from '../../ikoner/AddCircle.svg';
-import { Button, Loader } from '@navikt/ds-react';
+import { Loader } from '@navikt/ds-react';
 import { useHistory } from 'react-router-dom';
+import Sidebar from '../../components/sidebar/Sidebar';
 
 const TiltaksvariantOversikt = () => {
   const { data, isFetching } = useTiltaksvarianter();
@@ -17,17 +18,19 @@ const TiltaksvariantOversikt = () => {
       title="Tiltaksvarianter"
       subTitle="Se en oversikt over alle nasjonale tiltaksvarianter"
       dataTestId="header-tiltaksvarianter"
+      className="tiltaksvariant-oversikt"
     >
-      <div className="tiltaksvarianter-oversikt-actions">
+      <Sidebar />
+      <div className="tiltaksvariant-oversikt__actions">
         <Sokefelt />
-        <Button
-          variant="primary"
-          className="knapp opprett-ny-tiltaksvariant__knapp"
-          data-testid="knapp_opprett-tiltaksvariant"
-          onClick={() => history.push('/tiltaksvarianter/opprett')}
-        >
-          Opprett tiltaksvariant <AddCircle />
-        </Button>
+        {/*<Button*/}
+        {/*  variant="primary"*/}
+        {/*  className="knapp opprett-ny-tiltaksvariant__knapp"*/}
+        {/*  data-testid="knapp_opprett-tiltaksvariant"*/}
+        {/*  onClick={() => history.push('/tiltaksvarianter/opprett')}*/}
+        {/*>*/}
+        {/*  Opprett tiltaksvariant <AddCircle />*/}
+        {/*</Button>*/}
       </div>
       {isFetching && <Loader variant="neutral" size="2xlarge" />}
       {data && <Tiltaksvariantoversikt tiltaksvarianter={data} />}
