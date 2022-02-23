a7ff3279e5f7c610fd57babf207ae6764aa3235a
Steffen Lien <steffen.lien@nav.no>
2022-02-23 15:36:12 +0100

    refactor data model for tiltakstype

---
+++ backend/src/main/resources/db/migration/V21__alter_table_tiltakstype.sql
@@ -0,0 +1,17 @@
+-- Add new columns to table
+ALTER TABLE tiltakstype
+    ADD COLUMN sanity_id   INT,
+    ADD COLUMN tiltakskode tiltakskode,
+    ADD COLUMN dato_fra TIMESTAMP,
+    ADD COLUMN dato_til TIMESTAMP,
+    ADD COLUMN created_by TEXT,
+    ADD COLUMN updated_by TEXT;
+
+-- Change current table
+ALTER TABLE tiltakstype
+    RENAME COLUMN tittel TO navn;
+
+ALTER TABLE tiltakstype
+    DROP COLUMN beskrivelse,
+    DROP COLUMN ingress,
+    DROP COLUMN archived;
