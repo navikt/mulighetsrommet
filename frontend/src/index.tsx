84c2726881db592c1b0fa94741c24d2bebb32b5b
taniaholst <tania.holst@nav.no>
2022-02-25 14:06:52 +0100

    tc-22 prøver å fikse index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,27 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '@navikt/ds-css';
 import './index.less';
-import store from './core/redux/Store';
-import { Provider } from 'react-redux';
 import { OpenAPI } from './api';
 import Navspa from '@navikt/navspa';
+import { worker } from './mock/worker';
 
 OpenAPI.BASE = String(process.env.REACT_APP_BACKEND_API_ROOT);
 
 require('dotenv').config();
 
 if (process.env.REACT_APP_ENABLE_MOCK) {
-  const { worker } = require('./mock/worker');
   worker.start();
-}
 
-if (process.env.REACT_APP_ENABLE_MOCK) {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('mulighetsrommet-root')
-  );
+  const elem = document.createElement('div');
+  document.body.appendChild(elem);
+  ReactDOM.render(<App />, elem);
+  Navspa.eksporter('mulighetsrommet-flate', App);
 } else {
   Navspa.eksporter('mulighetsrommet-flate', App);
 }
