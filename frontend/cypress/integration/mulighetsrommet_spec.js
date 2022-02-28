9e3271eaa40a2d3327e55b3b6b12bc2245f388b8
taniaholst <tania.holst@nav.no>
2022-02-28 11:01:05 +0100

    la til fake test

---
+++ frontend/cypress/integration/mulighetsrommet_spec.js
@@ -0,0 +1,14 @@
+before('Start server', () => {
+  cy.configure();
+});
+
+describe('Mulighetsrommet', () => {
+  //TODO fiks denne når ny frontend er klar
+  // it('Check page a11y', () => {
+  //   cy.checkPageA11y();
+  // });
+
+  it('Testytest', () => {
+    cy.getByTestId('tabell_oversikt-tiltakstyper').find('tr').should('have.length.greaterThan', 1);
+  });
+});
