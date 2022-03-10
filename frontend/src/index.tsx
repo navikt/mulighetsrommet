260782171cc0d980e7f4fb48380ea8e5d038af17
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-10 15:21:07 +0100

    fix: set OpenAPI base url to empty path

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,10 @@ import App from './App';
 import '@navikt/ds-css';
 import './index.less';
 import { OpenAPI } from './api';
-import Navspa from '@navikt/navspa';
+// import Navspa from '@navikt/navspa';
 import { worker } from './mock/worker';
 
-OpenAPI.BASE = String(import.meta.env.VITE_BACKEND_API_ROOT);
+OpenAPI.BASE = String(import.meta.env.VITE_BACKEND_API_ROOT ?? '');
 
 if (import.meta.env.VITE_ENABLE_MOCK === 'true') {
   worker.start();
