443bb257ba082fb3d6e06ab7dfdf2db48841e62a
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-14 10:05:32 +0100

    fix: access application config from environment
    This makes it possible to overwrite the config per unit test

--- backend/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
+++ backend/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
@@ -1,24 +1,10 @@
 package no.nav.mulighetsrommet.api
 
-import com.typesafe.config.ConfigFactory
-import io.ktor.application.Application
-import io.ktor.application.ApplicationStopped
-import io.ktor.application.log
-import io.ktor.config.HoconApplicationConfig
-import io.ktor.routing.routing
+import io.ktor.application.*
+import io.ktor.routing.*
 import no.nav.mulighetsrommet.api.kafka.KafkaFactory
-import no.nav.mulighetsrommet.api.plugins.configureDependencyInjection
-import no.nav.mulighetsrommet.api.plugins.configureHTTP
-import no.nav.mulighetsrommet.api.plugins.configureMonitoring
-import no.nav.mulighetsrommet.api.plugins.configureRouting
-import no.nav.mulighetsrommet.api.plugins.configureSecurity
-import no.nav.mulighetsrommet.api.plugins.configureSerialization
-import no.nav.mulighetsrommet.api.plugins.configureWebjars
-import no.nav.mulighetsrommet.api.routes.healthRoutes
-import no.nav.mulighetsrommet.api.routes.innsatsgruppeRoutes
-import no.nav.mulighetsrommet.api.routes.swaggerRoutes
-import no.nav.mulighetsrommet.api.routes.tiltaksgjennomforingRoutes
-import no.nav.mulighetsrommet.api.routes.tiltakstypeRoutes
+import no.nav.mulighetsrommet.api.plugins.*
+import no.nav.mulighetsrommet.api.routes.*
 import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -27,8 +13,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
 
     // TODO: Fiks litt bedre config-oppsett for hele appen, sett i app context isteden.
-    val appConfig = HoconApplicationConfig(ConfigFactory.load())
-    val enableKafka = appConfig.property("ktor.kafka.enable").getString().toBoolean()
+    val enableKafka = environment.config.property("ktor.kafka.enable").getString().toBoolean()
 
     configureDependencyInjection()
     configureRouting()
@@ -47,8 +32,8 @@ fun Application.module() {
     }
 
     // TODO: Lag noe som er litt mer robust. Kun for å få deployet.
+    log.info("Kafka enabled=$enableKafka")
     if (enableKafka) {
-        log.debug("Kafka is enabled")
         val kafka: KafkaFactory by inject()
         environment.monitor.subscribe(ApplicationStopped) {
             println("Shutting down")
