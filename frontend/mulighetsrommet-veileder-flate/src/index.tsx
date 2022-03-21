f7a4c409c3e7e7ae224acc466d8bb79b4b2aaa91
taniaholst <tania.holst@nav.no>
2022-03-21 15:08:52 +0100

    la til navspa

--- frontend/mulighetsrommet-veileder-flate/src/index.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/index.tsx
@@ -4,18 +4,14 @@ import App from './App';
 import '@navikt/ds-css';
 import './index.less';
 import { OpenAPI } from 'mulighetsrommet-api';
-// import Navspa from '@navikt/navspa';
+import Navspa from '@navikt/navspa';
 import { worker } from './mock/worker';
 
 OpenAPI.BASE = String(import.meta.env.VITE_BACKEND_API_ROOT ?? '');
 
 if (import.meta.env.VITE_ENABLE_MOCK === 'true') {
   worker.start();
+  ReactDOM.render(<App />, document.getElementById('mulighetsrommet-root'));
+} else {
+  Navspa.eksporter('mulighetsrommet-veileder-flate', App);
 }
-
-ReactDOM.render(<App />, document.getElementById('mulighetsrommet-root'));
-
-// TODO skal se på dette sammen med Håkon
-// else if (import.meta.env.VITE_ENABLE_MOCK === 'true') {
-//   Navspa.eksporter('mulighetsrommet-veileder-flate', App);
-// }
