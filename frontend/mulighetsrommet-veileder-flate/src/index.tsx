906b643814bf9a2e6adbf580c483b24c491dc27b
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-22 10:17:19 +0100

    debug app properties

--- frontend/mulighetsrommet-veileder-flate/src/index.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/index.tsx
@@ -7,11 +7,14 @@ import { OpenAPI } from 'mulighetsrommet-api';
 import Navspa from '@navikt/navspa';
 import { worker } from './mock/worker';
 
+console.log('mvf', import.meta.env);
+
 OpenAPI.BASE = String(import.meta.env.VITE_BACKEND_API_ROOT ?? '');
 
 if (import.meta.env.VITE_ENABLE_MOCK === 'true') {
   worker.start();
   ReactDOM.render(<App />, document.getElementById('mulighetsrommet-root'));
 } else {
+  console.log('mvf', 'export');
   Navspa.eksporter('mulighetsrommet-veileder-flate', App);
 }
