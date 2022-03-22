cb6d69e4229879ea14a608b402786c64457fa01e
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-22 10:17:19 +0100

    debug app properties

--- frontend/mulighetsrommet-veileder-flate/vite.config.ts
+++ frontend/mulighetsrommet-veileder-flate/vite.config.ts
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
+    minify: false,
     manifest: 'asset-manifest.json',
   },
 });
