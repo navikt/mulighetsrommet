180f83b1a7b170db2a4691961d6d1b8563c710f3
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-25 14:55:40 +0100

    feat: ensure that the application gets exported correctly for personflatefs

---
+++ frontend/mulighetsrommet-veileder-flate/src/constants.ts
@@ -0,0 +1,7 @@
+export const APPLICATION_NAME = 'mulighetsrommet-veileder-flate';
+
+/**
+ * The id references an HTML tag available through `veilarbpersonflatefs`, in which this application
+ * gets mounted.
+ */
+export const MODAL_ACCESSIBILITY_WRAPPER = 'modal-a11y-wrapper';
