b64c48da31a7a1542c846176dfd7abb711b715ad
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-03-25 14:59:12 +0100

    feat: render mulighetsrommet-veileder-flate through Navspa during local development
    This change makes the development experience one step closer to
    production by getting rid of a conditional render in `src/index.tsx`.

--- frontend/mulighetsrommet-veileder-flate/src/index.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import '@navikt/ds-css';
 import './index.less';
@@ -12,7 +11,6 @@ OpenAPI.BASE = String(import.meta.env.VITE_BACKEND_API_ROOT ?? '');
 
 if (import.meta.env.VITE_ENABLE_MOCK === 'true') {
   worker.start();
-  ReactDOM.render(<App />, document.getElementById('root'));
-} else {
-  Navspa.eksporter(APPLICATION_NAME, App);
 }
+
+Navspa.eksporter(APPLICATION_NAME, App);
