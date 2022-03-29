f1f3d37d6596cff2be189f0625edfecb6abe4d3f
Steffen Lien <steffen.lien@nav.no>
2022-03-29 12:29:40 +0200

    add the damn folder

---
+++ frontend/mulighetsrommet-api-client/src/models/Tiltaksgjennomforing.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { Tiltakskode } from './Tiltakskode';
+
+export type Tiltaksgjennomforing = {
+    id: number;
+    tittel: string;
+    beskrivelse: string;
+    tiltakskode: Tiltakskode;
+    tiltaksnummer?: string;
+    fraDato: string | null;
+    tilDato: string | null;
+}
