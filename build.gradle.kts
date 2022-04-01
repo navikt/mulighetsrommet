8a7d128a5acb21f600dc756e153ec90163d1b751
Steffen Lien <steffen.lien@nav.no>
2022-04-01 14:11:52 +0200

    revert subprojects section in root

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,32 +9,3 @@ plugins {
 
 group = "no.nav.mulighetsrommet"
 version = "0.0.1"
-
-subprojects {
-    apply {
-        plugin("org.jetbrains.kotlin.jvm")
-    }
-    repositories {
-        mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
-        // Needed to get no.nav.common-java-modules to work. Deps from other repos
-        maven {
-            url = uri("https://packages.confluent.io/maven/")
-        }
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-
-    val implementation by configurations
-    val testImplementation by configurations
-    dependencies {
-        val kotestVersion = "5.1.0"
-        implementation("org.flywaydb:flyway-core:8.5.5")
-        implementation("com.zaxxer:HikariCP:5.0.1")
-        implementation("org.postgresql:postgresql:42.3.3")
-        testImplementation("io.mockk:mockk:1.12.3")
-        testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-        testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    }
-}
--- mulighetsrommet-kafka/build.gradle.kts
+++ mulighetsrommet-kafka/build.gradle.kts
@@ -1,4 +1,3 @@
-val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val prometeus_version: String by project
@@ -28,28 +27,49 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
     disabledRules.addAll("no-wildcard-imports")
 }
 
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    // Needed to get no.nav.common-java-modules to work. Deps from other repos
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
 dependencies {
+    val ktorVersion = "2.0.0-beta-1"
+    val kotestVersion = "5.2.2"
+    val hopliteVersion = "1.4.16"
     implementation(project(":mulighetsrommet-domain"))
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktor_version")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("com.sksamuel.hoplite:hoplite-core:$hoplite_version")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:$hoplite_version")
-    implementation("no.nav.common:kafka:$common_java_modules_version")
-    implementation("com.github.seratch:kotliquery:$kotliquery_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.3")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("com.sksamuel.hoplite:hoplite-core:$hopliteVersion")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:$hopliteVersion")
+    implementation("no.nav.common:kafka:2.2021.12.09_11.56-a71c36a61ba3")
+    implementation("com.github.seratch:kotliquery:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:$wiremock_version")
+    implementation("org.flywaydb:flyway-core:8.5.5")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.postgresql:postgresql:42.3.3")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.6.10")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.32.0")
+    testImplementation("io.mockk:mockk:1.12.3")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 flyway {
