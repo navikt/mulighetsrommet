06c7aaf89ce464de3f3e394aa438b229fc96f3d1
Steffen Lien <steffen.lien@nav.no>
2022-04-08 08:38:23 +0200

    make repeatable test data non-repeatable

--- mulighetsrommet-api/src/main/resources/db/migration/testdata/R__2_add_tiltaksgjennomforinger_data.sql
+++
@@ -1,7 +0,0 @@
--- INSERT INTO tiltaksgjennomforing (id, tittel, beskrivelse, tiltaksnummer, fra_dato, til_dato, tiltakskode)
--- VALUES (1, 'Truckførerkurs i Namsos',
---         'Her kan du lære å kræsje med truck.', 1000, '2020-06-01', '2020-12-01', 'ABIST')
--- ON CONFLICT (id) DO UPDATE SET tittel            = EXCLUDED.tittel,
---                                beskrivelse       = EXCLUDED.beskrivelse;
---
--- SELECT setval('tiltaksgjennomforing_id_seq', (SELECT MAX(id) from "tiltaksgjennomforing"));
