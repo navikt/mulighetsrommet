2295dc3d11e193046459c32ad9cde14d23c214a3
Steffen Lien <steffen.lien@nav.no>
2022-04-10 09:44:18 +0200

    add producer data for gjennomforinger

--- mulighetsrommet-api/src/main/resources/db/migration/testdata/test_data.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/testdata/test_data.sql
@@ -110,10 +110,3 @@ values  (1, 'Arbeid med Bistand (AB)', 'ABIST', '2001-01-01', '2019-12-31'),
         (2, 'Varig vernet arbeid (VVA)', 'VV', '2001-01-01', '2099-01-01'),
         (4, 'Sysselsettingstiltak for yrkeshemmede', 'YHEMMOFF', '2001-01-01', '2015-12-31')
 
--- INSERT INTO tiltaksgjennomforing (id, tittel, beskrivelse, tiltaksnummer, fra_dato, til_dato, tiltakskode)
--- VALUES (1, 'Truckførerkurs i Namsos',
---         'Her kan du lære å kræsje med truck.', 1000, '2020-06-01', '2020-12-01', 'ABIST')
--- ON CONFLICT (id) DO UPDATE SET tittel            = EXCLUDED.tittel,
---                                beskrivelse       = EXCLUDED.beskrivelse;
---
--- SELECT setval('tiltaksgjennomforing_id_seq', (SELECT MAX(id) from "tiltaksgjennomforing"));
