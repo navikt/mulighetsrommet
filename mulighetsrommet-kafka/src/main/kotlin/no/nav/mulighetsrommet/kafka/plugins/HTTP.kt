1784d3f79a9d349e51dad1871f4277bd4329dea0
Steffen Lien <steffen.lien@nav.no>
2022-04-20 11:56:48 +0200

    fix changes to imports

--- mulighetsrommet-kafka/src/main/kotlin/no/nav/mulighetsrommet/kafka/plugins/HTTP.kt
+++ mulighetsrommet-kafka/src/main/kotlin/no/nav/mulighetsrommet/kafka/plugins/HTTP.kt
@@ -4,19 +4,19 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.plugins.CORS
-import io.ktor.server.plugins.DefaultHeaders
+import io.ktor.server.plugins.cors.*
+import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
     install(CORS) {
-        method(HttpMethod.Options)
-        method(HttpMethod.Put)
-        method(HttpMethod.Delete)
-        method(HttpMethod.Patch)
-        header(HttpHeaders.Authorization)
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
 }
