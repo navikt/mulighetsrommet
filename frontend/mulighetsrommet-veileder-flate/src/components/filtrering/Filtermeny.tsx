fb3a40abc5edb4a53655bd1bcef5d5b11bb365cc
taniaholst <tania.holst@nav.no>
2022-04-25 08:55:48 +0200

    tc-66 tok bort åpne/lukke sidebar siden den skal være statisk, fjernet info-alert for geografisk tilknytning og tilhørende feature-toggle, litt css for å gjøre ting penere

--- frontend/mulighetsrommet-veileder-flate/src/components/filtrering/Filtermeny.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/filtrering/Filtermeny.tsx
@@ -2,26 +2,17 @@ import React from 'react';
 import { Heading } from '@navikt/ds-react';
 import InnsatsgruppeFilter from './InnsatsgruppeFilter';
 import './Filtermeny.less';
-import Ikonknapp from '../knapper/Ikonknapp';
-import { Close } from '@navikt/ds-icons';
 import { useAtom } from 'jotai';
 import Searchfield from './Searchfield';
 import { tiltakstypefilter } from '../../core/atoms/atoms';
 
-interface SidemenyProps {
-  handleClickSkjulSidemeny: () => void;
-}
-
-const Filtermeny = ({ handleClickSkjulSidemeny }: SidemenyProps) => {
+const Filtermeny = () => {
   const [filter, setFilter] = useAtom(tiltakstypefilter);
 
   return (
     <div className="tiltakstype-oversikt__filtermeny">
       <Heading size="medium" level="1" className="filtermeny__heading" role="heading">
         Filter
-        <Ikonknapp handleClick={handleClickSkjulSidemeny} ariaLabel="Lukkeknapp">
-          <Close aria-label="Lukkeknapp" />
-        </Ikonknapp>
       </Heading>
       <Searchfield sokefilter={filter.search!} setSokefilter={(search: string) => setFilter({ ...filter, search })} />
       <InnsatsgruppeFilter
--- frontend/mulighetsrommet-veileder-flate/src/views/tiltakstype-oversikt/ViewTiltakstypeOversikt.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/views/tiltakstype-oversikt/ViewTiltakstypeOversikt.tsx
@@ -1,54 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ViewTiltakstypeOversikt.less';
 import '../../layouts/MainView.less';
-import { Alert, Button, Heading, Loader } from '@navikt/ds-react';
+import { Alert, Loader } from '@navikt/ds-react';
 import Filtermeny from '../../components/filtrering/Filtermeny';
 import useTiltakstyper from '../../hooks/tiltakstype/useTiltakstyper';
 import TiltakstypeTabell from '../../components/tabell/TiltakstypeTabell';
 import { useAtom } from 'jotai';
-import Ikonknapp from '../../components/knapper/Ikonknapp';
-import { Close, Filter } from '@navikt/ds-icons';
 import InnsatsgruppefilterTags from '../../components/tags/InnsatsgruppefilterTags';
 import SearchFieldTag from '../../components/tags/SearchFieldTag';
-import { tiltakstypefilter, visSidemeny } from '../../core/atoms/atoms';
-import { logEvent } from '../../api/logger';
-import { ALERT_INFO, FAKE_DOOR, useFeatureToggles } from '../../api/feature-toggles';
-import Show from '../../utils/Show';
+import { tiltakstypefilter } from '../../core/atoms/atoms';
+import { FAKE_DOOR, useFeatureToggles } from '../../api/feature-toggles';
 
 const ViewTiltakstypeOversikt = () => {
   const [filtrertListe] = useAtom(tiltakstypefilter);
-  const [sidemenyVisning, setSidemenyVisning] = useAtom(visSidemeny);
-
-  const [visInfoboks, setVisInfoboks] = useState(true);
 
   const features = useFeatureToggles();
-  const visAlertInfoFeature = features.isSuccess && features.data[ALERT_INFO];
   const visFakeDoorFeature = features.isSuccess && features.data[FAKE_DOOR];
 
   const { data, isFetching, isError } = useTiltakstyper(filtrertListe); //isLoading vs isFetching?
-
-  const handleClick = () => {
-    logEvent('mulighetsrommet-alert-info');
-    setVisInfoboks(false);
-  };
-
-  const Infotekst = () => {
-    return (
-      <Alert variant="info" size="small">
-        Utvalget av arbeidsmarkedstiltakene du ser er tilpasset din tiltaksregion.
-        <Button variant="tertiary" size="small" onClick={handleClick}>
-          <Close aria-label="Lukknapp for alertstripe" />
-        </Button>
-      </Alert>
-    );
-  };
-
-  const handleClickSkjulSidemeny = () => {
-    setSidemenyVisning(!sidemenyVisning);
-    sidemenyVisning
-      ? (document.getElementById('tiltakstype-oversikt')!.style.gridTemplateColumns = 'auto')
-      : (document.getElementById('tiltakstype-oversikt')!.style.gridTemplateColumns = '15rem auto');
-  };
 
   return (
     <>
@@ -58,25 +27,14 @@ const ViewTiltakstypeOversikt = () => {
         </Alert>
       ) : (
         <div className="tiltakstype-oversikt" id="tiltakstype-oversikt" data-testid="tiltakstype-oversikt">
-          <Show if={sidemenyVisning}>
-            <Filtermeny handleClickSkjulSidemeny={handleClickSkjulSidemeny} />
-          </Show>
+          <Filtermeny />
           <div className="filtercontainer">
-            <Ikonknapp className="filterknapp" handleClick={handleClickSkjulSidemeny} ariaLabel="Filterknapp">
-              <Filter aria-label="Filterknapp" />
-            </Ikonknapp>
             <div className="filtertags">
               <InnsatsgruppefilterTags />
               <SearchFieldTag />
             </div>
           </div>
-          <Show if={visInfoboks && visAlertInfoFeature}>
-            <Infotekst />
-          </Show>
           <div className="tiltakstype-oversikt__tiltak">
-            <Heading level="1" size="xsmall">
-              Viser {data?.length} av {data?.length} tiltak
-            </Heading>
             {isFetching && !data && <Loader variant="neutral" size="2xlarge" />}
             {data && <TiltakstypeTabell tiltakstypeliste={data} />}
             {isError && <Alert variant="error">En feil oppstod. Vi har problemer med å hente tiltakstypene.</Alert>}
