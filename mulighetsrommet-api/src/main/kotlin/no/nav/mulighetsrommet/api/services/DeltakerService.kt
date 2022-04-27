3c3ed54e629382c4a06295a3f73946eb82ea3818
Steffen Lien <steffen.lien@nav.no>
2022-04-27 16:30:26 +0200

    fix minor issues

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/DeltakerService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/DeltakerService.kt
@@ -3,9 +3,8 @@ package no.nav.mulighetsrommet.api.services
 import kotliquery.Row
 import kotliquery.queryOf
 import no.nav.mulighetsrommet.api.database.Database
-import no.nav.mulighetsrommet.api.utils.DatabaseUtils
+import no.nav.mulighetsrommet.api.utils.DatabaseMapper
 import no.nav.mulighetsrommet.domain.Deltaker
-import no.nav.mulighetsrommet.domain.Deltakerstatus
 import org.slf4j.Logger
 
 class DeltakerService(private val db: Database, private val logger: Logger) {
@@ -14,7 +13,7 @@ class DeltakerService(private val db: Database, private val logger: Logger) {
         val query = """
             select id, tiltaksgjennomforing_id, person_id, fra_dato, til_dato, status from deltaker
         """.trimIndent()
-        val queryResult = queryOf(query).map { DatabaseUtils.toDeltaker(it) }.asList
+        val queryResult = queryOf(query).map { DatabaseMapper.toDeltaker(it) }.asList
         return db.session.run(queryResult)
     }
 
