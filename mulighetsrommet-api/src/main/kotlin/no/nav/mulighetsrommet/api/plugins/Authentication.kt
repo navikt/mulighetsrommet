604042c3932dd89bd8176e8054e42c73988f0d26
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-05-05 12:03:41 +0200

    configure cache for public keys

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Authentication.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Authentication.kt
@@ -1,18 +1,21 @@
 package no.nav.mulighetsrommet.api.plugins
 
-import com.auth0.jwk.UrlJwkProvider
+import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import no.nav.mulighetsrommet.api.AuthConfig
 import java.net.URI
+import java.util.concurrent.TimeUnit
 
 fun Application.configureAuthentication(auth: AuthConfig) {
     val (azure) = auth
 
     install(Authentication) {
         jwt {
-            val jwkProvider = UrlJwkProvider(URI(azure.jwksUri).toURL())
+            val jwkProvider = JwkProviderBuilder(URI(azure.jwksUri).toURL())
+                .cached(5, 12, TimeUnit.HOURS)
+                .build()
 
             verifier(jwkProvider, azure.issuer) {
                 withAudience(azure.audience)
