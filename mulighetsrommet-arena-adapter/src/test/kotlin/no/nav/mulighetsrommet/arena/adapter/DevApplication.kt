d76473c503e1a649479cb90aebf603c5e8bea8ae
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-05-06 14:24:32 +0200

    move dev application to test directory

--- mulighetsrommet-arena-adapter/src/test/kotlin/no/nav/mulighetsrommet/arena/adapter/DevApplication.kt
+++ mulighetsrommet-arena-adapter/src/test/kotlin/no/nav/mulighetsrommet/arena/adapter/DevApplication.kt
@@ -1,13 +1,6 @@
 package no.nav.mulighetsrommet.arena.adapter
 
 import com.sksamuel.hoplite.ConfigLoader
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.util.*
 import no.nav.common.kafka.util.KafkaPropertiesBuilder
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
