52d2de31ea01fc11439f3a60b60c1717684003a7
Steffen Lien <steffen.lien@nav.no>
2022-05-10 13:03:00 +0200

    fix auth tests

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/AuthenticationTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/AuthenticationTest.kt
@@ -2,65 +2,58 @@ package no.nav.mulighetsrommet.api
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import no.nav.security.mock.oauth2.MockOAuth2Server
 
 class AuthenticationTest : FunSpec({
-//
-//    val oauth = MockOAuth2Server()
-//
-//    beforeSpec {
-//        oauth.start()
-//    }
-//
-//    afterSpec {
-//        oauth.shutdown()
-//    }
-//
-//    context("protected endpoints") {
-//        test("should respond with 401 when request is not authenticated") {
-//            withMulighetsrommetApp(oauth) {
-//                handleRequest(HttpMethod.Get, "/api/innsatsgrupper").run {
-//                    response.status() shouldBe HttpStatusCode.Unauthorized
-//                }
-//            }
-//        }
-//
-//        test("should respond with 401 when the token has the wrong audience") {
-//            withMulighetsrommetApp(oauth) {
-//                handleRequest(HttpMethod.Get, "/api/innsatsgrupper") {
-//                    addHeader(
-//                        HttpHeaders.Authorization,
-//                        "Bearer ${oauth.issueToken(audience = "skatteetaten").serialize()}"
-//                    )
-//                }.run {
-//                    response.status() shouldBe HttpStatusCode.Unauthorized
-//                }
-//            }
-//        }
-//
-//        test("should respond with 401 when the token has the wrong issuer") {
-//            withMulighetsrommetApp(oauth) {
-//                handleRequest(HttpMethod.Get, "/api/innsatsgrupper") {
-//                    addHeader(
-//                        HttpHeaders.Authorization,
-//                        "Bearer ${oauth.issueToken(issuerId = "skatteetaten").serialize()}"
-//                    )
-//                }.run {
-//                    response.status() shouldBe HttpStatusCode.Unauthorized
-//                }
-//            }
-//        }
-//
-//        test("should respond with 200 when request is authenticated") {
-//            withMulighetsrommetApp(oauth) {
-//                handleRequest(HttpMethod.Get, "/api/innsatsgrupper") {
-//                    addHeader(HttpHeaders.Authorization, "Bearer ${oauth.issueToken().serialize()}")
-//                }.run {
-//                    response.status() shouldBe HttpStatusCode.OK
-//                }
-//            }
-//        }
-//    }
+
+    val oauth = MockOAuth2Server()
+
+    beforeSpec {
+        oauth.start()
+    }
+
+    afterSpec {
+        oauth.shutdown()
+    }
+
+    context("protected endpoints") {
+        test("should respond with 401 when request is not authenticated") {
+            withMulighetsrommetApp(oauth) {
+                val response = client.get("/api/innsatsgrupper")
+
+                response.status shouldBe HttpStatusCode.Unauthorized
+            }
+        }
+
+        test("should respond with 401 when the token has the wrong audience") {
+            withMulighetsrommetApp(oauth) {
+                val response = client.get("/api/innsatsgrupper") {
+                    header(HttpHeaders.Authorization, "Bearer ${oauth.issueToken(audience = "skatteetaten").serialize()}")
+                }
+                response.status shouldBe HttpStatusCode.Unauthorized
+            }
+        }
+
+        test("should respond with 401 when the token has the wrong issuer") {
+            withMulighetsrommetApp(oauth) {
+
+                val response = client.get("/api/innsatsgrupper") {
+                    header(HttpHeaders.Authorization, "Bearer ${oauth.issueToken(audience = "skatteetaten").serialize()}")
+                }
+                response.status shouldBe HttpStatusCode.Unauthorized
+        }
+        }
+
+        test("should respond with 200 when request is authenticated") {
+            withMulighetsrommetApp(oauth) {
+                val response = client.get("/api/innsatsgrupper") {
+                    header(HttpHeaders.Authorization, "Bearer ${oauth.issueToken().serialize()}")
+                }
+                response.status shouldBe HttpStatusCode.OK
+            }
+        }
+    }
 })
