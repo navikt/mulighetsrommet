06692aa94878b89743fc268d66d0bee609eec5b3
Steffen Lien <steffen.lien@nav.no>
2022-05-19 15:22:03 +0200

    add logging to client

--- mulighetsrommet-arena-adapter/build.gradle.kts
+++ mulighetsrommet-arena-adapter/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
 
     val hopliteVersion = "1.4.16"
