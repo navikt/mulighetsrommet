e8b8c0c7e73fd777eca99600c446de5e95f3d6f9
Sindre Moldeklev <sndrem@gmail.com>
2022-06-03 09:18:15 +0200

    Fikser kolbing til Sanity

--- frontend/mulighetsrommet-veileder-flate/src/sanityClient.ts
+++ frontend/mulighetsrommet-veileder-flate/src/sanityClient.ts
@@ -1,15 +1,16 @@
 import imageUrlBuilder from '@sanity/image-url';
-import { ClientConfig } from '@sanity/client';
-import { createClient } from 'next-sanity';
+import sanityClient from '@sanity/client';
 
-const config: ClientConfig = {
+const config = {
   projectId: 'xegcworx',
   dataset: 'production',
   apiVersion: '2022-05-13',
   useCdn: process.env.NODE_ENV === 'production',
 };
 
+const client = sanityClient(config);
+
 // @ts-ignore
 export const urlFor = (source: any) => imageUrlBuilder(config).image(source);
 
-export const sanityClient = createClient(config);
+export { client };
