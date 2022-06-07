3be973deaadbf906c08bff445d56f264b110a3c0
Sindre Moldeklev <sndrem@gmail.com>
2022-06-07 21:05:45 +0200

    Fetch basert på env-verdi fra mock

--- frontend/mulighetsrommet-veileder-flate/src/hooks/useSanity.ts
+++ frontend/mulighetsrommet-veileder-flate/src/hooks/useSanity.ts
@@ -1,9 +1,13 @@
 import { MulighetsrommetService } from 'mulighetsrommet-api-client';
 import { useQuery } from 'react-query';
+import { client } from '../sanityClient';
 import { QueryKeys } from '../core/api/QueryKeys';
 
 export function useSanity(groqQuery: string) {
-  return useQuery(QueryKeys.SanityQuery, () =>
-    MulighetsrommetService.getSanityQuery({ query: encodeURIComponent(groqQuery) })
-  );
+  // Dersom vi er i mock-modus, bruk sanity-client, hvis ikke bruk proxy fra backend
+  const fetcher = import.meta.env.VITE_MULIGHETSROMMET_API_MOCK
+    ? () => client.fetch(groqQuery)
+    : () => MulighetsrommetService.getSanityQuery({ query: encodeURIComponent(groqQuery) });
+
+  return useQuery(QueryKeys.SanityQuery, fetcher);
 }
