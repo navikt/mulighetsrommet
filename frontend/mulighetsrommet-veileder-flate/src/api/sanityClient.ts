7f163f260f17df1c51d03a8ac41c50c5d8e64524
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-06-14 10:54:59 +0200

    set sanity auth headers when applicable

--- frontend/mulighetsrommet-veileder-flate/src/api/sanityClient.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/sanityClient.ts
@@ -1,4 +1,5 @@
 import { headers } from './headers';
+
 type Dataset = 'production' | 'test' | 'dev';
 
 interface SanityHttpResponse<T> {
@@ -8,22 +9,26 @@ interface SanityHttpResponse<T> {
 }
 
 class Client {
-  accessToken: String;
+  headers: Headers;
   mock: boolean = import.meta.env.VITE_MULIGHETSROMMET_API_MOCK === 'true';
-  useCdn: boolean = process.env.NODE_ENV === 'production';
 
   constructor(accessToken: string) {
-    if (!accessToken && this.mock) throw new Error('You must provide an access token to connect to Sanity');
-    this.accessToken = accessToken;
+    if (!accessToken && this.mock) {
+      throw new Error('You must provide an access token to connect to Sanity');
+    }
+
+    this.headers = new Headers(headers);
+
+    if (accessToken) {
+      this.headers.delete('Authorization');
+      this.headers.append('Authorization', `Bearer ${accessToken}`);
+    }
   }
 
   async query<T>(query: string, dataset: Dataset = 'production'): Promise<SanityHttpResponse<T>> {
-    if (import.meta.env.VITE_MULIGHETSROMMET_API_MOCK) {
-      headers.append('Authorization', `Bearer ${this.accessToken}`);
-    }
-    const url = this.createSanityUrl(this.useCdn, this.mock, dataset, query);
+    const url = this.createSanityUrl(dataset, query);
     const response = await fetch(url, {
-      headers,
+      headers: this.headers,
     });
     if (!response.ok) {
       throw new Error('Could not fetch from Sanity', {
@@ -34,11 +39,9 @@ class Client {
     return response.json() as Promise<SanityHttpResponse<T>>;
   }
 
-  private createSanityUrl(useCdn: boolean, isMockEnabled: boolean, dataset: Dataset, query: string): string {
-    if (isMockEnabled) {
-      return `https://xegcworx.${
-        useCdn ? 'apicdn' : 'api'
-      }.sanity.io/v2022-06-08/data/query/${dataset}?query=${encodeURIComponent(query)}`;
+  private createSanityUrl(dataset: Dataset, query: string): string {
+    if (this.mock) {
+      return `${import.meta.env.VITE_SANITY_API_URL}/data/query/${dataset}?query=${encodeURIComponent(query)}`;
     } else {
       const apiBase = String(import.meta.env.VITE_MULIGHETSROMMET_API_BASE ?? '');
       return `${apiBase}/api/v1/sanity?query=${encodeURIComponent(query)}&dataset=${dataset}`;
--- frontend/mulighetsrommet-veileder-flate/src/env.d.ts
+++ frontend/mulighetsrommet-veileder-flate/src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_MULIGHETSROMMET_API_BASE?: string;
   readonly VITE_MULIGHETSROMMET_API_MOCK?: 'true' | 'false';
   readonly VITE_SANITY_ACCESS_TOKEN?: string;
+  readonly VITE_SANITY_API_URL?: string;
 }
 
 interface ImportMeta {
