8caceb951d0a335d2c0af8d59293dc5b5b43fa9a
Sindre Moldeklev <sndrem@gmail.com>
2022-06-15 16:08:29 +0200

    Fjerner mulighet til å spesifisere dataset mot sanity-api

--- frontend/mulighetsrommet-veileder-flate/src/api/queries/useSanity.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/queries/useSanity.ts
@@ -3,8 +3,5 @@ import { QueryKeys } from '../../core/api/QueryKeys';
 import { MulighetsrommetService } from 'mulighetsrommet-api-client';
 
 export function useSanity<T>(query: string) {
-  return useQuery(
-    [QueryKeys.SanityQuery, query],
-    () => MulighetsrommetService.sanityQuery({ query }) as Promise<T>
-  );
+  return useQuery([QueryKeys.SanityQuery, query], () => MulighetsrommetService.sanityQuery({ query }) as Promise<T>);
 }
