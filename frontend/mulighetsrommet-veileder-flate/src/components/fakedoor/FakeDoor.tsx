52fc7dd19a6c1fd7fea7154708f2e738decf2af5
Signe Carlsen <signe_c_97@hotmail.com>
2022-06-20 14:24:37 +0200

    Legg til link til sharepoint presentasjon

--- frontend/mulighetsrommet-veileder-flate/src/components/fakedoor/FakeDoor.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/fakedoor/FakeDoor.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './FakeDoor.less';
-import CountDown from "./CountDown";
+import CountDown from './CountDown';
+import { Link } from '@navikt/ds-react';
 
 const FakeDoor = () => {
   return (
     <div className="fakedoor-container">
-      <h1 className="fakedoor-header">Pilot - Informasjonstjeneste for arbeidsmarkedtiltak</h1>
-      <div className="fakedoor-text">
-        <div>
-          Her vil du som veileder kunne finne kvalitetssikret informasjon om de tiltakene som er aktuelle for din
-          bruker, slik at du kan gjøre gode vurderinger, gi effektiv veiledning og få brukeren raskere ut i rett
-          arbeidsmarkedstiltak.{' '}
+      <div className="fakedoor-text-container">
+        <h1 className="fakedoor-header">Pilot - Informasjonstjeneste for arbeidsmarkedtiltak</h1>
+        <div className="fakedoor-text">
+          <div>
+            Her vil du som veileder kunne finne kvalitetssikret informasjon om de tiltakene som er aktuelle for din
+            bruker, slik at du kan gjøre gode vurderinger, gi effektiv veiledning og få brukeren raskere ut i rett
+            arbeidsmarkedstiltak.
+          </div>
+          <br />
+          <CountDown />
+          <br />
+          <Link target="_blank" className="fakedoor-link" href="https://navno.sharepoint.com/:b:/s/team-valp/EZrCwwYUh-ZGr7CbYn1Lt8UBW3pNcabIfUeuZeWeuEcmaQ?e=1RH9RW">Les mer om piloteringen</Link>
         </div>
-        <CountDown />
-        <div>Les mer om piloteringen</div>
       </div>
     </div>
   );
 };
-
-function numberOfDaysUntilPilot() {
-  return 1;
-}
 
 export default FakeDoor;
