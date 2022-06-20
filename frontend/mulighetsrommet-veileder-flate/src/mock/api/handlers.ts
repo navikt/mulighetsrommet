8c90c7d54bcefa1ee7524be12942297efff12b5b
Sindre Moldeklev <sndrem@gmail.com>
2022-06-20 15:07:54 +0200

    Feilsøker om projectId ikke blir satt

--- frontend/mulighetsrommet-veileder-flate/src/mock/api/handlers.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/api/handlers.ts
@@ -59,7 +59,7 @@ function getSanityClient() {
 
   cachedClient = new SanityClient({
     apiVersion: '2020-06-01',
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID ?? 'xegcworx',
     dataset: import.meta.env.VITE_SANITY_DATASET,
     token: import.meta.env.VITE_SANITY_ACCESS_TOKEN,
   });
