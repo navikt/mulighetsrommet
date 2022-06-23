ab675c380a90ea1aa90e3eecc74407d02a8b40cf
Sindre Moldeklev <sndrem@gmail.com>
2022-06-23 09:26:57 +0200

    Oppdaterer mocker som feilet

--- frontend/mulighetsrommet-veileder-flate/src/mock/fixtures/tiltakstyper.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/fixtures/tiltakstyper.ts
@@ -4,112 +4,200 @@ import { Tiltakstype } from '../../api/models';
 export const tiltakstyper: Tiltakstype[] = [
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Opplæring',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Funksjonsassistanse',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Utvidet oppfølging',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Avklaring',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Arbeidsmarkedsopplæring (AMO)',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Ekspertbistand',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '1',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Jobbklubb',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Oppfølging',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Digital jobbklubb',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Fag- og yrkesopplæring',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Arbeidstrening',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Arbeidsforberedende trening',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Midlertidig lønnstilskudd',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '2',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Varig lønnstilskudd',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Varig tilrettelagt arbeid i skjermet virksomhet',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Varig tilrettelagt arbeid i ordinær virksomhet',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Inkluderingstilskudd',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Funksjonsassistanse i arbeidslivet',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Mentor',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Arbeidsrettet rehabilitering',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '3',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Individuell jobbstøtte',
   },
   {
     _id: faker.datatype.number({ min: 100000, max: 999999 }),
-    innsatsgruppe: '4',
+    innsatsgruppe: {
+      _id: '',
+      beskrivelse: '',
+      tittel: '',
+    },
     tiltakstypeNavn: 'Tilskudd til sommerjobb',
   },
 ];
