8d30c91a4a5e3208ae96218776939382a6e09472
Sindre Moldeklev <sndrem@gmail.com>
2022-06-23 09:54:29 +0200

    CHORE Fjerner mocks som ikke er i bruk lengre

--- frontend/mulighetsrommet-veileder-flate/src/mock/api/handlers.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/api/handlers.ts
@@ -1,39 +1,11 @@
-import { rest, RestHandler } from 'msw';
-import { db } from '../database';
-import { toTiltaksgjennomforing } from '../entities/tiltaksgjennomforing';
-import { toTiltakstype } from '../entities/tiltakstype';
-import { mockFeatures } from './features';
-import { badReq, notFound, ok } from './responses';
 import SanityClient from '@sanity/client';
+import { rest, RestHandler } from 'msw';
+import { mockFeatures } from './features';
+import { badReq, ok } from './responses';
 
 export const handlers: RestHandler[] = [
   rest.get('*/api/feature', (req, res, ctx) => {
     return res(ctx.delay(500), ctx.json(mockFeatures));
-  }),
-
-  rest.get('*/api/v1/innsatsgrupper', () => {
-    return ok(db.innsatsgruppe.getAll());
-  }),
-
-  rest.get('*/api/v1/tiltakstyper', () => {
-    return ok(db.tiltakstype.getAll().map(toTiltakstype));
-  }),
-
-  rest.get('*/api/v1/tiltaksgjennomforinger', () => {
-    return ok(db.tiltaksgjennomforing.getAll().map(toTiltaksgjennomforing));
-  }),
-  rest.get('*/api/v1/tiltaksgjennomforinger/:id', req => {
-    const { id } = req.params;
-
-    const entity = db.tiltaksgjennomforing.findFirst({
-      where: { id: { equals: Number(id) } },
-    });
-
-    if (!entity) {
-      return notFound();
-    }
-
-    return ok(toTiltaksgjennomforing(entity));
   }),
 
   rest.get('*/api/v1/sanity', async req => {
