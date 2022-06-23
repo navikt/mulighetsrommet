53e18b6149374e1a3e9c7064cc82ac8b62f491ae
Sindre Moldeklev <sndrem@gmail.com>
2022-06-23 10:29:42 +0200

    Legger til korrekt sortering for innsatsgrupper

--- frontend/mulighetsrommet-veileder-flate/src/api/queries/useInnsatsgrupper.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/queries/useInnsatsgrupper.ts
@@ -2,5 +2,5 @@ import { Innsatsgruppe } from '../models';
 import { useSanity } from './useSanity';
 
 export function useInnsatsgrupper() {
-  return useSanity<Innsatsgruppe[]>('*[_type == "innsatsgruppe"]');
+  return useSanity<Innsatsgruppe[]>('*[_type == "innsatsgruppe"] | order(order asc)');
 }
