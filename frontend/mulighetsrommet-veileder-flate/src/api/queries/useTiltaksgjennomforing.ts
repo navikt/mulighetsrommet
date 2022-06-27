1cb1624923dd9996d870a3eefa99edd035436d64
Signe Carlsen <signe_c_97@hotmail.com>
2022-06-27 10:31:16 +0200

    filtrer på en del felt

--- frontend/mulighetsrommet-veileder-flate/src/api/queries/useTiltaksgjennomforing.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/queries/useTiltaksgjennomforing.ts
@@ -1,11 +1,19 @@
 import { useAtom } from 'jotai';
-import {Tiltaksgjennomforingsfilter, tiltaksgjennomforingsfilter, Tiltaksgjenomforingsfiltergruppe} from '../../core/atoms/atoms';
+import {
+  Tiltaksgjennomforingsfilter,
+  tiltaksgjennomforingsfilter,
+  Tiltaksgjenomforingsfiltergruppe,
+} from '../../core/atoms/atoms';
 import { Tiltaksgjennomforing } from '../models';
 import { useSanity } from './useSanity';
 
 export default function useTiltaksgjennomforing() {
   const [filter] = useAtom(tiltaksgjennomforingsfilter);
-  return useSanity<Tiltaksgjennomforing[]>(`*[_type == "tiltaksgjennomforing" ${byggInnsatsgruppeFilter(filter.innsatsgrupper)} ${byggTiltakstypeFilter(filter.tiltakstyper)}]{
+  return useSanity<Tiltaksgjennomforing[]>(`*[_type == "tiltaksgjennomforing" 
+  ${byggInnsatsgruppeFilter(filter.innsatsgrupper)} 
+  ${byggTiltakstypeFilter(filter.tiltakstyper)}
+  ${byggSokefilter(filter.search ?? '')}
+  {
     _id,
     tiltaksgjennomforingNavn,
     lokasjon,
@@ -19,14 +27,24 @@ export default function useTiltaksgjennomforing() {
 
 function byggInnsatsgruppeFilter(innsatsgrupper: Tiltaksgjenomforingsfiltergruppe[]): string {
   return innsatsgrupper.length > 0
-      ? `&& tiltakstype->innsatsgruppe->tittel in [${innsatsgrupper
-          .map(gruppe => `"${gruppe.tittel}"`)
-          .join(', ')}]`
-      : '';
+    ? `&& tiltakstype->innsatsgruppe->tittel in [${innsatsgrupper.map(gruppe => `"${gruppe.tittel}"`).join(', ')}]`
+    : '';
 }
 
 function byggTiltakstypeFilter(tiltakstyper: Tiltaksgjenomforingsfiltergruppe[]): string {
   return tiltakstyper.length > 0
-      ? `&& tiltakstype->_id in [${tiltakstyper.map(type => `"${type.id}"`).join(', ')}]`
-      : '';
+    ? `&& tiltakstype->_id in [${tiltakstyper.map(type => `"${type.id}"`).join(', ')}]`
+    : '';
+}
+
+function byggSokefilter(search: string | undefined) {
+  return search ? `&& [tiltaksgjennomforingNavn, tiltaksnummer, tiltakstype->tiltakstypeNavn, lokasjon] match "${search}*"
+  ]
+  | score(
+    tiltaksgjennomforingNavn match "${search}",
+    tiltaksnummer match "${search}",
+    lokasjon match "${search}",
+    tiltakstype.tiltakstypeNavn match "${search}",
+  )
+  | order(_score desc)` : ']';
 }
