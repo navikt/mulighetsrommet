897b657a78ed8de35546ca9dbd8b8437ecaafcb3
taniaholst <tania.holst@nav.no>
2022-06-28 11:01:11 +0200

    migrerte til cypress 10.0

---
+++ frontend/mulighetsrommet-veileder-flate/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  baseUrl: 'http://localhost:3000',
+  viewportHeight: 1300,
+  viewportWidth: 1800,
+  requestTimeout: 10000,
+  defaultCommandTimeout: 10000,
+  retries: {
+    runMode: 2,
+  },
+  component: {
+    componentFolder: 'src',
+    testFiles: '.*/__tests__/.*spec.tsx',
+  },
+  video: false,
+  videoUploadOnPasses: false,
+});
