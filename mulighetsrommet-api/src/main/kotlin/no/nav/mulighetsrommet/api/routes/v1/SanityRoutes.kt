a4c5954a9aa1ff78f907e7cec14e2d2864d9837c
Sindre Moldeklev <sndrem@gmail.com>
2022-06-28 12:17:18 +0200

    Logger spørring mot Sanity

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/SanityRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/SanityRoutes.kt
@@ -7,15 +7,17 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.mulighetsrommet.api.services.SanityService
 import org.koin.ktor.ext.inject
+import org.slf4j.LoggerFactory
 
 fun Route.sanityRoutes() {
-
+    val log = LoggerFactory.getLogger(this.javaClass)
     val sanityService: SanityService by inject()
 
     route("/api/v1/sanity") {
         get {
             val query = call.request.queryParameters["query"]
                 ?: return@get call.respondText("No query parameter with value '?query' present. Cannot execute query against Sanity")
+            log.info("Query sanity with value: $query")
 
             val result = sanityService.executeQuery(query)
             call.respondText(result.toString(), ContentType.Application.Json)
