f6601ddf5dc867ad8702f2a2fd4f742bc19aab07
Sindre Moldeklev <sndrem@gmail.com>
2022-06-29 13:46:40 +0200

    Legger til env samme som for mock

--- frontend/mulighetsrommet-veileder-flate/cypress/e2e/mulighetsrommet.cy.js
+++ frontend/mulighetsrommet-veileder-flate/cypress/e2e/mulighetsrommet.cy.js
@@ -1,126 +1,123 @@
 describe('Tiltaksgjennomføringstabell', () => {
-  it('Skal bare virke', () => {
-    console.log('Hello World');
+  let antallTiltak;
+  it('Sjekk at det er tiltaksgjennomføringer i tabellen', () => {
+    cy.getByTestId('tabell_tiltakstyper').children().children().should('have.length.greaterThan', 1);
   });
-  //   let antallTiltak;
-  //   it('Sjekk at det er tiltaksgjennomføringer i tabellen', () => {
-  //     cy.getByTestId('tabell_tiltakstyper').children().children().should('have.length.greaterThan', 1);
-  //   });
 
-  //   it('Sjekk UU', () => {
-  //     cy.checkPageA11y();
-  //   });
+  it('Sjekk UU', () => {
+    cy.checkPageA11y();
+  });
 
-  //   it('Lagre antall tiltak uten filtrering', () => {
-  //     cy.getByTestId('antall-tiltak').then($navn => {
-  //       antallTiltak = $navn.text();
-  //     });
-  //   });
+  it('Lagre antall tiltak uten filtrering', () => {
+    cy.getByTestId('antall-tiltak').then($navn => {
+      antallTiltak = $navn.text();
+    });
+  });
 
-  //   it('Filtrer på Innsatsgrupper', () => {
-  //     cy.getByTestId('knapp_tilbakestill-filter').should('not.exist');
+  it('Filtrer på Innsatsgrupper', () => {
+    cy.getByTestId('knapp_tilbakestill-filter').should('not.exist');
 
-  //     cy.velgFilter('standardinnsats');
+    cy.velgFilter('standardinnsats');
 
-  //     cy.getByTestId('filtertags').children().should('have.length', 1);
-  //     cy.getByTestId('knapp_tilbakestill-filter').should('exist');
+    cy.getByTestId('filtertags').children().should('have.length', 1);
+    cy.getByTestId('knapp_tilbakestill-filter').should('exist');
 
-  //     cy.wait(1000)
-  //       .getByTestId('antall-tiltak')
-  //       .then($navn => {
-  //         expect(antallTiltak).not.to.eq($navn.text());
-  //       });
+    cy.wait(1000)
+      .getByTestId('antall-tiltak')
+      .then($navn => {
+        expect(antallTiltak).not.to.eq($navn.text());
+      });
 
-  //     cy.getByTestId('filtertag_lukkeknapp_standardinnsats').click();
-  //     cy.getByTestId('filtertags').children().should('have.length', 0);
-  //   });
+    cy.getByTestId('filtertag_lukkeknapp_standardinnsats').click();
+    cy.getByTestId('filtertags').children().should('have.length', 0);
+  });
 
-  //   it('Filtrer på Tiltakstyper', () => {
-  //     cy.apneLukketFilterAccordion('tiltakstyper', true);
-  //     cy.velgFilter('avklaring');
-  //     cy.velgFilter('oppfolging');
+  it('Filtrer på Tiltakstyper', () => {
+    cy.apneLukketFilterAccordion('tiltakstyper', true);
+    cy.velgFilter('avklaring');
+    cy.velgFilter('oppfolging');
 
-  //     cy.getByTestId('filtertags').children().should('have.length', 2);
+    cy.getByTestId('filtertags').children().should('have.length', 2);
 
-  //     cy.wait(1000)
-  //       .getByTestId('antall-tiltak')
-  //       .then($navn => {
-  //         expect(antallTiltak).not.to.eq($navn.text());
-  //       });
+    cy.wait(1000)
+      .getByTestId('antall-tiltak')
+      .then($navn => {
+        expect(antallTiltak).not.to.eq($navn.text());
+      });
 
-  //     cy.getByTestId('knapp_tilbakestill-filter').should('exist').click();
+    cy.getByTestId('knapp_tilbakestill-filter').should('exist').click();
 
-  //     cy.getByTestId('filter_checkbox_avklaring').should('not.be.checked');
-  //     cy.getByTestId('filter_checkbox_oppfolging').should('not.be.checked');
+    cy.getByTestId('filter_checkbox_avklaring').should('not.be.checked');
+    cy.getByTestId('filter_checkbox_oppfolging').should('not.be.checked');
 
-  //     cy.getByTestId('filtertags').children().should('have.length', 0);
-  //     cy.apneLukketFilterAccordion('tiltakstyper', false);
-  //   });
+    cy.getByTestId('filtertags').children().should('have.length', 0);
+    cy.apneLukketFilterAccordion('tiltakstyper', false);
+  });
 
-  //   it('Filtrer på søkefelt', () => {
-  //     cy.getByTestId('filter_sokefelt').type('Digitalt');
-  //     cy.getByTestId('filtertags').children().should('have.length', 1);
+  it('Filtrer på søkefelt', () => {
+    cy.getByTestId('filter_sokefelt').type('Digitalt');
+    cy.getByTestId('filtertags').children().should('have.length', 1);
 
-  //     cy.wait(1000)
-  //       .getByTestId('antall-tiltak')
-  //       .then($navn => {
-  //         expect(antallTiltak).not.to.eq($navn.text());
-  //       });
-  //     cy.getByTestId('filter_sokefelt').clear();
-  //   });
+    cy.wait(1000)
+      .getByTestId('antall-tiltak')
+      .then($navn => {
+        expect(antallTiltak).not.to.eq($navn.text());
+      });
+    cy.getByTestId('filter_sokefelt').clear();
+  });
 
-  //   it('Sortering', () => {
-  //     //Tester på de forskjellige typene: string, number, date og status
-  //     cy.sortering('tabellheader_tiltaksnavn');
-  //     cy.sortering('tabellheader_tiltaksnummer');
-  //     cy.sortering('tabellheader_oppstartsdato');
-  //     cy.sortering('tabellheader_status');
-  //   });
+  it('Sortering', () => {
+    //Tester på de forskjellige typene: string, number, date og status
+    cy.sortering('tabellheader_tiltaksnavn');
+    cy.sortering('tabellheader_tiltaksnummer');
+    cy.sortering('tabellheader_oppstartsdato');
+    cy.sortering('tabellheader_status');
+  });
 
-  //   it('Kopiknapp', () => {
-  //     cy.getByTestId('tabell_tiltaksnummer')
-  //       .last()
-  //       .then($text => {
-  //         const tiltaksnummer = $text.text();
-  //         cy.getByTestId('tabell_knapp_kopier').last().click();
+  it('Kopiknapp', () => {
+    cy.getByTestId('tabell_tiltaksnummer')
+      .last()
+      .then($text => {
+        const tiltaksnummer = $text.text();
+        cy.getByTestId('tabell_knapp_kopier').last().click();
 
-  //         cy.window().then(win => {
-  //           win.navigator.clipboard.readText().then(text => {
-  //             expect(text).to.eq(tiltaksnummer);
-  //           });
-  //         });
-  //       });
-  //   });
-  // });
+        cy.window().then(win => {
+          win.navigator.clipboard.readText().then(text => {
+            expect(text).to.eq(tiltaksnummer);
+          });
+        });
+      });
+  });
+});
 
-  // describe('Tiltaksgjennomføringstabell', () => {
-  //   it('Gå til siste tiltaksgjennomføring', () => {
-  //     cy.getByTestId('tabell_tiltaksgjennomforing').last().click({ force: true });
+describe('Tiltaksgjennomføringstabell', () => {
+  it('Gå til siste tiltaksgjennomføring', () => {
+    cy.getByTestId('tabell_tiltaksgjennomforing').last().click({ force: true });
 
-  //     cy.getByTestId('knapp_kopier').click();
-  //     cy.window().then(win => {
-  //       win.navigator.clipboard.readText().then(text => {
-  //         cy.url().should('include', text);
-  //       });
-  //     });
-  //   });
+    cy.getByTestId('knapp_kopier').click();
+    cy.window().then(win => {
+      win.navigator.clipboard.readText().then(text => {
+        cy.url().should('include', text);
+      });
+    });
+  });
 
-  //   it('Sjekk UU', () => {
-  //     cy.checkA11y({ exclude: ['.navds-tooltip'] });
-  //   });
+  it('Sjekk UU', () => {
+    cy.checkA11y({ exclude: ['.navds-tooltip'] });
+  });
 
-  //   it('Sjekk at fanene fungerer som de skal', () => {
-  //     cy.getByTestId('tab1').should('be.visible');
-  //     cy.getByTestId('tab2').should('not.be.visible');
+  it('Sjekk at fanene fungerer som de skal', () => {
+    cy.getByTestId('tab1').should('be.visible');
+    cy.getByTestId('tab2').should('not.be.visible');
 
-  //     cy.getByTestId('fane_detaljer-og-innhold').click();
+    cy.getByTestId('fane_detaljer-og-innhold').click();
 
-  //     cy.getByTestId('tab1').should('not.be.visible');
-  //     cy.getByTestId('tab2').should('be.visible');
-  //   });
+    cy.getByTestId('tab1').should('not.be.visible');
+    cy.getByTestId('tab2').should('be.visible');
+  });
 
-  //   it('Gå tilbake til tiltaksoversikten', () => {
-  //     cy.tilbakeTilListevisning();
-  //     cy.getByTestId('tabell_tiltakstyper').children().children().should('have.length.greaterThan', 1);
-  //   });
+  it('Gå tilbake til tiltaksoversikten', () => {
+    cy.tilbakeTilListevisning();
+    cy.getByTestId('tabell_tiltakstyper').children().children().should('have.length.greaterThan', 1);
+  });
 });
