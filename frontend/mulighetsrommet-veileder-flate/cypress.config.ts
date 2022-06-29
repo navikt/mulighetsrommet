7ff4bf9d44408785fa36ce4b54cdf35cea7f2ef0
taniaholst <tania.holst@nav.no>
2022-06-29 08:58:14 +0200

    cypress migrering

---
+++ frontend/mulighetsrommet-veileder-flate/cypress.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  viewportHeight: 1300,
+  viewportWidth: 1800,
+  requestTimeout: 10000,
+  defaultCommandTimeout: 10000,
+  retries: {
+    runMode: 2,
+  },
+  video: false,
+  videoUploadOnPasses: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+  // component: {
+  //   setupNodeEvents(on, config) {},
+  //   specPattern: 'src/.*/__tests__/.*spec.tsx',
+  // },
+});
