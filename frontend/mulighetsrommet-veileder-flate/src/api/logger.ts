9675c81444af1e1058d5b04d83c3750efa2ddda3
taniaholst <tania.holst@nav.no>
2022-07-01 10:22:39 +0200

    fikset logger

--- frontend/mulighetsrommet-veileder-flate/src/api/logger.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/logger.ts
@@ -1,38 +1,26 @@
-// import {
-//   createFrontendLogger,
-//   createMockFrontendLogger,
-//   DEFAULT_FRONTENDLOGGER_API_URL,
-// } from '@navikt/frontendlogger/lib';
-// import { LOG_NAME } from '../constants';
-//
-// export const logger = import.meta.env.DEV
-//   ? createMockFrontendLogger(LOG_NAME)
-//   : createFrontendLogger(LOG_NAME, DEFAULT_FRONTENDLOGGER_API_URL);
-//
-// export const logError = (fields?: {}, tags?: {}): void => {
-//   logger.event(`${LOG_NAME}.error`, fields, tags);
-// };
-//
-// export const logMetrikk = (metrikkNavn: string, fields?: {}, tags?: {}): void => {
-//   logger.event(`${LOG_NAME}.metrikker.${metrikkNavn}`, fields, tags);
-// };
-//
-// const frontendlogger = (window as any).frontendlogger;
-//
-// export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
-//   if (import.meta.env.VITE_MULIGHETSROMMET_API_MOCK === 'true') {
-//     // console.log('Event', logTag, 'Fields:', fields, 'Tags:', tags); // tslint:disable-line
-//   } else if (logger?.event) {
-//     logger.event(logTag, fields || {}, tags || {});
-//   }
-// };
+import {
+  createFrontendLogger,
+  createMockFrontendLogger,
+  DEFAULT_FRONTENDLOGGER_API_URL,
+} from '@navikt/frontendlogger/lib';
+import { LOG_NAME } from '../constants';
 
-const frontendlogger = (window as any).frontendlogger;
+export const logger = import.meta.env.DEV
+  ? createMockFrontendLogger(LOG_NAME)
+  : createFrontendLogger(LOG_NAME, DEFAULT_FRONTENDLOGGER_API_URL);
+
+export const logError = (fields?: {}, tags?: {}): void => {
+  logger.event(`${LOG_NAME}.error`, fields, tags);
+};
+
+export const logMetrikk = (metrikkNavn: string, fields?: {}, tags?: {}): void => {
+  logger.event(`${LOG_NAME}.metrikker.${metrikkNavn}`, fields, tags);
+};
 
 export const logEvent = (logTag: string, fields?: {}, tags?: {}): void => {
-  if (frontendlogger && frontendlogger.event) {
-    frontendlogger.event(logTag, fields ? fields : {}, tags ? tags : {});
-  } else {
+  if (import.meta.env.VITE_MULIGHETSROMMET_API_MOCK === 'true') {
     console.log('Event', logTag, 'Fields:', fields, 'Tags:', tags); // tslint:disable-line
+  } else if (logger?.event) {
+    logger.event(logTag, fields || {}, tags || {});
   }
 };
