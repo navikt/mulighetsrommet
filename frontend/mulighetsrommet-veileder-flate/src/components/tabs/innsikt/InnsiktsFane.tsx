9e2d978d5222f18035399f55203d8a7f263822e1
Signe Carlsen <signe_c_97@hotmail.com>
2022-07-04 13:32:58 +0200

    formater koden

--- frontend/mulighetsrommet-veileder-flate/src/components/tabs/innsikt/InnsiktsFane.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/tabs/innsikt/InnsiktsFane.tsx
@@ -7,20 +7,18 @@ export type InnsiktsFaneProps = {
   tiltakstype: string;
 };
 
-const tiltakstyperMedStatstikk = ['Oppfølging', 'Digital Oppfølging', 'Avklaring', 'ARR', 'AFT', 'Jobbklubb']
+const tiltakstyperMedStatstikk = ['Oppfølging', 'Digital Oppfølging', 'Avklaring', 'ARR', 'AFT', 'Jobbklubb'];
 
-const InnsiktsFane = ({tiltakstype}: InnsiktsFaneProps) => {
+const InnsiktsFane = ({ tiltakstype }: InnsiktsFaneProps) => {
   return (
     <div className={'tiltaksdetaljer__maksbredde'}>
       {tiltakstyperMedStatstikk.includes(tiltakstype) && (
         <>
-
-        <AutoSizer disableHeight>
-      {({width}) => <BarChart tiltakstype={tiltakstype} width={width} height={300} />}
-        </AutoSizer>
+          <AutoSizer disableHeight>
+            {({ width }) => <BarChart tiltakstype={tiltakstype} width={width} height={300} />}
+          </AutoSizer>
         </>
-        )
-      }
+      )}
     </div>
   );
 };
