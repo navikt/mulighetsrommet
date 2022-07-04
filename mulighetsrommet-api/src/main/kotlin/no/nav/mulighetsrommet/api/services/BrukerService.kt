94c2cc0d1aae5814dcf6b0da784565e608e3d9ef
Sindre Moldeklev <sndrem@gmail.com>
2022-07-04 15:18:02 +0200

    Prøv å hente data fra veilarbvedtaksstotte

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/BrukerService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/BrukerService.kt
@@ -1,25 +1,39 @@
 package no.nav.mulighetsrommet.api.services
 
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
 
 class BrukerService {
-    fun hentBrukerdata(fnr: String): Brukerdata {
-        return Brukerdata(
-            fnr = fnr,
-            innsatsgruppe = Innsatsgruppe.STANDARD_INNSATS
-        )
+    private val client: HttpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+        // expectSuccess = true
+        defaultRequest {
+            url("https://veilarbvedtaksstotte.dev-fss-pub.nais.io/veilarbvedtaksstotte/api/") // TODO Må være basert på miljø (hente fra property-fil?)
+        }
+    }
+
+    suspend fun hentBrukerdata(fnr: String): Brukerdata {
+        val response = client.get("siste-14a-vedtak?fnr=$fnr")
+        response.let {
+            return it.body()
+        }
     }
 }
 
 @Serializable
 data class Brukerdata(
     val fnr: String,
-    val innsatsgruppe: Innsatsgruppe
+    val innsatsgruppe: Innsatsgruppe,
 )
 
-enum class Innsatsgruppe(val verdi: String) {
-    STANDARD_INNSATS("Standard innsats"),
-    SITUASJONSBESTEMT_INNSATS("Situasjonsbestemt innsats"),
-    SPESIELT_TILPASSET_INNSATS("Spesielt tilpasset innsats"),
-    VARIG_TILPASSET_INNSATS("Varig tilpasset innsats"),
+enum class Innsatsgruppe {
+    STANDARD_INNSATS, SITUASJONSBESTEMT_INNSATS, SPESIELT_TILPASSET_INNSATS, GRADERT_VARIG_TILPASSET_INNSATS, VARIG_TILPASSET_INNSATS
 }
