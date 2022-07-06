c2d1bf98e6ddecfeccaaeb9016846de7800f90fe
Sindre Moldeklev <sndrem@gmail.com>
2022-07-06 16:11:58 +0200

    Nullable felter

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/oppfolging/VeilarboppfolgingClientImpl.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/oppfolging/VeilarboppfolgingClientImpl.kt
@@ -41,15 +41,15 @@ class VeilarboppfolgingClientImpl(
 
 @Serializable
 data class Oppfolgingsstatus(
-    val oppfolgingsenhet: Oppfolgingsenhet,
-    val veilederId: String,
-    val formidlingsgruppe: String,
-    val servicegruppe: String,
-    val hovedmaalkode: String
+    val oppfolgingsenhet: Oppfolgingsenhet?,
+    val veilederId: String?,
+    val formidlingsgruppe: String?,
+    val servicegruppe: String?,
+    val hovedmaalkode: String?
 )
 
 @Serializable
 data class Oppfolgingsenhet(
-    val navn: String,
-    val enhetId: String
+    val navn: String?,
+    val enhetId: String?
 )
