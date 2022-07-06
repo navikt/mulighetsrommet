47713a3420fb9bb66ad1b24df3429d4a37013180
Sindre Moldeklev <sndrem@gmail.com>
2022-07-06 14:26:39 +0200

    Legger til Autorization-header

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/oppfolging/VeilarboppfolgingClientImpl.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/oppfolging/VeilarboppfolgingClientImpl.kt
@@ -2,6 +2,7 @@ package no.nav.mulighetsrommet.api.clients.oppfolging
 
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import no.nav.mulighetsrommet.api.setup.http.baseClient
 import org.slf4j.LoggerFactory
 
@@ -16,7 +17,7 @@ class VeilarboppfolgingClientImpl(
     override suspend fun hentOppfolgingsstatus(fnr: String, accessToken: String?) {
         val response =
             client.get("$baseUrl/person/$fnr/oppfolgingsstatus") {
-                // header(HttpHeaders.Authorization, "Bearer ${veilarboppfolgingTokenProvider(accessToken)}")
+                header(HttpHeaders.Authorization, "Bearer ${veilarboppfolgingTokenProvider(accessToken)}")
             }
         log.info("Hentet oppfølgingsstatus for fnr: $fnr - Status: ${response.status}")
         TODO("Hent oppfølging av status er ikke implementert enda")
