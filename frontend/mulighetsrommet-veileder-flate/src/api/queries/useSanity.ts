1b0aeebff9374677469280fbbb40ea845fa0482b
Signe Carlsen <signe_c_97@hotmail.com>
2022-07-06 14:54:55 +0200

    legg til test

--- frontend/mulighetsrommet-veileder-flate/src/api/queries/useSanity.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/queries/useSanity.ts
@@ -5,6 +5,7 @@ import useDebounce from '../../hooks/useDebounce';
 
 export function useSanity<T>(query: string) {
   const debouncedQuery = useDebounce(query, 300);
+  console.log(query)
   return useQuery(
     [QueryKeys.SanityQuery, debouncedQuery],
     () => MulighetsrommetService.sanityQuery({ query: debouncedQuery }) as Promise<T>
