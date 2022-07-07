6c1a46666509eb9e86fcfcd919351c81062c8248
Sindre Moldeklev <sndrem@gmail.com>
2022-07-07 10:50:07 +0200

    ignoreUnknownKeys = true

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/setup/http/RestClient.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/setup/http/RestClient.kt
@@ -4,11 +4,16 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 internal val defaultHttpClient = HttpClient(CIO) {
     expectSuccess = false
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                ignoreUnknownKeys = true
+            }
+        )
     }
 }
 
