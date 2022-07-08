79e55e4471f4a13cd6851bf65a14cb092655edfc
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-07-08 15:29:55 +0200

    configure sanity workspaces

--- frontend/mulighetsrommet-cms/sanity.config.ts
+++ frontend/mulighetsrommet-cms/sanity.config.ts
@@ -1,17 +1,36 @@
-import { createConfig } from "sanity";
+import { createConfig, createPlugin } from "sanity";
 import { deskTool } from "sanity/desk";
 import { schemaTypes } from "./schemas";
 
-export default createConfig({
-  name: "default",
-  title: "mulighetsrommet",
-
-  projectId: "xegcworx",
-  dataset: "production",
-
-  plugins: [deskTool()],
-
+const mulighetsrommetConfig = createPlugin({
+  name: "mulighetsrommet",
+  plugins: [
+    deskTool(),
+  ],
   schema: {
     types: schemaTypes,
   },
 });
+
+export default createConfig([
+  {
+    name: "default",
+    title: "Prod",
+
+    projectId: "xegcworx",
+    dataset: "production",
+    basePath: "/prod",
+
+    plugins: [mulighetsrommetConfig()],
+  },
+  {
+    name: "dev",
+    title: "Dev",
+
+    projectId: "xegcworx",
+    dataset: "dev",
+    basePath: "/dev",
+
+    plugins: [mulighetsrommetConfig()],
+  },
+]);
