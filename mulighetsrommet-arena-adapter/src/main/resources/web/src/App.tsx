1ae4b6eaebb79c7f1d540a1bc9aa40627a3b24b6
Steffen Lien <steffen.lien@nav.no>
2022-07-11 10:50:02 +0200

    add crappy design for topic control

--- mulighetsrommet-arena-adapter/src/main/resources/web/src/App.tsx
+++ mulighetsrommet-arena-adapter/src/main/resources/web/src/App.tsx
@@ -1,10 +1,64 @@
+import {
+  Box,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  SimpleGrid,
+  Stack,
+  Switch,
+  Text,
+} from "@chakra-ui/react";
+import type React from "react";
+import { Fragment } from "react";
+
+const Layout = ({ children }: React.PropsWithChildren) => (
+  <Box as="main" w="100%">
+    <Container mt="8" maxW="container.lg">
+      <LinkBox mb="8">
+        <LinkOverlay href="https://youtu.be/RJHctyXPmkg?t=5">
+          <Heading size="4xl">MAAM</Heading>
+        </LinkOverlay>
+        <Heading size="xs">mulighetsrommet-arena-adapter-manager</Heading>
+      </LinkBox>
+      {children}
+    </Container>
+  </Box>
+);
+
+function TopicEnableDisable() {
+  // TODO: Bytt ut med fetch
+  const topics = [
+    "teamarenanais.aapen-arena-tiltakgjennomforingendret-v1-q2",
+    "teamarenanais.aapen-arena-tiltakdeltakerendret-v1-q2",
+    "teamarenanais.aapen-arena-tiltakendret-v1-q2",
+    "teamarenanais.aapen-arena-sakendret-v1-q2",
+  ];
+  return (
+    <Box>
+      <Heading mb="4">Topic control</Heading>
+      <Box boxShadow="sm" p="5" borderWidth="1px" rounded="md">
+        <FormControl as={SimpleGrid} columns={{ base: 0, lg: 2 }} spacing={2}>
+          {topics.map((topic) => (
+            <Fragment key={topic}>
+              <FormLabel>{topic}</FormLabel>
+              <Switch size="lg" />
+            </Fragment>
+          ))}
+        </FormControl>
+      </Box>
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <h1>mulighetsrommet-arena-adapter-management</h1>
-    </div>
-  )
+    <Layout>
+      <TopicEnableDisable />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- mulighetsrommet-arena-adapter/src/main/resources/web/src/main.tsx
+++ mulighetsrommet-arena-adapter/src/main/resources/web/src/main.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const customeTheme = extendTheme({
+  colors: {},
+  fonts: {},
+  fontSizes: {},
+  breakpoints: {
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+  },
+});
+
+const theme = extendTheme({ customeTheme });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
