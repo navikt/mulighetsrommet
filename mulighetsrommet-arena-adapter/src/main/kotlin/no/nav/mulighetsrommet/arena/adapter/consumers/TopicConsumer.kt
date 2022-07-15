7fe634870dcfd713f7f3576354abf7f780f37ed4
Steffen Lien <steffen.lien@nav.no>
2022-07-15 13:15:54 +0200

    adjust how we initialize consumers

---
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/consumers/TopicConsumer.kt
@@ -0,0 +1,14 @@
+package no.nav.mulighetsrommet.arena.adapter.consumers
+
+import kotlinx.serialization.json.JsonElement
+
+abstract class TopicConsumer {
+    abstract val key: String
+    abstract val topic: String
+
+    open fun shouldProcessEvent(payload: JsonElement): Boolean = true
+
+    abstract fun resolveKey(payload: JsonElement): String
+
+    abstract fun processEvent(payload: JsonElement)
+}
