b1634ce02e009d4f54802d97c7ee8464082abae5
Signe Carlsen <signe_c_97@hotmail.com>
2022-07-20 13:12:30 +0200

    fjern maxlifetime

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/database/Database.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/database/Database.kt
@@ -41,7 +41,6 @@ class Database(databaseConfig: DatabaseConfig) {
         hikariConfig.username = databaseConfig.user
         hikariConfig.password = databaseConfig.password.value
         hikariConfig.maximumPoolSize = 3
-        hikariConfig.maxLifetime = 30000
         hikariConfig.validate()
 
         dataSource = HikariDataSource(hikariConfig)
