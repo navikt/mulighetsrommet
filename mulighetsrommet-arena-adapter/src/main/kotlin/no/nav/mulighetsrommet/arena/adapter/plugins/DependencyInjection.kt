00465bd0cbc08ee58e6db30a8453f0ed8a8ae516
Steffen Lien <steffen.lien@nav.no>
2022-08-02 11:56:42 +0200

    adjust consumers to fit koin di

--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/plugins/DependencyInjection.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/plugins/DependencyInjection.kt
@@ -7,14 +7,16 @@ import no.nav.mulighetsrommet.arena.adapter.consumers.SakEndretConsumer
 import no.nav.mulighetsrommet.arena.adapter.consumers.TiltakEndretConsumer
 import no.nav.mulighetsrommet.arena.adapter.consumers.TiltakdeltakerEndretConsumer
 import no.nav.mulighetsrommet.arena.adapter.consumers.TiltakgjennomforingEndretConsumer
+import no.nav.mulighetsrommet.arena.adapter.kafka.ConsumerSetup
 import no.nav.mulighetsrommet.arena.adapter.kafka.KafkaConsumerOrchestrator
 import no.nav.mulighetsrommet.arena.adapter.kafka.TopicConsumer
 import no.nav.mulighetsrommet.arena.adapter.repositories.EventRepository
+import no.nav.mulighetsrommet.arena.adapter.repositories.TopicRepository
 import no.nav.mulighetsrommet.arena.adapter.services.TopicService
 import no.nav.mulighetsrommet.database.Database
 import no.nav.mulighetsrommet.database.DatabaseConfig
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.SLF4JLogger
 import java.util.Properties
@@ -35,14 +37,7 @@ fun Application.configureDependencyInjection(appConfig: AppConfig, kafkaPreset: 
 }
 
 private fun consumers(kafkaConfig: KafkaConfig) = module {
-    single<List<TopicConsumer<*>>>(named("consumers")) {
-        listOf(
-            TiltakEndretConsumer(kafkaConfig.getTopic("tiltakendret"), get(), get()),
-            TiltakgjennomforingEndretConsumer(kafkaConfig.getTopic("tiltakgjennomforingendret"), get(), get()),
-            TiltakdeltakerEndretConsumer(kafkaConfig.getTopic("tiltakdeltakerendret"), get(), get()),
-            SakEndretConsumer(kafkaConfig.getTopic("sakendret"), get(), get()),
-        )
-    }
+    single { ConsumerSetup(kafkaConfig, get(), get()) }
 }
 
 private fun db(databaseConfig: DatabaseConfig) = module(createdAtStart = true) {
@@ -57,6 +52,7 @@ private fun kafka(kafkaConfig: KafkaConfig, kafkaPreset: Properties) = module {
 
 private fun repositories() = module {
     single { EventRepository(get()) }
+    single { TopicRepository(get()) }
 }
 
 private fun services() = module {
