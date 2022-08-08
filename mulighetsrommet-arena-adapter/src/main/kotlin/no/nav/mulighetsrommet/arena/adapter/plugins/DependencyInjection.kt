26f8a832a8e403b8169198d030ec8e566818f934
Sondre Lefsaker <sondre.lefsaker@nav.no>
2022-08-08 09:24:47 +0200

    make topic service configurable

--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/plugins/DependencyInjection.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/plugins/DependencyInjection.kt
@@ -14,6 +14,7 @@ import no.nav.mulighetsrommet.arena.adapter.repositories.TopicRepository
 import no.nav.mulighetsrommet.arena.adapter.services.TopicService
 import no.nav.mulighetsrommet.database.Database
 import no.nav.mulighetsrommet.database.DatabaseConfig
+import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.SLF4JLogger
@@ -32,7 +33,7 @@ fun Application.configureDependencyInjection(
             consumers(appConfig.kafka),
             kafka(appConfig.kafka, kafkaPreset),
             repositories(),
-            services(),
+            services(appConfig.services),
             clients(appConfig.services, tokenClient)
         )
     }
@@ -65,8 +66,8 @@ private fun repositories() = module {
     single { TopicRepository(get()) }
 }
 
-private fun services() = module {
-    single { TopicService(get(), get()) }
+private fun services(services: ServiceConfig): Module = module {
+    single { TopicService(get(), get(), services.topicService) }
 }
 
 private fun clients(serviceConfig: ServiceConfig, tokenClient: AzureAdMachineToMachineTokenClient) = module {
