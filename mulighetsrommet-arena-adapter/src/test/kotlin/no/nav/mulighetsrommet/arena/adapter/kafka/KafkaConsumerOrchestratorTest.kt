1f297466d712314f9162d8b79acd861d7e52b888
Signe Carlsen <signe_c_97@hotmail.com>
2022-08-09 14:40:07 +0200

    WIP

--- mulighetsrommet-arena-adapter/src/test/kotlin/no/nav/mulighetsrommet/arena/adapter/kafka/KafkaConsumerOrchestratorTest.kt
+++ mulighetsrommet-arena-adapter/src/test/kotlin/no/nav/mulighetsrommet/arena/adapter/kafka/KafkaConsumerOrchestratorTest.kt
@@ -1,26 +1,22 @@
 package no.nav.mulighetsrommet.arena.adapter.kafka
 
 import io.kotest.common.runBlocking
+import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.TestCaseOrder
+import io.kotest.extensions.testcontainers.TestContainerExtension
+import io.kotest.extensions.testcontainers.kafka.createStringStringProducer
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import kotlinx.coroutines.delay
-import no.nav.common.kafka.producer.KafkaProducerClient
-import no.nav.common.kafka.producer.util.KafkaProducerClientBuilder
 import no.nav.common.kafka.util.KafkaPropertiesBuilder
 import no.nav.mulighetsrommet.arena.adapter.repositories.Topic
 import no.nav.mulighetsrommet.arena.adapter.repositories.TopicRepository
 import no.nav.mulighetsrommet.database.DatabaseConfig
 import no.nav.mulighetsrommet.database.Password
 import no.nav.mulighetsrommet.database.kotest.extensions.DatabaseListener
-import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.admin.KafkaAdminClient
-import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 import java.util.*
@@ -40,108 +36,134 @@ internal class KafkaConsumerOrchestratorTest : FunSpec({
 
     testOrder = TestCaseOrder.Sequential
 
-    val topicName = "tiltakendret"
-
-    lateinit var producer: KafkaProducerClient<String, String>
     lateinit var consumerProperties: Properties
     val listener =
         DatabaseListener(createDatabaseConfigWithRandomSchema())
     register(listener)
 
-    val kafkaContainer =
-        KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
-
-    beforeSpec {
-        kafkaContainer.start()
-
-        val brokerUrl = kafkaContainer.bootstrapServers
-
-        val producerProperties = KafkaPropertiesBuilder.producerBuilder()
-            .withBrokerUrl(brokerUrl)
-            .withBaseProperties()
-            .withProducerId("producer")
-            .withSerializers(
-                StringSerializer::class.java,
-                StringSerializer::class.java
-            )
-            .build()
-
-        producer =
-            KafkaProducerClientBuilder.builder<String, String>()
-                .withProperties(producerProperties)
-                .build()
-
-        consumerProperties = KafkaPropertiesBuilder.consumerBuilder()
-            .withBrokerUrl(brokerUrl)
-            .withBaseProperties()
-            .withConsumerGroupId("consumer")
-            .withDeserializers(
-                ByteArrayDeserializer::class.java,
-                ByteArrayDeserializer::class.java
-            )
-            .build()
-
-        val admin =
-            KafkaAdminClient.create(
-                mapOf(
-                    CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to
-                        brokerUrl
-                )
-            )
-
-        admin.deleteTopics(
-            listOf(
-                topicName
-            )
+    val kafka = install(
+        TestContainerExtension(
+            KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
         )
-
-        admin.createTopics(
-            listOf(
-                NewTopic(topicName, 1, 1)
-            )
-        )
-
-        admin.close()
+    ) {
+        withEmbeddedZookeeper()
     }
 
-    test("consumer starts processing event from producer") {
-        val topicRepository: TopicRepository =
-            mockk(relaxed = true)
-        every { topicRepository.selectAll() } answers {
-            listOf(
-                Topic(1, "key", topicName, mockk(), true)
-            )
-        }
-        val consumer: TopicConsumer<Any> = mockk()
-        every { consumer.consumerConfig.topic } answers { topicName }
+    val topicName1 = "topic1"
+    val topicName2 = "topic2"
+    val topicName3 = "topic3"
 
-        val kafka = KafkaConsumerOrchestrator(
+    val topicRepository: TopicRepository =
+        mockk(relaxed = true)
+
+    every { topicRepository.selectAll() } answers {
+        listOf(
+            Topic(1, "key1", topicName1, mockk(), true),
+            Topic(2, "key2", topicName2, mockk(), true),
+            Topic(3, "key3", topicName3, mockk(), true)
+        )
+    }
+
+    beforeSpec {
+
+        val brokerUrl = kafka.bootstrapServers
+
+        consumerProperties =
+            KafkaPropertiesBuilder.consumerBuilder()
+                .withBrokerUrl(brokerUrl)
+                .withBaseProperties()
+                .withConsumerGroupId("consumer")
+                .withDeserializers(
+                    ByteArrayDeserializer::class.java,
+                    ByteArrayDeserializer::class.java
+                )
+                .build()
+    }
+
+    test("consumer starts processing event from producer 2") {
+        val consumer1: TopicConsumer<Any> = mockk()
+        val consumer2: TopicConsumer<Any> = mockk()
+        val consumer3: TopicConsumer<Any> = mockk()
+
+        val consumerRepository =
+            KafkaConsumerRepository(listener.db)
+
+        every { consumer1.consumerConfig.topic } answers { topicName1 }
+        // every { runBlocking { consumer1.processEvent(any()) } } returns Unit
+
+        every { consumer2.consumerConfig.topic } answers { topicName2 }
+        every { runBlocking { consumer2.processEvent(any()) } } throws Exception()
+
+        every { consumer3.consumerConfig.topic } answers { topicName3 }
+
+        val orchestrator = KafkaConsumerOrchestrator(
             consumerProperties,
             listener.db,
-            ConsumerGroup(listOf(consumer)),
+            ConsumerGroup(listOf(consumer1, consumer2, consumer3)),
             topicRepository,
             200
         )
 
+        val key1 = "key1"
+        val value1 = "value1"
+        val key2 = "key2"
+        val value2 = "value2"
+
+        val producer =
+            kafka.createStringStringProducer()
+
         producer.send(
             ProducerRecord(
-                topicName,
-                "key",
-                "value"
+                topicName2,
+                key2,
+                value2
+            )
+        )
+        producer.send(
+            ProducerRecord(
+                topicName1,
+                key1,
+                value1
             )
         )
         producer.close()
 
-        runBlocking { delay(3000) }
-
-        verify(exactly = 1) {
+        verify(exactly = 1, timeout = 1000) {
             runBlocking {
-                consumer.processEvent(any())
+                consumer2.processEvent(
+                    JsonElementDeserializer().deserialize(
+                        topicName2,
+                        value2.toByteArray()
+                    )
+                )
             }
         }
-    }
 
-    afterSpec {
-        kafkaContainer.stop()
+        verify(exactly = 1, timeout = 1000) {
+            runBlocking {
+                consumer1.processEvent(
+                    JsonElementDeserializer().deserialize(
+                        topicName1,
+                        value1.toByteArray()
+                    )
+                )
+            }
+        }
+
+        verify(exactly = 0, timeout = 1000) {
+            runBlocking {
+                consumer3.processEvent(any())
+            }
+        }
+
+        println(consumerRepository.getRecords(topicName1, 0, 100))
+
+        println(
+            consumerRepository.hasRecordWithKey(
+                topicName2,
+                0,
+                key2.toByteArray()
+            )
+        )
     }
 })
