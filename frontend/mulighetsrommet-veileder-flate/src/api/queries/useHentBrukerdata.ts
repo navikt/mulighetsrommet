1a5efc20a5d720379b2262e7948d1ec00b707655
Steffen Lien <steffen.lien@nav.no>
2022-08-12 10:25:10 +0200

    delete unused hooks

--- frontend/mulighetsrommet-veileder-flate/src/api/queries/useHentBrukerdata.ts
+++ frontend/mulighetsrommet-veileder-flate/src/api/queries/useHentBrukerdata.ts
@@ -1,6 +1,6 @@
 import { Bruker, MulighetsrommetService } from 'mulighetsrommet-api-client';
 import { useQuery } from 'react-query';
-import { QueryKeys } from '../../core/api/QueryKeys';
+import { QueryKeys } from '../../core/api/query-keys';
 import { useHentFnrFraUrl } from '../../hooks/useHentFnrFraUrl';
 
 export function useHentBrukerdata() {
