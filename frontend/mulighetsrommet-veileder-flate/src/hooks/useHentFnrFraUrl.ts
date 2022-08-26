da0659d761adb939f26da162452d5a9bd821a324
Sindre Moldeklev <sndrem@gmail.com>
2022-08-26 14:51:28 +0200

    Prøver å hente fnr fra url

--- frontend/mulighetsrommet-veileder-flate/src/hooks/useHentFnrFraUrl.ts
+++ frontend/mulighetsrommet-veileder-flate/src/hooks/useHentFnrFraUrl.ts
@@ -1,6 +1,11 @@
 import { useParams } from 'react-router-dom';
 
 export function useHentFnrFraUrl() {
-  const { fnr = 'undefined' } = useParams<{ fnr: string }>();
+  const { fnr } = useParams<{ fnr: string }>();
+
+  if (!fnr) {
+    return window.location.pathname.substring(1, window.location.pathname.lastIndexOf('/')); // Hent ut fnr fra pathname
+  }
+
   return fnr;
 }
