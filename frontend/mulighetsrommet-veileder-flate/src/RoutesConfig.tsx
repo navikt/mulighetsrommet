61333cc1f60e234f4febe207d07492cd89197117
Sindre Moldeklev <sndrem@gmail.com>
2022-08-30 20:36:06 +0200

    Fallback fra dialog til forside fra routeconfig

--- frontend/mulighetsrommet-veileder-flate/src/RoutesConfig.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/RoutesConfig.tsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useHentFnrFraUrl } from './hooks/useHentFnrFraUrl';
 import ViewTiltakstypeDetaljer from './views/tiltaksgjennomforing-detaljer/ViewTiltakstypeDetaljer';
 import ViewTiltakstypeOversikt from './views/tiltaksgjennomforing-oversikt/ViewTiltakstypeOversikt';
 
 const RoutesConfig = () => {
+  const fnr = useHentFnrFraUrl();
   return (
     <Routes>
       (
       <>
         <Route path="/" element={<ViewTiltakstypeOversikt />} />
         <Route path="tiltak/:tiltaksnummer" element={<ViewTiltakstypeDetaljer />} />
+        <Route path=":tiltaksnummer" element={<Navigate to={`/${fnr}`} />}></Route>
+        {/* Fallback dersom veileder navigerer fra Dialogen til Arbeidsmarkedstiltak */}
       </>
       )
     </Routes>
