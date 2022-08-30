f9c01db882bce563dab88c5e8d1a031ec8026ad1
Steffen Lien <steffen.lien@nav.no>
2022-08-30 14:25:35 +0200

    fix issue with swagger ui not using openapi

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Webjars.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Webjars.kt
@@ -2,9 +2,12 @@ package no.nav.mulighetsrommet.api.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.webjars.*
+import no.nav.mulighetsrommet.api.SwaggerConfig
 
-fun Application.configureWebjars() {
-    install(Webjars) {
-        path = "assets"
+fun Application.configureWebjars(swaggerConfig: SwaggerConfig?) {
+    if (swaggerConfig?.enable == true) {
+        install(Webjars) {
+            path = "assets"
+        }
     }
 }
