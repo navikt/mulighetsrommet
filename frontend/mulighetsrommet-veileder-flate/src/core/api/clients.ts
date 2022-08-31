251c2a7912161df8078617198c9234215663185e
Sindre Moldeklev <sndrem@gmail.com>
2022-08-31 12:58:16 +0200

    Henter baseurl korrekt

--- frontend/mulighetsrommet-veileder-flate/src/core/api/clients.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/clients.ts
@@ -1,3 +1,5 @@
 import { MulighetsrommetClient } from 'mulighetsrommet-api-client';
 
-export const mulighetsrommetClient = new MulighetsrommetClient();
+export const mulighetsrommetClient = new MulighetsrommetClient({
+  BASE: import.meta.env.VITE_MULIGHETSROMMET_API_BASE ?? '',
+});
