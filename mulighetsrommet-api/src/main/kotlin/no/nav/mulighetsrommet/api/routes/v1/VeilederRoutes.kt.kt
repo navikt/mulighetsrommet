b232dce576a958cd89364896fd1625104259e646
Signe Carlsen <signe_c_97@hotmail.com>
2022-09-05 15:55:33 +0200

    rydd opp i log statements

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/VeilederRoutes.kt.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/VeilederRoutes.kt.kt
@@ -8,18 +8,15 @@ import io.ktor.server.routing.*
 import no.nav.mulighetsrommet.api.services.VeilederService
 import no.nav.mulighetsrommet.api.utils.getAccessToken
 import org.koin.ktor.ext.inject
-import org.slf4j.LoggerFactory
 
 fun Route.veilederRoutes() {
     val veilederService: VeilederService by inject()
-    val log = LoggerFactory.getLogger(this.javaClass)
 
     route("/api/v1/veileder") {
         get {
             val accessToken = call.getAccessToken()
             val callId = call.request.header(HttpHeaders.XRequestId)
 
-            log.info("Innkommende request id $callId")
             call.respond(veilederService.hentVeilederdata(accessToken, callId))
         }
     }
