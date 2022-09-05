06feecd68355030c145197e0e8da02e53e4c87dd
Signe Carlsen <signe_c_97@hotmail.com>
2022-09-05 14:14:54 +0200

    logg inkommende reguest id

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/BrukerRoutes.kt.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/BrukerRoutes.kt.kt
@@ -2,6 +2,7 @@ package no.nav.mulighetsrommet.api.routes.v1
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.mulighetsrommet.api.services.BrukerService
@@ -11,6 +12,8 @@ import org.koin.ktor.ext.inject
 fun Route.brukerRoutes() {
     val brukerService: BrukerService by inject()
 
+    val logger = application.environment.log
+
     route("/api/v1/bruker") {
         get {
             val fnr = call.request.queryParameters["fnr"] ?: return@get call.respondText(
@@ -18,6 +21,7 @@ fun Route.brukerRoutes() {
                 status = HttpStatusCode.BadRequest
             )
             val accessToken = call.getAccessToken()
+            logger.debug("Request id fra inkommende request", call.request.header(HttpHeaders.XRequestId))
             call.respond(brukerService.hentBrukerdata(fnr, accessToken))
         }
     }
