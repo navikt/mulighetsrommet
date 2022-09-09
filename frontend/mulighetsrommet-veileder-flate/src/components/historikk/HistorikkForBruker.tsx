f161011d546b34b0061101ce0a287864f8e49f58
Sindre Moldeklev <sndrem@gmail.com>
2022-09-09 15:22:48 +0200

    Sorterer og grupperer historikk

--- frontend/mulighetsrommet-veileder-flate/src/components/historikk/HistorikkForBruker.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/historikk/HistorikkForBruker.tsx
@@ -1,6 +1,7 @@
 import { Alert, Loader } from '@navikt/ds-react';
 import classNames from 'classnames';
 import { HistorikkForBruker as IHistorikkForBruker } from 'mulighetsrommet-api-client';
+import { format } from 'path';
 import { useHentHistorikk } from '../../core/api/queries/useHentHistorikk';
 import { formaterDato } from '../../utils/Utils';
 import './HistorikkForBruker.less';
@@ -40,10 +41,24 @@ export function HistorikkForBruker() {
     return <Alert variant="info">Fant ikke historikk for bruker</Alert>;
   }
 
+  const sorterPaaFraDato = (a: IHistorikkForBruker, b: IHistorikkForBruker) => {
+    if (!a.fraDato || !b.fraDato) return -1; // Flytt deltakelser uten fraDato bakerst
+
+    return new Date(a.fraDato ?? '').getTime() - new Date(b.fraDato ?? '').getTime();
+  };
+
+  const venter = data?.filter(deltak => ['VENTER'].includes(deltak.status ?? '')).sort(sorterPaaFraDato) ?? [];
+  const deltar = data?.filter(deltak => ['DELTAR'].includes(deltak.status ?? '')).sort(sorterPaaFraDato) ?? [];
+  const avsluttet = data?.filter(deltak => ['AVSLUTTET'].includes(deltak.status ?? '')).sort(sorterPaaFraDato) ?? [];
+  const ikkeAktuell =
+    data?.filter(deltak => ['IKKE_AKTUELL'].includes(deltak.status ?? '')).sort(sorterPaaFraDato) ?? [];
+
+  const tiltak = [...venter, ...deltar, ...avsluttet, ...ikkeAktuell];
+
   return (
     <div className="historikk-for-bruker">
       <ul className="historikk-for-bruker-liste">
-        {data?.map(historikk => {
+        {tiltak?.map(historikk => {
           return (
             <li key={historikk.id} className="historikk-for-bruker-element">
               <div>
@@ -54,11 +69,9 @@ export function HistorikkForBruker() {
                   <p className="historikk-text-content">{historikk.tiltakstype}</p>
                   <p className="historikk-text-content">{historikk.arrangor}</p>
                 </div>
-                <p className="historikk-text-content">
-                  {[historikk.fraDato, historikk.tilDato]
-                    .filter(Boolean)
-                    .map(dato => formaterDato(dato!))
-                    .join(' - ')}
+                <p className="historikk-text-content historikk-datoer">
+                  <span> {formaterDato(historikk.fraDato ?? '')}</span> -{' '}
+                  <span>{formaterDato(historikk.tilDato ?? '')}</span>
                 </p>
               </div>
               <aside>
--- frontend/mulighetsrommet-veileder-flate/src/utils/Utils.ts
+++ frontend/mulighetsrommet-veileder-flate/src/utils/Utils.ts
@@ -14,10 +14,16 @@ export function kebabCase(string: string | { label: string }) {
   return specialChar(string).replace(/\s+/g, '-');
 }
 
-export function formaterDato(dato: string | Date) {
-  return new Date(dato).toLocaleString('no-NO', {
+export function formaterDato(dato: string | Date, fallback = ''): string {
+  const result = new Date(dato).toLocaleString('no-NO', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   });
+
+  if (result === 'Invalid Date') {
+    return fallback;
+  }
+
+  return result;
 }
