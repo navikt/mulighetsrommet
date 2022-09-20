ff83dfe63223afa3627a2e1c09f6887a5e2e739f
Signe Carlsen <signe_c_97@hotmail.com>
2022-09-20 10:02:49 +0200

    legg til call-id header i base client

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/setup/http/RestClient.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/setup/http/RestClient.kt
@@ -5,8 +5,10 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
+import org.slf4j.MDC
 
 internal val baseClient = HttpClient(CIO) {
     expectSuccess = false
@@ -19,5 +21,6 @@ internal val baseClient = HttpClient(CIO) {
     }
     defaultRequest {
         header("Nav-Consumer-Id", "mulighetsrommet-api")
+        MDC.get("call-id")?.let { header(HttpHeaders.XRequestId, it) }
     }
 }
