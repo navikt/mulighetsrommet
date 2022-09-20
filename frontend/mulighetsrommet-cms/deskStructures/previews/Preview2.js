86aad1c9b9dd4917e7bb71c7a70296f2964fcda0
Signe Carlsen <signe_c_97@hotmail.com>
2022-09-20 15:02:11 +0200

    legg under hverandre fargekodet

--- frontend/mulighetsrommet-cms/deskStructures/previews/Preview2.js
+++ frontend/mulighetsrommet-cms/deskStructures/previews/Preview2.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "part:@sanity/base/client";
+import Switch from "react-switch";
 
 const client = sanityClient.withConfig({ apiVersion: "2021-10-21" });
 
-function MinHeight({ children }) {
-  return <div style={{ minHeight: "550px" }}>{children}</div>;
+function MarginBottom({ children }) {
+  return <div style={{ marginBottom: "4rem" }}>{children}</div>;
 }
 
 export function Preview2({ document }) {
   const [tiltaksdata, setTiltaksdata] = useState(null);
+  const [fargekodet, setFargekodet] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +22,12 @@ export function Preview2({ document }) {
 
     fetchData();
   }, [document]);
+
+  function TekstFraTiltakstype({ children }) {
+    return (
+      <p style={{ color: fargekodet ? "#1434A4" : "black" }}>{children}</p>
+    );
+  }
 
   function tilListe(el) {
     if (el.listItem === "bullet") {
@@ -49,7 +57,19 @@ export function Preview2({ document }) {
   return (
     <div style={{ padding: "20px" }}>
       <h1>{displayed.tiltaksgjennomforingNavn}</h1>
-      <small>Tiltakstype: {tiltaksdata.tiltakstypeNavn}</small>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <small>Tiltakstype: {tiltaksdata.tiltakstypeNavn}</small>
+        <div>
+          <medium>Marker tekst fra tiltakstype</medium>
+          <Switch
+            onChange={() => setFargekodet(!fargekodet)}
+            checked={fargekodet}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            onColor={"#1434A4"}
+          />
+        </div>
+      </div>
       <div
         style={
           {
@@ -60,30 +80,36 @@ export function Preview2({ document }) {
         }
       >
         <div>
-          <MinHeight>
+          <MarginBottom>
             <h3>Beskrivelse</h3>
             <p>{displayed.beskrivelse}</p>
-            <p>{tiltaksdata?.beskrivelse}</p>
-          </MinHeight>
-          <MinHeight>
+            <TekstFraTiltakstype>
+              {tiltaksdata?.beskrivelse}
+            </TekstFraTiltakstype>
+          </MarginBottom>
+          <MarginBottom>
             <h3>For hvem</h3>
-            <p>{tiltaksdata.faneinnhold?.forHvem?.map(tilListe)}</p>
+            <TekstFraTiltakstype>
+              {tiltaksdata.faneinnhold?.forHvem?.map(tilListe)}
+            </TekstFraTiltakstype>
             <p>{displayed.faneinnhold?.forHvem?.map(tilListe)}</p>
-          </MinHeight>
-          <MinHeight>
+          </MarginBottom>
+          <MarginBottom>
             <h3>Detaljer og innhold</h3>
-            <p>
+            <TekstFraTiltakstype>
               {tiltaksdata.faneinnhold.detaljerOgInnhold.map((el) => {
                 return tilListe(el);
               })}
-            </p>
+            </TekstFraTiltakstype>
             <p>{displayed.faneinnhold?.detaljerOgInnhold?.map(tilListe)}</p>
-          </MinHeight>
-          <MinHeight>
+          </MarginBottom>
+          <MarginBottom>
             <h3>Påmelding og varighet</h3>
-            <p>{tiltaksdata.faneinnhold?.pameldingOgVarighet?.map(tilListe)}</p>
+            <TekstFraTiltakstype>
+              {tiltaksdata.faneinnhold?.pameldingOgVarighet?.map(tilListe)}
+            </TekstFraTiltakstype>
             <p>{displayed.faneinnhold?.pameldingOgVarighet?.map(tilListe)}</p>
-          </MinHeight>
+          </MarginBottom>
         </div>
       </div>
     </div>
