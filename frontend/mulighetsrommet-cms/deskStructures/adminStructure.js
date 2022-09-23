2e0a2f227b4db8dc6495dc51ae9f0b780b5b2703
Sindre Moldeklev <sndrem@gmail.com>
2022-09-23 15:00:27 +0200

    Refaktorer struktur til common

--- frontend/mulighetsrommet-cms/deskStructures/adminStructure.js
+++ frontend/mulighetsrommet-cms/deskStructures/adminStructure.js
@@ -1,81 +1,8 @@
 import S from "@sanity/desk-tool/structure-builder";
+import { commonStructure } from "./commonStructure";
 
 const adminStructure = [
-  S.listItem()
-    .title("Filtrerte tiltaksgjennomføringer")
-    .child(
-      S.list()
-        .title("Filter")
-        .items([
-          S.listItem()
-            .title("Per fylke")
-            .child(
-              S.documentTypeList("enhet")
-                .title("Per fylke")
-                .filter('type == "Fylke"')
-                .defaultOrdering([{ field: "navn", direction: "asc" }])
-                .child((enhet) =>
-                  S.documentList()
-                    .defaultOrdering([
-                      { field: "_createdAt", direction: "desc" },
-                    ])
-                    .title("Tiltaksgjennomføringer")
-                    .filter(
-                      '_type == "tiltaksgjennomforing" && ($enhet == fylke._ref)'
-                    )
-                    .params({ enhet })
-                )
-            ),
-          S.listItem()
-            .title("Per kontor")
-            .child(
-              S.documentTypeList("enhet")
-                .title("Per kontor")
-                .filter('type == "Lokal"')
-                .defaultOrdering([{ field: "navn", direction: "asc" }])
-                .child((enhet) =>
-                  S.documentList()
-                    .title("Tiltaksgjennomføringer")
-                    .filter(
-                      '_type == "tiltaksgjennomforing" && $enhet in enheter[]._ref'
-                    )
-                    .defaultOrdering([
-                      { field: "_createdAt", direction: "desc" },
-                    ])
-                    .params({ enhet })
-                )
-            ),
-          S.listItem()
-            .title("Per tiltakstype")
-            .child(
-              S.documentTypeList("tiltakstype")
-                .title("Per tiltakstype")
-                .defaultOrdering([
-                  { field: "tiltakstypeNavn", direction: "asc" },
-                ])
-                .child((tiltakstype) =>
-                  S.documentList()
-                    .defaultOrdering([
-                      { field: "_createdAt", direction: "desc" },
-                    ])
-                    .title("Tiltaksgjennomføringer")
-                    .filter(
-                      '_type == "tiltaksgjennomforing" && $tiltakstype == tiltakstype._ref'
-                    )
-                    .params({ tiltakstype })
-                )
-            ),
-        ])
-    ),
-
-  S.listItem()
-    .title("Alle tiltaksgjennomføringer")
-    .child(
-      S.documentList()
-        .defaultOrdering([{ field: "_createdAt", direction: "desc" }])
-        .title("Alle tiltaksgjennomføringer")
-        .filter('_type == "tiltaksgjennomforing"')
-    ),
+  ...commonStructure(),
   S.divider(),
   ...S.documentTypeListItems().filter(
     (listItem) => !["tiltaksgjennomforing"].includes(listItem.getId())
