9082e4a2617cc9f208c3c29181c41b345f86db50
Sindre Moldeklev <sndrem@gmail.com>
2022-09-29 10:07:00 +0200

    Fjerner sjekk på navident ved oppslag på delt tiltak

--- frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useHentDeltMedbrukerStatus.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useHentDeltMedbrukerStatus.ts
@@ -11,11 +11,10 @@ export function useHentDeltMedBrukerStatus() {
   const { data: veilederData } = useHentVeilederdata();
   const bruker_fnr = useHentFnrFraUrl();
   const { data: sistDeltMedBruker, refetch } = useQuery<DelMedBruker>(
-    [QueryKeys.DeltMedBrukerStatus, bruker_fnr, veilederData?.ident, tiltaksnummer],
+    [QueryKeys.DeltMedBrukerStatus, bruker_fnr, tiltaksnummer],
     () =>
       mulighetsrommetClient.delMedBruker.getDelMedBruker({
         fnr: bruker_fnr,
-        navident: veilederData?.ident,
         tiltaksnummer,
       })
   );
