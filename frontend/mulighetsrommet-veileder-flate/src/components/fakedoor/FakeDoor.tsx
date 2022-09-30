dfc4df2edaeddc3c961bfaa59bac3b9c05209bdd
Signe Carlsen <signe_c_97@hotmail.com>
2022-09-30 14:01:04 +0200

    fjern fakedoor.less

--- frontend/mulighetsrommet-veileder-flate/src/components/fakedoor/FakeDoor.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/fakedoor/FakeDoor.tsx
@@ -1,12 +1,12 @@
-import './FakeDoor.less';
+import styles from './FakeDoor.module.scss';
 import imgUrl from '../../illustrasjonfakedoor.png';
 
 const FakeDoor = () => {
   return (
-    <div className="fakedoor-container">
-      <div className="fakedoor-text-container">
-        <h1 className="fakedoor-header">Takk for at du deltok i piloten</h1>
-        <div className="fakedoor-text">
+    <div className={styles.fakedoorContainer}>
+      <div className={styles.fakedoorTextContainer}>
+        <h1 className={styles.fakedoorHeader}>Takk for at du deltok i piloten</h1>
+        <div>
           <div>
             Piloten om arbeidsmarkedstiltak er nå avsluttet. Tusen takk for at du var med på å teste den nye tjenesten.
             Dine erfaringer og innspill er verdifulle og vil bidra til utviklingen av bedre digitale produkter i Nav.
@@ -14,7 +14,7 @@ const FakeDoor = () => {
           <br />
         </div>
       </div>
-      <div className="fakedoor-image">
+      <div className={styles.fakedoorImage}>
         <img src={imgUrl} id="fakedoor-bilde" alt="Illustrasjon av løsning" />
       </div>
     </div>
