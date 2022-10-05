a469723fd4665c6c6949eb241fecaad5ef8c17bb
Sindre Moldeklev <sndrem@gmail.com>
2022-10-05 13:39:27 +0200

    Kvitter oss med litt prop-drilling

--- frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/KontaktinfoFane.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/KontaktinfoFane.tsx
@@ -1,28 +1,22 @@
 import { Heading } from '@navikt/ds-react';
-import { Arrangor, Tiltaksansvarlig } from '../../../core/api/models';
 import ArrangorInfo from './ArrangorInfo';
 import styles from './KontaktinfoFane.module.scss';
 import TiltaksansvarligInfo from './TiltaksansvarligInfo';
 
-interface KontaktinfoFaneProps {
-  tiltaksansvarlige: Tiltaksansvarlig[];
-  arrangorinfo?: Arrangor;
-}
-
-const KontaktinfoFane = ({ tiltaksansvarlige, arrangorinfo }: KontaktinfoFaneProps) => {
+const KontaktinfoFane = () => {
   return (
     <div className={styles.kontaktinfo}>
       <div>
         <Heading size="large" level="2" className={styles.header}>
           Arrangør
         </Heading>
-        {arrangorinfo ? <ArrangorInfo arrangorinfo={arrangorinfo} /> : null}
+        <ArrangorInfo />
       </div>
       <div>
         <Heading size="large" level="2" className={styles.header}>
           Tiltaksansvarlig
         </Heading>
-        <TiltaksansvarligInfo tiltaksansvarlige={tiltaksansvarlige} />
+        <TiltaksansvarligInfo />
       </div>
     </div>
   );
