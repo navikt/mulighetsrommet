3070ccd51ab6c50a980040dac0daf363f8310719
Sindre Moldeklev <sndrem@gmail.com>
2022-10-10 10:18:01 +0200

    Sjekker om veileder har tilgang til bruker før deling til dialogen

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/DialogRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/DialogRoutes.kt
@@ -5,16 +5,21 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import no.nav.mulighetsrommet.api.plugins.getNavIdent
+import no.nav.mulighetsrommet.api.plugins.getNorskIdent
 import no.nav.mulighetsrommet.api.services.DialogRequest
 import no.nav.mulighetsrommet.api.services.DialogService
+import no.nav.mulighetsrommet.api.services.PoaoTilgangService
 import no.nav.mulighetsrommet.api.utils.getAccessToken
 import org.koin.ktor.ext.inject
 
 fun Route.dialogRoutes() {
     val dialogService: DialogService by inject()
+    val poaoTilgangService: PoaoTilgangService by inject()
 
     route("/api/v1/dialog") {
         post {
+            poaoTilgangService.verifyAccessToUserFromVeileder(getNavIdent(), getNorskIdent())
             val dialogRequest = call.receive<DialogRequest>()
             val fnr = call.request.queryParameters["fnr"] ?: return@post call.respondText(
                 "Mangler eller ugyldig fnr",
