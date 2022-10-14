be8a428d7da7f0ed186b4b41b5050f6366070a97
Signe Carlsen <signe_c_97@hotmail.com>
2022-10-14 11:06:12 +0200

    cache arrangørnavn basert på id

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/ArrangorService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/ArrangorService.kt
@@ -1,16 +1,33 @@
 package no.nav.mulighetsrommet.api.services
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.mulighetsrommet.api.clients.amtenhetsregister.AmtEnhetsregisterClient
 import no.nav.mulighetsrommet.api.clients.arenaordsproxy.ArenaOrdsProxyClient
+import java.util.concurrent.TimeUnit
 
 class ArrangorService(
     private val arenaOrdsProxyClient: ArenaOrdsProxyClient,
     private val amtEnhetsregisterClient: AmtEnhetsregisterClient
 ) {
+
+    val arrangorCache: Cache<Int, String> = Caffeine.newBuilder()
+        .expireAfterWrite(30, TimeUnit.MINUTES)
+        .maximumSize(500)
+        .build()
+
     suspend fun hentArrangorNavn(arrangorId: Int): String? {
+        val cahchedArrangorNavn = arrangorCache.getIfPresent(arrangorId)
+
+        if (cahchedArrangorNavn != null) return cahchedArrangorNavn
+
         val arbeidsgiverInfo = arenaOrdsProxyClient.hentArbeidsgiver(arrangorId)
         val virksomhetInfo =
             arbeidsgiverInfo?.virksomhetsnummer?.let { amtEnhetsregisterClient.hentVirksomhetsNavn(it.toInt()) }
-        return virksomhetInfo?.overordnetEnhetNavn
+
+        val arrangorNavn = virksomhetInfo?.overordnetEnhetNavn
+        arrangorNavn.let { arrangorCache.put(arrangorId, it) }
+
+        return arrangorNavn
     }
 }
--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/ArrangorServiceTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/ArrangorServiceTest.kt
@@ -5,6 +5,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.mulighetsrommet.api.clients.amtenhetsregister.AmtEnhetsregisterClient
 import no.nav.mulighetsrommet.api.clients.arenaordsproxy.ArenaOrdsProxyClient
 import no.nav.mulighetsrommet.api.domain.ArbeidsgiverDTO
@@ -17,7 +18,7 @@ class ArrangorServiceTest : FunSpec({
 
     val arrangorService = ArrangorService(arenaOrdsProxyClient, amtEnhetsregister)
 
-    test("henter navn på arrangør basert på virksomhetsnummer tilhørende arrangør id") {
+    beforeSpec {
         every { runBlocking { arenaOrdsProxyClient.hentArbeidsgiver(1) } } returns ArbeidsgiverDTO(
             virksomhetsnummer = "111",
             organisasjonsnummerMorselskap = "456"
@@ -39,8 +40,21 @@ class ArrangorServiceTest : FunSpec({
             overordnetEnhetOrganisasjonsnummer = "1011",
             overordnetEnhetNavn = "Overordnetbedrift 2"
         )
+    }
 
+    test("henter navn på arrangør basert på virksomhetsnummer tilhørende arrangør id") {
         arrangorService.hentArrangorNavn(1) shouldBe "Overordnetbedrift 1"
         arrangorService.hentArrangorNavn(2) shouldBe "Overordnetbedrift 2"
+    }
+
+    test("arrangør navn blir cachet basert på arrangør id") {
+        arrangorService.hentArrangorNavn(1)
+        arrangorService.hentArrangorNavn(1)
+        arrangorService.hentArrangorNavn(2)
+        arrangorService.hentArrangorNavn(2)
+        arrangorService.hentArrangorNavn(1)
+
+        verify(exactly = 2) { runBlocking { amtEnhetsregister.hentVirksomhetsNavn(any()) } }
+        verify(exactly = 2) { runBlocking { arenaOrdsProxyClient.hentArbeidsgiver(any()) } }
     }
 })
