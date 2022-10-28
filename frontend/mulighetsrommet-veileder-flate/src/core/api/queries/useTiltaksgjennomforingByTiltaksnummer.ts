731fb0f92342c98ca604e5bcd72074f15c0e7434
Signe Carlsen <signe_c_97@hotmail.com>
2022-10-28 15:00:14 +0200

    legg til url for preview i sanity

--- frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useTiltaksgjennomforingByTiltaksnummer.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useTiltaksgjennomforingByTiltaksnummer.ts
@@ -2,11 +2,14 @@ import groq from 'groq';
 import { Tiltaksgjennomforing } from '../models';
 import { useGetTiltaksgjennomforingIdFraUrl } from './useGetTiltaksgjennomforingIdFraUrl';
 import { useSanity } from './useSanity';
+import { useGetQueryParam } from '../../../hooks/useGetQueryParam';
 
 export default function useTiltaksgjennomforingByTiltaksnummer() {
   const tiltaksgjennomforingId = useGetTiltaksgjennomforingIdFraUrl();
+  const preview = useGetQueryParam('preview') === 'true';
+  const ekskluderDrafts = preview ? '' : '&& !(_id in path("drafts.**"))';
   return useSanity<Tiltaksgjennomforing>(
-    groq`*[_type == "tiltaksgjennomforing" && _id == '${tiltaksgjennomforingId}' && !(_id in path("drafts.**"))] {
+    groq`*[_type == "tiltaksgjennomforing" && _id == '${tiltaksgjennomforingId}' ${ekskluderDrafts}] {
     _id,
     tiltaksgjennomforingNavn,
     beskrivelse,
