a15acd8e5fed4d69f4bb4ce6ac83829628caf66e
Sindre Moldeklev <sndrem@gmail.com>
2022-10-31 15:48:38 +0100

    Lagt til rediger-knapp for del med bruker

--- frontend/mulighetsrommet-veileder-flate/src/components/modal/delemodal/Delemodal.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/modal/delemodal/Delemodal.tsx
@@ -1,21 +1,26 @@
-import { Alert, BodyShort, Button, Heading, Ingress, Modal, Textarea } from '@navikt/ds-react';
-import { useReducer } from 'react';
+import { ErrorColored, SuccessColored } from '@navikt/ds-icons';
+import { Alert, BodyShort, Button, Heading, Modal, Textarea } from '@navikt/ds-react';
+import classNames from 'classnames';
+import { useReducer, useRef } from 'react';
+import { mulighetsrommetClient } from '../../../core/api/clients';
+import { useFeatureToggles } from '../../../core/api/feature-toggles';
 import { logEvent } from '../../../core/api/logger';
+import { useHentDeltMedBrukerStatus } from '../../../core/api/queries/useHentDeltMedbrukerStatus';
 import { useHentFnrFraUrl } from '../../../hooks/useHentFnrFraUrl';
+import { useNavigerTilDialogen } from '../../../hooks/useNavigerTilDialogen';
+import { capitalize, erPreview } from '../../../utils/Utils';
+import Lenke from '../../lenke/Lenke';
 import modalStyles from '../Modal.module.scss';
 import delemodalStyles from './Delemodal.module.scss';
 import { Actions, State } from './DelemodalActions';
-import Lenke from '../../lenke/Lenke';
-import { mulighetsrommetClient } from '../../../core/api/clients';
-import { ErrorColored, SuccessColored } from '@navikt/ds-icons';
-import { capitalize, erPreview } from '../../../utils/Utils';
-import { useHentDeltMedBrukerStatus } from '../../../core/api/queries/useHentDeltMedbrukerStatus';
-import { useFeatureToggles } from '../../../core/api/feature-toggles';
-import { useNavigerTilDialogen } from '../../../hooks/useNavigerTilDialogen';
-import classNames from 'classnames';
 
 export const logDelMedbrukerEvent = (
-  action: 'Åpnet dialog' | 'Delte med bruker' | 'Del med bruker feilet' | 'Avbrutt del med bruker'
+  action:
+    | 'Åpnet dialog'
+    | 'Delte med bruker'
+    | 'Del med bruker feilet'
+    | 'Avbrutt del med bruker'
+    | 'Redigerer tekstfelt'
 ) => {
   logEvent('mulighetsrommet.del-med-bruker', {
     value: action,
@@ -45,6 +50,10 @@ function reducer(state: State, action: Actions): State {
       return { ...state, tekst: action.payload, sendtStatus: 'IKKE_SENDT' };
     case 'Reset':
       return initInitialState(state.tekst);
+    case 'Redigerer tekstfelt':
+      return { ...state, redigererTekstfelt: true };
+    case 'Tilbakestill tekstfelt':
+      return initInitialState(state.malTekst);
     default:
       return state;
   }
@@ -56,6 +65,7 @@ function initInitialState(startTekst: string): State {
     sendtStatus: 'IKKE_SENDT',
     dialogId: '',
     malTekst: startTekst,
+    redigererTekstfelt: false,
   };
 }
 
@@ -79,6 +89,7 @@ const Delemodal = ({
     useHentDeltMedBrukerStatus();
   const { navigerTilDialogen } = useNavigerTilDialogen();
   const senderTilDialogen = state.sendtStatus === 'SENDER';
+  const tekstfeltRef = useRef<HTMLTextAreaElement | null>(null);
 
   const getAntallTegn = () => {
     if (startText.length === 0) {
@@ -122,6 +133,16 @@ const Delemodal = ({
     navigerTilDialogen(fnr, state.dialogId);
   };
 
+  const fokuserTekstfelt = () => {
+    dispatch({ type: 'Redigerer tekstfelt' });
+    logDelMedbrukerEvent('Redigerer tekstfelt');
+    tekstfeltRef?.current?.focus();
+  };
+
+  const tilbakestillTekstfelt = () => {
+    dispatch({ type: 'Tilbakestill tekstfelt' });
+  };
+
   return (
     <Modal
       shouldCloseOnOverlayClick={false}
@@ -132,94 +153,124 @@ const Delemodal = ({
       aria-label="modal"
       data-testid="delemodal"
     >
-      {state.sendtStatus !== 'SENDT_OK' && state.sendtStatus !== 'SENDING_FEILET' && (
-        <Modal.Content>
-          <Heading size="large" level="1" data-testid="modal_header">
-            Del tiltak med bruker
-          </Heading>
-          <Ingress>{'Tiltak gjennom NAV: ' + tiltaksgjennomforingsnavn}</Ingress>
-          <BodyShort size="small">
-            Bruker blir varslet på SMS/e-post, og kan se informasjon om tiltaket i aktivitetsplanen på Min side.
-          </BodyShort>
-          <Textarea
-            value={state.tekst}
-            onChange={e => dispatch({ type: 'Sett tekst', payload: e.currentTarget.value })}
-            label=""
-            minRows={10}
-            maxRows={50}
-            data-testid="textarea_tilbakemelding"
-            maxLength={getAntallTegn()}
-            error={handleError()}
-          />
-          <div className={modalStyles.modal_btngroup}>
-            <Button
-              onClick={handleSend}
-              data-testid="modal_btn-send"
-              disabled={senderTilDialogen || state.tekst.length === 0 || erPreview}
-            >
-              {senderTilDialogen ? 'Sender...' : 'Send via Dialogen'}
-            </Button>
+      <Modal.Content>
+        {state.sendtStatus !== 'SENDT_OK' && state.sendtStatus !== 'SENDING_FEILET' && (
+          <div>
+            <p className={modalStyles.muted} data-testid="modal_header">
+              Del med bruker
+            </p>
+            <Heading size="large" level="1">
+              {'Tiltak gjennom NAV: ' + tiltaksgjennomforingsnavn}
+            </Heading>
 
-            <Button
-              variant="secondary"
-              onClick={() => clickCancel()}
-              data-testid="modal_btn-cancel"
-              disabled={senderTilDialogen}
-            >
-              Avbryt
-            </Button>
+            <Textarea
+              value={state.tekst}
+              onChange={e => dispatch({ type: 'Sett tekst', payload: e.currentTarget.value })}
+              label=""
+              minRows={10}
+              maxRows={50}
+              data-testid="textarea_tilbakemelding"
+              maxLength={getAntallTegn()}
+              error={handleError()}
+              ref={tekstfeltRef}
+              className={delemodalStyles.textarea}
+            />
+            <div className={modalStyles.modal_btngroup}>
+              <div className={delemodalStyles.btn_row}>
+                <Button
+                  onClick={handleSend}
+                  data-testid="modal_btn-send"
+                  disabled={senderTilDialogen || state.tekst.length === 0 || erPreview}
+                >
+                  {senderTilDialogen ? 'Sender...' : 'Send via Dialogen'}
+                </Button>
+
+                <Button
+                  variant="tertiary"
+                  onClick={() => clickCancel()}
+                  data-testid="modal_btn-cancel"
+                  disabled={senderTilDialogen}
+                >
+                  Avbryt
+                </Button>
+              </div>
+              <div>
+                {state.redigererTekstfelt ? (
+                  <Button
+                    variant="tertiary"
+                    onClick={tilbakestillTekstfelt}
+                    data-testid="modal_btn-cancel"
+                    disabled={senderTilDialogen}
+                  >
+                    Tilbakestill
+                  </Button>
+                ) : (
+                  <Button
+                    variant="tertiary"
+                    onClick={fokuserTekstfelt}
+                    data-testid="modal_btn-cancel"
+                    disabled={senderTilDialogen}
+                  >
+                    Rediger melding
+                  </Button>
+                )}
+              </div>
+            </div>
+            {erPreview && (
+              <Alert variant="warning">
+                Det er ikke mulig å dele tiltak med bruker i forhåndsvisning. Brukers navn og veileders navn blir
+                automatisk satt utenfor forhåndsvisningsmodus.
+              </Alert>
+            )}
           </div>
-          {erPreview && (
-            <Alert variant="warning">
-              Det er ikke mulig å dele tiltak med bruker i forhåndsvisning. Brukers navn og veileders navn blir
-              automatisk satt utenfor forhåndsvisningsmodus.
-            </Alert>
-          )}
-        </Modal.Content>
-      )}
-      {state.sendtStatus === 'SENDT_OK' && (
-        <Modal.Content className={delemodalStyles.delemodal_tilbakemelding}>
-          <SuccessColored className={delemodalStyles.delemodal_svg} />
-          <Heading level="1" size="large" data-testid="modal_header">
-            Meldingen er sendt
-          </Heading>
-          <BodyShort>Du kan fortsette dialogen om dette tiltaket i Dialogen.</BodyShort>
-          <div className={classNames(modalStyles.modal_btngroup, modalStyles.modal_btngroup_success)}>
-            <Button variant="primary" onClick={gaTilDialogen} data-testid="modal_btn-dialog">
-              Gå til Dialogen
-            </Button>
-            <Button variant="secondary" onClick={() => clickCancel(false)} data-testid="modal_btn-cancel">
-              Lukk
-            </Button>
+        )}
+        {state.sendtStatus === 'SENDT_OK' && (
+          <div className={delemodalStyles.delemodal_tilbakemelding}>
+            <SuccessColored className={delemodalStyles.delemodal_svg} />
+            <Heading level="1" size="large" data-testid="modal_header">
+              Meldingen er sendt
+            </Heading>
+            <BodyShort>Du kan fortsette dialogen om dette tiltaket i Dialogen.</BodyShort>
+            <div className={classNames(modalStyles.modal_btngroup, modalStyles.modal_btngroup_success)}>
+              <Button variant="primary" onClick={gaTilDialogen} data-testid="modal_btn-dialog">
+                Gå til Dialogen
+              </Button>
+              <Button variant="secondary" onClick={() => clickCancel(false)} data-testid="modal_btn-cancel">
+                Lukk
+              </Button>
+            </div>
           </div>
-        </Modal.Content>
-      )}
-      {state.sendtStatus === 'SENDING_FEILET' && (
-        <Modal.Content className={classNames(delemodalStyles.delemodal_tilbakemelding)}>
-          <ErrorColored className={delemodalStyles.delemodal_svg} />
-          <Heading level="1" size="large" data-testid="modal_header">
-            Tiltaket kunne ikke deles med brukeren
-          </Heading>
-          <BodyShort>
-            Vi kunne ikke dele informasjon digitalt med denne brukeren. Dette kan være fordi hen ikke ønsker eller kan
-            benytte de digitale tjenestene våre.{' '}
-            <Lenke
-              isExternal
-              to="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Manuell-oppf%C3%B8lging-i-Modia-arbeidsrettet-oppf%C3%B8lging.aspx"
-            >
-              Les mer om manuell oppfølging{' '}
-            </Lenke>
-          </BodyShort>
-          <div className={modalStyles.modal_btngroup}>
-            <Button variant="primary" onClick={() => dispatch({ type: 'Reset' })} data-testid="modal_btn-reset">
-              Prøv på nytt
-            </Button>
-            <Button variant="secondary" onClick={() => clickCancel()} data-testid="modal_btn-cancel">
-              Lukk
-            </Button>
+        )}
+        {state.sendtStatus === 'SENDING_FEILET' && (
+          <div className={classNames(delemodalStyles.delemodal_tilbakemelding)}>
+            <ErrorColored className={delemodalStyles.delemodal_svg} />
+            <Heading level="1" size="large" data-testid="modal_header">
+              Tiltaket kunne ikke deles med brukeren
+            </Heading>
+            <BodyShort>
+              Vi kunne ikke dele informasjon digitalt med denne brukeren. Dette kan være fordi hen ikke ønsker eller kan
+              benytte de digitale tjenestene våre.{' '}
+              <Lenke
+                isExternal
+                to="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Manuell-oppf%C3%B8lging-i-Modia-arbeidsrettet-oppf%C3%B8lging.aspx"
+              >
+                Les mer om manuell oppfølging{' '}
+              </Lenke>
+            </BodyShort>
+            <div className={modalStyles.modal_btngroup}>
+              <Button variant="primary" onClick={() => dispatch({ type: 'Reset' })} data-testid="modal_btn-reset">
+                Prøv på nytt
+              </Button>
+              <Button variant="secondary" onClick={() => clickCancel()} data-testid="modal_btn-cancel">
+                Lukk
+              </Button>
+            </div>
           </div>
-        </Modal.Content>
-      )}
+        )}
+        <p className={modalStyles.muted}>
+          Kandidatene vil få et varsel fra NAV, og kan logge inn på nav.no for å lese meldingen
+        </p>
+      </Modal.Content>
     </Modal>
   );
 };
