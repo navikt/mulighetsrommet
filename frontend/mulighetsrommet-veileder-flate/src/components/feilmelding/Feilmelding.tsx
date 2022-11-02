617e1464ed7ea7936663a31c12b89e953a21b507
Signe Carlsen <signe_c_97@hotmail.com>
2022-11-02 15:05:36 +0100

    flytt innhold inn igjen i feilmelding component

--- frontend/mulighetsrommet-veileder-flate/src/components/feilmelding/Feilmelding.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/feilmelding/Feilmelding.tsx
@@ -27,28 +27,20 @@ export const Feilmelding = ({ header, beskrivelse, children, ikonvariant, utenMa
     }
   };
 
-  const innhold = () => {
-    return (
-      <>
-        {ikon()}
-        <Heading level="4" size={'small'}>
-          {header}
-        </Heading>
-        <BodyShort size={'small'} className={styles.beskrivelse}>
-          {beskrivelse}
-        </BodyShort>
-        {children}
-      </>
-    );
-  };
-
   const classNamesArray = utenMargin
     ? [styles.feilmelding_container]
     : [styles.feilmelding_container, styles.feilmelding_margin];
 
   return (
     <div data-testid="feilmelding-container" aria-live="assertive" className={classNames(...classNamesArray)}>
-      {innhold()}
+      {ikon()}
+      <Heading level="4" size={'small'}>
+        {header}
+      </Heading>
+      <BodyShort size={'small'} className={styles.beskrivelse}>
+        {beskrivelse}
+      </BodyShort>
+      {children}
     </div>
   );
 };
