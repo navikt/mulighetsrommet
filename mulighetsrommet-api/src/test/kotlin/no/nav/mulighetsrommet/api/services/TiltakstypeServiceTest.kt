803c2a3225c488ab7b2c28668faaa8bccc9925c4
Signe Carlsen <signe_c_97@hotmail.com>
2022-11-09 11:39:14 +0100

    test paginering for tiltakstype

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/TiltakstypeServiceTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/TiltakstypeServiceTest.kt
@@ -3,6 +3,9 @@ package no.nav.mulighetsrommet.api.services
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.TestCaseOrder
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import no.nav.mulighetsrommet.api.utils.DEFAULT_PAGINATION_LIMIT
+import no.nav.mulighetsrommet.api.utils.PaginationParams
 import no.nav.mulighetsrommet.database.kotest.extensions.FlywayDatabaseListener
 import no.nav.mulighetsrommet.database.kotest.extensions.createApiDatabaseTestSchema
 import no.nav.mulighetsrommet.domain.adapter.AdapterTiltak
@@ -77,8 +80,52 @@ class TiltakstypeServiceTest : FunSpec({
                     )
                 )
             }
-            test("hei") {
-                service.getTiltakstyper() shouldHaveSize 2
+
+            test("default pagination gets first 50 tiltak") {
+                val tiltakstyper =
+                    service.getTiltakstyper()
+
+                tiltakstyper.size shouldBe DEFAULT_PAGINATION_LIMIT
+                tiltakstyper.first().id shouldBe 1
+                tiltakstyper.last().id shouldBe 50
+            }
+
+            test("pagination with page 4 and size 20 should give tiltak with id 61-80") {
+                val tiltakstyper =
+                    service.getTiltakstyper(
+                        paginationParams = PaginationParams(
+                            4,
+                            20
+                        )
+                    )
+
+                tiltakstyper.size shouldBe 20
+                tiltakstyper.first().id shouldBe 61
+                tiltakstyper.last().id shouldBe 80
+            }
+
+            test("pagination with page 3 default size should give tiltak with id 101-105") {
+                val tiltakstyper =
+                    service.getTiltakstyper(
+                        paginationParams = PaginationParams(
+                            3
+                        )
+                    )
+                tiltakstyper.size shouldBe 5
+                tiltakstyper.first().id shouldBe 101
+                tiltakstyper.last().id shouldBe 105
+            }
+
+            test("pagination with default page and size 200 should give tiltak with id 1-105") {
+                val tiltakstyper =
+                    service.getTiltakstyper(
+                        paginationParams = PaginationParams(
+                            nullableLimit = 200
+                        )
+                    )
+                tiltakstyper.size shouldBe 105
+                tiltakstyper.first().id shouldBe 1
+                tiltakstyper.last().id shouldBe 105
             }
         }
     }
