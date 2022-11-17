485bdc8d96709f0f03e17588c2dafaec55fef9d4
Sindre Moldeklev <sndrem@gmail.com>
2022-11-17 15:32:29 +0100

    :tada: Spesifikk server-config for vite-config

--- frontend/mr-admin-flate/vite.config.ts
+++ frontend/mr-admin-flate/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import vitePluginRequire from "vite-plugin-require";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: "127.0.0.1",
+  },
   plugins: [react(), vitePluginRequire()],
   base: "./",
   build: {
