2796c75d9e683373cbbf2726da7ea44967f61c08
Sindre Moldeklev <sndrem@gmail.com>
2022-11-21 10:48:01 +0100

    :sparkles: Kan hente tiltaksgjennomføringer

---
+++ frontend/mr-admin-flate/src/mocks/apiHandlers.ts
@@ -0,0 +1,13 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+import { mock_tiltaksgjennomforinger } from "./fixtures/mock_tiltaksgjennomforinger";
+import { mock_tiltakstyper } from "./fixtures/mock_tiltakstyper";
+
+export const apiHandlers = [
+  rest.get("*/api/v1/tiltakstyper", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mock_tiltakstyper));
+  }),
+  rest.get("*/api/v1/tiltaksgjennomforinger", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mock_tiltaksgjennomforinger));
+  }),
+];
--- frontend/mr-admin-flate/src/mocks/eksterneSystemerhandlers.ts
+++ frontend/mr-admin-flate/src/mocks/eksterneSystemerhandlers.ts
@@ -1,8 +1,6 @@
-// src/mocks/handlers.js
 import { rest } from "msw";
 import { mockFeatures } from "./api/features";
-
-export const handlers = [
+export const eksterneSystemerHandlers = [
   rest.get("*/api/feature", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockFeatures));
   }),
