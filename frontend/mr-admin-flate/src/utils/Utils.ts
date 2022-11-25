bb9307fa0517ffee5f4dfcdce02dd0d7e61c5d4d
Signe Carlsen <signe_c_97@hotmail.com>
2022-11-25 14:22:54 +0100

    legg til navn og ident i header

--- frontend/mr-admin-flate/src/utils/Utils.ts
+++ frontend/mr-admin-flate/src/utils/Utils.ts
@@ -1,35 +1,5 @@
-export const erTomtObjekt = (objekt: Object): boolean => {
-  return Object.keys(objekt).length === 0;
-};
-
-export const inneholderUrl = (string: string) => {
-  return window.location.href.indexOf(string) > -1;
-};
-
-export const erPreview = inneholderUrl('/preview/');
-
-export function specialChar(string: string | { label: string }) {
-  return string.toString().toLowerCase().split('æ').join('ae').split('ø').join('o').split('å').join('a');
-}
-
-export function kebabCase(string: string | { label: string }) {
-  return specialChar(string).replace(/\s+/g, '-');
-}
-
 export function capitalize(text?: string): string {
-  return text ? text.slice(0, 1).toUpperCase() + text.slice(1, text.length).toLowerCase() : '';
-}
-
-export function formaterDato(dato: string | Date, fallback = ''): string {
-  const result = new Date(dato).toLocaleString('no-NO', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  });
-
-  if (result === 'Invalid Date') {
-    return fallback;
-  }
-
-  return result;
+  return text
+    ? text.slice(0, 1).toUpperCase() + text.slice(1, text.length).toLowerCase()
+    : "";
 }
