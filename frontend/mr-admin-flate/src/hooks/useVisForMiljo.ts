7a59b7472f5a2d54b7da654cec861558a84c5722
Sindre Moldeklev <sndrem@gmail.com>
2022-12-06 12:15:23 +0100

    :sparkles: Kan raskt endre rolle ved lokal utvikling

---
+++ frontend/mr-admin-flate/src/hooks/useVisForMiljo.ts
@@ -0,0 +1,7 @@
+export function useVisForMiljo(miljoer: string[]): boolean {
+  const url = window?.location?.host;
+
+  return (
+    miljoer.findIndex((el) => url.toLowerCase().includes(el.toLowerCase())) > -1
+  );
+}
