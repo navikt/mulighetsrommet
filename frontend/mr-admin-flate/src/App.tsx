0b40adacae71ef17a9d038f8e2cef2563fabfa65
Signe Carlsen <signe_c_97@hotmail.com>
2022-12-08 09:42:51 +0100

    fjern mellomrom som ikke skulle være der

--- frontend/mr-admin-flate/src/App.tsx
+++ frontend/mr-admin-flate/src/App.tsx
@@ -20,7 +20,6 @@ export function App() {
     case "TILTAKSANSVARLIG":
       return (
         <Suspense fallback={<Loader size="xlarge" />}>
-          {" "}
           <AutentisertTiltaksansvarligApp />
         </Suspense>
       );
