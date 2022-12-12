4731b2fc7f9c1eca18935dfd6bebcb8b4e05012d
Sindre Moldeklev <sndrem@gmail.com>
2022-12-12 18:49:26 +0100

    Henter ut navn på avdeling for hovedenhet

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/MicrosoftGraphService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/MicrosoftGraphService.kt
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.prometheus.client.cache.caffeine.CacheMetricsCollector
 import no.nav.mulighetsrommet.api.clients.msgraph.MicrosoftGraphClient
+import no.nav.mulighetsrommet.api.domain.MSGraphBrukerdata
 import no.nav.mulighetsrommet.ktor.plugins.Metrikker
 import no.nav.mulighetsrommet.utils.CacheUtils
 import java.util.UUID
@@ -11,7 +12,7 @@ import java.util.concurrent.TimeUnit
 
 class MicrosoftGraphService(private val client: MicrosoftGraphClient) {
 
-    private val hovedenhetCache: Cache<UUID, String> = Caffeine.newBuilder()
+    private val hovedenhetCache: Cache<UUID, MSGraphBrukerdata> = Caffeine.newBuilder()
         .expireAfterWrite(4, TimeUnit.HOURS)
         .maximumSize(500)
         .recordStats()
@@ -23,7 +24,7 @@ class MicrosoftGraphService(private val client: MicrosoftGraphClient) {
         cacheMetrics.addCache("hovedenhetCache", hovedenhetCache)
     }
 
-    suspend fun hentHovedEnhetForNavAnsatt(navAnsattAzureId: UUID): String {
+    suspend fun hentHovedEnhetForNavAnsatt(navAnsattAzureId: UUID): MSGraphBrukerdata {
         return CacheUtils.tryCacheFirstNotNull(hovedenhetCache, navAnsattAzureId) {
             client.hentHovedenhetForBruker(navAnsattAzureId)
         }
--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/MicrosoftGraphServiceTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/services/MicrosoftGraphServiceTest.kt
@@ -5,8 +5,8 @@ import io.kotest.core.spec.style.FunSpec
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
-import no.nav.mulighetsrommet.api.clients.msgraph.MSGraphUser
 import no.nav.mulighetsrommet.api.clients.msgraph.MicrosoftGraphClient
+import no.nav.mulighetsrommet.api.domain.MSGraphBrukerdata
 import org.assertj.core.api.Assertions.assertThat
 import java.util.*
 
@@ -15,12 +15,12 @@ class MicrosoftGraphServiceTest : FunSpec({
 
     context("Hent hovedenhet for nav-ansatt") {
         test("Når man kaller hentHovedenhet for en nav ansatts azureId får man svar og repeterende forespørsler kommer fra cache") {
-            val mockResponse = MSGraphUser(streetAddress = "2990")
+            val mockResponse = MSGraphBrukerdata(hovedenhetKode = "2990", hovedenhetNavn = "IT-Avdelingen")
 
             val client: MicrosoftGraphClient = mockk()
             coEvery {
                 client.hentHovedenhetForBruker(navAnsattAzureId)
-            } returns mockResponse.streetAddress
+            } returns mockResponse
 
             val service = MicrosoftGraphService(client)
             val result = service.hentHovedEnhetForNavAnsatt(navAnsattAzureId)
@@ -29,7 +29,8 @@ class MicrosoftGraphServiceTest : FunSpec({
             service.hentHovedEnhetForNavAnsatt(navAnsattAzureId)
             service.hentHovedEnhetForNavAnsatt(navAnsattAzureId)
 
-            assertThat(result).isEqualTo("2990")
+            assertThat(result.hovedenhetKode).isEqualTo("2990")
+            assertThat(result.hovedenhetNavn).isEqualTo("IT-Avdelingen")
             coVerify(exactly = 1) {
                 client.hentHovedenhetForBruker(navAnsattAzureId)
             }
