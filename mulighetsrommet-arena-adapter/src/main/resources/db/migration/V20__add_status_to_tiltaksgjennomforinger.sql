397447417876a936d031efbbd38032e6ead522e7
Signe Carlsen <signe_c_97@hotmail.com>
2023-01-03 17:25:46 +0100

    fjern ekstra mellomrom

--- mulighetsrommet-arena-adapter/src/main/resources/db/migration/V20__add_status_to_tiltaksgjennomforinger.sql
+++ mulighetsrommet-arena-adapter/src/main/resources/db/migration/V20__add_status_to_tiltaksgjennomforinger.sql
@@ -3,8 +3,8 @@ alter table tiltaksgjennomforing
 
 with tg as (select aem.tiltaksgjennomforing_id,
                    case
-                       when (ae.payload ->> 'op_type') = 'D' then (ae.payload - > 'before' ->> 'TILTAKSTATUSKODE')
-                       else (ae.payload - > 'after' ->> 'TILTAKSTATUSKODE') end
+                       when (ae.payload ->> 'op_type') = 'D' then (ae.payload -> 'before' ->> 'TILTAKSTATUSKODE')
+                       else (ae.payload -> 'after' ->> 'TILTAKSTATUSKODE') end
                        as status
             from arena_entity_mapping aem
                      join arena_events ae on aem.arena_id = ae.arena_id and aem.arena_table = ae.arena_table
