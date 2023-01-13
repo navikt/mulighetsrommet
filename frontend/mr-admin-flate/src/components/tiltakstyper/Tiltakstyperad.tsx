0d36290ad1f15e82247474123c165493f9e2654f
Sindre Moldeklev <sndrem@gmail.com>
2023-01-13 14:19:19 +0100

    :sparkles: Viser fra og til-dato for tiltakstyper

--- frontend/mr-admin-flate/src/components/tiltakstyper/Tiltakstyperad.tsx
+++ frontend/mr-admin-flate/src/components/tiltakstyper/Tiltakstyperad.tsx
@@ -3,6 +3,7 @@ import { BodyLong, BodyShort } from "@navikt/ds-react";
 import styles from "./Tiltakstyperad.module.scss";
 import { Next } from "@navikt/ds-icons";
 import { Link } from "react-router-dom";
+import { formaterDato } from "../../utils/Utils";
 
 interface Props {
   tiltakstype: Tiltakstype;
@@ -14,7 +15,21 @@ export function Tiltakstyperad({ tiltakstype }: Props) {
       <Link to={`${tiltakstype.id}`}>
         <BodyLong size={"medium"}>{tiltakstype.navn}</BodyLong>
       </Link>
-      <BodyShort size={"small"}>{tiltakstype.arenaKode}</BodyShort>
+      <BodyShort size="small">{tiltakstype.arenaKode}</BodyShort>
+      <div className={styles.dato}>
+        <BodyShort
+          size="small"
+          title={`Startdato ${formaterDato(tiltakstype.fraDato)}`}
+        >
+          {formaterDato(tiltakstype.fraDato)}
+        </BodyShort>
+        <BodyShort
+          size="small"
+          title={`Sluttdato ${formaterDato(tiltakstype.tilDato)}`}
+        >
+          {formaterDato(tiltakstype.tilDato)}
+        </BodyShort>
+      </div>
       <div className={styles.pil}>
         <Next />
       </div>
