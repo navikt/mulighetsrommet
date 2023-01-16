fa041eb6c756e1ea2b06f9f7d0a440fec1f044b1
taniaholst <tania.holst@nav.no>
2023-01-16 13:00:00 +0100

    filendring

--- frontend/mr-admin-flate/src/components/tiltakstyper/TiltakstypeRad.tsx
+++ frontend/mr-admin-flate/src/components/tiltakstyper/TiltakstypeRad.tsx
@@ -1,7 +1,8 @@
 import { Tiltakstype } from "mulighetsrommet-api-client";
 import { BodyShort } from "@navikt/ds-react";
-import styles from "./TiltakstypeRad.module.scss";
+import styles from "./Tiltakstyperad.module.scss";
 import { Next } from "@navikt/ds-icons";
+import { formaterDato } from "../../utils/Utils";
 
 interface Props {
   tiltakstype: Tiltakstype;
@@ -15,6 +16,20 @@ export function TiltakstypeRad({ tiltakstype }: Props) {
     >
       <BodyShort size="medium">{tiltakstype.navn}</BodyShort>
       <BodyShort size="small">{tiltakstype.arenaKode}</BodyShort>
+      <div className={styles.dato}>
+        <BodyShort
+          size="small"
+          title={`Startdato ${formaterDato(tiltakstype.fraDato)}`}
+        >
+          {formaterDato(tiltakstype.fraDato)}
+        </BodyShort>
+        <BodyShort
+          size="small"
+          title={`Sluttdato ${formaterDato(tiltakstype.tilDato)}`}
+        >
+          {formaterDato(tiltakstype.tilDato)}
+        </BodyShort>
+      </div>
       <Next className={styles.pil} />
     </li>
   );
