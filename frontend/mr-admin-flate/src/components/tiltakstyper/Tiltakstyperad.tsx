ac4e1571ad372a1126bb513b621444d61dac3742
taniaholst <tania.holst@nav.no>
2023-01-16 12:59:24 +0100

    refaktorering

--- frontend/mr-admin-flate/src/components/tiltakstyper/Tiltakstyperad.tsx
+++ frontend/mr-admin-flate/src/components/tiltakstyper/Tiltakstyperad.tsx
@@ -1,8 +1,7 @@
 import { Tiltakstype } from "mulighetsrommet-api-client";
 import { BodyShort } from "@navikt/ds-react";
-import styles from "./Tiltakstyperad.module.scss";
+import styles from "./TiltakstypeRad.module.scss";
 import { Next } from "@navikt/ds-icons";
-import { formaterDato } from "../../utils/Utils";
 
 interface Props {
   tiltakstype: Tiltakstype;
@@ -16,20 +15,6 @@ export function TiltakstypeRad({ tiltakstype }: Props) {
     >
       <BodyShort size="medium">{tiltakstype.navn}</BodyShort>
       <BodyShort size="small">{tiltakstype.arenaKode}</BodyShort>
-      <div className={styles.dato}>
-        <BodyShort
-          size="small"
-          title={`Startdato ${formaterDato(tiltakstype.fraDato)}`}
-        >
-          {formaterDato(tiltakstype.fraDato)}
-        </BodyShort>
-        <BodyShort
-          size="small"
-          title={`Sluttdato ${formaterDato(tiltakstype.tilDato)}`}
-        >
-          {formaterDato(tiltakstype.tilDato)}
-        </BodyShort>
-      </div>
       <Next className={styles.pil} />
     </li>
   );
