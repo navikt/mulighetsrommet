2e4b8422cfc92bea6aa44077479f82be2ae81b43
Sindre Moldeklev <sndrem@gmail.com>
2023-01-17 15:10:45 +0100

    :adhesive_bandage: Klient-navigering for å unngå full reloads
    Bruker react-router-dom sin Link-komponent for å unngå full reloads av siden og dermed tap av state fra blant annet react-query

--- frontend/mr-admin-flate/src/components/tiltaksgjennomforinger/TiltaksgjennomforingRad.tsx
+++ frontend/mr-admin-flate/src/components/tiltaksgjennomforinger/TiltaksgjennomforingRad.tsx
@@ -1,8 +1,9 @@
 import { Next } from "@navikt/ds-icons";
 import { BodyShort } from "@navikt/ds-react";
 import { Tiltaksgjennomforing } from "mulighetsrommet-api-client";
-import styles from "./Tiltaksgjennomforing.module.scss";
+import { Link } from "react-router-dom";
 import { useSideForNavigering } from "../../hooks/useSideForNavigering";
+import styles from "./Tiltaksgjennomforing.module.scss";
 
 interface TiltaksgjennomforingRadProps {
   tiltaksgjennomforing: Tiltaksgjennomforing;
@@ -10,34 +11,37 @@ interface TiltaksgjennomforingRadProps {
 }
 
 export function TiltaksgjennomforingRad({
-                                          tiltaksgjennomforing,
-                                          fagansvarlig = false,
-                                        }: TiltaksgjennomforingRadProps) {
-  const navigerSomFagEllerTiltaksansvarlig = () => {
-    location.href = fagansvarlig
-        ? `${side}/${tiltaksgjennomforing.tiltakstype.id}/tiltaksgjennomforing/${tiltaksgjennomforing.id}`
-        : `${side}/tiltaksgjennomforing/${tiltaksgjennomforing.id}`;
-  };
+  tiltaksgjennomforing,
+  fagansvarlig = false,
+}: TiltaksgjennomforingRadProps) {
   const side = useSideForNavigering();
 
+  const navigerSomFagEllerTiltaksansvarlig = () => {
+    return fagansvarlig
+      ? `${side}/${tiltaksgjennomforing.tiltakstype.id}/tiltaksgjennomforing/${tiltaksgjennomforing.id}`
+      : `${side}/tiltaksgjennomforing/${tiltaksgjennomforing.id}`;
+  };
+
   return (
-      <li
-          className={styles.rad}
-          onClick={navigerSomFagEllerTiltaksansvarlig}
-          data-testid="tiltaksgjennomforingsrad"
-      >
+    <li
+      className={styles.rad}
+      onClick={navigerSomFagEllerTiltaksansvarlig}
+      data-testid="tiltaksgjennomforingsrad"
+    >
+      <Link to={navigerSomFagEllerTiltaksansvarlig()}>
         <BodyShort>{tiltaksgjennomforing.navn}</BodyShort>
-        <BodyShort size="small">{tiltaksgjennomforing.tiltaksnummer}</BodyShort>
-        <BodyShort
-            size="small"
-            title={tiltaksgjennomforing.tiltakstype.arenaKode}
-        >
-          {tiltaksgjennomforing.tiltakstype.navn}
-        </BodyShort>
-        <BodyShort size="small">
-          {tiltaksgjennomforing.virksomhetsnummer}
-        </BodyShort>
-        <Next className={styles.pil}/>
-      </li>
+      </Link>
+      <BodyShort size="small">{tiltaksgjennomforing.tiltaksnummer}</BodyShort>
+      <BodyShort
+        size="small"
+        title={tiltaksgjennomforing.tiltakstype.arenaKode}
+      >
+        {tiltaksgjennomforing.tiltakstype.navn}
+      </BodyShort>
+      <BodyShort size="small">
+        {tiltaksgjennomforing.virksomhetsnummer}
+      </BodyShort>
+      <Next className={styles.pil} />
+    </li>
   );
 }
--- frontend/mr-admin-flate/src/components/tiltakstyper/TiltakstypeRad.tsx
+++ frontend/mr-admin-flate/src/components/tiltakstyper/TiltakstypeRad.tsx
@@ -1,19 +1,20 @@
-import { Tiltakstype } from "mulighetsrommet-api-client";
-import { BodyShort } from "@navikt/ds-react";
-import styles from "./TiltakstypeRad.module.scss";
 import { Next } from "@navikt/ds-icons";
+import { BodyShort } from "@navikt/ds-react";
+import { Tiltakstype } from "mulighetsrommet-api-client";
+import { Link } from "react-router-dom";
 import { formaterDato } from "../../utils/Utils";
+import styles from "./TiltakstypeRad.module.scss";
 
 interface Props {
   tiltakstype: Tiltakstype;
 }
 
 export function TiltakstypeRad({ tiltakstype }: Props) {
-  const navigerTilTiltakstype = () =>
-    (location.href = `/tiltakstyper/${tiltakstype.id}`);
   return (
-    <li className={styles.rad} onClick={navigerTilTiltakstype}>
-      <BodyShort size="medium">{tiltakstype.navn}</BodyShort>
+    <li className={styles.rad}>
+      <Link to={`/tiltakstyper/${tiltakstype.id}`}>
+        <BodyShort size="medium">{tiltakstype.navn}</BodyShort>
+      </Link>
       <BodyShort size="small">{tiltakstype.arenaKode}</BodyShort>
       <div className={styles.dato}>
         <BodyShort
