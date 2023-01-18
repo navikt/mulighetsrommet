3b1efb334e728bf3e2e54bad2536babd5fbd0fd1
Sindre Moldeklev <sndrem@gmail.com>
2023-01-18 11:48:57 +0100

    not null for felter som ikke skal være null

--- mulighetsrommet-arena-adapter/src/main/resources/db/migration/V22__add_arenafelter_to_tiltakstype.sql
+++ mulighetsrommet-arena-adapter/src/main/resources/db/migration/V22__add_arenafelter_to_tiltakstype.sql
@@ -26,3 +26,35 @@ alter table tiltakstype
     add column vis_begrunnelse_for_innsoking bool,
     add column henvisningsbrev_og_hovedbrev_til_arbeidsgiver bool,
     add column kopibrev_og_hovedbrev_til_arbeidsgiver bool;
+
+update tiltakstype
+    set tiltaksgruppekode = '',
+        administrasjonskode = 'IND',
+        send_tilsagnsbrev_til_deltaker = false,
+        skal_ha_anskaffelsesprosess = false,
+        vis_lonnstilskuddskalkulator = false,
+        tiltaksgjennomforing_krever_sluttdato = false,
+        tiltaksgjennomforing_krever_vedtak = false,
+        tiltaksgjennomforing_reservert_for_ia_bedrift = false,
+        har_rett_paa_tilleggsstonader = false,
+        har_rett_paa_utdanning = false,
+        tiltaksgjennomforing_genererer_tilsagnsbrev_automatisk = false,
+        vis_begrunnelse_for_innsoking = false,
+        henvisningsbrev_og_hovedbrev_til_arbeidsgiver = false,
+        kopibrev_og_hovedbrev_til_arbeidsgiver = false;
+
+alter table tiltakstype
+    alter column tiltaksgruppekode set not null,
+    alter column administrasjonskode set not null,
+    alter column send_tilsagnsbrev_til_deltaker set not null,
+    alter column skal_ha_anskaffelsesprosess set not null,
+    alter column vis_lonnstilskuddskalkulator set not null,
+    alter column tiltaksgjennomforing_krever_sluttdato set not null,
+    alter column tiltaksgjennomforing_krever_vedtak set not null,
+    alter column tiltaksgjennomforing_reservert_for_ia_bedrift set not null,
+    alter column har_rett_paa_tilleggsstonader set not null,
+    alter column har_rett_paa_utdanning set not null,
+    alter column tiltaksgjennomforing_genererer_tilsagnsbrev_automatisk set not null,
+    alter column vis_begrunnelse_for_innsoking set not null,
+    alter column henvisningsbrev_og_hovedbrev_til_arbeidsgiver set not null,
+    alter column kopibrev_og_hovedbrev_til_arbeidsgiver set not null;
