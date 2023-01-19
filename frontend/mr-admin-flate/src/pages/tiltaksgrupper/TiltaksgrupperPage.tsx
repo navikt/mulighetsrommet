fb121859f253851f9e7785b54325070b5ada717a
Signe Carlsen <signe_c_97@hotmail.com>
2023-01-19 14:22:47 +0100

    fjern toggle

--- frontend/mr-admin-flate/src/pages/tiltaksgrupper/TiltaksgrupperPage.tsx
+++ frontend/mr-admin-flate/src/pages/tiltaksgrupper/TiltaksgrupperPage.tsx
@@ -1,21 +1,17 @@
 import { Button, Heading, Search } from "@navikt/ds-react";
 import { Link } from "react-router-dom";
-import { useFeatureToggles } from "../../api/features/feature-toggles";
 import React from "react";
 import styles from "../Page.module.scss";
 import { TiltaksgruppeOversikt } from "../../components/tiltaksgrupper/TiltaksgruppeOversikt";
 
 export function TiltaksgrupperPage() {
-  const { data: toggles } = useFeatureToggles();
   return (
     <>
       <div className={styles.header_wrapper}>
         <Heading size="large">Oversikt over tiltaksgrupper</Heading>
-        {toggles?.["mulighetsrommet.enable-opprett-tiltakstype"] ? (
-          <Link to="opprett" className={styles.opprettknappseksjon}>
-            <Button variant="tertiary">Opprett ny tiltaksgruppe</Button>
-          </Link>
-        ) : null}
+        <Link to="opprett" className={styles.opprettknappseksjon}>
+          <Button variant="tertiary">Opprett ny tiltaksgruppe</Button>
+        </Link>
       </div>
       <TiltaksgruppeOversikt />
     </>
