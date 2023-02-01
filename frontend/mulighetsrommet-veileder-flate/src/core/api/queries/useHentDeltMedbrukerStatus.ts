47617cbb876fe10c4f62103c61b8225da69ebf18
Sindre Moldeklev <sndrem@gmail.com>
2023-02-01 08:05:41 +0100

    Revert "Fiks linting"
    This reverts commit 55d372672a268135a8d310c72d20c107807f4c0c.

--- frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useHentDeltMedbrukerStatus.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useHentDeltMedbrukerStatus.ts
@@ -10,24 +10,24 @@ import useTiltaksgjennomforingById from './useTiltaksgjennomforingById';
 export function useHentDeltMedBrukerStatus() {
   const { data: tiltaksgjennomforing } = useTiltaksgjennomforingById();
   const { data: veilederData } = useHentVeilederdata();
-  const norskIdent = useHentFnrFraUrl();
+  const brukerFnr = useHentFnrFraUrl();
 
   const { data: sistDeltMedBruker, refetch: refetchDelMedBruker } = useQuery<DelMedBruker>(
-    [QueryKeys.DeltMedBrukerStatus, norskIdent, tiltaksgjennomforing?._id],
+    [QueryKeys.DeltMedBrukerStatus, brukerFnr, tiltaksgjennomforing?._id],
     () =>
       mulighetsrommetClient.delMedBruker.getDelMedBruker({
-        fnr: norskIdent,
-        sanityId: tiltaksgjennomforing?._id!!,
+        fnr: brukerFnr,
+        tiltaksnummer: tiltaksgjennomforing?.tiltaksnummer?.toString()!!,
       }),
-    { enabled: !erPreview || !tiltaksgjennomforing?._id }
+    { enabled: !erPreview || !tiltaksgjennomforing?.tiltaksnummer }
   );
 
-  async function lagreVeilederHarDeltTiltakMedBruker(dialogId: string, sanityId: string) {
+  async function lagreVeilederHarDeltTiltakMedBruker(dialogId: string, tiltaksnummer: string) {
     if (!veilederData?.ident) return;
 
     await mulighetsrommetClient.delMedBruker.postDelMedBruker({
-      sanityId,
-      requestBody: { norskIdent, navident: veilederData?.ident, sanityId, dialogId },
+      tiltaksnummer,
+      requestBody: { bruker_fnr: brukerFnr, navident: veilederData?.ident, tiltaksnummer, dialogId },
     });
 
     await refetchDelMedBruker();
