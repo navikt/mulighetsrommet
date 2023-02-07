54e3c80e3aa2895ce43f33f62267ddcc86194985
taniaholst <tania.holst@nav.no>
2023-02-07 12:38:10 +0100

    :fire: fjernet tilbakemeldingskomponent og feature-toggle

--- frontend/mulighetsrommet-veileder-flate/src/hooks/useFeedbackTilfredshet.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query';
-
-export function useFeedbackTilfredshet(tilfredshet: number) {
-  const { data } = useQuery(['feedbackTilfredshet', tilfredshet], () => tilfredshet, {
-    enabled: tilfredshet > 0,
-  });
-
-  return { data };
-}
