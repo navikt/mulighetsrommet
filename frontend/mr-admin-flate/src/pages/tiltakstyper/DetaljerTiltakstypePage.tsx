92c9ac3a0587ded0fe53cd3346457b9782aa8432
Sindre Moldeklev <sndrem@gmail.com>
2023-02-08 15:53:14 +0100

    :sparkles: Visning av tiltakstypedetaljer i tab

--- frontend/mr-admin-flate/src/pages/tiltakstyper/DetaljerTiltakstypePage.tsx
+++ frontend/mr-admin-flate/src/pages/tiltakstyper/DetaljerTiltakstypePage.tsx
@@ -1,11 +1,14 @@
-import { Alert, Heading } from "@navikt/ds-react";
+import { Alert, Heading, Tabs } from "@navikt/ds-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useTiltakstypeById } from "../../api/tiltakstyper/useTiltakstypeById";
 import { Laster } from "../../components/Laster";
 import { Tilbakelenke } from "../../components/navigering/Tilbakelenke";
+import { TiltakstypeDetaljer } from "./Tiltakstypedetaljer";
 
 export function DetaljerTiltakstypePage() {
   const optionalTiltakstype = useTiltakstypeById();
+  const [tabValgt, setTabValgt] = useState("arenaInfo");
 
   if (optionalTiltakstype.isFetching) {
     return <Laster tekst="Laster tiltakstype" />;
@@ -27,10 +30,14 @@ export function DetaljerTiltakstypePage() {
       <Heading size="large" level="1">
         {tiltakstype.navn}
       </Heading>
-      <dl>
-        <dt>Tiltakskode:</dt>
-        <dd>{tiltakstype.arenaKode}</dd>
-      </dl>
+      <Tabs value={tabValgt} onChange={setTabValgt}>
+        <Tabs.List>
+          <Tabs.Tab value="arenaInfo" label="Arenainfo" />
+        </Tabs.List>
+        <Tabs.Panel value="arenaInfo" className="h-24 w-full bg-gray-50 p-4">
+          <TiltakstypeDetaljer tiltakstype={tiltakstype} />
+        </Tabs.Panel>
+      </Tabs>
     </>
   );
 }
