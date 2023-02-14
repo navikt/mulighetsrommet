6d9405a1b248f239a4689660a4334fbfbc310cee
Signe <signe.marie.oen.carlsen@nav.no>
2023-02-14 11:22:09 +0100

    replay events i launch

--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/routes/ManagerRoutes.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/routes/ManagerRoutes.kt
@@ -1,11 +1,12 @@
 package no.nav.mulighetsrommet.arena.adapter.routes
 
 import io.ktor.http.*
-import io.ktor.server.application.call
+import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.*
 import kotlinx.serialization.Serializable
 import no.nav.mulighetsrommet.arena.adapter.kafka.KafkaConsumerOrchestrator
 import no.nav.mulighetsrommet.arena.adapter.models.db.ArenaEvent
@@ -13,10 +14,14 @@ import no.nav.mulighetsrommet.arena.adapter.repositories.Topic
 import no.nav.mulighetsrommet.arena.adapter.services.ArenaEventService
 import no.nav.mulighetsrommet.arena.adapter.tasks.ReplayEvents
 import org.koin.ktor.ext.inject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 fun Route.managerRoutes() {
     val kafka: KafkaConsumerOrchestrator by inject()
     val arenaEventService: ArenaEventService by inject()
+
+    val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     singlePageApplication {
         useResources = true
@@ -37,9 +42,15 @@ fun Route.managerRoutes() {
     put("/events/replay") {
         val (table, status) = call.receive<ReplayEventsTaskData>()
 
-        arenaEventService.setReplayStatusForEvents(table = table, status = status)
+        launch {
+            try {
+                arenaEventService.setReplayStatusForEvents(table = table, status = status)
 
-        replayEvents.schedule()
+                replayEvents.schedule()
+            } catch (e: Throwable) {
+                logger.error("Replaying of events failed with error: $e")
+            }
+        }
 
         call.respond(HttpStatusCode.Created)
     }
