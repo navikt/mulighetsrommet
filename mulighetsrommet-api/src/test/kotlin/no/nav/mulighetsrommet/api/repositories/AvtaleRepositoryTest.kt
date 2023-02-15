8610ba85c4fdf03f2a85017af2c3f76e15f378ef
Sindre Moldeklev <sndrem@gmail.com>
2023-02-15 11:46:37 +0100

    Fjern assertions som skaper støy

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/repositories/AvtaleRepositoryTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/repositories/AvtaleRepositoryTest.kt
@@ -9,7 +9,6 @@ import no.nav.mulighetsrommet.api.utils.AvtaleFilter
 import no.nav.mulighetsrommet.database.kotest.extensions.FlywayDatabaseTestListener
 import no.nav.mulighetsrommet.database.kotest.extensions.createApiDatabaseTestSchema
 import no.nav.mulighetsrommet.domain.dto.Avtalestatus
-import no.nav.mulighetsrommet.domain.dto.Avtaletype
 
 class AvtaleRepositoryTest : FunSpec({
     testOrder = TestCaseOrder.Sequential
@@ -28,7 +27,7 @@ class AvtaleRepositoryTest : FunSpec({
                 navn = "Avtale om opplæring av blinde krokodiller"
             )
             val avtale2 = avtaleFixture.createAvtaleForTiltakstype(
-                navn = "Avtale om undervisning av underlige ulver"
+                navn = "Avtale om undervisning av underlige ulver",
             )
             val avtaleRepository = avtaleFixture.upsertAvtaler(listOf(avtale1, avtale2))
 
@@ -39,17 +38,13 @@ class AvtaleRepositoryTest : FunSpec({
 
             result.second shouldHaveSize 1
             result.second[0].navn shouldBe "Avtale om opplæring av blinde krokodiller"
-            result.second[0].avtaletype shouldBe Avtaletype.Rammeavtale
-            result.second[0].avtalestatus shouldBe Avtalestatus.Aktiv
         }
 
         test("Filtrere på avtalestatus returnere avtaler med korrekt status") {
             val avtale1 = avtaleFixture.createAvtaleForTiltakstype(
-                navn = "Avtale om opplæring av blinde krokodiller",
                 avtalestatus = Avtalestatus.Aktiv,
             )
             val avtale2 = avtaleFixture.createAvtaleForTiltakstype(
-                navn = "Avtale om undervisning av underlige ulver",
                 avtalestatus = Avtalestatus.Avbrutt,
             )
             val avtaleRepository = avtaleFixture.upsertAvtaler(listOf(avtale1, avtale2))
@@ -60,18 +55,14 @@ class AvtaleRepositoryTest : FunSpec({
             )
 
             result.second shouldHaveSize 1
-            result.second[0].navn shouldBe "Avtale om undervisning av underlige ulver"
-            result.second[0].avtaletype shouldBe Avtaletype.Rammeavtale
             result.second[0].avtalestatus shouldBe Avtalestatus.Avbrutt
         }
 
         test("Filtrere på enhet returnerer avtaler for gitt enhet") {
             val avtale1 = avtaleFixture.createAvtaleForTiltakstype(
-                navn = "Avtale om opplæring av blinde krokodiller",
                 enhet = "1801"
             )
             val avtale2 = avtaleFixture.createAvtaleForTiltakstype(
-                navn = "Avtale om undervisning av underlige ulver",
                 enhet = "1900"
             )
             val avtaleRepository = avtaleFixture.upsertAvtaler(listOf(avtale1, avtale2))
@@ -81,9 +72,6 @@ class AvtaleRepositoryTest : FunSpec({
             )
 
             result.second shouldHaveSize 1
-            result.second[0].navn shouldBe "Avtale om opplæring av blinde krokodiller"
-            result.second[0].avtaletype shouldBe Avtaletype.Rammeavtale
-            result.second[0].avtalestatus shouldBe Avtalestatus.Aktiv
             result.second[0].enhet shouldBe "1801"
         }
     }
