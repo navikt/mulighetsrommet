66e7065d999ad69689503730f18f3fca8ef61eff
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-02-23 08:51:10 +0100

    implement tests for arena event decoder

--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/ArenaEvent.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/ArenaEvent.kt
@@ -1,7 +1,10 @@
 package no.nav.mulighetsrommet.arena.adapter.models.db
 
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonObject
 import no.nav.mulighetsrommet.arena.adapter.models.arena.ArenaTable
+import no.nav.mulighetsrommet.serialization.json.JsonIgnoreUnknownKeys
 
 data class ArenaEvent(
     val arenaTable: ArenaTable,
@@ -57,41 +60,9 @@ data class ArenaEvent(
 
     inline fun <reified T> decodePayload(): T {
         return if (operation == Operation.Delete) {
-            json.decodeFromJsonElement(payload.jsonObject["before"]!!)
+            JsonIgnoreUnknownKeys.decodeFromJsonElement(payload.jsonObject["before"]!!)
         } else {
-            json.decodeFromJsonElement(payload.jsonObject["after"]!!)
-        }
-    }
-
-    companion object {
-        val json = Json { ignoreUnknownKeys = true }
-
-        fun decodeFromJson(payload: JsonElement): ArenaEvent {
-            val table = ArenaTable.fromTable(json.decodeFromJsonElement(payload.jsonObject.getValue("table")))
-
-            val operation = Operation.fromOpType(payload.jsonObject.getValue("op_type").jsonPrimitive.content)
-
-            val data = if (operation == Operation.Delete) {
-                payload.jsonObject.getValue("before").jsonObject
-            } else {
-                payload.jsonObject.getValue("after").jsonObject
-            }
-
-            val arenaId = when (table) {
-                ArenaTable.Tiltakstype -> data.getValue("TILTAKSKODE").jsonPrimitive.content
-                ArenaTable.Sak -> data.getValue("SAK_ID").jsonPrimitive.int.toString()
-                ArenaTable.AvtaleInfo -> data.getValue("AVTALE_ID").jsonPrimitive.int.toString()
-                ArenaTable.Deltaker -> data.getValue("TILTAKDELTAKER_ID").jsonPrimitive.int.toString()
-                ArenaTable.Tiltaksgjennomforing -> data.getValue("TILTAKGJENNOMFORING_ID").jsonPrimitive.int.toString()
-            }
-
-            return ArenaEvent(
-                arenaTable = table,
-                arenaId = arenaId,
-                operation = operation,
-                payload = payload,
-                status = ProcessingStatus.Pending
-            )
+            JsonIgnoreUnknownKeys.decodeFromJsonElement(payload.jsonObject["after"]!!)
         }
     }
 }
---
+++ mulighetsrommet-arena-adapter/src/test/kotlin/no/nav/mulighetsrommet/arena/adapter/events/ArenaEventConsumerTest.kt
@@ -0,0 +1,52 @@
+package no.nav.mulighetsrommet.arena.adapter.events
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+import no.nav.mulighetsrommet.arena.adapter.fixtures.*
+import no.nav.mulighetsrommet.arena.adapter.models.arena.ArenaTable.*
+import no.nav.mulighetsrommet.arena.adapter.models.db.ArenaEvent.Operation.*
+
+class ArenaEventConsumerTest : FunSpec({
+    test("decode ArenaEvent") {
+        forAll(
+            row(
+                createArenaTiltakEvent(Delete) { it.copy(TILTAKSKODE = "TILTAK") },
+                Delete,
+                Tiltakstype,
+                "TILTAK"
+            ),
+            row(
+                createArenaSakEvent(Insert) { it.copy(SAK_ID = 1) },
+                Insert,
+                Sak,
+                "1"
+            ),
+            row(
+                createArenaAvtaleInfoEvent(Update) { it.copy(AVTALE_ID = 2) },
+                Update,
+                AvtaleInfo,
+                "2"
+            ),
+            row(
+                createArenaTiltakgjennomforingEvent(Insert) { it.copy(TILTAKGJENNOMFORING_ID = 3) },
+                Insert,
+                Tiltaksgjennomforing,
+                "3"
+            ),
+            row(
+                createArenaTiltakdeltakerEvent(Insert) { it.copy(TILTAKDELTAKER_ID = 4) },
+                Insert,
+                Deltaker,
+                "4"
+            ),
+        ) { event, operation, table, id ->
+            val decoded = decodeArenaEvent(event.payload)
+
+            decoded.operation shouldBe operation
+            decoded.arenaTable shouldBe table
+            decoded.arenaId shouldBe id
+        }
+    }
+})
