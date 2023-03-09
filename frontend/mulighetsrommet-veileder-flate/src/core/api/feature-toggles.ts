3c1b81114606d44df0566cf2bec75bc3219034c9
Sindre Moldeklev <sndrem@gmail.com>
2023-03-09 08:43:41 +0100

    :triangular_flag_on_post: Fjerner toggle for deltakerhistorikk

--- frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
@@ -1,23 +1,20 @@
-import { headers } from './headers';
 import { useQuery } from 'react-query';
+import { headers } from './headers';
 
 export const ENABLE_ARBEIDSFLATE = 'mulighetsrommet.enable-arbeidsflate';
-export const VIS_HISTORIKK = 'mulighetsrommet.vis-historikk';
 export const VIS_INNSIKTSFANE = 'mulighetsrommet.vis-innsiktsfane';
 export const VIS_TILGJENGELIGHETSSTATUS = 'mulighetsrommet.vis-tilgjengelighetsstatus';
 
-export const ALL_TOGGLES = [ENABLE_ARBEIDSFLATE, VIS_HISTORIKK, VIS_INNSIKTSFANE, VIS_TILGJENGELIGHETSSTATUS];
+export const ALL_TOGGLES = [ENABLE_ARBEIDSFLATE, VIS_INNSIKTSFANE, VIS_TILGJENGELIGHETSSTATUS];
 
 export interface Features {
   [ENABLE_ARBEIDSFLATE]: boolean;
-  [VIS_HISTORIKK]: boolean;
   [VIS_INNSIKTSFANE]: boolean;
   [VIS_TILGJENGELIGHETSSTATUS]: boolean;
 }
 
 export const initialFeatures: Features = {
   [ENABLE_ARBEIDSFLATE]: false,
-  [VIS_HISTORIKK]: false,
   [VIS_INNSIKTSFANE]: false,
   [VIS_TILGJENGELIGHETSSTATUS]: false,
 };
--- frontend/mulighetsrommet-veileder-flate/src/mock/api/features.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/api/features.ts
@@ -1,14 +1,12 @@
 import {
   ENABLE_ARBEIDSFLATE,
   Features,
-  VIS_HISTORIKK,
   VIS_INNSIKTSFANE,
   VIS_TILGJENGELIGHETSSTATUS,
 } from '../../core/api/feature-toggles';
 
 export const mockFeatures: Features = {
   [ENABLE_ARBEIDSFLATE]: true,
-  [VIS_HISTORIKK]: true,
   [VIS_INNSIKTSFANE]: false,
   [VIS_TILGJENGELIGHETSSTATUS]: false,
 };
