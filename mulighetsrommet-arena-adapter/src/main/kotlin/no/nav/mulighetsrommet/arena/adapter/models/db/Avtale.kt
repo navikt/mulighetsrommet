2c06c618253d6133fe89515253be91a09a1ea6dd
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-03-13 11:19:34 +0100

    remove unnecessary @Serializable

--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/Avtale.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/Avtale.kt
@@ -1,24 +1,17 @@
 package no.nav.mulighetsrommet.arena.adapter.models.db
 
-import kotlinx.serialization.Serializable
 import no.nav.mulighetsrommet.arena.adapter.models.arena.Avtalestatuskode
-import no.nav.mulighetsrommet.domain.serializers.LocalDateTimeSerializer
-import no.nav.mulighetsrommet.domain.serializers.UUIDSerializer
 import java.time.LocalDateTime
 import java.util.*
 
-@Serializable
 data class Avtale(
-    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val aar: Int,
     val lopenr: Int,
     val tiltakskode: String,
     val leverandorId: Int,
     val navn: String,
-    @Serializable(with = LocalDateTimeSerializer::class)
     val fraDato: LocalDateTime,
-    @Serializable(with = LocalDateTimeSerializer::class)
     val tilDato: LocalDateTime,
     val ansvarligEnhet: String,
     val rammeavtale: Boolean,
--- mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/Tiltaksgjennomforing.kt
+++ mulighetsrommet-arena-adapter/src/main/kotlin/no/nav/mulighetsrommet/arena/adapter/models/db/Tiltaksgjennomforing.kt
@@ -1,23 +1,16 @@
 package no.nav.mulighetsrommet.arena.adapter.models.db
 
-import kotlinx.serialization.Serializable
-import no.nav.mulighetsrommet.domain.serializers.LocalDateTimeSerializer
-import no.nav.mulighetsrommet.domain.serializers.UUIDSerializer
 import java.time.LocalDateTime
 import java.util.*
 
-@Serializable
 data class Tiltaksgjennomforing(
-    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val tiltaksgjennomforingId: Int,
     val sakId: Int,
     val tiltakskode: String,
     val arrangorId: Int?,
     val navn: String?,
-    @Serializable(with = LocalDateTimeSerializer::class)
     val fraDato: LocalDateTime,
-    @Serializable(with = LocalDateTimeSerializer::class)
     val tilDato: LocalDateTime? = null,
     val apentForInnsok: Boolean = true,
     val antallPlasser: Int? = null,
