38eecbc629558740249d52aeb79c6a3f2e10bb14
Sindre Moldeklev <sndrem@gmail.com>
2023-03-22 08:47:13 +0100

    Henter ut nøkkeltall for antall deltakere for en avtale

--- frontend/mr-admin-flate/src/mocks/fixtures/mock_avtale_nokkeltall.ts
+++ frontend/mr-admin-flate/src/mocks/fixtures/mock_avtale_nokkeltall.ts
@@ -2,4 +2,5 @@ import { AvtaleNokkeltall } from "mulighetsrommet-api-client";
 
 export const mockAvtaleNokkeltall: AvtaleNokkeltall = {
   antallTiltaksgjennomforinger: 587,
+  antallDeltakere: 143,
 };
