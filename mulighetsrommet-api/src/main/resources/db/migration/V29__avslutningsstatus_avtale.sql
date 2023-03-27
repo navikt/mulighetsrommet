9e0d4685cd0a0b6f262320bb7971f2041319df14
Signe <signe.marie.oen.carlsen@nav.no>
2023-03-27 15:54:44 +0200

    ny migreringsfil

--- mulighetsrommet-api/src/main/resources/db/migration/V29__avslutningsstatus_avtale.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V29__avslutningsstatus_avtale.sql
@@ -2,19 +2,3 @@ alter table avtale
 drop
 column avtalestatus,
     add avslutningsstatus avslutningsstatus not null default 'IKKE_AVSLUTTET';
-
-create table ansatt_avtale
-(
-    navident   text                    not null,
-    avtale_id  uuid                    not null
-        constraint fk_avtale references avtale (id) on delete cascade,
-    created_at timestamp default now() not null,
-    updated_at timestamp default now() not null,
-    primary key (navident, avtale_id)
-);
-
-create index ansatt_avtale_oppslag_navident_idx
-    on ansatt_avtale (navident);
-
-create index ansatt_avtale_oppslag_avtaleid_idx
-    on ansatt_avtale (avtale_id);
--- mulighetsrommet-api/src/main/resources/db/migration/V40__add_columns_for_forhandsgodkjente_avtaler.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V40__add_columns_for_forhandsgodkjente_avtaler.sql
@@ -3,3 +3,20 @@ alter table avtale
     add column antall_plasser int,
     add column url text;
 
+create table ansatt_avtale
+(
+    navident   text                    not null,
+    avtale_id  uuid                    not null
+        constraint fk_avtale references avtale (id) on delete cascade,
+    created_at timestamp default now() not null,
+    updated_at timestamp default now() not null,
+    primary key (navident, avtale_id)
+);
+
+create index ansatt_avtale_oppslag_navident_idx
+    on ansatt_avtale (navident);
+
+create index ansatt_avtale_oppslag_avtaleid_idx
+    on ansatt_avtale (avtale_id);
+
+
