c9e32cc61ebed93381ad16a9a3eba5fe4a46230e
Sindre Moldeklev <sndrem@gmail.com>
2023-04-12 07:54:41 +0200

    Flytter navigering opp i header

--- frontend/mr-admin-flate/src/pages/tiltaksgjennomforinger/TiltaksgjennomforingerPage.tsx
+++ frontend/mr-admin-flate/src/pages/tiltaksgjennomforinger/TiltaksgjennomforingerPage.tsx
@@ -1,15 +1,13 @@
 import { Heading } from "@navikt/ds-react";
-import { ContainerLayout } from "../../layouts/ContainerLayout";
-import { MainContainer } from "../../layouts/MainContainer";
-import { NavigeringHeader } from "../forside/NavigeringHeader";
-import styles from "../Page.module.scss";
 import { Tiltaksgjennomforingfilter } from "../../components/filter/Tiltaksgjennomforingfilter";
 import { TiltaksgjennomforingOversikt } from "../../components/tiltaksgjennomforinger/TiltaksgjennomforingOversikt";
+import { ContainerLayout } from "../../layouts/ContainerLayout";
+import { MainContainer } from "../../layouts/MainContainer";
+import styles from "../Page.module.scss";
 
 export function TiltaksgjennomforingerPage() {
   return (
     <MainContainer>
-      <NavigeringHeader />
       <ContainerLayout>
         <Heading level="2" size="large" className={styles.header_wrapper}>
           Oversikt over tiltaksgjennomføringer
--- frontend/mr-admin-flate/src/pages/tiltakstyper/TiltakstyperPage.tsx
+++ frontend/mr-admin-flate/src/pages/tiltakstyper/TiltakstyperPage.tsx
@@ -3,13 +3,11 @@ import { Tiltakstypefilter } from "../../components/filter/Tiltakstypefilter";
 import { TiltakstyperOversikt } from "../../components/tiltakstyper/TiltakstyperOversikt";
 import { ContainerLayout } from "../../layouts/ContainerLayout";
 import { MainContainer } from "../../layouts/MainContainer";
-import { NavigeringHeader } from "../forside/NavigeringHeader";
 import styles from "../Page.module.scss";
 
 export function TiltakstyperPage() {
   return (
     <MainContainer>
-      <NavigeringHeader />
       <ContainerLayout>
         <Heading level="2" size="large" className={styles.header_wrapper}>
           Oversikt over tiltakstyper
