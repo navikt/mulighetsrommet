ed7cefea827bb8c63994508a57b0aff5c005b7f4
Signe <signe.marie.oen.carlsen@nav.no>
2023-04-28 15:44:12 +0200

    slett mr api sanity sync

--- mulighetsrommet-api-sanity-sync/src/main/kotlin/no/nav/mulighetsrommet/sanity/Config.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.mulighetsrommet.sanity
-
-import no.nav.mulighetsrommet.database.DatabaseAdapter
-
-data class Config(
-    val db: DatabaseAdapter.Config,
-    val sanity: SanityClient.Config,
-)
