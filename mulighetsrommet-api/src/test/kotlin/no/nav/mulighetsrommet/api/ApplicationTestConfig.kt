135a4d66854a444103aaec4789c8bc943e0ff477
Sindre Moldeklev <sndrem@gmail.com>
2023-04-28 14:01:54 +0200

    Fiks testoppsett

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/ApplicationTestConfig.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/ApplicationTestConfig.kt
@@ -5,6 +5,7 @@ import no.nav.mulighetsrommet.api.producers.TiltaksgjennomforingKafkaProducer
 import no.nav.mulighetsrommet.api.producers.TiltakstypeKafkaProducer
 import no.nav.mulighetsrommet.api.services.SanityService
 import no.nav.mulighetsrommet.api.tasks.SynchronizeNorgEnheter
+import no.nav.mulighetsrommet.api.tasks.SynchronizeTiltaksgjennomforingEnheter
 import no.nav.mulighetsrommet.database.Database
 import no.nav.mulighetsrommet.database.FlywayDatabaseAdapter
 import no.nav.mulighetsrommet.database.kotest.extensions.createDatabaseTestSchema
@@ -52,6 +53,9 @@ fun createTestApplicationConfig(oauth: MockOAuth2Server) = AppConfig(
     arenaAdapter = createServiceClientConfig("arena-adapter"),
     tasks = TaskConfig(
         synchronizeNorgEnheter = SynchronizeNorgEnheter.Config(
+            delayOfMinutes = 10,
+        ),
+        synchronizeEnheterFraSanityTilApi = SynchronizeTiltaksgjennomforingEnheter.Config(
             delayOfMinutes = 10,
         ),
     ),
