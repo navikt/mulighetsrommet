27feb95a631542f708fb2c29366d4a514b1cc1bb
Signe <signe.marie.oen.carlsen@nav.no>
2023-05-04 13:11:16 +0200

    fjern index

--- frontend/mr-admin-flate/src/components/notifikasjoner/Notifikasjonsliste.tsx
+++ frontend/mr-admin-flate/src/components/notifikasjoner/Notifikasjonsliste.tsx
@@ -2,7 +2,7 @@ import { useFeatureToggles } from "../../api/features/feature-toggles";
 import styles from "./BrukerNotifikasjoner.module.scss";
 import { useNotifikasjonerForAnsatt } from "../../api/notifikasjoner/useNotifikasjonerForAnsatt";
 import { Notifikasjonssrad } from "./Notifikasjonssrad";
-import {Laster} from "../laster/Laster";
+import { Laster } from "../laster/Laster";
 
 export function Notifikasjonsliste() {
   const { data: features } = useFeatureToggles();
@@ -24,11 +24,7 @@ export function Notifikasjonsliste() {
     <section className={styles.container}>
       {data.map((n, i) => {
         return (
-          <Notifikasjonssrad
-            key={n.id}
-            index={i}
-            notifikasjon={n}
-          ></Notifikasjonssrad>
+          <Notifikasjonssrad key={n.id} notifikasjon={n}></Notifikasjonssrad>
         );
       })}
     </section>
