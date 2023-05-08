9ff3e2eb88a4e4bf7155f7b0a6a04632f4dcc490
Sindre Moldeklev <sndrem@gmail.com>
2023-05-08 11:51:24 +0200

    Se varslingsboks hvis du har uleste notifikasjoner

--- frontend/mr-admin-flate/src/components/notifikasjoner/BrukerNotifikasjoner.tsx
+++ frontend/mr-admin-flate/src/components/notifikasjoner/BrukerNotifikasjoner.tsx
@@ -3,22 +3,27 @@ import { useHentAnsatt } from "../../api/administrator/useHentAdministrator";
 import { useFeatureToggles } from "../../api/features/feature-toggles";
 import styles from "./BrukerNotifikasjoner.module.scss";
 import { Varsel } from "./Varsel";
+import { useNotificationSummary } from "../../api/notifikasjoner/useNotificationSummary";
+import { Link } from "react-router-dom";
 
 export function BrukerNotifikasjoner() {
   const { data: features } = useFeatureToggles();
   const { data: bruker } = useHentAnsatt();
+  const { data: notificationSummary } = useNotificationSummary();
+  const antallUlesteNotifikasjoner = notificationSummary?.unreadCount || -1;
 
   if (!features?.["mulighetsrommet.admin-flate-se-notifikasjoner"]) return null;
+
+  if (antallUlesteNotifikasjoner <= 0) return null;
 
   return (
     <section className={styles.container}>
       <Heading level="3" size="medium">
         Hei {bruker?.fornavn}
       </Heading>
-      <Varsel
-        tittel="Varsler"
-        melding="Her kommer det funksjonalitet for varsler"
-      />
+      <Link className={styles.link} to="/notifikasjoner">
+        <Varsel tittel="Varsler" melding="Du har nye varsler" />
+      </Link>
     </section>
   );
 }
