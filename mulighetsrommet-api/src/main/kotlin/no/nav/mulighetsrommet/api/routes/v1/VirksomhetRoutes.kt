85af64f87c0b4b8a22e059c6dfb681ec4dd7f023
Sindre Moldeklev <sndrem@gmail.com>
2023-05-15 13:16:35 +0200

    getOrFail istedenfor custom-validering på endepunkter

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/VirksomhetRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/VirksomhetRoutes.kt
@@ -1,10 +1,10 @@
 package no.nav.mulighetsrommet.api.routes.v1
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
 import no.nav.mulighetsrommet.api.clients.brreg.OrgnummerUtil
 import no.nav.mulighetsrommet.api.services.BrregService
 import org.koin.ktor.ext.inject
@@ -14,10 +14,7 @@ fun Route.virksomhetRoutes() {
 
     route("api/v1/internal/virksomhet") {
         get("{orgnr}") {
-            val orgnr = call.parameters["orgnr"] ?: return@get call.respondText(
-                text = "Mangler verdi for 'orgnr'",
-                status = HttpStatusCode.BadRequest,
-            )
+            val orgnr = call.parameters.getOrFail("orgnr")
 
             if (!OrgnummerUtil.erOrgnr(orgnr)) {
                 throw BadRequestException("Verdi sendt inn er ikke et organisasjonsnummer. Organisasjonsnummer er 9 siffer og bare tall.")
@@ -27,10 +24,7 @@ fun Route.virksomhetRoutes() {
         }
 
         get("sok/{sok}") {
-            val sokestreng = call.parameters["sok"] ?: return@get call.respondText(
-                text = "Mangler verdi for 'sok'",
-                status = HttpStatusCode.BadRequest,
-            )
+            val sokestreng = call.parameters.getOrFail("sok")
 
             if (sokestreng.isBlank()) {
                 throw BadRequestException("'sok' kan ikke være en tom streng")
