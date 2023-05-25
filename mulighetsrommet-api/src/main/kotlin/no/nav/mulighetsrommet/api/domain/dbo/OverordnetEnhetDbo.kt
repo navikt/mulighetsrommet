702ec25ff2d8d9918959aef192c80bd573eada93
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2023-05-25 10:36:30 +0200

    Splitt upsert av enhet og overenhet
    Fordi enhetene som kommer fra kafka topic ikke har liste med
    underenheter, men de kommer hver for seg

---
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/domain/dbo/OverordnetEnhetDbo.kt
@@ -0,0 +1,20 @@
+package no.nav.mulighetsrommet.api.domain.dbo
+
+import no.nav.mulighetsrommet.api.domain.dto.VirksomhetDto
+
+data class OverordnetEnhetDbo(
+    val organisasjonsnummer: String,
+    val navn: String,
+    val underenheter: List<VirksomhetDto>,
+)
+
+fun VirksomhetDto.toOverordnetEnhetDbo(): OverordnetEnhetDbo {
+    if (overordnetEnhet != null || underenheter == null) {
+        throw IllegalArgumentException("Virksomhet $organisasjonsnummer er ikke en full overordnet enhet")
+    }
+    return OverordnetEnhetDbo(
+        organisasjonsnummer = organisasjonsnummer,
+        navn = navn,
+        underenheter = underenheter,
+    )
+}
