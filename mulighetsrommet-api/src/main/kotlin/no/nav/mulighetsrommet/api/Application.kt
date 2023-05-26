1afc17059ffab4926a2766ebf9c9bb28caec479d
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-05-26 13:30:18 +0200

    enable failed record processor

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
@@ -68,10 +68,13 @@ fun Application.configure(config: AppConfig) {
     val scheduler: Scheduler by inject()
 
     environment.monitor.subscribe(ApplicationStarted) {
+        kafka.enableFailedRecordProcessor()
+
         scheduler.start()
     }
 
     environment.monitor.subscribe(ApplicationStopPreparing) {
+        kafka.disableFailedRecordProcessor()
         kafka.stopPollingTopicChanges()
 
         scheduler.stop()
