d86b0aff892742ac4834e99028de49864f812f50
Sindre Moldeklev <sndrem@gmail.com>
2023-06-01 09:49:00 +0200

    Slettelogikk i backend

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/AvtaleService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/AvtaleService.kt
@@ -1,18 +1,25 @@
 package no.nav.mulighetsrommet.api.services
 
 import arrow.core.flatMap
+import arrow.core.getOrElse
+import io.ktor.http.*
 import io.ktor.server.plugins.*
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 import no.nav.mulighetsrommet.api.domain.dto.AvtaleNokkeltallDto
 import no.nav.mulighetsrommet.api.repositories.AvtaleRepository
 import no.nav.mulighetsrommet.api.repositories.TiltaksgjennomforingRepository
 import no.nav.mulighetsrommet.api.routes.v1.AvtaleRequest
 import no.nav.mulighetsrommet.api.routes.v1.responses.PaginatedResponse
 import no.nav.mulighetsrommet.api.routes.v1.responses.Pagination
+import no.nav.mulighetsrommet.api.utils.AdminTiltaksgjennomforingFilter
 import no.nav.mulighetsrommet.api.utils.AvtaleFilter
 import no.nav.mulighetsrommet.api.utils.PaginationParams
 import no.nav.mulighetsrommet.database.utils.QueryResult
 import no.nav.mulighetsrommet.domain.dto.AvtaleAdminDto
 import no.nav.mulighetsrommet.domain.dto.AvtaleNotificationDto
+import no.nav.mulighetsrommet.ktor.exception.StatusException
+import java.time.LocalDate
 import java.util.*
 
 class AvtaleService(
@@ -35,8 +42,41 @@ class AvtaleService(
             .map { it!! } // If upsert is succesfull it should exist here
     }
 
-    fun delete(id: UUID): QueryResult<Int> {
-        return avtaler.delete(id)
+    fun delete(id: UUID, currentDate: LocalDate = LocalDate.now()): SletteAvtaleDto {
+        val optionalAvtale = avtaler.get(id).getOrNull()
+            ?: throw NotFoundException("Fant ikke avtale for sletting")
+
+        if (optionalAvtale.startDato < currentDate && optionalAvtale.sluttDato > currentDate) {
+            return SletteAvtaleDto(
+                statusCode = HttpStatusCode.BadRequest,
+                message = "Avtalen er mellom start- og sluttdato og må avsluttes før den kan slettes.",
+            )
+        }
+
+        val gjennomforingerForAvtale =
+            tiltaksgjennomforinger.getAll(filter = AdminTiltaksgjennomforingFilter(avtaleId = id, dagensDato = currentDate))
+                .getOrElse { Pair(0, emptyList()) }
+        if (gjennomforingerForAvtale.first > 0) {
+            return SletteAvtaleDto(
+                statusCode = HttpStatusCode.BadRequest,
+                message = "Avtalen har ${gjennomforingerForAvtale.first} tiltaksgjennomføringer koblet til seg. Du må frikoble gjennomføringene før du kan slette avtalen.",
+            )
+        }
+
+        val deleteResponse = avtaler.delete(id)
+
+        return deleteResponse.map {
+            SletteAvtaleDto(statusCode = HttpStatusCode.OK, message = "Avtalen ble slettet")
+        }.mapLeft {
+            SletteAvtaleDto(
+                statusCode = HttpStatusCode.InternalServerError,
+                "Det oppsto en feil ved sletting av avtalen",
+                cause = it.error.message,
+            )
+        }.getOrNull() ?: throw StatusException(
+            status = HttpStatusCode.InternalServerError,
+            description = "Det skjedde en feil ved sletting av avtale med id: $id",
+        )
     }
 
     fun getAll(
@@ -68,3 +108,11 @@ class AvtaleService(
         return avtaler.getAllAvtalerSomNarmerSegSluttdato()
     }
 }
+
+@Serializable
+data class SletteAvtaleDto(
+    @Contextual
+    val statusCode: HttpStatusCode,
+    val message: String,
+    val cause: String? = null,
+)
