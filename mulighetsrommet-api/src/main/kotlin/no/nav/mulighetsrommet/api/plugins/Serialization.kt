42dc5c1602b760f4a84bb20a7bea720a6759ac9f
Sindre Moldeklev <sndrem@gmail.com>
2023-06-21 09:37:21 +0200

    Bugfix encodeDefault for navEnheter

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Serialization.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Serialization.kt
@@ -3,14 +3,12 @@ package no.nav.mulighetsrommet.api.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import kotlinx.serialization.json.Json
+import no.nav.mulighetsrommet.serialization.json.JsonIgnoreUnknownKeys
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(
-            Json {
-                ignoreUnknownKeys = true
-            },
+            JsonIgnoreUnknownKeys,
         )
     }
 }
