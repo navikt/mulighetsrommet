bc16d2e3ba22b72099e5a34267b8cec61a6e93f9
Sindre Moldeklev <sndrem@gmail.com>
2023-06-29 13:33:32 +0200

    Fjern collation

--- mulighetsrommet-api/src/main/resources/db/migration/V75__collation_nav_ansatt.sql
+++
@@ -1,9 +0,0 @@
-alter table nav_ansatt
-    alter column fornavn set data type text collate "nb-NO-x-icu";
-
-alter table nav_ansatt
-    alter column etternavn set data type text collate "nb-NO-x-icu";
-
-alter table avtale
-    add column created_at timestamp default now() not null,
-    add column updated_at timestamp default now() not null;
