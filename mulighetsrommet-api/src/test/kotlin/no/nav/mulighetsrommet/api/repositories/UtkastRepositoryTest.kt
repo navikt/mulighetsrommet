6ab3c39e7845c5afec51f7ee23d321ae3ae01874
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-07-03 15:45:19 +0200

    fix failing test

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/repositories/UtkastRepositoryTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/repositories/UtkastRepositoryTest.kt
@@ -7,9 +7,10 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import no.nav.mulighetsrommet.api.clients.norg2.Norg2Type
 import no.nav.mulighetsrommet.api.createDatabaseTestConfig
-import no.nav.mulighetsrommet.api.domain.dbo.*
+import no.nav.mulighetsrommet.api.domain.dbo.UtkastDbo
+import no.nav.mulighetsrommet.api.domain.dbo.Utkasttype
+import no.nav.mulighetsrommet.api.fixtures.MulighetsrommetTestDomain
 import no.nav.mulighetsrommet.database.kotest.extensions.FlywayDatabaseTestListener
 import no.nav.mulighetsrommet.database.kotest.extensions.truncateAll
 import java.time.LocalDateTime
@@ -18,64 +19,45 @@ import java.util.*
 class UtkastRepositoryTest : FunSpec({
     val database = extension(FlywayDatabaseTestListener(createDatabaseTestConfig()))
 
+    val domain = MulighetsrommetTestDomain()
+
     beforeEach {
         database.db.truncateAll()
-        val navAnsatte = NavAnsattRepository(database.db)
-        val enheter = NavEnhetRepository(database.db)
-        enheter.upsert(
-            NavEnhetDbo(
-                navn = "IT-avdelingen",
-                enhetsnummer = "2990",
-                status = NavEnhetStatus.AKTIV,
-                type = Norg2Type.LOKAL,
-                overordnetEnhet = null,
-            ),
-        )
-        navAnsatte.upsert(
-            NavAnsattDbo(
-                navIdent = "B123456",
-                fornavn = "Bertil",
-                etternavn = "Betabruker",
-                hovedenhet = "2990",
-                azureId = UUID.randomUUID(),
-                mobilnummer = null,
-                epost = "",
-                roller = emptyList(),
-                skalSlettesDato = null,
-            ),
-        )
+        domain.initialize(database.db)
     }
 
     context("CRUD for Utkast") {
         val utkastRepository = UtkastRepository(database.db)
 
+        val utkastId = UUID.randomUUID()
+        val utkast = UtkastDbo(
+            id = utkastId,
+            opprettetAv = domain.ansatt1.navIdent,
+            utkastData = Json.parseToJsonElement("{\"id\":\"123\",\"navn\":\"Min gjennomføring er kul\"}"),
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+            type = Utkasttype.Tiltaksgjennomforing,
+        )
+
         test("Upsert, Get og Delete") {
-            val utkastId = UUID.randomUUID()
-            val utkast = UtkastDbo(
-                id = utkastId,
-                opprettetAv = "B123456",
-                utkastData = Json.parseToJsonElement("{\"id\":\"123\",\"navn\":\"Min gjennomføring er kul\"}"),
-                createdAt = LocalDateTime.now(),
-                updatedAt = LocalDateTime.now(),
-                type = Utkasttype.Tiltaksgjennomforing,
-            )
             utkastRepository.upsert(utkast).shouldBeRight().should {
-                it?.opprettetAv shouldBe "B123456"
+                it?.opprettetAv shouldBe domain.ansatt1.navIdent
                 Json.encodeToString(it?.utkastData) shouldContain "Min gjennomføring er kul"
                 it?.type shouldBe Utkasttype.Tiltaksgjennomforing
             }
 
-            val redigertUtkast =
-                utkast.copy(utkastData = Json.parseToJsonElement("{\"id\":\"123\",\"navn\":\"Min gjennomføring er fet\"}"))
+            val redigertUtkast = utkast.copy(
+                utkastData = Json.parseToJsonElement("{\"id\":\"123\",\"navn\":\"Min gjennomføring er fet\"}"),
+            )
             utkastRepository.upsert(redigertUtkast).shouldBeRight().should {
-                it?.opprettetAv shouldBe "B123456"
+                it?.opprettetAv shouldBe domain.ansatt1.navIdent
                 Json.encodeToString(it?.utkastData) shouldContain "Min gjennomføring er fet"
                 it?.type shouldBe Utkasttype.Tiltaksgjennomforing
             }
 
             utkastRepository.get(utkastId).shouldBeRight().should {
                 it?.id shouldBe utkastId
-                it?.opprettetAv shouldBe "B123456"
+                it?.opprettetAv shouldBe domain.ansatt1.navIdent
                 Json.encodeToString(it?.utkastData) shouldContain "Min gjennomføring er fet"
                 it?.type shouldBe Utkasttype.Tiltaksgjennomforing
             }
