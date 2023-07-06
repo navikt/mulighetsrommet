43068c8e01ba87cffbee76cbe695c43412219b1a
Sindre Moldeklev <sndrem@gmail.com>
2023-07-06 09:39:50 +0200

    Riktig sjekk av miljø

--- frontend/mr-admin-flate/src/api/getEnvironment.ts
+++ frontend/mr-admin-flate/src/api/getEnvironment.ts
@@ -1,5 +1,5 @@
-const isProduction = window.location.href.includes("www.intern.nav.no");
-const isDevelopment = window.location.href.includes("www.intern.dev.nav.no");
+const isProduction = window.location.href.includes(".intern.nav.no");
+const isDevelopment = window.location.href.includes(".intern.dev.nav.no");
 
 export const getEnvironment = () => {
   if (isProduction) {
