539268799aae121cef4a42d10bb9ec10db3921dc
Tania Holst <taniaholst@Tanias-MBP.home.arpa>
2023-07-20 13:36:44 +0200

    la til tiny-invariant bibliotek og oppdaterte kode

--- frontend/mr-admin-flate/src/api/avtaler/avtalenotat/useMineAvtalenotater.ts
+++ frontend/mr-admin-flate/src/api/avtaler/avtalenotat/useMineAvtalenotater.ts
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { mulighetsrommetClient } from "../../clients";
 import { QueryKeys } from "../../QueryKeys";
 import { useGetAvtaleIdFromUrl } from "../../../hooks/useGetAvtaleIdFromUrl";
+import invariant from "tiny-invariant";
 
 export function useMineAvtalenotater() {
   const avtaleId = useGetAvtaleIdFromUrl();
-  const enabled = !!avtaleId;
+  invariant(avtaleId, "AvtaleId er ikke satt");
 
   return useQuery(
     QueryKeys.mineAvtalenotater(avtaleId!!),
@@ -13,6 +14,6 @@ export function useMineAvtalenotater() {
       mulighetsrommetClient.avtaleNotater.getMineAvtaleNotater({
         avtaleId: avtaleId!,
       }),
-    { enabled },
+    {},
   );
 }
