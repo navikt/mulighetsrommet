0e5118af97086887259f81f2f83d893a96ba3403
Sindre Moldeklev <sndrem@gmail.com>
2023-07-24 14:15:36 +0200

    Fjerner noen invariants som gikk i veien for bruk av hook

--- frontend/mr-admin-flate/src/api/avtaler/useAvtale.ts
+++ frontend/mr-admin-flate/src/api/avtaler/useAvtale.ts
@@ -1,16 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { mulighetsrommetClient } from "../clients";
-import { QueryKeys } from "../QueryKeys";
 import { useGetAvtaleIdFromUrl } from "../../hooks/useGetAvtaleIdFromUrl";
-import invariant from "tiny-invariant";
+import { QueryKeys } from "../QueryKeys";
+import { mulighetsrommetClient } from "../clients";
 
 export function useAvtale(overstyrAvtaleId?: string) {
   const avtaleId = overstyrAvtaleId || useGetAvtaleIdFromUrl();
-  invariant(avtaleId, "Klarte ikke hente id for avtale.");
 
   return useQuery(
     QueryKeys.avtale(avtaleId!!),
-    () => mulighetsrommetClient.avtaler.getAvtale({ id: avtaleId }),
-    {},
+    () => mulighetsrommetClient.avtaler.getAvtale({ id: avtaleId! }),
+    { enabled: !!avtaleId },
   );
 }
--- frontend/mr-admin-flate/src/api/tiltaksgjennomforing/useTiltaksgjennomforing.ts
+++ frontend/mr-admin-flate/src/api/tiltaksgjennomforing/useTiltaksgjennomforing.ts
@@ -1,22 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { mulighetsrommetClient } from "mulighetsrommet-veileder-flate/src/core/api/clients";
-import invariant from "tiny-invariant";
-import { QueryKeys } from "../QueryKeys";
 import { useGetAdminTiltaksgjennomforingsIdFraUrl } from "../../hooks/useGetAdminTiltaksgjennomforingsIdFraUrl";
+import { QueryKeys } from "../QueryKeys";
 
 export function useTiltaksgjennomforing() {
   const tiltaksgjennomforingId = useGetAdminTiltaksgjennomforingsIdFraUrl();
-  invariant(
-    tiltaksgjennomforingId,
-    "Klarte ikke hente id for tiltaksgjennomføring.",
-  );
 
   return useQuery(
     QueryKeys.tiltaksgjennomforing(tiltaksgjennomforingId!!),
     () =>
       mulighetsrommetClient.tiltaksgjennomforinger.getTiltaksgjennomforing({
-        id: tiltaksgjennomforingId,
+        id: tiltaksgjennomforingId!!,
       }),
-    {},
+    { enabled: !!tiltaksgjennomforingId },
   );
 }
