a12b47303e3c258503daeb7dedebe4708e9bc9b0
Sindre Moldeklev <sndrem@gmail.com>
2023-07-26 07:33:55 +0200

    Fjern toggle for joyride og innsikt i veilederflate

--- frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
@@ -2,21 +2,15 @@ import { useQuery } from 'react-query';
 import { headers } from './headers';
 
 export const ENABLE_ARBEIDSFLATE = 'mulighetsrommet.enable-arbeidsflate';
-export const VIS_INNSIKTSFANE = 'mulighetsrommet.vis-innsiktsfane';
-export const VIS_JOYRIDE = 'mulighetsrommet.joyride-veilederflate';
 
-export const ALL_TOGGLES = [ENABLE_ARBEIDSFLATE, VIS_INNSIKTSFANE, VIS_JOYRIDE];
+export const ALL_TOGGLES = [ENABLE_ARBEIDSFLATE];
 
 export interface Features {
   [ENABLE_ARBEIDSFLATE]: boolean;
-  [VIS_INNSIKTSFANE]: boolean;
-  [VIS_JOYRIDE]: boolean;
 }
 
 export const initialFeatures: Features = {
   [ENABLE_ARBEIDSFLATE]: false,
-  [VIS_INNSIKTSFANE]: false,
-  [VIS_JOYRIDE]: false,
 };
 
 const toggles = ALL_TOGGLES.map(element => 'feature=' + element).join('&');
