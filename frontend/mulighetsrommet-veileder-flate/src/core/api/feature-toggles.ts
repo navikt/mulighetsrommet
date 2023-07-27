e1ea4e7320fa6bfc8dc52f7c2c6fcae71428736d
Sindre Moldeklev <sndrem@gmail.com>
2023-07-27 12:13:51 +0200

    Fjerner kobling til poao-unleash

--- frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/feature-toggles.ts
@@ -1,36 +1,11 @@
-import { useQuery } from 'react-query';
-import { headers } from './headers';
 import { Toggles } from 'mulighetsrommet-api-client';
-import { QueryKeys } from './query-keys';
+import { useQuery } from 'react-query';
 import { mulighetsrommetClient } from './clients';
+import { QueryKeys } from './query-keys';
 
 export const ALL_TOGGLES = [...Object.values(Toggles)];
 
 export type Features = Record<Toggles, boolean>;
-
-export const initialFeatures: Features = {
-  'mulighetsrommet.admin-flate-opprett-avtale': false,
-  'mulighetsrommet.admin-flate-rediger-avtale': false,
-  'mulighetsrommet.admin-flate-opprett-tiltaksgjennomforing': false,
-  'mulighetsrommet.admin-flate-slett-avtale': false,
-  'mulighetsrommet.admin-flate-slett-tiltaksgjennomforing': false,
-  'mulighetsrommet.admin-flate-rediger-tiltaksgjennomforing': false,
-  'mulighetsrommet.admin-flate-vis-deltakerliste-fra-komet': false,
-  'mulighetsrommet.enable-arbeidsflate': true,
-};
-
-const toggles = ALL_TOGGLES.map(element => 'feature=' + element).join('&');
-export const fetchConfig = {
-  headers,
-};
-
-export const useFeatureToggles = () => {
-  return useQuery<Features>(['features'], () =>
-    fetch(`/veilarbpersonflatefs/api/feature?${toggles}`, fetchConfig).then(Response => {
-      return Response.ok ? Response.json() : initialFeatures;
-    })
-  );
-};
 
 /**
  * Hook for å bruke en spesifikk feature toggle for å skjule eller vise funksjonalitet
