63afec0dbc535e21be44a14926e51a36190c1770
Sindre Moldeklev <sndrem@gmail.com>
2023-08-01 13:49:13 +0200

    Tags for notifikasjonstype

--- frontend/mr-admin-flate/src/components/notifikasjoner/Notifikasjonsrad.tsx
+++ frontend/mr-admin-flate/src/components/notifikasjoner/Notifikasjonsrad.tsx
@@ -1,22 +1,45 @@
-import { BodyLong, BodyShort, Heading } from "@navikt/ds-react";
+import { BodyLong, BodyShort, Heading, Tag } from "@navikt/ds-react";
 import classNames from "classnames";
-import { UserNotification } from "mulighetsrommet-api-client";
-import { useState } from "react";
+import { NotificationType, UserNotification } from "mulighetsrommet-api-client";
+import { ReactNode, useState } from "react";
+import { Slide, ToastContainer } from "react-toastify";
 import { formaterDatoTid } from "../../utils/Utils";
 import { CheckmarkButton } from "./CheckmarkButton";
 import styles from "./Notifikasjoner.module.scss";
-import { Slide, ToastContainer } from "react-toastify";
 
 interface NotifikasjonssradProps {
   notifikasjon: UserNotification;
   lest: boolean;
 }
 
+function tag(type: NotificationType, lest: boolean): ReactNode {
+  switch (type) {
+    case NotificationType.NOTIFICATION:
+      return (
+        <Tag
+          data-testid="notifikasjon-tag"
+          variant={lest ? "warning-moderate" : "warning-filled"}
+        >
+          Notifikasjon
+        </Tag>
+      );
+    case NotificationType.TASK:
+      return (
+        <Tag
+          data-testid="oppgave-tag"
+          variant={lest ? "info-moderate" : "info-filled"}
+        >
+          Oppgave
+        </Tag>
+      );
+  }
+}
+
 export function Notifikasjonssrad({
   notifikasjon,
   lest,
 }: NotifikasjonssradProps) {
-  const { title, description, createdAt } = notifikasjon;
+  const { title, description, createdAt, type } = notifikasjon;
 
   const [read, setRead] = useState<boolean>(lest);
 
@@ -40,6 +63,7 @@ export function Notifikasjonssrad({
         <BodyShort size="small" title={createdAt} className={styles.muted}>
           {formaterDatoTid(createdAt)}
         </BodyShort>
+        <BodyShort>{tag(type, lest)}</BodyShort>
       </div>
       <div>
         <CheckmarkButton id={notifikasjon.id} read={read} setRead={setRead} />
