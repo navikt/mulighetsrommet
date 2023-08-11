8b5f58d3aa5a22474f4a08ee382498195efd9032
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-08-11 14:24:30 +0200

    fix code scanning warnings about unused variable

--- frontend/mulighetsrommet-cms/components/ShowFieldIfTiltakstypeMatches.tsx
+++ frontend/mulighetsrommet-cms/components/ShowFieldIfTiltakstypeMatches.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useClient, useFormValue } from "sanity";
 import { API_VERSION } from "../sanity.config";
+
 export function ShowFieldIfTiltakstypeMatches(props: any, tiltakstype: string) {
   const client = useClient({ apiVersion: API_VERSION });
   const [visKomponent, setVisKomponent] = useState(false);
@@ -9,7 +10,7 @@ export function ShowFieldIfTiltakstypeMatches(props: any, tiltakstype: string) {
   useEffect(() => {
     if (!tiltakstypeRef) {
       console.warn(
-        "Du prøver å bruke komponenten på et skjema som ikke refererer til tiltakstype. Det går ikke."
+        "Du prøver å bruke komponenten på et skjema som ikke refererer til tiltakstype. Det går ikke.",
       );
       return;
     }
@@ -17,7 +18,7 @@ export function ShowFieldIfTiltakstypeMatches(props: any, tiltakstype: string) {
     const hentTiltakstype = async () => {
       const res = await client.fetch(
         "*[_type == 'tiltakstype' && _id == $ref][0]",
-        { ref: tiltakstypeRef }
+        { ref: tiltakstypeRef },
       );
 
       setVisKomponent(res.tiltakstypeNavn === tiltakstype);
