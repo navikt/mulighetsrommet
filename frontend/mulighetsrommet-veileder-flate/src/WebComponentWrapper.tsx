3aa81d1335bc2e1baeb2e9c9105524f4b4c65a9d
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-08-24 09:06:20 +0200

    remove unnecessary node during setup

--- frontend/mulighetsrommet-veileder-flate/src/WebComponentWrapper.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/WebComponentWrapper.tsx
@@ -11,28 +11,49 @@ interface ViteAssetManifest {
 }
 
 export class Arbeidsmarkedstiltak extends HTMLElement {
+  static FNR_PROP = 'data-fnr';
+
   static get observedAttributes() {
-    return ['data-fnr'];
+    return [Arbeidsmarkedstiltak.FNR_PROP];
   }
 
   /**
    * Blir satt etter at applikasjonen har mountet.
    *
-   * Ved endringer på "data-fnr"-attributtet så blir denne funksjonen kalt slik at vi får
+   * Ved endringer på `FNR_PROP`-attributtet så blir denne funksjonen kalt slik at vi får
    * propagert endringene til resten av applikasjonen.
    */
   setFnr?: (fnr: string) => void;
 
   connectedCallback() {
-    // Cant mount on shadowRoot, create a extra div for mounting modal
-    const shadowDomFirstChild = document.createElement('div');
-    // This will be app entry point, need to be outside modal-mount node
+    // This will be app entry point
     const appRoot = document.createElement('div');
     appRoot.id = APPLICATION_WEB_COMPONENT_NAME;
-    const shadowRoot = this.attachShadow({ mode: 'closed' });
-    shadowRoot.appendChild(shadowDomFirstChild);
-    shadowDomFirstChild.appendChild(appRoot);
 
+    // The ShadowRoot is rendered separately from the main DOM tree, ensuring that styling
+    // does not bleed across trees
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    shadowRoot.appendChild(appRoot);
+
+    this.loadStyles(shadowRoot);
+
+    const fnr = this.getAttribute(Arbeidsmarkedstiltak.FNR_PROP);
+
+    const root = createRoot(appRoot);
+    root.render(
+      <AppContext fnr={fnr} setFnrRef={setFnr => (this.setFnr = setFnr)}>
+        <App />
+      </AppContext>
+    );
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (name === Arbeidsmarkedstiltak.FNR_PROP && this.setFnr) {
+      this.setFnr(newValue);
+    }
+  }
+
+  loadStyles(shadowRoot: ShadowRoot) {
     fetch(joinPaths(import.meta.env.BASE_URL, 'asset-manifest.json'))
       .then(response => {
         if (!response.ok) {
@@ -55,21 +76,6 @@ export class Arbeidsmarkedstiltak extends HTMLElement {
         // eslint-disable-next-line no-console
         console.log('ERROR', error);
       });
-
-    const fnr = this.getAttribute('data-fnr');
-
-    const root = createRoot(appRoot);
-    root.render(
-      <AppContext fnr={fnr} setFnrRef={setFnr => (this.setFnr = setFnr)}>
-        <App />
-      </AppContext>
-    );
-  }
-
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    if (name === 'data-fnr' && this.setFnr) {
-      this.setFnr(newValue);
-    }
   }
 }
 
