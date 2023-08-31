7d9b919877951b137c267537cef5dcacf8c6a9d7
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-08-31 12:53:29 +0200

    slett sync enheter fra api til db

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/ApplicationTestConfig.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/ApplicationTestConfig.kt
@@ -56,10 +56,6 @@ fun createTestApplicationConfig(oauth: MockOAuth2Server) = AppConfig(
             delayOfMinutes = 10,
             disabled = true,
         ),
-        synchronizeEnheterFraSanityTilApi = SynchronizeTiltaksgjennomforingEnheter.Config(
-            delayOfMinutes = 10,
-            disabled = true,
-        ),
         synchronizeNavAnsatte = SynchronizeNavAnsatte.Config(
             disabled = true,
         ),
