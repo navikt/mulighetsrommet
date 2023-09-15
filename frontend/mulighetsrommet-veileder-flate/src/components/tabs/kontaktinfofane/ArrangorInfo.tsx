d911c24cc65fefd179bea95872494e9996c92426
taniaholst <tania.holst@nav.no>
2023-09-15 14:59:39 +0200

    css justeringer i kontaktinfo og høyde på detaljer

--- frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/ArrangorInfo.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/ArrangorInfo.tsx
@@ -1,4 +1,4 @@
-import { BodyShort } from '@navikt/ds-react';
+import { BodyShort, Heading } from '@navikt/ds-react';
 import styles from './Kontaktinfo.module.scss';
 import { VeilederflateTiltaksgjennomforing } from 'mulighetsrommet-api-client';
 import { logEvent } from '../../../core/api/logger';
@@ -18,19 +18,21 @@ const ArrangorInfo = ({ data }: ArrangorInfoProps) => {
 
   return (
     <div className={styles.arrangor_info}>
-      <BodyShort size="medium" className={styles.header}>
+      <Heading size="small" className={styles.header}>
         Arrangør
-      </BodyShort>
+      </Heading>
+
       <div className={styles.container}>
         <BodyShort className={styles.navn} size="small">
           {kontaktinfoArrangor.selskapsnavn}
         </BodyShort>
-        <div className={styles.infofelt}>
-          <div className={styles.rad}>
-            <BodyShort size="small">Lokasjon</BodyShort>
-            <BodyShort size="small">{kontaktinfoArrangor.lokasjon}</BodyShort>
+
+        <BodyShort size="small">
+          <div className={styles.infofelt}>
+            <div className={styles.kolonne}>Lokasjon:</div>
+            <div className={styles.kolonne}>{kontaktinfoArrangor.lokasjon}</div>
           </div>
-        </div>
+        </BodyShort>
       </div>
 
       {kontaktperson && (
@@ -38,23 +40,25 @@ const ArrangorInfo = ({ data }: ArrangorInfoProps) => {
           <BodyShort className={styles.navn} size="small">
             {kontaktperson.navn}
           </BodyShort>
-          <div className={styles.infofelt}>
-            <div className={styles.rad}>
-              <BodyShort size="small">Telefon</BodyShort>
-              <BodyShort size="small">{kontaktperson.telefon}</BodyShort>
-            </div>
-            <div className={styles.rad}>
-              <BodyShort size="small">Epost</BodyShort>
-              <BodyShort size="small">
+
+          <BodyShort size="small">
+            <div className={styles.infofelt}>
+              <div className={styles.kolonne}>
+                <span>Telefon:</span>
+                <span>Epost:</span>
+              </div>
+
+              <div className={styles.kolonne}>
+                <span>{kontaktperson.telefon}</span>
                 <a
                   href={`mailto:${kontaktperson.epost}`}
                   onClick={() => logEvent('mulighetsrommet.arrangor.kontaktperson.epost')}
                 >
                   {kontaktperson.epost}
                 </a>
-              </BodyShort>
+              </div>
             </div>
-          </div>
+          </BodyShort>
         </div>
       )}
     </div>
