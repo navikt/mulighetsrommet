a437ce53e4f49fa4fc89fee1814e1c7067724d64
Sindre Moldeklev <sndrem@gmail.com>
2023-09-21 14:24:14 +0200

    Fjerner unødvendig route

--- frontend/mulighetsrommet-veileder-flate/src/RoutesConfig.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/RoutesConfig.tsx
@@ -1,9 +1,9 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { ViewTiltaksgjennomforingDetaljerContainer } from './views/tiltaksgjennomforing-detaljer/ViewTiltaksgjennomforingDetaljerContainer';
-import ViewTiltaksgjennomforingOversikt from './views/tiltaksgjennomforing-oversikt/ViewTiltaksgjennomforingOversikt';
 import { Toggles } from 'mulighetsrommet-api-client';
+import { Route, Routes } from 'react-router-dom';
 import { useFeatureToggle } from './core/api/feature-toggles';
 import { Landingsside } from './views/landingsside/Landingsside';
+import { ViewTiltaksgjennomforingDetaljerContainer } from './views/tiltaksgjennomforing-detaljer/ViewTiltaksgjennomforingDetaljerContainer';
+import ViewTiltaksgjennomforingOversikt from './views/tiltaksgjennomforing-oversikt/ViewTiltaksgjennomforingOversikt';
 
 const RoutesConfig = () => {
   const enableLandingssideFeature = useFeatureToggle(Toggles.MULIGHETSROMMET_VEILEDERFLATE_LANDINGSSIDE);
@@ -11,7 +11,6 @@ const RoutesConfig = () => {
   return (
     <Routes>
       <Route path="tiltak/:tiltaksnummer" element={<ViewTiltaksgjennomforingDetaljerContainer />} />
-      <Route path=":tiltaksnummer" element={<Navigate to="/" />} />
       <Route path="oversikt" element={<ViewTiltaksgjennomforingOversikt />} />
       {enableLandingsside ? <Route index element={<Landingsside />} /> : null}
       <Route path="*" element={<ViewTiltaksgjennomforingOversikt />} />
