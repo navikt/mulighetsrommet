e7a4e4487db890a4f7d18c4ad60fe3de27803441
Sindre Moldeklev <sndrem@gmail.com>
2023-09-22 09:52:42 +0200

    Prøver med a-tag istedenfor react-router-dom sin Link

--- frontend/mulighetsrommet-veileder-flate/src/views/landingsside/Landingsside.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/views/landingsside/Landingsside.tsx
@@ -1,17 +1,16 @@
 import { PlusIcon } from '@navikt/aksel-icons';
-import { Link } from 'react-router-dom';
 import { HistorikkForBrukerModalInnhold } from '../../components/historikk/HistorikkForBrukerModalInnhold';
-import styles from './Landingsside.module.scss';
 import { routes } from '../../routes';
+import styles from './Landingsside.module.scss';
 
 export function Landingsside() {
   return (
     <main className="mulighetsrommet-veileder-flate">
       <div className={styles.container}>
         <div>
-          <Link className={styles.cta_link} to={`/${routes.oversikt()}`}>
+          <a className={styles.cta_link} href={`/${routes.oversikt()}`}>
             <PlusIcon color="white" fontSize={30} aria-hidden /> Finn nytt arbeidsmarkedstiltak
-          </Link>
+          </a>
         </div>
         <div>
           <h3>Tiltakshistorikk</h3>
