a2551385b060edc9a2402cbda2b1a1ba2a3aa6ac
Sindre Moldeklev <sndrem@gmail.com>
2023-10-02 13:55:24 +0200

    Tillat personlig intro for Del med bruker

--- frontend/mulighetsrommet-veileder-flate/src/components/modal/delemodal/DelMedBrukerContent.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/modal/delemodal/DelMedBrukerContent.tsx
@@ -1,12 +1,12 @@
 import { Alert, BodyShort, Button, ErrorMessage, Textarea } from "@navikt/ds-react";
 import { DelMedBruker, VeilederflateTiltaksgjennomforing } from "mulighetsrommet-api-client";
-import React, { Dispatch, useEffect, useRef, useState } from "react";
+import React, { Dispatch, useEffect, useRef } from "react";
 import { erPreview, formaterDato } from "../../../utils/Utils";
 import { logDelMedbrukerEvent } from "./Delemodal";
 import delemodalStyles from "./Delemodal.module.scss";
 import { Actions, State } from "./DelemodalActions";
 
-const MAKS_ANTALL_TEGN_HILSEN = 300;
+const MAKS_ANTALL_TEGN = 500;
 
 interface Props {
   state: State;
@@ -25,44 +25,89 @@ export function DelMedBrukerContent({
   harDeltMedBruker,
   tiltaksgjennomforing,
 }: Props) {
-  const [visPersonligMelding, setVisPersonligMelding] = useState(false);
+  const { skrivPersonligMelding, skrivPersonligIntro } = state;
+  const personligIntroRef = useRef<HTMLTextAreaElement>(null);
   const personligHilsenRef = useRef<HTMLTextAreaElement>(null);
   const datoSidenSistDelt =
     harDeltMedBruker?.createdAt && formaterDato(new Date(harDeltMedBruker.createdAt));
 
   useEffect(() => {
-    personligHilsenRef?.current?.focus();
-  }, [visPersonligMelding]);
+    if (skrivPersonligIntro) {
+      personligIntroRef?.current?.focus();
+      return;
+    }
+    if (skrivPersonligMelding) {
+      personligHilsenRef?.current?.focus();
+    }
+  }, [skrivPersonligMelding, skrivPersonligIntro]);
 
   const enablePersonligMelding = () => {
+    dispatch({ type: "Skriv personlig melding", payload: true });
     dispatch({ type: "Sett hilsen", payload: state.originalHilsen });
-    setVisPersonligMelding(true);
     logDelMedbrukerEvent("Sett hilsen");
   };
 
+  const enablePersonligIntro = () => {
+    dispatch({ type: "Skriv personlig intro", payload: true });
+    dispatch({ type: "Sett intro", payload: state.introtekst });
+    logDelMedbrukerEvent("Sett intro");
+  };
+
   const handleError = () => {
-    if (state.hilsen.length > MAKS_ANTALL_TEGN_HILSEN) return "For mange tegn";
+    if (state.hilsen.length > MAKS_ANTALL_TEGN || state.introtekst.length > MAKS_ANTALL_TEGN)
+      return "For mange tegn";
   };
 
   const redigerHilsen = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     dispatch({ type: "Sett hilsen", payload: e.currentTarget.value });
   };
 
+  const redigerIntro = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    dispatch({ type: "Sett intro", payload: e.currentTarget.value });
+  };
   return (
     <>
       {harDeltMedBruker ? (
         <Alert variant="warning">{`Dette tiltaket ble delt med bruker ${datoSidenSistDelt}.`}</Alert>
       ) : null}
 
-      {visPersonligMelding && !state.deletekst ? null : (
+      {skrivPersonligIntro ? null : (
+        <Button
+          data-testid="personlig_intro_btn"
+          onClick={enablePersonligIntro}
+          variant="secondary"
+          className={delemodalStyles.personlig_melding_btn}
+        >
+          Legg til personlig introduksjon
+        </Button>
+      )}
+
+      {skrivPersonligIntro ? (
+        <Textarea
+          ref={personligIntroRef}
+          className={delemodalStyles.personligHilsen}
+          size="medium"
+          value={state.introtekst}
+          label=""
+          hideLabel
+          onChange={redigerIntro}
+          maxLength={MAKS_ANTALL_TEGN}
+          data-testid="textarea_intro"
+          error={handleError()}
+        />
+      ) : null}
+
+      {skrivPersonligMelding && skrivPersonligIntro && !state.deletekst ? null : (
         <BodyShort
           title="Teksten er hentet fra tiltakstypen og kan ikke redigeres."
           className={delemodalStyles.deletekst}
         >
-          {`${state.deletekst}${visPersonligMelding ? "" : `\n\n${state.hilsen}`}`}
+          {`${skrivPersonligIntro ? "" : `${state.introtekst}\n`}${state.deletekst}${
+            skrivPersonligMelding ? "" : `\n\n${state.hilsen}`
+          }`}
         </BodyShort>
       )}
-      {visPersonligMelding ? null : (
+      {skrivPersonligMelding ? null : (
         <Button
           data-testid="personlig_hilsen_btn"
           onClick={enablePersonligMelding}
@@ -73,7 +118,7 @@ export function DelMedBrukerContent({
         </Button>
       )}
 
-      {visPersonligMelding ? (
+      {skrivPersonligMelding ? (
         <>
           <Textarea
             ref={personligHilsenRef}
@@ -83,7 +128,7 @@ export function DelMedBrukerContent({
             label=""
             hideLabel
             onChange={redigerHilsen}
-            maxLength={MAKS_ANTALL_TEGN_HILSEN}
+            maxLength={MAKS_ANTALL_TEGN}
             data-testid="textarea_hilsen"
             error={handleError()}
           />
