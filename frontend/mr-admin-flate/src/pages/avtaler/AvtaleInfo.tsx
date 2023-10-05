d697de22a0802d43697b719f1d6ecb20c8caa429
Sindre Moldeklev <sndrem@gmail.com>
2023-10-05 09:38:00 +0200

    Sluttdato istedenfor startdato for prolongering

--- frontend/mr-admin-flate/src/pages/avtaler/AvtaleInfo.tsx
+++ frontend/mr-admin-flate/src/pages/avtaler/AvtaleInfo.tsx
@@ -1,29 +1,25 @@
+import { ExternalLinkIcon } from "@navikt/aksel-icons";
 import { Alert, Heading } from "@navikt/ds-react";
-import { Avtalestatus, Avtaletype, Toggles } from "mulighetsrommet-api-client";
+import { Avtalestatus, Avtaletype } from "mulighetsrommet-api-client";
 import { useState } from "react";
 import { useAvtale } from "../../api/avtaler/useAvtale";
-import AvbrytAvtaleModal from "../../components/modal/AvbrytAvtaleModal";
+import { useDeleteAvtale } from "../../api/avtaler/useDeleteAvtale";
 import { Bolk } from "../../components/detaljside/Bolk";
 import { Metadata, Separator } from "../../components/detaljside/Metadata";
 import { VisHvisVerdi } from "../../components/detaljside/VisHvisVerdi";
 import { Laster } from "../../components/laster/Laster";
+import AvbrytAvtaleModal from "../../components/modal/AvbrytAvtaleModal";
+import SlettAvtaleGjennomforingModal from "../../components/modal/SlettAvtaleGjennomforingModal";
 import { addYear, avtaletypeTilTekst, formaterDato } from "../../utils/Utils";
+import { erAnskaffetTiltak } from "../../utils/tiltakskoder";
 import styles from "../DetaljerInfo.module.scss";
 import { AvtaleKnapperad } from "./AvtaleKnapperad";
-import SlettAvtaleGjennomforingModal from "../../components/modal/SlettAvtaleGjennomforingModal";
-import { useDeleteAvtale } from "../../api/avtaler/useDeleteAvtale";
-import { ExternalLinkIcon } from "@navikt/aksel-icons";
-import { erAnskaffetTiltak } from "../../utils/tiltakskoder";
-import { useFeatureToggle } from "../../api/features/feature-toggles";
 
 export function AvtaleInfo() {
   const { data: avtale, isLoading, error, refetch } = useAvtale();
   const [slettModal, setSlettModal] = useState(false);
   const [avbrytModal, setAvbrytModal] = useState(false);
   const mutation = useDeleteAvtale();
-  const { data: enableOpsjoner } = useFeatureToggle(
-    Toggles.MULIGHETSROMMET_ADMIN_FLATE_OPSJONER_FOR_AVTALER,
-  );
 
   if (!avtale && isLoading) {
     return <Laster tekst="Laster avtaleinformasjon..." />;
@@ -88,10 +84,10 @@ export function AvtaleInfo() {
           <Bolk aria-label="Start- og sluttdato">
             <Metadata header="Startdato" verdi={formaterDato(avtale.startDato)} />
             <Metadata header="Sluttdato" verdi={formaterDato(avtale.sluttDato)} />
-            {enableOpsjoner && avtale.avtaletype === Avtaletype.RAMMEAVTALE ? (
+            {avtale.avtaletype === Avtaletype.RAMMEAVTALE && avtale.sluttDato ? (
               <Metadata
                 header="Maks varighet inkl. opsjon"
-                verdi={formaterDato(addYear(new Date(avtale.startDato), 5))}
+                verdi={formaterDato(addYear(new Date(avtale.sluttDato), 5))}
               />
             ) : null}
           </Bolk>
