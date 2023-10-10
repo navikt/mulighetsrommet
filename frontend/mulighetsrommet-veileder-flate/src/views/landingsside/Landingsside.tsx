3ef0c67bdf80f11498f1d903274a6fc3a2885d39
Sindre Moldeklev <sndrem@gmail.com>
2023-10-10 07:26:52 +0200

    Fjern unødvendig kommentar

--- frontend/mulighetsrommet-veileder-flate/src/views/landingsside/Landingsside.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/views/landingsside/Landingsside.tsx
@@ -1,17 +1,14 @@
 import { PlusIcon } from "@navikt/aksel-icons";
+import { Link } from "react-router-dom";
 import { HistorikkForBrukerModalInnhold } from "../../components/historikk/HistorikkForBrukerModalInnhold";
 import { routes } from "../../routes";
 import styles from "./Landingsside.module.scss";
-import { Link } from "react-router-dom";
 
 export function Landingsside() {
   return (
     <main className="mulighetsrommet-veileder-flate">
       <div className={styles.container}>
         <div>
-          {/**
-           * A-tag her istedenfor Link fra react-router-dom pga. merkelig oppførsel fra Aktivitetsplanen og Dialogen.
-           */}
           <Link className={styles.cta_link} to={`/${routes.oversikt}`}>
             <PlusIcon color="white" fontSize={30} aria-hidden /> Finn nytt arbeidsmarkedstiltak
           </Link>
