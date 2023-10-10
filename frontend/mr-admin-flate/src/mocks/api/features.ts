104daac41c4e98cb48748fafb1e8a4c622b7cded
Sindre Moldeklev <sndrem@gmail.com>
2023-10-10 14:40:26 +0200

    Fjern toggle for redigering av gjennomføring

--- frontend/mr-admin-flate/src/mocks/api/features.ts
+++ frontend/mr-admin-flate/src/mocks/api/features.ts
@@ -5,7 +5,6 @@ export const mockFeatures: Features = {
   "mulighetsrommet.admin-flate-rediger-avtale": true,
   "mulighetsrommet.admin-flate-opprett-tiltaksgjennomforing": true,
   "mulighetsrommet.admin-flate-slett-avtale": true,
-  "mulighetsrommet.admin-flate-rediger-tiltaksgjennomforing": true,
   "mulighetsrommet.admin-flate-slett-tiltaksgjennomforing": true,
   "mulighetsrommet.admin-flate-vis-deltakerliste-fra-komet": false,
   "mulighetsrommet.enable-arbeidsflate": true,
