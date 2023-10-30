c2e5fca46ff2324f03c21ffbdd7f432e2bdfb7f9
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-10-30 07:55:34 +0100

    fix security warnings and remove unused dependency

--- frontend/mulighetsrommet-veileder-flate/vite.config.ts
+++ frontend/mulighetsrommet-veileder-flate/vite.config.ts
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
-import vitePluginRequire from 'vite-plugin-require';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   server: {
     port: 3000,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     open: true,
   },
   plugins: [
     react(),
-    vitePluginRequire(),
     visualizer({
-      filename: 'bundle-stats.html',
+      filename: "bundle-stats.html",
     }),
   ],
   build: {
-    manifest: 'asset-manifest.json',
+    manifest: "asset-manifest.json",
     chunkSizeWarningLimit: 1400,
     sourcemap: true,
   },
