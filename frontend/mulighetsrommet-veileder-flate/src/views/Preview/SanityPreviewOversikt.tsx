2399afc645fa584dfb55cb4f896faf529ba40e46
Sindre Moldeklev <sndrem@gmail.com>
2023-11-03 15:28:58 +0100

    Fjern SokeSelect for oppfolgingsenhet

--- frontend/mulighetsrommet-veileder-flate/src/views/Preview/SanityPreviewOversikt.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/views/Preview/SanityPreviewOversikt.tsx
@@ -12,7 +12,6 @@ import { SokeSelect } from "mulighetsrommet-frontend-common/components/SokeSelec
 
 export const SanityPreviewOversikt = () => {
   const [geografiskEnhet, setGeografiskEnhet] = useState<NavEnhet>();
-  const [oppfolgingsenhet, setOppfolgingsenhet] = useState<NavEnhet>();
   const {
     data: tiltaksgjennomforinger = [],
     isLoading,
@@ -33,57 +32,30 @@ export const SanityPreviewOversikt = () => {
 
   return (
     <>
-      <div style={{ display: "flex", gap: "2rem" }}>
-        <SokeSelect
-          label="Brukers geografiske enhet"
-          description="Simuler en brukers geografiske enhet"
-          value={
-            geografiskEnhet !== undefined
-              ? { label: geografiskEnhet.navn, value: geografiskEnhet.enhetsnummer }
-              : null
-          }
-          name="geografisk-enhet"
-          size="medium"
-          onChange={(e) =>
-            setGeografiskEnhet(
-              enheter.find((enhet: NavEnhet) => enhet.enhetsnummer === e.target.value),
-            )
-          }
-          placeholder="Velg brukers geografiske enhet"
-          options={enheter
-            .filter((enhet: NavEnhet) => enhet.type === NavEnhetType.LOKAL)
-            .map((enhet: NavEnhet) => ({
-              label: enhet.navn,
-              value: enhet.enhetsnummer,
-            }))}
-        />
-        <SokeSelect
-          label="Brukers oppfølgingsenhet"
-          description="Simuler en brukers oppfølgingsenhet"
-          value={
-            oppfolgingsenhet !== undefined
-              ? { label: oppfolgingsenhet.navn, value: oppfolgingsenhet.enhetsnummer }
-              : null
-          }
-          name="oppfolgingsenhet"
-          size="medium"
-          onChange={(e) =>
-            setOppfolgingsenhet(
-              enheter.find((enhet: NavEnhet) => enhet.enhetsnummer === e.target.value),
-            )
-          }
-          placeholder="Velg brukers oppfølgingsenhet"
-          options={enheter
-            .filter(
-              (enhet: NavEnhet) =>
-                enhet.type !== NavEnhetType.LOKAL && enhet.type !== NavEnhetType.FYLKE,
-            )
-            .map((enhet: NavEnhet) => ({
-              label: enhet.navn,
-              value: enhet.enhetsnummer,
-            }))}
-        />
-      </div>
+      <SokeSelect
+        label="Brukers geografiske enhet"
+        description="Simuler en brukers geografiske enhet"
+        value={
+          geografiskEnhet !== undefined
+            ? { label: geografiskEnhet.navn, value: geografiskEnhet.enhetsnummer }
+            : null
+        }
+        name="geografisk-enhet"
+        size="medium"
+        onChange={(e) =>
+          setGeografiskEnhet(
+            enheter.find((enhet: NavEnhet) => enhet.enhetsnummer === e.target.value),
+          )
+        }
+        placeholder="Velg brukers geografiske enhet"
+        options={enheter
+          .filter((enhet: NavEnhet) => enhet.type === NavEnhetType.LOKAL)
+          .map((enhet: NavEnhet) => ({
+            label: enhet.navn,
+            value: enhet.enhetsnummer,
+          }))}
+      />
+
       <Separator />
 
       <div className={styles.tiltakstype_oversikt} data-testid="tiltakstype-oversikt">
