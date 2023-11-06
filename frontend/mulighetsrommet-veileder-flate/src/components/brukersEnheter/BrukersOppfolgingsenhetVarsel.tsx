d2e959b5712f0a93f4d1a514f29ce2d3b2b263b0
Sindre Moldeklev <sndrem@gmail.com>
2023-11-06 15:40:09 +0100

    Endrer tekst etter innspill fra Tonje

--- frontend/mulighetsrommet-veileder-flate/src/components/brukersEnheter/BrukersOppfolgingsenhetVarsel.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/brukersEnheter/BrukersOppfolgingsenhetVarsel.tsx
@@ -1,18 +1,17 @@
 import { Alert } from "@navikt/ds-react";
-import { Bruker, NavEnhetType } from "mulighetsrommet-api-client";
+import { Bruker } from "mulighetsrommet-api-client";
+import { brukersGeografiskeOgOppfolgingsenhetErLokalkontorMenIkkeSammeKontor } from "../../utils/Utils";
 
 interface Props {
   brukerdata: Bruker;
 }
 
 export function BrukersOppfolgingsenhetVarsel({ brukerdata }: Props) {
-  if (
-    brukerdata.oppfolgingsenhet?.type === NavEnhetType.LOKAL &&
-    brukerdata?.oppfolgingsenhet.enhetsnummer !== brukerdata?.geografiskEnhet?.enhetsnummer
-  ) {
+  if (brukersGeografiskeOgOppfolgingsenhetErLokalkontorMenIkkeSammeKontor(brukerdata)) {
     return (
       <Alert variant="info">
-        Brukers oppfølgingsenhet er et lokalkontor som ikke er likt brukers geografiske enhet.
+        Bruker har en annen oppfølgingsenhet enn geografisk enhet. Det er aktuelle tiltak knyttet
+        til brukers oppfølgingsenhet som vises i oversikten.
       </Alert>
     );
   }
