7068114ae0c0ce051105689f3ad3ed6c6cfa0135
Sindre Moldeklev <sndrem@gmail.com>
2023-11-08 07:01:13 +0100

    Sett opp mulighetsrommet-api-client til å kunne eksportere til tester

--- frontend/mr-admin-flate/src/utils/Utils.ts
+++ frontend/mr-admin-flate/src/utils/Utils.ts
@@ -1,7 +1,9 @@
-import { Avtale } from "mulighetsrommet-api-client";
-import { Avtaletype } from "mulighetsrommet-api-client/build/models/Avtaletype";
-import { Tilgjengelighetsstatus } from "mulighetsrommet-api-client/build/models/Tilgjengelighetsstatus";
-import { TiltaksgjennomforingStatus } from "mulighetsrommet-api-client/build/models/TiltaksgjennomforingStatus";
+import {
+  Avtale,
+  Avtaletype,
+  Tilgjengelighetsstatus,
+  TiltaksgjennomforingStatus,
+} from "mulighetsrommet-api-client";
 
 export function capitalize(text?: string): string {
   return text ? text.slice(0, 1).toUpperCase() + text.slice(1, text.length).toLowerCase() : "";
