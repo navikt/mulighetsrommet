e1e6601dc29bd45d5dadf5726356f987f7ac4595
taniaholst <tania.holst@nav.no>
2023-11-14 09:34:03 +0100

    :white_check_mark: Config playwright vitest

--- frontend/mr-admin-flate/playwright.config.ts
+++ frontend/mr-admin-flate/playwright.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
