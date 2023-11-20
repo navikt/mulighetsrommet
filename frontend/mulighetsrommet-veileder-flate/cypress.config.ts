20e74a26dc2a74db353f0d6f7d11996e8d7d5081
Sindre Moldeklev <sndrem@gmail.com>
2023-11-20 13:51:55 +0100

    Test 127.0.0.1 istedenfor localhost

--- frontend/mulighetsrommet-veileder-flate/cypress.config.ts
+++ frontend/mulighetsrommet-veileder-flate/cypress.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'cypress';
-import { plugin } from './cypress/plugins';
+import { defineConfig } from "cypress";
+import { plugin } from "./cypress/plugins";
 
 export default defineConfig({
   viewportHeight: 1300,
@@ -14,6 +14,6 @@ export default defineConfig({
     setupNodeEvents(on) {
       plugin(on);
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://127.0.0.1:3000",
   },
 });
