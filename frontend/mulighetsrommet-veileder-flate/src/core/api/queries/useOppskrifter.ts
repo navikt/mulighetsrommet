0fc38c7e4814c15076234d585ec2c1cd5c5e7645
Sindre Moldeklev <sndrem@gmail.com>
2023-11-22 19:08:54 +0100

    Hvis preview, hent fra preview-perspective

--- frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useOppskrifter.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/api/queries/useOppskrifter.ts
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "../query-keys";
 import { mulighetsrommetClient } from "../clients";
+import { erPreview } from "../../../utils/Utils";
 
 export function useOppskrifter(tiltakstypeId?: string) {
   return useQuery({
     queryKey: QueryKeys.oppskrifter(tiltakstypeId!!),
-    queryFn: () => mulighetsrommetClient.oppskrifter.getOppskrifter({ tiltakstypeId }),
+    queryFn: () =>
+      mulighetsrommetClient.oppskrifter.getOppskrifter({
+        tiltakstypeId,
+        perspective: erPreview() ? "previewDrafts" : "published",
+      }),
     enabled: !!tiltakstypeId,
   });
 }
