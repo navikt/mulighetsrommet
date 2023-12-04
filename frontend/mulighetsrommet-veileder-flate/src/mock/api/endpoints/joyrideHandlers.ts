c452147ad4ef02b257a689e5d255b566116d63a0
Sindre Moldeklev <sndrem@gmail.com>
2023-12-04 10:46:31 +0100

    .text for handler

--- frontend/mulighetsrommet-veileder-flate/src/mock/api/endpoints/joyrideHandlers.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/api/endpoints/joyrideHandlers.ts
@@ -3,6 +3,6 @@ import { VeilederJoyrideRequest } from "mulighetsrommet-api-client";
 
 export const joyrideHandlers = [
   http.post<PathParams, VeilederJoyrideRequest>("*/api/v1/internal/joyride/lagre", async () => {
-    return HttpResponse.json({});
+    return HttpResponse.text("ok");
   }),
 ];
