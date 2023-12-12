dc604111d36cd569172d5d279ec59a08b0624ec3
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-12-12 16:32:25 +0100

    remove old dependency constraints

--- common/ktor/build.gradle.kts
+++ common/ktor/build.gradle.kts
@@ -19,25 +19,12 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.metricsMicrometer)
     implementation(libs.ktor.server.netty)
-    constraints {
-        implementation("io.netty:netty-codec-http2:4.1.101.Final") {
-            because("sikkerhetshull i transitiv avhengighet rapportert via dependabot. kan fjernes etter at vi har versjon av ktor > 2.3.6")
-        }
-    }
     implementation(libs.ktor.server.statusPages)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.authJwt)
 
     // Audit-logging
     implementation(libs.nav.common.auditLog)
-    constraints {
-        implementation(libs.logback.core) {
-            because("sikkerhetshull i transitiv avhengighet rapportert via snyk")
-        }
-        implementation(libs.logback.classic) {
-            because("sikkerhetshull i transitiv avhengighet rapportert via snyk")
-        }
-    }
 
     // Cache
     implementation(libs.caffeine)
