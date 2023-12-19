2ef747dc11409f3cd5d1e9d3d86bdb3dccea5fb0
Sondre Lefsaker <sondre.lefsaker@nav.no>
2023-12-19 18:31:58 +0100

    legg til funksjon for å kopiere innhold fra annen avtale

---
+++ frontend/mr-admin-flate/src/components/avtaler/AvtaleListe.tsx
@@ -0,0 +1,48 @@
+import { Alert, BodyShort } from "@navikt/ds-react";
+import { Laster } from "../laster/Laster";
+import styles from "../tiltaksgjennomforinger/TiltaksgjennomforingerListe.module.scss";
+import { AvtaleFilter } from "../../api/atoms";
+import { Avtale } from "mulighetsrommet-api-client";
+import { ReactNode } from "react";
+import { useAvtaler } from "../../api/avtaler/useAvtaler";
+import { AvtalestatusTag } from "../statuselementer/AvtalestatusTag";
+
+interface Props {
+  filter: Partial<AvtaleFilter>;
+  action: (avtale: Avtale) => ReactNode;
+}
+
+export function AvtaleListe(props: Props) {
+  const { data, isError, isPending } = useAvtaler(props.filter);
+
+  if (isError) {
+    return <Alert variant="error">Vi hadde problemer med å hente avtaler</Alert>;
+  }
+
+  if (isPending) {
+    return <Laster size="xlarge" tekst="Laster avtaler..." />;
+  }
+
+  const avtaler = data.data;
+
+  return (
+    <div className={styles.gjennomforingsliste_container}>
+      <div className={styles.gjennomforingsliste_headers}>
+        <BodyShort>Tittel</BodyShort>
+        <BodyShort>Avtalenummer</BodyShort>
+        <BodyShort>Status</BodyShort>
+      </div>
+
+      <ul className={styles.gjennomforingsliste}>
+        {avtaler.map((avtale) => (
+          <li key={avtale.id} className={styles.gjennomforingsliste_element}>
+            <BodyShort>{avtale.navn}</BodyShort>
+            <BodyShort>{avtale.avtalenummer}</BodyShort>
+            <AvtalestatusTag avtale={avtale} />
+            {props.action(avtale)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ frontend/mr-admin-flate/src/components/avtaler/AvtaleRedaksjoneltInnholdForm.tsx
@@ -0,0 +1,95 @@
+import { Button, Heading, HStack, Modal, Search } from "@navikt/ds-react";
+import { Avtale, EmbeddedTiltakstype } from "mulighetsrommet-api-client";
+import { RedaksjoneltInnholdForm } from "../redaksjonelt-innhold/RedaksjoneltInnholdForm";
+import { useFormContext } from "react-hook-form";
+import { useState } from "react";
+import { RedaksjoneltInnholdContainer } from "../redaksjonelt-innhold/RedaksjoneltInnholdContainer";
+import styles from "../modal/LeggTilGjennomforingModal.module.scss";
+import { AvtaleListe } from "./AvtaleListe";
+import { InferredAvtaleSchema } from "./AvtaleSchema";
+
+interface Props {
+  tiltakstype: EmbeddedTiltakstype;
+}
+
+export function AvtaleRedaksjoneltInnholdForm({ tiltakstype }: Props) {
+  const [key, setKey] = useState(0);
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [search, setSearch] = useState("");
+
+  const { setValue } = useFormContext<InferredAvtaleSchema>();
+
+  function kopierRedaksjoneltInnhold({ beskrivelse, faneinnhold }: Avtale) {
+    setValue("beskrivelse", beskrivelse ?? null);
+    setValue("faneinnhold", faneinnhold ?? null);
+  }
+
+  return (
+    <>
+      <RedaksjoneltInnholdContainer>
+        <HStack justify="end">
+          <Button
+            size="small"
+            variant="tertiary"
+            type="button"
+            title="Kopier redaksjonelt innhold fra en annen avtale under samme tiltakstype"
+            onClick={() => setModalOpen(true)}
+          >
+            Kopier redaksjonelt innhold fra avtale
+          </Button>
+        </HStack>
+      </RedaksjoneltInnholdContainer>
+
+      <RedaksjoneltInnholdForm key={`redaksjonelt-innhold-${key}`} tiltakstype={tiltakstype} />
+
+      <Modal
+        open={modalOpen}
+        onClose={() => {
+          setSearch("");
+          setModalOpen(false);
+        }}
+        className={styles.modal_container}
+        aria-label="modal"
+        width="50rem"
+      >
+        <Modal.Header closeButton>
+          <Heading size="medium">Kopier redaksjonelt innhold fra avtale</Heading>
+        </Modal.Header>
+
+        <Modal.Body className={styles.modal_content}>
+          <Search
+            label="Søk på navn eller avtalenummer"
+            variant="simple"
+            hideLabel={false}
+            autoFocus
+            onChange={(search) => setSearch(search.trim())}
+            value={search}
+          />
+
+          <AvtaleListe
+            filter={{ sok: search, tiltakstyper: [tiltakstype.id] }}
+            action={(avtale) => (
+              <Button
+                size="small"
+                variant="tertiary"
+                type="button"
+                onClick={() => {
+                  kopierRedaksjoneltInnhold(avtale);
+
+                  // Ved å endre `key` så tvinger vi en update av den underliggende Slate-komponenten slik at
+                  // innhold i komponenten blir resatt til å reflektere den nye tilstanden i skjemaet
+                  setKey(key + 1);
+
+                  setModalOpen(false);
+                }}
+              >
+                Kopier innhold
+              </Button>
+            )}
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
