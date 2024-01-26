5e2e8e4a799f66b9c47f22cfba15a3fbc1812dad
Sindre Moldeklev <sndrem@gmail.com>
2024-01-26 09:33:09 +0100

    Fjern sjekk for upsert av tiltaksgjennomføring

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/TiltaksgjennomforingRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/TiltaksgjennomforingRoutes.kt
@@ -1,10 +1,8 @@
 package no.nav.mulighetsrommet.api.routes.v1
 
-import arrow.core.Either
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -18,7 +16,6 @@ import no.nav.mulighetsrommet.api.plugins.getNavIdent
 import no.nav.mulighetsrommet.api.repositories.DeltakerRepository
 import no.nav.mulighetsrommet.api.repositories.TiltakstypeRepository
 import no.nav.mulighetsrommet.api.routes.v1.responses.BadRequest
-import no.nav.mulighetsrommet.api.routes.v1.responses.ValidationError
 import no.nav.mulighetsrommet.api.routes.v1.responses.respondWithStatusResponse
 import no.nav.mulighetsrommet.api.services.TiltaksgjennomforingService
 import no.nav.mulighetsrommet.api.utils.getAdminTiltaksgjennomforingsFilter
@@ -46,7 +43,7 @@ fun Route.tiltaksgjennomforingRoutes(appConfig: AppConfig) {
                 val request = call.receive<TiltaksgjennomforingRequest>()
                 val navIdent = getNavIdent()
 
-                // TODO Fjern tiltakstypesjekk når vi har blitt master for alle tiltakstyper
+               /* // TODO Fjern tiltakstypesjekk når vi har blitt master for alle tiltakstyper
                 val tiltakstype = tiltakstyper.get(request.tiltakstypeId)
                     ?: throw BadRequestException("Fant ikke tiltakstype med id: ${request.tiltakstypeId}")
 
@@ -63,7 +60,7 @@ fun Route.tiltaksgjennomforingRoutes(appConfig: AppConfig) {
                             ),
                         ),
                     )
-                }
+                }*/
 
                 val result = service.upsert(request, navIdent)
                     .mapLeft { BadRequest(errors = it) }
