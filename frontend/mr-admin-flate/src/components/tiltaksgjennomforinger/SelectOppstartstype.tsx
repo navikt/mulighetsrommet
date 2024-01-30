4c063f139de95e346c767d52ba8a02aacb039c58
Sindre Moldeklev <sndrem@gmail.com>
2024-01-30 15:30:27 +0100

    Helptekst for varsel om endring av oppstartstype

--- frontend/mr-admin-flate/src/components/tiltaksgjennomforinger/SelectOppstartstype.tsx
+++ frontend/mr-admin-flate/src/components/tiltaksgjennomforinger/SelectOppstartstype.tsx
@@ -1,4 +1,4 @@
-import { Alert } from "@navikt/ds-react";
+import { Alert, HelpText } from "@navikt/ds-react";
 import { TiltaksgjennomforingOppstartstype } from "mulighetsrommet-api-client";
 import { ControlledSokeSelect } from "mulighetsrommet-frontend-common";
 import { useController } from "react-hook-form";
@@ -59,7 +59,31 @@ function OppstartstypeWarning({ gjennomforingId }: OppstartstypePropsWarning) {
   return isError || summary.antallDeltakere > 0 ? (
     <Alert variant="warning">
       Deltakerstatus påvirkes av oppstartstypen. Hvis du endrer oppstartstypen så kan deltakelser
-      som er avsluttet få en ny status. Statusen vises i aktivitetsplanen og deltakeroversikten.
+      som er avsluttet få en ny status. Statusen vises i aktivitetsplanen og deltakeroversikten.{" "}
+      <HelpText title="Hvilke konsekvenser får dette?">
+        <ul>
+          <li>
+            På tiltak med <code>løpende inntak</code> vil avsluttede deltakere ha statusen &quot;Har
+            sluttet&quot; i aktivitetsplanen og Deltakeroversikten. Dersom gjennomføringen blir
+            endret til <code>felles oppstart</code> så vil statusen endres til enten
+            &quot;Fullført&quot; eller &quot;Avbrutt&quot;. Aktivitetskortet i aktivitetsplanen til
+            bruker vil flyttes til statuskolonnen &quot;Avbrutt&quot; dersom statusen på
+            tiltaksdeltakelsen endres til dette.
+          </li>
+          <li>
+            På tiltak med <code>felles oppstart</code> vil avsluttede deltakere ha status
+            &quot;Fullført&quot; eller &quot;Avbrutt&quot;. Dersom gjennomføringen blir endret til{" "}
+            <code>løpende inntak</code> så vil disse statusene blir endret til &quot;Har
+            sluttet&quot; (og aktivitetskortet i aktivitetsplanen vil ligge i statuskolonnen
+            &quot;Fullført&quot;).
+          </li>
+          <li>
+            Dersom statusen endres vil det være synlig i aktivitetsplanen at det er en endring på
+            det aktuelle aktivitetskortet (en visuell markering i form av en blå prikk som betyr at
+            det er en endring siden sist du var inne på planen)
+          </li>
+        </ul>
+      </HelpText>
     </Alert>
   ) : null;
 }
