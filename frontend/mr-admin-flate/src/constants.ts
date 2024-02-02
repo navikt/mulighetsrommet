71013c9dce7e54591997e4edea221fce1153aee4
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-02-02 11:29:55 +0100

    fjern ubrukt kode

--- frontend/mr-admin-flate/src/constants.ts
+++ frontend/mr-admin-flate/src/constants.ts
@@ -1,18 +1,5 @@
-import { Shortcut } from "./components/navbar/Navbar";
-
 export const APPLICATION_NAME = "mr-admin-flate";
 
 export const PAGE_SIZE = 15;
 export const AVTALE_PAGE_SIZE = 15;
 
-export const shortcuts: Shortcut[] = [
-  {
-    url: "/tiltakstyper",
-    navn: "Tiltakstyper",
-  },
-  { url: "/avtaler", navn: "Avtaler" },
-  {
-    url: "/tiltaksgjennomforinger",
-    navn: "Tiltaksgjennomføringer",
-  },
-];
