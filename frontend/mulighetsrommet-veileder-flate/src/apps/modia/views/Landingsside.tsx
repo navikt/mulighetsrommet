650780ebb3ee0d916e14c333487b70a7f5a0fd97
Sindre Moldeklev <sndrem@gmail.com>
2024-02-16 09:35:40 +0100

    Startet på visning av historikk som kort på landingsside

--- frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/Landingsside.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/Landingsside.tsx
@@ -1,25 +1,40 @@
-import { Heading } from "@navikt/ds-react";
 import { PlusIcon } from "@navikt/aksel-icons";
+import { Heading, VStack } from "@navikt/ds-react";
 import { Link } from "react-router-dom";
-import { HistorikkForBrukerModalInnhold } from "@/apps/modia/historikk/HistorikkForBrukerModalInnhold";
 import styles from "./Landingsside.module.scss";
+import { HistorikkKort } from "../historikk/HistorikkKort";
+import { useHistorikkFraKomet } from "../../../core/api/queries/useHistorikkFraKomet";
 
 export function Landingsside() {
   return (
     <main className="mulighetsrommet-veileder-flate">
-      <div className={styles.container}>
+      <VStack gap="10" className={styles.container}>
         <div>
           <Link className={styles.cta_link} to="/arbeidsmarkedstiltak/oversikt">
             <PlusIcon color="white" fontSize={30} aria-hidden /> Finn nytt arbeidsmarkedstiltak
           </Link>
         </div>
-        <div>
-          <Heading level="3" size="medium">
-            Tiltakshistorikk
-          </Heading>
-          <HistorikkForBrukerModalInnhold />
-        </div>
-      </div>
+        <Historikk />
+      </VStack>
     </main>
+  );
+}
+
+function Historikk() {
+  const { data: historikk } = useHistorikkFraKomet();
+
+  if (!historikk) {
+    return null;
+  }
+
+  return (
+    <VStack gap="5">
+      <Heading level="3" size="medium">
+        Historikk
+      </Heading>
+      {historikk.map((hist) => {
+        return <HistorikkKort key={hist.id} historikk={hist} />;
+      })}
+    </VStack>
   );
 }
