bae1b9208ea7c3afeb5e6fd36dddf1ac0d40f52f
Sindre Moldeklev <sndrem@gmail.com>
2024-02-20 16:00:34 +0100

    Sjekk om brukeren finnes

--- mulighetsrommet-api/src/main/resources/db/migration/R__datastream.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/R__datastream.sql
@@ -1,9 +1,26 @@
-ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
-GRANT SELECT ON tiltakstype TO "datastream";
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'mulighetsrommet-api')
+        THEN
+            ALTER USER "mulighetsrommet-api" WITH REPLICATION;
+        END IF;
+    END
+$$;
 
-ALTER USER "mulighetsrommet-api" WITH REPLICATION;
-ALTER USER "datastream" WITH REPLICATION;
-CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype;
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'datastream')
+        THEN
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+            GRANT SELECT ON tiltakstype TO "datastream";
 
-
+            ALTER USER "datastream" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype;
+            SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$;
