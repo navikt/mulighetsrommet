9ebdf126dfe36c07c7d49b5cc30b5d726062f42a
Sindre Moldeklev <sndrem@gmail.com>
2024-02-20 15:39:37 +0100

    Ingen view, bare tiltakstype-tabell

--- mulighetsrommet-api/src/main/resources/db/migration/R__datastream.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/R__datastream.sql
@@ -1,12 +1,9 @@
-create or replace view tiltakstype_bigquery_view as
-select navn
-from tiltakstype;
-
 ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
-GRANT SELECT ON tiltakstype_bigquery_view TO "datastream";
+GRANT SELECT ON tiltakstype TO "datastream";
 
 ALTER USER "mulighetsrommet-api" WITH REPLICATION;
 ALTER USER "datastream" WITH REPLICATION;
-CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype_bigquery_view;
+CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype;
 SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+
 
