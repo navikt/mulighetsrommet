ddd610109362bc045e0e67f4ccb120977d2ecd06
Sindre Moldeklev <sndrem@gmail.com>
2024-02-21 10:58:52 +0100

    Opprett replication hvis den ikke eksisterer

--- mulighetsrommet-api/src/main/resources/db/migration/V122__datastream.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V122__datastream.sql
@@ -1,8 +1,8 @@
 DO
 $$
     BEGIN
-        IF EXISTS
-                (SELECT 1 from pg_roles where rolname = 'datastream')
+        IF NOT EXISTS
+                (SELECT 1 from pg_replication_slots where slot_name = 'ds_replication')
         THEN
             SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
         END IF;
