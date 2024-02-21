bc210d645909c9a754242973393b9e029f642a11
Sindre Moldeklev <sndrem@gmail.com>
2024-02-21 10:28:39 +0100

    Datastream-oppsett

--- mulighetsrommet-api/src/main/resources/db/migration/V121__datastream.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V121__datastream.sql
@@ -1,1 +1,11 @@
-CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype;
+DO
+$$
+    BEGIN
+        IF EXISTS
+                (SELECT 1 from pg_roles where rolname = 'datastream')
+        THEN
+            CREATE PUBLICATION "ds_publication" FOR TABLE tiltakstype;
+        END IF;
+    END
+$$;
+
