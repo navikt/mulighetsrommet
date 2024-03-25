a4e48d4e023599779dab8fd38167891c96238926
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-03-25 08:38:44 +0100

    Legg på versjonering av sessionStorage

--- frontend/mulighetsrommet-veileder-flate/src/core/atoms/atoms.ts
+++ frontend/mulighetsrommet-veileder-flate/src/core/atoms/atoms.ts
@@ -1,6 +1,13 @@
 import { atom } from "jotai";
 import { atomWithHash } from "jotai-location";
 
+// Bump version number when sessionStorage should be cleared
+const version = sessionStorage.getItem("version");
+if (version !== "0.1") {
+  sessionStorage.clear();
+  sessionStorage.setItem("version", "0.1");
+}
+
 export const paginationAtom = atomWithHash(
   "pagination",
   { page: 1, pageSize: 50 },
