bb96f382677c824d9e369d25d414aa58ac3ac3aa
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-03-26 12:11:56 +0100

    standardiser på hvordan url'er utledes basert på miljø

--- frontend/mr-admin-flate/src/logging/amplitude.ts
+++ frontend/mr-admin-flate/src/logging/amplitude.ts
@@ -1,6 +1,6 @@
 import * as amplitude from "@amplitude/analytics-browser";
-import { getEnvironment } from "../api/getEnvironment";
 import { Event } from "./taxonomy";
+import { Environment, environment } from "../environment";
 
 type LogEvent = (params: { name: string; data?: any }) => void;
 
@@ -8,18 +8,18 @@ type LogEvent = (params: { name: string; data?: any }) => void;
 let amplitudeLogger: LogEvent | undefined = undefined;
 
 function getApiKeyFromEnvironment() {
-  switch (getEnvironment()) {
-    case "production":
+  switch (environment) {
+    case Environment.PROD:
       return "94b44c9f436db8d9d42b493fdd7d98c9";
-    case "development":
+    case Environment.DEV:
       return "42076036673d4b35d862b4282840f30b";
-    case "local":
+    case Environment.LOCAL:
       return "mock";
   }
 }
 
 function initializeAmplitude() {
-  if (getEnvironment() === "local") {
+  if (environment === Environment.LOCAL) {
     amplitudeLogger = (params: { name: string; data?: any }) => {
       // eslint-disable-next-line no-console
       console.log("[Mock Amplitude Event]", {
