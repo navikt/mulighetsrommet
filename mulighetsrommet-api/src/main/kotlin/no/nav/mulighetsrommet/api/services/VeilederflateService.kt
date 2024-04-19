7e0b8ae7fe445157a89ea5228286b367ae7e1513
Sindre Moldeklev <sndrem@gmail.com>
2024-04-19 13:43:44 +0200

    personvernBekreftet = false for individuelle tiltak

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/VeilederflateService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/services/VeilederflateService.kt
@@ -330,7 +330,7 @@ class VeilederflateService(
                     varsler = emptyList(),
                     tiltaksansvarlige = kontaktpersoner,
                 ),
-                personvernBekreftet = true,
+                personvernBekreftet = false, // Individuelle tiltak har ikke informasjon om personvern
             )
         }
     }
