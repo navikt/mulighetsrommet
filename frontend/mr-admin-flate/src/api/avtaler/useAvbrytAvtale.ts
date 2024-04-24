2d98c076387b46a75e6c1eac391f795540c9c0d3
taniaholst <tania.holst@nav.no>
2024-04-24 12:36:00 +0200

    endret request

--- frontend/mr-admin-flate/src/api/avtaler/useAvbrytAvtale.ts
+++ frontend/mr-admin-flate/src/api/avtaler/useAvbrytAvtale.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { mulighetsrommetClient } from "@/api/client";
-import { ApiError, AvbrytGjennomforingRequest } from "mulighetsrommet-api-client";
+import { ApiError, AvbrytAvtaleRequest } from "mulighetsrommet-api-client";
 import { QueryKeys } from "@/api/QueryKeys";
 
 export function useAvbrytAvtale() {
@@ -9,12 +9,9 @@ export function useAvbrytAvtale() {
   return useMutation<
     unknown,
     ApiError,
-    { id: string; aarsak: AvbrytGjennomforingRequest.aarsak | string | null }
+    { id: string; aarsak: AvbrytAvtaleRequest.aarsak | string | null }
   >({
-    mutationFn: (data: {
-      id: string;
-      aarsak?: AvbrytGjennomforingRequest.aarsak | string | null;
-    }) => {
+    mutationFn: (data: { id: string; aarsak?: AvbrytAvtaleRequest.aarsak | string | null }) => {
       return mulighetsrommetClient.avtaler.avbrytAvtale({
         id: data.id,
         requestBody: { aarsak: data.aarsak },
