ce59b6f519a7ce27ba451b86c766de4cf81c5f26
taniaholst <tania.holst@nav.no>
2024-05-03 09:21:12 +0200

    la til errormelding for når beskrivelsen er mer enn 100 tegn på avbryt avtale og gjennomføring

--- frontend/mr-admin-flate/src/components/modal/AvbrytModalError.tsx
+++ frontend/mr-admin-flate/src/components/modal/AvbrytModalError.tsx
@@ -7,14 +7,16 @@ interface Props {
   customAarsak: string | null;
 }
 export function AvbrytModalError({ mutation, aarsak, customAarsak }: Props) {
+  function beskrivelser() {
+    if (aarsak === "annet" && !customAarsak)
+      return "Beskrivelse er obligatorisk når “Annet” er valgt som årsak";
+    else if (customAarsak!.length > 100) return "Beskrivelse kan ikke inneholde mer enn 100 tegn";
+    else return resolveErrorMessage(mutation.error);
+  }
+
   return (
     <div className={styles.error}>
-      <b>
-        •{" "}
-        {aarsak === "annet" && !customAarsak
-          ? "Beskrivelse er obligatorisk når “Annet” er valgt som årsak"
-          : resolveErrorMessage(mutation.error)}
-      </b>
+      <b>• {beskrivelser()}</b>
     </div>
   );
 }
