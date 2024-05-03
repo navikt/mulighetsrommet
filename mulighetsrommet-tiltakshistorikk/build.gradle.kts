9a934a8a331638f6e8fbcbea2fd566edf8853862
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-05-03 14:33:45 +0200

    samle log-avhengigheter

--- mulighetsrommet-tiltakshistorikk/build.gradle.kts
+++ mulighetsrommet-tiltakshistorikk/build.gradle.kts
@@ -15,17 +15,20 @@ tasks.shadowJar {
 }
 
 dependencies {
-    implementation(projects.common.domain)
     implementation(projects.common.database)
     testImplementation(testFixtures(projects.common.database))
     implementation(projects.common.ktor)
     testImplementation(testFixtures(projects.common.ktor))
-    implementation(projects.common.ktorClients)
     implementation(projects.common.metrics)
 
     // Kotlin
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.arrow.core)
+    implementation(libs.arrow.core.serialization)
+
+    // Logging
+    implementation(libs.bundles.logging)
 
     // Ktor
     testImplementation(libs.ktor.client.mock)
@@ -54,9 +57,4 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.assertj.db)
     testImplementation(libs.nav.mockOauth2Server)
-
-    // Logging
-    implementation(libs.logback.classic)
-    implementation(libs.logback.logstashLogbackEncoder)
-    implementation(libs.slf4j)
 }
