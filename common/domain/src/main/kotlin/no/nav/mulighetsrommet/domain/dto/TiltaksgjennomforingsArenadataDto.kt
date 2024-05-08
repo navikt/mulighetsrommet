9099c81ff2c2ccf83f2daefc9601d11f33911e2c
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-05-08 14:07:36 +0200

    bare returner tiltaksnummer fra arenadata-endepunkt

--- common/domain/src/main/kotlin/no/nav/mulighetsrommet/domain/dto/TiltaksgjennomforingsArenadataDto.kt
+++ common/domain/src/main/kotlin/no/nav/mulighetsrommet/domain/dto/TiltaksgjennomforingsArenadataDto.kt
@@ -4,9 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TiltaksgjennomforingsArenadataDto(
-    val opprettetAar: Int?,
-    val lopenr: Int?,
-    val virksomhetsnummer: String?,
-    val ansvarligNavEnhetId: String?,
-    val status: String,
+    val opprettetAar: Int,
+    val lopenr: Int,
 )
