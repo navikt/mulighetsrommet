f8a506dfe5f68d69ad904a97f63c7433743ef46c
Sindre Moldeklev <9053627+sndrem@users.noreply.github.com>
2024-05-14 09:06:26 +0200

    Fiks skriveleif

--- common/domain/src/main/kotlin/no/nav/mulighetsrommet/domain/dto/Innsatsgruppe.kt
+++ common/domain/src/main/kotlin/no/nav/mulighetsrommet/domain/dto/Innsatsgruppe.kt
@@ -2,7 +2,7 @@ package no.nav.mulighetsrommet.domain.dto
 
 enum class Innsatsgruppe(val tittel: String, val order: Int) {
     STANDARD_INNSATS("Standard innsats", 0),
-    SITUASJONSBESTEMT_INNSATS("Situasjonsbestem innsats", 1),
+    SITUASJONSBESTEMT_INNSATS("Situasjonsbestemt innsats", 1),
     SPESIELT_TILPASSET_INNSATS("Spesielt tilpasset innsats", 2),
     GRADERT_VARIG_TILPASSET_INNSATS("Gradert varig tilpasset innsats", 3),
     VARIG_TILPASSET_INNSATS("Varig tilpasset innsats", 4),
