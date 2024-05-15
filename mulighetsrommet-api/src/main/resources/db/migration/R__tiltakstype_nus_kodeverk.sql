3f12a4a53e023557c1b6bac96d38224dc7c8089c
Sindre Moldeklev <sndrem@gmail.com>
2024-05-15 10:22:35 +0200

    Tester for nus-data og uthenting av data

--- mulighetsrommet-api/src/main/resources/db/migration/R__tiltakstype_nus_kodeverk.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/R__tiltakstype_nus_kodeverk.sql
@@ -18,8 +18,10 @@ from (values ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '30', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '37',
               '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '38', '2437'),
+             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '355', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '3551', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '3552', '2437'),
+             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '357', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '3571', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '40', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '41', '2437'),
@@ -35,9 +37,13 @@ from (values ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '30', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '47',
               '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '48', '2437'),
+             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '455', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '4551', '2437'),
              ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '4552', '2437'),
-             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '4571', '2437')) as t(tiltakskode, code, version)
+             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '457', '2437'),
+             ('GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode, '4571', '2437')
+      ) as t(tiltakskode, code, version)
+-- Sjekker om tiltakstypen eksisterer så ikke testene brekker når vi kjører de
 where exists(select 1 from tiltakstype where tiltakskode = 'GRUPPE_FAG_OG_YRKESOPPLAERING'::tiltakskode)
 on conflict (tiltakskode, code, version) do nothing;
 
