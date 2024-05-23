41ae1bf83d43b08d66d825c61e5324cd53515b84
Sindre Moldeklev <sndrem@gmail.com>
2024-05-23 10:12:10 +0200

    Skru av landingsside lokalt

--- frontend/mulighetsrommet-veileder-flate/src/mock/api/features.ts
+++ frontend/mulighetsrommet-veileder-flate/src/mock/api/features.ts
@@ -2,7 +2,7 @@ import { Features } from "@/api/feature-toggles";
 
 export const mockFeatures: Features = {
   "mulighetsrommet.veilederflate.visDeltakerRegistrering": true,
-  "mulighetsrommet-veilederflate-landingsside": true,
+  "mulighetsrommet-veilederflate-landingsside": false,
   "mulighetsrommet-veilederflate-arena-oppskrifter": true,
   "mulighetsrommet.admin-flate.enableDebugger": false,
   "mulighetsrommet.admin-flate.tilgjengeliggjore-tiltak-for-arrangor": false,
