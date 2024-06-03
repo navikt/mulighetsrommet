473e496d55d3c290f16df17b8779e926040cf1f2
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-06-03 07:10:42 +0200

    Endepunkt for å hente historikk

---
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/domain/dto/TiltakshistorikkAdminDto.kt
@@ -0,0 +1,28 @@
+package no.nav.mulighetsrommet.api.domain.dto
+
+import kotlinx.serialization.Serializable
+import no.nav.mulighetsrommet.domain.dbo.Deltakerstatus
+import no.nav.mulighetsrommet.domain.serializers.LocalDateTimeSerializer
+import no.nav.mulighetsrommet.domain.serializers.UUIDSerializer
+import java.time.LocalDateTime
+import java.util.*
+
+@Serializable
+data class TiltakshistorikkAdminDto(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val fraDato: LocalDateTime? = null,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val tilDato: LocalDateTime? = null,
+    val status: Deltakerstatus,
+    val tiltaksnavn: String?,
+    val tiltakstype: String,
+    val arrangor: Arrangor?,
+) {
+    @Serializable
+    data class Arrangor(
+        val organisasjonsnummer: String,
+        val navn: String?,
+    )
+}
