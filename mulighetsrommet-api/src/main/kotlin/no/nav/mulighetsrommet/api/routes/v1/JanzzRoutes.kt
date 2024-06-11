e822a4cabe3cd72553a3789ff0cabc874bd02b9c
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-06-11 13:20:37 +0200

    Kjør spørringene til pam i parallell

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/JanzzRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/JanzzRoutes.kt
@@ -4,6 +4,8 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import no.nav.mulighetsrommet.api.clients.AccessType
 import no.nav.mulighetsrommet.api.clients.pamOntologi.PamOntologiClient
 import no.nav.mulighetsrommet.domain.dto.AmoKategorisering
@@ -17,15 +19,21 @@ fun Route.janzzRoutes() {
         get("sertifiseringer/sok") {
             val q: String by call.request.queryParameters
             val obo = AccessType.OBO(call.getAccessToken())
-            call.respond(
-                (pam.sokAutorisasjon(q, obo) + pam.sokAndreGodkjenninger(q, obo))
-                    .map {
+
+            val autoriseringer = async { pam.sokAutorisasjon(q, obo) }
+            val andreGodkjenninger = async { pam.sokAndreGodkjenninger(q, obo) }
+
+            val sertifiseringer = awaitAll(autoriseringer, andreGodkjenninger)
+                .flatMap { typeaheads ->
+                    typeaheads.map {
                         AmoKategorisering.Sertifisering(
                             konseptId = it.konseptId,
                             label = it.label,
                         )
-                    },
-            )
+                    }
+                }
+
+            call.respond(sertifiseringer)
         }
     }
 }
