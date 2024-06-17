04dfa3adb441d8d1522658152d40410bbb808360
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-06-17 14:49:50 +0200

    flytt ekstern tiltakstype-dto til domain-modul

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/kafka/producers/TiltakstypeKafkaProducer.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/kafka/producers/TiltakstypeKafkaProducer.kt
@@ -3,7 +3,7 @@ package no.nav.mulighetsrommet.kafka.producers
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import no.nav.common.kafka.producer.KafkaProducerClient
-import no.nav.mulighetsrommet.api.domain.dto.TiltakstypeEksternDto
+import no.nav.mulighetsrommet.domain.dto.TiltakstypeV2Dto
 import org.apache.kafka.clients.producer.ProducerRecord
 import java.util.*
 
@@ -15,7 +15,7 @@ class TiltakstypeKafkaProducer(
         val topic: String,
     )
 
-    fun publish(value: TiltakstypeEksternDto) {
+    fun publish(value: TiltakstypeV2Dto) {
         val record: ProducerRecord<String, String?> = ProducerRecord(
             config.topic,
             value.id.toString(),
