f8e8071fee45c74641eed94c673ec693f5c7d1cb
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-06-20 10:40:24 +0200

    Bruk machineToMachine token til pam-ontologi

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/JanzzRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/v1/JanzzRoutes.kt
@@ -6,10 +6,8 @@ import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
-import no.nav.mulighetsrommet.api.clients.AccessType
 import no.nav.mulighetsrommet.api.clients.pamOntologi.PamOntologiClient
 import no.nav.mulighetsrommet.domain.dto.AmoKategorisering
-import no.nav.mulighetsrommet.ktor.extensions.getAccessToken
 import org.koin.ktor.ext.inject
 
 fun Route.janzzRoutes() {
@@ -18,10 +16,9 @@ fun Route.janzzRoutes() {
     route("/api/v1/intern/janzz") {
         get("sertifiseringer/sok") {
             val q: String by call.request.queryParameters
-            val obo = AccessType.OBO(call.getAccessToken())
 
-            val autoriseringer = async { pam.sokAutorisasjon(q, obo) }
-            val andreGodkjenninger = async { pam.sokAndreGodkjenninger(q, obo) }
+            val autoriseringer = async { pam.sokAutorisasjon(q) }
+            val andreGodkjenninger = async { pam.sokAndreGodkjenninger(q) }
 
             val sertifiseringer = awaitAll(autoriseringer, andreGodkjenninger)
                 .flatMap { typeaheads ->
