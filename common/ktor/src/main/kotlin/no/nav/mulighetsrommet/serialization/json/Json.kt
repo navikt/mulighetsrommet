b546665c6a20a4e2fa05c9e90e2099ac36ee0f67
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-06-25 14:23:14 +0200

    don't require excplicit nulls when decoding kafka message

--- common/ktor/src/main/kotlin/no/nav/mulighetsrommet/serialization/json/Json.kt
+++ common/ktor/src/main/kotlin/no/nav/mulighetsrommet/serialization/json/Json.kt
@@ -1,8 +1,16 @@
 package no.nav.mulighetsrommet.serialization.json
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
 val JsonIgnoreUnknownKeys = Json {
     ignoreUnknownKeys = true
     encodeDefaults = true
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+val JsonRelaxExplicitNulls = Json {
+    ignoreUnknownKeys = true
+    encodeDefaults = true
+    explicitNulls = false
 }
