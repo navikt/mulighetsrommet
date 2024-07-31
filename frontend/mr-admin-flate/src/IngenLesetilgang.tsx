5ece66f664cb79334390b712f2bf7f898727b91e
Sindre Moldeklev <sndrem@gmail.com>
2024-07-31 08:41:18 +0200

    Hvis ingen lesetilgang -> Gi beskjed

---
+++ frontend/mr-admin-flate/src/IngenLesetilgang.tsx
@@ -0,0 +1,18 @@
+import { BodyShort, Heading } from "@navikt/ds-react";
+import styles from "./IngenLesetilgang.module.scss";
+
+export function IngenLesetilgang() {
+  return (
+    <main className={styles.main}>
+      <Heading size="medium">Ingen lesetilgang</Heading>
+      <BodyShort spacing>Din bruker mangler tilgang til NAV Tiltaksadministrasjon</BodyShort>
+      <BodyShort spacing>
+        Gå til{" "}
+        <a href="https://navno.sharepoint.com/:fl:/g/contentstorage/CSP_11960ac4-f590-409d-8872-73a98cf165b4/EawaFw1m8WNOqXUHldvs2isBUAaAZzfSGWqwJ3UKLKcvzw?e=MMEdPv&nav=cz0lMkZjb250ZW50c3RvcmFnZSUyRkNTUF8xMTk2MGFjNC1mNTkwLTQwOWQtODg3Mi03M2E5OGNmMTY1YjQmZD1iJTIxeEFxV0VaRDFuVUNJY25PcGpQRmx0RkhKaW55M1VyNUtybS0wWWZEVlN0d3J4WHhVczNFMlNLMHRRRVYxTTZ4SSZmPTAxN0RCNEwyRk1ESUxRMlpYUk1OSEtTNUlIU1hONlpXUkwmYz0lMkYmYT1Mb29wQXBwJnA9JTQwZmx1aWR4JTJGbG9vcC1wYWdlLWNvbnRhaW5lciZ4PSU3QiUyMnclMjIlM0ElMjJUMFJUVUh4dVlYWnVieTV6YUdGeVpYQnZhVzUwTG1OdmJYeGlJWGhCY1ZkRldrUXhibFZEU1dOdVQzQnFVRVpzZEVaSVNtbHVlVE5WY2pWTGNtMHRNRmxtUkZaVGRIZHllRmg0VlhNelJUSlRTekIwVVVWV01VMDJlRWw4TURFM1JFSTBUREpDU3pkVU0xRklSa3hGVGtKSVRFNUpOMHMzVFZKYVZrWldTdyUzRCUzRCUyMiUyQyUyMmklMjIlM0ElMjIxM2Y1MGZlYi0wODAwLTRhMTQtOTkxZS05YWM3MDY0NWFjZmUlMjIlN0Q%3D">
+          Tilganger
+        </a>
+        -siden for å se hvordan du bestiller korrekte tilganger.
+      </BodyShort>
+    </main>
+  );
+}
