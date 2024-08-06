085197c6557eac25c63a04f803fcc59c005b8328
Sindre Moldeklev <sndrem@gmail.com>
2024-08-06 08:36:23 +0200

    Uthenting av tilsagn som eget view

---
+++ mulighetsrommet-api/src/main/resources/db/migration/R__tilsagn_admin_view.sql
@@ -0,0 +1,24 @@
+drop view if exists tilsagn_admin_dto_view;
+
+create view tilsagn_admin_dto_view as
+select
+    tilsagn.id,
+    tilsagn.tiltaksgjennomforing_id,
+    tilsagn.periode_start,
+    tilsagn.periode_slutt,
+    tilsagn.belop,
+    tilsagn.sendt_tidspunkt,
+    tilsagn.annullert_tidspunkt,
+    tilsagn.lopenummer,
+    tilsagn.kostnadssted,
+    nav_enhet.navn              as kostnadssted_navn,
+    nav_enhet.overordnet_enhet  as kostnadssted_overordnet_enhet,
+    nav_enhet.type              as kostnadssted_type,
+    nav_enhet.status            as kostnadssted_status,
+    arrangor.id                         as arrangor_id,
+    arrangor.organisasjonsnummer        as arrangor_organisasjonsnummer,
+    arrangor.navn                       as arrangor_navn,
+    arrangor.slettet_dato is not null   as arrangor_slettet
+from tilsagn
+         inner join nav_enhet on nav_enhet.enhetsnummer = tilsagn.kostnadssted
+         inner join arrangor on arrangor.id = tilsagn.arrangor_id
