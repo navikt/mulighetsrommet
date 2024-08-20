f7ca10a9a4feab370f64b6729f054efda3698f9c
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-08-20 13:35:49 +0200

    Amo kategorisering justeringer
     - Flere bransjer
     - Bransje helptext
     - Norskprøve title

--- frontend/frontend-common/components/label/LabelWithHelpText.tsx
+++ frontend/frontend-common/components/label/LabelWithHelpText.tsx
@@ -4,15 +4,16 @@ import { HelpText, HStack } from "@navikt/ds-react";
 export interface LabelWithHelpTextProps {
   label: string;
   helpTextTitle: string;
+  placement?: "top" | "bottom" | "right" | "left" | "top-start" | "top-end" | "bottom-start" | "bottom-end" | "right-start" | "right-end" | "left-start" | "left-end";
   children: ReactNode;
 }
 
 export function LabelWithHelpText(props: LabelWithHelpTextProps) {
-  const { label, helpTextTitle, children } = props;
+  const { label, helpTextTitle, placement = "top", children } = props;
   return (
     <HStack gap="1">
       <legend>{label}</legend>
-      <HelpText style={{ marginTop: "-4px", }} title={helpTextTitle}>{children}</HelpText>
+      <HelpText placement={placement} style={{ marginTop: "-4px", }} title={helpTextTitle}>{children}</HelpText>
     </HStack>
   );
 }
