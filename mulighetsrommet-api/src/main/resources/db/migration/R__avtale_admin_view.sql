2dfd803952e8323a343726daf96adf8c84a3b594
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-09-03 14:10:09 +0200

    skriv om til lateral joins pga bedre ytelse

--- mulighetsrommet-api/src/main/resources/db/migration/R__avtale_admin_view.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/R__avtale_admin_view.sql
@@ -1,97 +1,13 @@
 drop view if exists avtale_admin_dto_view;
 
 create view avtale_admin_dto_view as
-with personopplysninger as (select avtale_id,
-                                   jsonb_agg(personopplysning) as personopplysninger_json
-                            from avtale_personopplysning
-                            group by avtale_id),
-     administratorer as (select avtale_id,
-                                jsonb_agg(
-                                        jsonb_build_object(
-                                                'navIdent', avtale_administrator.nav_ident,
-                                                'navn', concat(nav_ansatt.fornavn, ' ', nav_ansatt.etternavn)
-                                        )
-                                ) as administratorer_json
-                         from avtale_administrator
-                                  join nav_ansatt on nav_ansatt.nav_ident = avtale_administrator.nav_ident
-                         group by avtale_id),
-     nav_enheter as (select avtale_id,
-                            jsonb_agg(
-                                    jsonb_build_object(
-                                            'enhetsnummer', avtale_nav_enhet.enhetsnummer,
-                                            'navn', nav_enhet.navn,
-                                            'type', nav_enhet.type,
-                                            'status', nav_enhet.status,
-                                            'overordnetEnhet', nav_enhet.overordnet_enhet
-                                    )
-                            ) as nav_enheter_json
-                     from avtale_nav_enhet
-                              join nav_enhet on nav_enhet.enhetsnummer = avtale_nav_enhet.enhetsnummer
-                     group by avtale_id),
-     opsjon_logg as (select avtale_id,
-                            jsonb_agg(
-                                    jsonb_build_object('id', avtale_opsjon_logg.id,
-                                                       'aktivertDato', avtale_opsjon_logg.registrert_dato,
-                                                       'sluttDato', avtale_opsjon_logg.sluttdato,
-                                                       'status', avtale_opsjon_logg.status,
-                                                       'forrigeSluttdato', avtale_opsjon_logg.forrige_sluttdato
-                                    )) as opsjon_logg_json
-                     from avtale_opsjon_logg
-                     group by avtale_id),
-     amo_kategorisering as (select avtale_id,
-                                   jsonb_build_object(
-                                           'kurstype', avtale_amo_kategorisering.kurstype,
-                                           'bransje', avtale_amo_kategorisering.bransje,
-                                           'forerkort', avtale_amo_kategorisering.forerkort,
-                                           'norskprove', avtale_amo_kategorisering.norskprove,
-                                           'sertifiseringer',
-                                           coalesce((select jsonb_strip_nulls(jsonb_agg(jsonb_build_object(
-                                                   'label', s.label,
-                                                   'konseptId', s.konsept_id
-                                                                                        )))
-                                                     from amo_sertifisering s
-                                                              join avtale_amo_kategorisering_sertifisering aks
-                                                                   on aks.konsept_id = s.konsept_id
-                                                     where aks.avtale_id = avtale_amo_kategorisering.avtale_id),
-                                                    '[]'::jsonb),
-                                           'innholdElementer', avtale_amo_kategorisering.innhold_elementer
-                                   ) as amo_kategorisering_json
-                            from avtale_amo_kategorisering),
-     arrangor_underenheter as (select avtale_id,
-                                      jsonb_agg(
-                                              jsonb_build_object(
-                                                      'id', arrangor.id,
-                                                      'organisasjonsnummer', arrangor.organisasjonsnummer,
-                                                      'navn', arrangor.navn,
-                                                      'slettet', arrangor.slettet_dato is not null
-                                              )
-                                      ) as arrangor_underenheter_json
-                               from avtale_arrangor_underenhet
-                                        join arrangor on avtale_arrangor_underenhet.arrangor_id = arrangor.id
-                               group by avtale_id),
-     arrangor_kontaktpersoner as (select avtale_id,
-                                         jsonb_agg(
-                                                 jsonb_build_object(
-                                                         'id', avtale_arrangor_kontaktperson.arrangor_kontaktperson_id,
-                                                         'arrangorId', arrangor_kontaktperson.arrangor_id,
-                                                         'navn', arrangor_kontaktperson.navn,
-                                                         'telefon', arrangor_kontaktperson.telefon,
-                                                         'epost', arrangor_kontaktperson.epost,
-                                                         'beskrivelse', arrangor_kontaktperson.beskrivelse,
-                                                         'ansvarligFor', arrangor_kontaktperson.ansvarlig_for
-                                                 )
-                                         ) arrangor_kontaktpersoner_json
-                                  from avtale_arrangor_kontaktperson
-                                           join arrangor_kontaktperson
-                                                on avtale_arrangor_kontaktperson.arrangor_kontaktperson_id =
-                                                   arrangor_kontaktperson.id
-                                  group by avtale_id)
 select avtale.id,
        avtale.navn,
        coalesce(avtale.avtalenummer, avtale.lopenummer) as avtalenummer,
        avtale.websaknummer,
        avtale.start_dato,
        avtale.slutt_dato,
+       avtale.opsjon_maks_varighet,
        avtale.opphav,
        avtale.avtaletype,
        avtale.avbrutt_tidspunkt,
@@ -101,7 +17,6 @@ select avtale.id,
        avtale.faneinnhold,
        avtale.opsjonsmodell,
        avtale.opsjon_custom_opsjonsmodell_navn,
-       avtale.opsjon_maks_varighet,
        avtale.personvern_bekreftet,
        avtale.avbrutt_aarsak,
        avtale.arena_ansvarlig_enhet                     as arena_nav_enhet_enhetsnummer,
@@ -130,10 +45,85 @@ from avtale
          join tiltakstype on tiltakstype.id = avtale.tiltakstype_id
          join arrangor on arrangor.id = avtale.arrangor_hovedenhet_id
          left join nav_enhet arena_nav_enhet on avtale.arena_ansvarlig_enhet = arena_nav_enhet.enhetsnummer
-         left join personopplysninger on avtale.id = personopplysninger.avtale_id
-         left join administratorer on avtale.id = administratorer.avtale_id
-         left join nav_enheter on avtale.id = nav_enheter.avtale_id
-         left join opsjon_logg on avtale.id = opsjon_logg.avtale_id
-         left join amo_kategorisering on avtale.id = amo_kategorisering.avtale_id
-         left join arrangor_underenheter on avtale.id = arrangor_underenheter.avtale_id
-         left join arrangor_kontaktpersoner on avtale.id = arrangor_kontaktpersoner.avtale_id
+         left join lateral (select jsonb_agg(personopplysning) as personopplysninger_json
+                            from avtale_personopplysning
+                            where avtale_id = avtale.id) on true
+         left join lateral (select jsonb_agg(
+                                           jsonb_build_object(
+                                                   'navIdent', avtale_administrator.nav_ident,
+                                                   'navn', concat(nav_ansatt.fornavn, ' ', nav_ansatt.etternavn)
+                                           )
+                                   ) as administratorer_json
+                            from avtale_administrator
+                                     join nav_ansatt on nav_ansatt.nav_ident = avtale_administrator.nav_ident
+                            where avtale_administrator.avtale_id = avtale.id) on true
+         left join lateral (select jsonb_agg(
+                                           jsonb_build_object(
+                                                   'enhetsnummer', avtale_nav_enhet.enhetsnummer,
+                                                   'navn', nav_enhet.navn,
+                                                   'type', nav_enhet.type,
+                                                   'status', nav_enhet.status,
+                                                   'overordnetEnhet', nav_enhet.overordnet_enhet
+                                           )
+                                   ) as nav_enheter_json
+                            from avtale_nav_enhet
+                                     left join nav_enhet on nav_enhet.enhetsnummer = avtale_nav_enhet.enhetsnummer
+                            where avtale_nav_enhet.avtale_id = avtale.id) on true
+         left join lateral (select jsonb_agg(
+                                           jsonb_build_object('id', avtale_opsjon_logg.id,
+                                                              'aktivertDato', avtale_opsjon_logg.registrert_dato,
+                                                              'sluttDato', avtale_opsjon_logg.sluttdato,
+                                                              'status', avtale_opsjon_logg.status,
+                                                              'forrigeSluttdato', avtale_opsjon_logg.forrige_sluttdato
+                                           )) as opsjon_logg_json
+                            from avtale_opsjon_logg
+                            where avtale_id = avtale.id) on true
+         left join lateral (select jsonb_build_object(
+                                           'kurstype', avtale_amo_kategorisering.kurstype,
+                                           'bransje', avtale_amo_kategorisering.bransje,
+                                           'forerkort', avtale_amo_kategorisering.forerkort,
+                                           'norskprove', avtale_amo_kategorisering.norskprove,
+                                           'sertifiseringer',
+                                           coalesce((select jsonb_strip_nulls(
+                                                                    jsonb_agg(
+                                                                            jsonb_build_object(
+                                                                                    'label', s.label,
+                                                                                    'konseptId', s.konsept_id
+                                                                            )
+                                                                    ))
+                                                     from amo_sertifisering s
+                                                              join avtale_amo_kategorisering_sertifisering aks
+                                                                   on aks.konsept_id = s.konsept_id
+                                                     where aks.avtale_id = avtale_amo_kategorisering.avtale_id),
+                                                    '[]'::jsonb),
+                                           'innholdElementer', avtale_amo_kategorisering.innhold_elementer
+                                   ) as amo_kategorisering_json
+                            from avtale_amo_kategorisering
+                            where avtale_id = avtale.id) on true
+         left join lateral (select jsonb_agg(
+                                           jsonb_build_object(
+                                                   'id', arrangor.id,
+                                                   'organisasjonsnummer', arrangor.organisasjonsnummer,
+                                                   'navn', arrangor.navn,
+                                                   'slettet', arrangor.slettet_dato is not null
+                                           )
+                                   ) as arrangor_underenheter_json
+                            from avtale_arrangor_underenhet
+                                     join arrangor on avtale_arrangor_underenhet.arrangor_id = arrangor.id
+                            where avtale_id = avtale.id ) on true
+         left join lateral (select jsonb_agg(
+                                           jsonb_build_object(
+                                                   'id', avtale_arrangor_kontaktperson.arrangor_kontaktperson_id,
+                                                   'arrangorId', arrangor_kontaktperson.arrangor_id,
+                                                   'navn', arrangor_kontaktperson.navn,
+                                                   'telefon', arrangor_kontaktperson.telefon,
+                                                   'epost', arrangor_kontaktperson.epost,
+                                                   'beskrivelse', arrangor_kontaktperson.beskrivelse,
+                                                   'ansvarligFor', arrangor_kontaktperson.ansvarlig_for
+                                           )
+                                   ) arrangor_kontaktpersoner_json
+                            from avtale_arrangor_kontaktperson
+                                     join arrangor_kontaktperson
+                                          on avtale_arrangor_kontaktperson.arrangor_kontaktperson_id =
+                                             arrangor_kontaktperson.id
+                            where avtale_id = avtale.id) on true
