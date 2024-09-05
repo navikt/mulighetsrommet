2caee1f3451c2c20f8226ab0136e02bc923391fa
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-09-05 09:49:45 +0200

    Design endringer landingsside

--- frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/Landingsside.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/Landingsside.tsx
@@ -1,4 +1,10 @@
-import { ExternalLinkIcon, PlusIcon } from "@navikt/aksel-icons";
+import {
+  ArrowForwardIcon,
+  ExternalLinkIcon,
+  HourglassBottomFilledIcon,
+  LocationPinIcon,
+  PlusIcon,
+} from "@navikt/aksel-icons";
 import {
   Alert,
   Link as AkselLink,
@@ -30,86 +36,105 @@ function Feilmelding({ message }: { message: string }) {
 
 export function Landingsside() {
   const { logEvent } = useLogEvent();
-  const [activeTab, setActiveTab] = useState<"aktive" | "historikk">("aktive");
+  const [activeTab, setActiveTab] = useState<"aktive" | "historikk" | "delt-i-dialogen">("aktive");
 
   return (
     <main className="mulighetsrommet-veileder-flate">
-      <VStack gap="4" padding="4" style={{ maxWidth: "1000px" }}>
-        <FeedbackFraUrl />
-        <HStack justify="space-between">
-          <Heading size="large">Oversikt over brukerens tiltak</Heading>
-          <Link
-            data-testid="finn-nytt-arbeidsmarkedstiltak-btn"
-            className={styles.cta_link}
-            to="/arbeidsmarkedstiltak/oversikt"
-          >
-            <PlusIcon color="white" fontSize={30} aria-hidden /> Finn nytt arbeidsmarkedstiltak
-          </Link>
-        </HStack>
-        <Tabs
-          onChange={(fane) => {
-            logEvent({
-              name: "arbeidsmarkedstiltak.landingsside.fane-valgt",
-              data: {
-                action: fane,
-              },
-            });
-          }}
-          defaultValue={activeTab}
+      <HStack gap="4" align="start" justify="start">
+        <Link
+          data-testid="finn-nytt-arbeidsmarkedstiltak-btn"
+          className={styles.cta_link}
+          to="/arbeidsmarkedstiltak/oversikt"
         >
-          <Tabs.List className={styles.tabslist}>
-            <Tabs.Tab label="Aktive" value="aktive" onClick={() => setActiveTab("aktive")} />
-            <Tabs.Tab
-              label="Historikk"
-              value="historikk"
-              onClick={() => setActiveTab("historikk")}
-            />
-            <Tabs.Tab
-              label="Delt med bruker"
-              value="delt-med-bruker"
-              onClick={() => setActiveTab("historikk")}
-            />
-          </Tabs.List>
-          <Tabs.Panel value="aktive">
-            <ErrorBoundary
-              FallbackComponent={() =>
-                Feilmelding({
-                  message:
-                    "Noe gikk galt, og du får dessverre ikke sett alle deltakelser. Prøv igjen senere. ",
-                })
-              }
-            >
-              <Suspense fallback={<Skeleton variant="rounded" height="10rem" width="40rem" />}>
-                <Aktive />
-              </Suspense>
-            </ErrorBoundary>
-          </Tabs.Panel>
-          <Tabs.Panel value="historikk">
-            <ErrorBoundary
-              FallbackComponent={() =>
-                Feilmelding({
-                  message:
-                    "Noe gikk galt, og du får dessverre ikke sett historikk. Prøv igjen senere.",
-                })
-              }
-            >
-              <Suspense
-                fallback={
-                  <VStack gap="5">
-                    <Skeleton variant="rounded" height="10rem" width="40rem" />
-                    <Skeleton variant="rounded" height="10rem" width="40rem" />
-                  </VStack>
+          <PlusIcon color="white" fontSize={30} aria-hidden /> Finn nytt arbeidsmarkedstiltak
+        </Link>
+        <VStack gap="4" style={{ maxWidth: "1000px" }}>
+          <FeedbackFraUrl />
+          <Heading size="large">Oversikt over brukerens tiltak</Heading>
+          <Tabs
+            onChange={(fane) => {
+              logEvent({
+                name: "arbeidsmarkedstiltak.landingsside.fane-valgt",
+                data: {
+                  action: fane,
+                },
+              });
+            }}
+            defaultValue={activeTab}
+          >
+            <Tabs.List className={styles.tabslist}>
+              <Tabs.Tab
+                label={
+                  <HStack gap="1">
+                    <LocationPinIcon />
+                    Aktive
+                  </HStack>
+                }
+                value="aktive"
+                onClick={() => setActiveTab("aktive")}
+              />
+              <Tabs.Tab
+                label={
+                  <HStack gap="1">
+                    <HourglassBottomFilledIcon />
+                    Historikk
+                  </HStack>
+                }
+                value="historikk"
+                onClick={() => setActiveTab("historikk")}
+              />
+              <Tabs.Tab
+                label={
+                  <HStack gap="1">
+                    <ArrowForwardIcon />
+                    Delt i dialogen
+                  </HStack>
+                }
+                value="delt-i-dialogen"
+                onClick={() => setActiveTab("delt-i-dialogen")}
+              />
+            </Tabs.List>
+            <Tabs.Panel value="aktive">
+              <ErrorBoundary
+                FallbackComponent={() =>
+                  Feilmelding({
+                    message:
+                      "Noe gikk galt, og du får dessverre ikke sett alle deltakelser. Prøv igjen senere. ",
+                  })
                 }
               >
-                <Historikk />
-              </Suspense>
-            </ErrorBoundary>
-          </Tabs.Panel>
-          <Tabs.Panel value="delt-med-bruker">
-            <DelMedBrukerHistorikk />
-          </Tabs.Panel>
-        </Tabs>
-      </VStack>
+                <Suspense fallback={<Skeleton variant="rounded" height="10rem" width="40rem" />}>
+                  <Aktive />
+                </Suspense>
+              </ErrorBoundary>
+            </Tabs.Panel>
+            <Tabs.Panel value="historikk">
+              <ErrorBoundary
+                FallbackComponent={() =>
+                  Feilmelding({
+                    message:
+                      "Noe gikk galt, og du får dessverre ikke sett historikk. Prøv igjen senere.",
+                  })
+                }
+              >
+                <Suspense
+                  fallback={
+                    <VStack gap="5">
+                      <Skeleton variant="rounded" height="10rem" width="40rem" />
+                      <Skeleton variant="rounded" height="10rem" width="40rem" />
+                    </VStack>
+                  }
+                >
+                  <Historikk />
+                </Suspense>
+              </ErrorBoundary>
+            </Tabs.Panel>
+            <Tabs.Panel value="delt-i-dialogen">
+              <DelMedBrukerHistorikk />
+            </Tabs.Panel>
+          </Tabs>
+        </VStack>
+      </HStack>
     </main>
   );
 }
@@ -121,15 +146,14 @@ function Aktive() {
     return null;
   }
 
-  let { aktive } = data;
-  aktive = [];
+  const { aktive } = data;
 
   return (
     <VStack padding="2" gap="4">
       {aktive.map((utkast) => {
         return <DeltakelseKort key={utkast.id} deltakelse={utkast} />;
       })}
-      {aktive.length === 0 && <TomEske />}
+      {aktive.length === 0 && <TomEske title="Ingen aktive tiltak" />}
       <Alert variant="info">
         For oversikt over tiltakstypene “Sommerjobb”, “Midlertidig lønnstilskudd”, og “Varig
         lønnstilskudd” se <TeamTiltakLenke />
@@ -164,7 +188,7 @@ function Historikk() {
       {historiske.map((hist) => {
         return <DeltakelseKort key={hist.id} deltakelse={hist} />;
       })}
-      {historiske.length === 0 && <TomEske />}
+      {historiske.length === 0 && <TomEske title="Ingen tidligere tiltak" />}
       <Alert variant="info">
         Vi viser bare historikk 5 år tilbake i tid. For oversikt over tiltakstypene “Sommerjobb”,
         “Midlertidig lønnstilskudd”, og “Varig lønnstilskudd” se <TeamTiltakLenke />
@@ -173,12 +197,12 @@ function Historikk() {
   );
 }
 
-function TomEske() {
+function TomEske(props: { title: string }) {
   return (
     <Box background="bg-default" borderRadius="medium" padding="5">
       <VStack align="center">
         <img src={tomEskeImg} alt="" className={styles.tom_eske_img} />
-        <Heading size="medium">Ingen tiltak funnet</Heading>
+        <Heading size="medium">{props.title}</Heading>
       </VStack>
     </Box>
   );
