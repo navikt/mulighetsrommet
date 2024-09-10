02f3bb98c9c5162d4149be3c20971dc404579922
Sindre Moldeklev <sndrem@gmail.com>
2024-09-10 14:15:36 +0200

    Fjerner toggle for landingsside

--- frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/ModiaArbeidsmarkedstiltakOversikt.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/apps/modia/views/ModiaArbeidsmarkedstiltakOversikt.tsx
@@ -1,9 +1,9 @@
-import { useFeatureToggle } from "@/api/feature-toggles";
 import { useModiaArbeidsmarkedstiltak } from "@/api/queries/useArbeidsmarkedstiltak";
 import { ModiaFiltertags } from "@/apps/modia/filtrering/ModiaFiltertags";
 import { useHentAlleTiltakDeltMedBruker } from "@/apps/modia/hooks/useHentAlleTiltakDeltMedBruker";
 import { FiltrertFeilInnsatsgruppeVarsel } from "@/apps/modia/varsler/FiltrertFeilInnsatsgruppeVarsel";
 import { Feilmelding } from "@/components/feilmelding/Feilmelding";
+import { Filtermeny } from "@/components/filtrering/Filtermeny";
 import { OversiktenJoyride } from "@/components/joyride/OversiktenJoyride";
 import { ArbeidsmarkedstiltakList } from "@/components/oversikt/ArbeidsmarkedstiltakList";
 import { Tilbakeknapp } from "@/components/tilbakeknapp/Tilbakeknapp";
@@ -11,15 +11,13 @@ import {
   isFilterReady,
   useResetArbeidsmarkedstiltakFilterMedBrukerIKontekst,
 } from "@/hooks/useArbeidsmarkedstiltakFilter";
-import { Toggles } from "@mr/api-client";
 import { ListSkeleton, useOpenFilterWhenThreshold, useTitle } from "@mr/frontend-common";
-import { TilToppenKnapp } from "@mr/frontend-common/components/tilToppenKnapp/TilToppenKnapp";
-import { useEffect, useState } from "react";
-import { ModiaOversiktBrukerVarsler } from "../varsler/ModiaOversiktBrukerVarsler";
 import { FilterAndTableLayout } from "@mr/frontend-common/components/filterAndTableLayout/FilterAndTableLayout";
 import { NullstillFilterKnapp } from "@mr/frontend-common/components/nullstillFilterKnapp/NullstillFilterKnapp";
+import { TilToppenKnapp } from "@mr/frontend-common/components/tilToppenKnapp/TilToppenKnapp";
+import { useEffect, useState } from "react";
 import { HistorikkButton } from "../historikk/HistorikkButton";
-import { Filtermeny } from "@/components/filtrering/Filtermeny";
+import { ModiaOversiktBrukerVarsler } from "../varsler/ModiaOversiktBrukerVarsler";
 
 export function ModiaArbeidsmarkedstiltakOversikt() {
   useTitle("Arbeidsmarkedstiltak - Oversikt");
@@ -29,8 +27,6 @@ export function ModiaArbeidsmarkedstiltakOversikt() {
   const { filter, filterHasChanged, resetFilterToDefaults } =
     useResetArbeidsmarkedstiltakFilterMedBrukerIKontekst();
 
-  const landingssideFeature = useFeatureToggle(Toggles.MULIGHETSROMMET_VEILEDERFLATE_LANDINGSSIDE);
-  const landingssideEnabled = landingssideFeature.isSuccess && landingssideFeature.data;
   const [isHistorikkModalOpen, setIsHistorikkModalOpen] = useState(false);
   const [tagsHeight, setTagsHeight] = useState(0);
 
@@ -42,12 +38,10 @@ export function ModiaArbeidsmarkedstiltakOversikt() {
 
   return (
     <>
-      {landingssideEnabled ? (
-        <Tilbakeknapp
-          tekst="Gå til oversikt over brukerens tiltak"
-          tilbakelenke="/arbeidsmarkedstiltak"
-        />
-      ) : null}
+      <Tilbakeknapp
+        tekst="Gå til oversikt over brukerens tiltak"
+        tilbakelenke="/arbeidsmarkedstiltak"
+      />
       <FilterAndTableLayout
         filterOpen={filterOpen}
         setFilterOpen={setFilterOpen}
