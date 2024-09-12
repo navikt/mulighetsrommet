9772ef98e6f51b414ce059b5781b4222542a662d
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-09-12 14:32:40 +0200

    Set obo token before each api-client call in remix

--- frontend/arrangor-flate/server.js
+++ frontend/arrangor-flate/server.js
@@ -54,14 +54,6 @@ app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (_, re
 app.use(metricsMiddleware);
 app.use(morgan("tiny"));
 
-if (process.env.VITE_MULIGHETSROMMET_API_MOCK !== 'true') {
-  app.use(
-    "/api",
-    oboMiddleware(),
-    proxyMiddleware(),
-  );
-}
-
 // handle SSR requests
 app.all("*", remixHandler);
 
@@ -74,52 +66,3 @@ app.listen(port, () => {
       : `Serveren kjører på port ${port}`,
   );
 });
-
-function oboMiddleware() {
-  return  async (req, res, next) => {
-    if (process.env.NODE_ENV !== 'production') {
-      return next();
-    }
-    const token = getToken(req);
-    if (!token) {
-      console.log("token missing", token);
-      return res.redirect("https://arrangor-refusjon.intern.dev.nav.no/oauth2/login");
-    }
-    const validation = await validateToken(token);
-    if (!validation.ok) {
-      console.log("token validation failed", token);
-      return res.redirect("https://arrangor-refusjon.intern.dev.nav.no/oauth2/login");
-    }
-    // TODO: Tror man skal kanskje heller bruke den generiske requestObboToken i stedet
-    const obo = await requestTokenxOboToken(token,  `${process.env.NAIS_CLUSTER_NAME}.team-mulighetsrommet.mulighetsrommet-api`);
-    if (obo.ok) {
-      console.log("OBO-exchange success");
-      req.headers['obo-token'] = obo.token;
-      return next();
-    } else {
-      console.log('OBO-exchange failed', obo.error);
-      return res.status(403).send();
-    }
-  }
-}
-
-function proxyMiddleware() {
-  return  createProxyMiddleware({
-    target: process.env.VITE_MULIGHETSROMMET_API_BASE,
-    changeOrigin: true,
-		pathRewrite: { '^/': '/api/' },
-    logger: console,
-    on: {
-      proxyReq: (proxyRequest, request) => {
-        const obo = request.headers['obo-token'];
-        if (obo) {
-          proxyRequest.removeHeader('obo-token');
-          proxyRequest.removeHeader('cookie');
-          proxyRequest.setHeader('Authorization', `Bearer ${obo}`);
-        } else {
-          console.log("Access token var not present in session");
-        }
-      },
-    },
-  });
-}
