d0aedd51c787592a591855763f520eab8deec4bf
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-09-17 16:05:12 +0200

    legg til litt mellomrom mellom alert og innhold

--- frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/KontaktinfoFane.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/tabs/kontaktinfofane/KontaktinfoFane.tsx
@@ -1,4 +1,4 @@
-import { Alert, HGrid } from "@navikt/ds-react";
+import { Alert, HGrid, VStack } from "@navikt/ds-react";
 import { VeilederflateTiltak } from "@mr/api-client";
 import { TiltakDetaljerFaneContainer } from "../TiltakDetaljerFaneContainer";
 import ArrangorInfo from "./ArrangorInfo";
@@ -13,19 +13,25 @@ interface Props {
 export function KontaktinfoFane({ tiltak }: Props) {
   return (
     <TiltakDetaljerFaneContainer harInnhold={true} className={styles.kontaktinfo_container}>
-      {tiltak.faneinnhold?.kontaktinfoInfoboks && (
-        <Alert variant="info" style={{ whiteSpace: "pre-wrap" }}>
-          {tiltak.faneinnhold.kontaktinfoInfoboks}
-        </Alert>
-      )}
-      <HGrid columns="1fr 1fr" align="start" gap="5">
-        {isTiltakMedArrangor(tiltak) ? (
-          <ArrangorInfo arrangor={tiltak.arrangor} faneinnhold={tiltak.faneinnhold?.kontaktinfo} />
-        ) : (
-          <Alert variant="info">Kontaktinfo til ekstern part er ikke tilgjengelig</Alert>
+      <VStack gap="5">
+        {tiltak.faneinnhold?.kontaktinfoInfoboks && (
+          <Alert variant="info" style={{ whiteSpace: "pre-wrap" }}>
+            {tiltak.faneinnhold.kontaktinfoInfoboks}
+          </Alert>
         )}
-        <NavKontaktpersonInfo kontaktinfo={tiltak.kontaktinfo} />
-      </HGrid>
+
+        <HGrid columns="1fr 1fr" align="start" gap="5">
+          {isTiltakMedArrangor(tiltak) ? (
+            <ArrangorInfo
+              arrangor={tiltak.arrangor}
+              faneinnhold={tiltak.faneinnhold?.kontaktinfo}
+            />
+          ) : (
+            <Alert variant="info">Kontaktinfo til ekstern part er ikke tilgjengelig</Alert>
+          )}
+          <NavKontaktpersonInfo kontaktinfo={tiltak.kontaktinfo} />
+        </HGrid>
+      </VStack>
     </TiltakDetaljerFaneContainer>
   );
 }
