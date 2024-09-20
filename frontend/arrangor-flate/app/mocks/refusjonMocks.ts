ed96fa7f6075a99ed44d5083e3b2d85775f9bccb
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2024-09-20 10:42:53 +0200

    Update openapi.yaml refusjonskrav

--- frontend/arrangor-flate/app/mocks/refusjonMocks.ts
+++ frontend/arrangor-flate/app/mocks/refusjonMocks.ts
@@ -1,38 +1,54 @@
-import { Refusjonskrav, RefusjonskravStatus } from "@mr/api-client";
+import { RefusjonskravDto } from "@mr/api-client";
 import { http, HttpResponse, PathParams } from "msw";
+import { v4 as uuid } from "uuid";
 
-const mockKrav: Refusjonskrav[] = [
+const mockKrav: RefusjonskravDto[] = [
   {
-    id: "6",
-    belop: "308 530",
-    fristForGodkjenning: "31.08.2024",
-    kravnr: "6",
-    periode: "01.06.2024 - 30.06.2024",
-    status: RefusjonskravStatus.KLAR_FOR_INNSENDING,
-    tiltaksnr: "2024/123456",
+    id: uuid(),
+    beregning: {
+      type: "AFT",
+      sats: 20205,
+      belop: 308530,
+    },
+    periodeStart: "01.06.2024",
+    periodeSlutt: "30.06.2024",
+    tiltaksgjennomforing: {
+      id: uuid(),
+      navn: "AFT tiltak Moss",
+    },
   },
   {
-    id: "5",
-    belop: "123 000",
-    fristForGodkjenning: "31.07.2024",
-    kravnr: "5",
-    periode: "01.05.2024 - 31.05.2024",
-    status: RefusjonskravStatus.NARMER_SEG_FRIST,
-    tiltaksnr: "2024/123456",
+    id: uuid(),
+    beregning: {
+      type: "AFT",
+      sats: 20205,
+      belop: 18530,
+    },
+    periodeStart: "01.05.2024",
+    periodeSlutt: "31.05.2024",
+    tiltaksgjennomforing: {
+      id: uuid(),
+      navn: "AFT tiltak Moss",
+    },
   },
   {
-    id: "4",
-    belop: "85 000",
-    fristForGodkjenning: "30.06.2024",
-    kravnr: "4",
-    periode: "01.01.2024 - 31.01.2024",
-    status: RefusjonskravStatus.ATTESTERT,
-    tiltaksnr: "2024/123456",
+    id: uuid(),
+    beregning: {
+      type: "AFT",
+      sats: 20205,
+      belop: 85000,
+    },
+    periodeStart: "01.01.2024",
+    periodeSlutt: "31.01.2024",
+    tiltaksgjennomforing: {
+      id: uuid(),
+      navn: "Amo tiltak Halden",
+    },
   },
 ];
 
 export const refusjonHandlers = [
-  http.get<PathParams, Refusjonskrav[]>("*/api/v1/intern/refusjon/:orgnr/krav", () =>
+  http.get<PathParams, RefusjonskravDto[]>("*/api/v1/intern/refusjon/:orgnr/krav", () =>
     HttpResponse.json(mockKrav),
   ),
 ];
