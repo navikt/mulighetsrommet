37c175e8d899e4f7c35c20ae25bd6deba6dacf48
Sindre Moldeklev <sndrem@gmail.com>
2024-11-08 09:24:29 +0100

    Bruk metode i Playwright-test

--- frontend/mr-admin-flate/tests/adminflate.avtale.spec.ts
+++ frontend/mr-admin-flate/tests/adminflate.avtale.spec.ts
@@ -9,13 +9,12 @@ test("Opprett ny avtale", async ({ page }) => {
   await page.locator("text=Opprett ny avtale").click();
   const heading = page.locator("h2");
   await test.expect(heading).toHaveText("Opprett ny avtale");
-  const navnInput = page.locator('input[name="navn"]');
-  await navnInput.fill("Testavtale fra Playwright");
-  const websaknummerInput = page.locator('input[name="websaknummer"]');
-  await websaknummerInput.fill("24/123");
+  await locateAndFillInput(page, "navn", "Testavtale fra Playwright");
+  await locateAndFillInput(page, "websaknummer", "24/123");
+  // TODO Flere tester for å fullføre skjema
 });
 
-async function localeAndFillInput(page: Page, inputName: string, fill: string) {
+async function locateAndFillInput(page: Page, inputName: string, fill: string) {
   const input = page.locator(`input[name=${inputName}]`);
   await input.fill(fill);
 }
