dbf35a32ed7c678755c849b18b2f8c3ffb031bcc
Sondre Lefsaker <sondre.lefsaker@nav.no>
2024-11-15 12:50:33 +0100

    fjern ubrukte typer

--- frontend/arrangor-flate/app/domene/domene.ts
+++ frontend/arrangor-flate/app/domene/domene.ts
@@ -4,20 +4,6 @@ import {
   RefusjonKravDeltakelse,
 } from "@mr/api-client";
 
-export interface Krav {
-  id: string;
-  kravnr: string;
-  periode: string;
-  belop: string;
-  fristForGodkjenning: string;
-}
-
-export enum KravStatus {
-  Attestert,
-  KlarForInnsending,
-  NarmerSegFrist,
-}
-
 export interface Refusjonskrav {
   id: string;
   detaljer: {
@@ -28,29 +14,4 @@ export interface Refusjonskrav {
   beregning: RefusjonKravAftBeregning;
   deltakere: RefusjonKravDeltakelse[];
   betalingsinformasjon: Betalingsinformasjon;
-}
-
-export type TilsagnDetaljer = {
-  antallPlasser: number;
-  prisPerPlass: number;
-  tilsagnsBelop: number;
-  tilsagnsPeriode: string;
-  sum: number;
-};
-
-export interface RolleTilgangRequest {
-  personident: string;
-}
-
-export interface Rolletilgang {
-  roller: TiltaksarrangorRoller[];
-}
-
-interface TiltaksarrangorRoller {
-  organisasjonsnummer: string;
-  roller: RolleType[];
-}
-
-export enum RolleType {
-  "TILTAK_ARRANGOR_REFUSJON",
 }
