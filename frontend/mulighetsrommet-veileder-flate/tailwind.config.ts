d764c01ef22447822bd0c13593aa98e28471dc6e
Sindre Moldeklev <sndrem@gmail.com>
2025-01-10 12:41:26 +0100

    Legger til tailwind i Modia

--- frontend/mulighetsrommet-veileder-flate/tailwind.config.ts
+++ frontend/mulighetsrommet-veileder-flate/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
 
+import akselTailwind from "@navikt/ds-tailwind";
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/apps/**/index.html"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-  presets: [require("@navikt/ds-tailwind")],
+  presets: [akselTailwind],
 } satisfies Config;
