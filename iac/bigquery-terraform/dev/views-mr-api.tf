8e1f49712933f833ba92736156c3896a363a798e
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-01-18 15:14:05 +0100

    oppdater bigquery views

--- iac/bigquery-terraform/dev/views-mr-api.tf
+++ iac/bigquery-terraform/dev/views-mr-api.tf
@@ -42,11 +42,11 @@ WHERE tiltakskode IS NOT NULL
 EOF
 }
 
-module "mr_api_tiltaksgjennomforing_view" {
+module "mr_api_gjennomforing_view" {
   source              = "../modules/google-bigquery-view"
   deletion_protection = false
   dataset_id          = module.mr_api_datastream.dataset_id
-  view_id             = "tiltaksgjennomforing_view"
+  view_id             = "gjennomforing_view"
   view_schema = jsonencode(
     [
       {
@@ -95,7 +95,7 @@ SELECT
   avsluttet_tidspunkt,
   start_dato,
   slutt_dato
-FROM `${var.gcp_project["project"]}.${module.mr_api_datastream.dataset_id}.public_tiltaksgjennomforing`
+FROM `${var.gcp_project["project"]}.${module.mr_api_datastream.dataset_id}.public_gjennomforing`
 WHERE slutt_dato is null or slutt_dato >= DATE '2018-01-01'
 EOF
 }
