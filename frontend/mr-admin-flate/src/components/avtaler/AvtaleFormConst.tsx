3fd7650198b1802a76f26b765f3728a818ddf66a
MaximilianLloyd <maximilian.oysta.lloyd@nav.no>
2025-02-18 12:13:11 +0100

    fix typecheck

--- frontend/mr-admin-flate/src/components/avtaler/AvtaleFormConst.tsx
+++ frontend/mr-admin-flate/src/components/avtaler/AvtaleFormConst.tsx
@@ -44,14 +44,12 @@ export function defaultAvtaleData(
     administratorer: avtale?.administratorer?.map((admin) => admin.navIdent) || [ansatt.navIdent],
     navn: avtale?.navn ?? "",
     avtaletype: avtale?.avtaletype,
-    arrangor: {
-      hovedenhet: avtale?.arrangor?.organisasjonsnummer ?? "",
-      underenheter: !avtale?.arrangor?.underenheter
-        ? []
-        : avtale.arrangor.underenheter.map((underenhet) => underenhet.organisasjonsnummer),
-      kontaktpersoner:
-        avtale?.arrangor?.kontaktpersoner.map((p: ArrangorKontaktperson) => p.id) ?? [],
-    },
+    arrangorHovedenhet: avtale?.arrangor?.organisasjonsnummer ?? "",
+    arrangorUnderenheter: !avtale?.arrangor?.underenheter
+      ? []
+      : avtale.arrangor.underenheter.map((underenhet) => underenhet.organisasjonsnummer),
+    arrangorKontaktpersoner:
+      avtale?.arrangor?.kontaktpersoner.map((p: ArrangorKontaktperson) => p.id) ?? [],
     startOgSluttDato: {
       startDato: avtale?.startDato ? avtale.startDato : undefined,
       sluttDato: avtale?.sluttDato ? avtale.sluttDato : undefined,
