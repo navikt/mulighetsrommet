78bb0f062115ce53eb36a52367eee72382d4d941
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-03-07 18:03:20 +0100

    flytt logikk fra gjennomføring-queries utbetaling-service

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/utbetaling/UtbetalingService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/utbetaling/UtbetalingService.kt
@@ -5,6 +5,7 @@ import arrow.core.right
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.encodeToJsonElement
 import kotliquery.TransactionalSession
+import kotliquery.queryOf
 import no.nav.mulighetsrommet.api.ApiDatabase
 import no.nav.mulighetsrommet.api.QueryContext
 import no.nav.mulighetsrommet.api.arrangorflate.GodkjennUtbetaling
@@ -21,6 +22,7 @@ import no.nav.mulighetsrommet.api.utbetaling.task.JournalforUtbetaling
 import no.nav.mulighetsrommet.ktor.exception.BadRequest
 import no.nav.mulighetsrommet.ktor.exception.NotFound
 import no.nav.mulighetsrommet.model.*
+import org.intellij.lang.annotations.Language
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.Instant
@@ -38,13 +40,12 @@ class UtbetalingService(
     fun genererUtbetalingForMonth(date: LocalDate): List<UtbetalingDto> = db.transaction {
         val periode = Periode.forMonthOf(date)
 
-        queries.gjennomforing
-            .getGjennomforesInPeriodeUtenUtbetaling(periode)
-            .mapNotNull { gjennomforing ->
-                val utbetaling = when (gjennomforing.tiltakstype.tiltakskode) {
-                    Tiltakskode.ARBEIDSFORBEREDENDE_TRENING -> createUtbetalingAft(
+        getGjennomforingerForGenereringAvUtbetalinger(periode)
+            .mapNotNull { (gjennomforingId, avtaletype) ->
+                val utbetaling = when (avtaletype) {
+                    Avtaletype.Forhaandsgodkjent -> createUtbetalingAft(
                         utbetalingId = UUID.randomUUID(),
-                        gjennomforingId = gjennomforing.id,
+                        gjennomforingId = gjennomforingId,
                         periode = periode,
                     )
 
@@ -208,6 +209,32 @@ class UtbetalingService(
 
     fun getUtbetalingKompaktByGjennomforing(id: UUID): List<AdminUtbetalingKompakt> = db.session {
         queries.utbetaling.getByGjennomforing(id).map { utbetaling -> toAdminUtbetalingKompakt(utbetaling) }
+    }
+
+    private fun QueryContext.getGjennomforingerForGenereringAvUtbetalinger(
+        periode: Periode,
+    ): List<Pair<UUID, Avtaletype>> {
+        @Language("PostgreSQL")
+        val query = """
+            select gjennomforing.id, avtale.avtaletype
+            from gjennomforing
+                join avtale on gjennomforing.avtale_id = avtale.id
+            where (gjennomforing.start_dato <= :periode_slutt)
+              and (gjennomforing.slutt_dato >= :periode_start or gjennomforing.slutt_dato is null)
+              and (gjennomforing.avsluttet_tidspunkt > :periode_start or gjennomforing.avsluttet_tidspunkt is null)
+              and not exists (
+                    select 1
+                    from utbetaling
+                    where utbetaling.gjennomforing_id = gjennomforing.id
+                      and utbetaling.periode && daterange(:periode_start, :periode_slutt)
+              )
+        """.trimIndent()
+
+        val params = mapOf("periode_start" to periode.start, "periode_slutt" to periode.slutt)
+
+        return session.list(queryOf(query, params)) {
+            Pair(it.uuid("id"), Avtaletype.valueOf(it.string("avtaletype")))
+        }
     }
 
     private fun QueryContext.validateAndUpsertDelutbetaling(
