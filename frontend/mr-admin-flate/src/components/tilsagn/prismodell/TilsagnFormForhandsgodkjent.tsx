9653d2b74120114a40287e3a7a86a27adeea280e
Sindre Moldeklev <sndrem@gmail.com>
2025-03-10 12:50:41 +0100

    Ikke valider at sats er et positivt tall

--- frontend/mr-admin-flate/src/components/tilsagn/prismodell/TilsagnFormForhandsgodkjent.tsx
+++ frontend/mr-admin-flate/src/components/tilsagn/prismodell/TilsagnFormForhandsgodkjent.tsx
@@ -1,8 +1,8 @@
 import { useFindForhandsgodkjentSats } from "@/api/tilsagn/useFindForhandsgodkjentSats";
 import { TilsagnBeregningPreview } from "@/components/tilsagn/prismodell/TilsagnBeregningPreview";
-import { InferredTilsagn } from "@/components/tilsagn/prismodell/TilsagnSchema";
 import { TilsagnForm } from "@/components/tilsagn/prismodell/TilsagnForm";
-import { TilsagnBeregningForhandsgodkjent, GjennomforingDto } from "@mr/api-client-v2";
+import { InferredTilsagn } from "@/components/tilsagn/prismodell/TilsagnSchema";
+import { GjennomforingDto, TilsagnBeregningForhandsgodkjent } from "@mr/api-client-v2";
 import { HGrid, TextField } from "@navikt/ds-react";
 import { useEffect } from "react";
 import { DeepPartial, useFormContext } from "react-hook-form";
@@ -39,8 +39,8 @@ function BeregningInputSkjema({ gjennomforing }: Pick<Props, "gjennomforing">) {
   const periodeSlutt = watch("periodeSlutt");
 
   const sats = useFindForhandsgodkjentSats(gjennomforing.tiltakstype.tiltakskode, periodeStart);
+
   useEffect(() => {
-    // FIXME: Satt til 0 for at validering og beregning ikke skal stoppe opp. Kan det gjøres på en bedre måte?
     const pris = sats?.pris ?? 0;
     setValue("beregning.sats", pris);
   }, [sats, setValue]);
