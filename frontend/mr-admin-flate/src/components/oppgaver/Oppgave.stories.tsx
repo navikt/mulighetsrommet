262b890e4c8e5f2745ec900014d2b76d0b5aa0ad
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-03-14 19:06:47 +0100

    utled navn på tiltakstype for oppgaver i backend

--- frontend/mr-admin-flate/src/components/oppgaver/Oppgave.stories.tsx
+++ frontend/mr-admin-flate/src/components/oppgaver/Oppgave.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Oppgave } from "./Oppgave";
 import { mockOppgaver } from "@/mocks/fixtures/mock_oppgaver";
-import { mockTiltakstyper } from "@/mocks/fixtures/mock_tiltakstyper";
 
 const meta: Meta<typeof Oppgave> = {
   component: Oppgave,
@@ -13,7 +12,6 @@ type Story = StoryObj<typeof Oppgave>;
 export const OppgaveStory: Story = {
   args: {
     oppgave: mockOppgaver[0],
-    tiltakstype: mockTiltakstyper.ARBFORB,
   },
 };
 
--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/utbetaling/db/DelutbetalingOppgaveData.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/utbetaling/db/DelutbetalingOppgaveData.kt
@@ -1,12 +1,12 @@
 package no.nav.mulighetsrommet.api.utbetaling.db
 
 import no.nav.mulighetsrommet.api.utbetaling.model.DelutbetalingDto
-import no.nav.mulighetsrommet.model.Tiltakskode
+import no.nav.mulighetsrommet.oppgaver.OppgaveTiltakstype
 import java.util.*
 
 data class DelutbetalingOppgaveData(
     val delutbetaling: DelutbetalingDto,
     val gjennomforingId: UUID,
-    val tiltakskode: Tiltakskode,
     val gjennomforingsnavn: String,
+    val tiltakstype: OppgaveTiltakstype,
 )
