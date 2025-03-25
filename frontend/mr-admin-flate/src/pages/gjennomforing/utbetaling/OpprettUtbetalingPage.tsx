1e038f877a224dd38559a0edb15473f04047320d
Sindre Moldeklev <sndrem@gmail.com>
2025-03-25 14:21:06 +0100

    Hent kontonummer ved manuell utbetaling

--- frontend/mr-admin-flate/src/pages/gjennomforing/utbetaling/OpprettUtbetalingPage.tsx
+++ frontend/mr-admin-flate/src/pages/gjennomforing/utbetaling/OpprettUtbetalingPage.tsx
@@ -1,15 +1,19 @@
 import { Link, useParams } from "react-router";
 import { useAdminGjennomforingById } from "../../../api/gjennomforing/useAdminGjennomforingById";
 import { OpprettUtbetalingForm } from "./OpprettUtbetalingForm";
+import { useKontonummerForArrangor } from "../../../api/arrangor/useKontonummerForArrangor";
 
 export function OpprettUtbetalingPage() {
   const { gjennomforingId } = useParams();
   const { data: gjennomforing } = useAdminGjennomforingById(gjennomforingId!);
+  const { data: kontonummer } = useKontonummerForArrangor(
+    gjennomforing.arrangor.organisasjonsnummer,
+  );
 
   return (
     <div className="flex flex-col gap-4">
       <Link to={`/gjennomforinger/${gjennomforingId}/utbetalinger`}>Tilbake</Link>
-      <OpprettUtbetalingForm gjennomforing={gjennomforing} />
+      <OpprettUtbetalingForm gjennomforing={gjennomforing} kontonummer={kontonummer.data} />
     </div>
   );
 }
