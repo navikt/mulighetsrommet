86a558a84f4fdb5071b70662c5e725ba582d625d
Sindre Moldeklev <sndrem@gmail.com>
2025-03-26 14:33:29 +0100

    Egen side for innsendt utbetaling

--- frontend/arrangor-flate/app/routes/$orgnr.utbetaling.$id.innsendt-utbetaling.tsx
+++ frontend/arrangor-flate/app/routes/$orgnr.utbetaling.$id.innsendt-utbetaling.tsx
@@ -1,17 +1,16 @@
-import { ArrangorflateService, ArrangorflateTilsagn, ArrFlateUtbetaling } from "api-client";
 import { formaterKontoNummer } from "@mr/frontend-common/utils/utils";
 import { FilePdfIcon } from "@navikt/aksel-icons";
 import { Button, GuidePanel, VStack } from "@navikt/ds-react";
-import { LoaderFunction } from "react-router";
-import { useLoaderData, useParams } from "react-router";
+import { ArrangorflateService, ArrangorflateTilsagn, ArrFlateUtbetaling } from "api-client";
+import { LoaderFunction, useLoaderData, useParams } from "react-router";
+import { apiHeaders } from "~/auth/auth.server";
 import { Definisjonsliste } from "~/components/Definisjonsliste";
 import { PageHeader } from "~/components/PageHeader";
-import { UtbetalingDetaljer } from "~/components/utbetaling/UtbetalingDetaljer";
 import { Separator } from "~/components/Separator";
+import { UtbetalingDetaljer } from "~/components/utbetaling/UtbetalingDetaljer";
+import { LinkWithTabState } from "../components/LinkWithTabState";
 import { internalNavigation } from "../internal-navigation";
 import { problemDetailResponse, useOrgnrFromUrl } from "../utils";
-import { LinkWithTabState } from "../components/LinkWithTabState";
-import { apiHeaders } from "~/auth/auth.server";
 
 type UtbetalingKvitteringData = {
   utbetaling: ArrFlateUtbetaling;
@@ -57,7 +56,7 @@ export default function UtbetalingKvittering() {
   return (
     <>
       <PageHeader
-        title="Kvittering"
+        title="Innsendt krav om utbetaling"
         tilbakeLenke={{
           navn: "Tilbake til utbetalinger",
           url: internalNavigation(orgnr).utbetalinger,
@@ -73,7 +72,9 @@ export default function UtbetalingKvittering() {
         </a>
       </div>
       <Separator />
-
+      <GuidePanel className="text-center my-2" poster>
+        Krav om utbetaling er nå sendt inn og vil bli behandlet av Nav
+      </GuidePanel>
       <VStack gap="5" className="max-w-[50%] mt-5">
         <UtbetalingDetaljer utbetaling={utbetaling} tilsagn={tilsagn} />
         <Separator />
