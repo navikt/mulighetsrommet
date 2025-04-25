289d348dcb9996502b9fab5eedffbd1a115d2911
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-04-25 10:51:49 +0200

    midlertidig fiks roller fra /me-endepunkt for riktig tilgangsstyring i frontend

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/navansatt/api/NavAnsattRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/navansatt/api/NavAnsattRoutes.kt
@@ -1,9 +1,11 @@
 package no.nav.mulighetsrommet.api.navansatt.api
 
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import no.nav.mulighetsrommet.api.navansatt.model.Rolle
+import no.nav.mulighetsrommet.api.navansatt.service.NavAnsattPrincipal
 import no.nav.mulighetsrommet.api.navansatt.service.NavAnsattService
 import no.nav.mulighetsrommet.api.plugins.getNavAnsattAzureId
 import no.nav.mulighetsrommet.ktor.extensions.getAccessToken
@@ -37,9 +39,11 @@ fun Route.navAnsattRoutes() {
         get("/me") {
             val azureId = getNavAnsattAzureId()
             val obo = AccessType.OBO(call.getAccessToken())
+            val principal = call.principal<NavAnsattPrincipal>()
 
             val ansatt = ansattService.getOrSynchronizeNavAnsatt(azureId, obo).let {
-                NavAnsattDto.fromNavAnsatt(it)
+                val midlertidigOverstyrtRoller = (principal?.roller ?: it.roller).map { it.rolle }.toSet()
+                NavAnsattDto.fromNavAnsatt(it).copy(roller = midlertidigOverstyrtRoller)
             }
 
             call.respond(ansatt)
