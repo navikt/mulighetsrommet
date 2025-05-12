6d696df119081144c84332f57c2bc80fc97513da
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-12 13:43:29 +0200

    migrate kafka record created_at to timestamptz

---
+++ mulighetsrommet-arena-adapter/src/main/resources/db/migration/V46__kafka_records.sql
@@ -0,0 +1,3 @@
+alter table kafka_consumer_record
+    alter last_retry type timestamptz using last_retry at time zone 'UTC',
+    alter created_at type timestamptz using created_at at time zone 'UTC';
