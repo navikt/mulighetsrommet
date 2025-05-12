c5fac1f1c6756364e666eb69b3e72e3d8132e72e
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-12 15:41:15 +0200

    rename test

--- mulighetsrommet-tiltakshistorikk/src/test/kotlin/no/nav/tiltak/historikk/db/DeltakerQueriesTest.kt
+++ mulighetsrommet-tiltakshistorikk/src/test/kotlin/no/nav/tiltak/historikk/db/DeltakerQueriesTest.kt
@@ -1,4 +1,4 @@
-package no.nav.tiltak.historikk
+package no.nav.tiltak.historikk.db
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldBeEmpty
@@ -8,13 +8,13 @@ import no.nav.amt.model.AmtDeltakerV1Dto
 import no.nav.mulighetsrommet.arena.ArenaDeltakerDbo
 import no.nav.mulighetsrommet.database.kotest.extensions.FlywayDatabaseTestListener
 import no.nav.mulighetsrommet.model.*
-import no.nav.tiltak.historikk.db.DeltakerQueries
-import no.nav.tiltak.historikk.db.GruppetiltakQueries
+import no.nav.tiltak.historikk.TestFixtures
+import no.nav.tiltak.historikk.databaseConfig
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
-class DeltakerRepositoryTest : FunSpec({
+class DeltakerQueriesTest : FunSpec({
     val database = extension(FlywayDatabaseTestListener(databaseConfig))
 
     val gruppeAmo = TestFixtures.tiltak
