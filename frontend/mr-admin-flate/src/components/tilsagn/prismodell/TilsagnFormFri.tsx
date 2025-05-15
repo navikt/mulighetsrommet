ee582d3b5a22aa73a1e6798ba077a86b4886af0a
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-05-15 16:28:25 +0200

    Vis prisbetingelser for fri prismodell i tilsagn, om tilgjengelig

--- frontend/mr-admin-flate/src/components/tilsagn/prismodell/TilsagnFormFri.tsx
+++ frontend/mr-admin-flate/src/components/tilsagn/prismodell/TilsagnFormFri.tsx
@@ -4,6 +4,7 @@ import { DeepPartial, useFormContext } from "react-hook-form";
 import { TextField } from "@navikt/ds-react";
 import { TilsagnBeregningPreview } from "@/components/tilsagn/prismodell/TilsagnBeregningPreview";
 import { InferredTilsagn } from "@/components/tilsagn/prismodell/TilsagnSchema";
+import { avtaletekster } from "@/components/ledetekster/avtaleLedetekster";
 
 type FriTilsagn = InferredTilsagn & { beregning: TilsagnBeregningFri };
 
@@ -13,33 +14,50 @@ interface Props {
   onAvbryt: () => void;
   defaultValues: DeepPartial<FriTilsagn>;
   regioner: string[];
+  prisbetingelser: string | null;
 }
 
 export function TilsagnFormFri(props: Props) {
   return (
     <TilsagnForm
       {...props}
-      beregningInput={<BeregningInputSkjema />}
+      beregningInput={<BeregningInputSkjema prisbetingelser={props.prisbetingelser} />}
       beregningOutput={<BeregningOutputPreview />}
     />
   );
 }
 
-function BeregningInputSkjema() {
+interface BeregningInputSkjemaProps {
+  prisbetingelser: string | null;
+}
+
+function BeregningInputSkjema({ prisbetingelser }: BeregningInputSkjemaProps) {
   const {
     register,
     formState: { errors },
   } = useFormContext<FriTilsagn>();
 
   return (
-    <TextField
-      size="small"
-      type="number"
-      label="Beløp"
-      style={{ width: "180px" }}
-      error={errors.beregning?.belop?.message}
-      {...register("beregning.belop", { valueAsNumber: true })}
-    />
+    <>
+      {prisbetingelser && (
+        <div className="pb-3">
+          <TextField
+          size="small"
+          value={prisbetingelser}
+          label={avtaletekster.prisOgBetalingLabel}
+          readOnly
+        />
+        </div>
+      )}
+      <TextField
+        size="small"
+        type="number"
+        label="Beløp"
+        style={{ width: "180px" }}
+        error={errors.beregning?.belop?.message}
+        {...register("beregning.belop", { valueAsNumber: true })}
+      />
+    </>
   );
 }
 
