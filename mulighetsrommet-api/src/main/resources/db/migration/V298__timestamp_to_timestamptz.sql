b45fafe9a9b1a42d49cffad9d3bea4ff47997653
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-16 07:46:30 +0200

    konverter behandlet- og besluttet-tidspunkt til timestamptz

--- mulighetsrommet-api/src/main/resources/db/migration/V298__timestamp_to_timestamptz.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V298__timestamp_to_timestamptz.sql
@@ -2,7 +2,9 @@ alter table altinn_person_rettighet
     alter expiry type timestamptz using expiry at time zone 'UTC';
 
 alter table totrinnskontroll
-    alter behandlet_tidspunkt drop default;
+    alter behandlet_tidspunkt drop default,
+    alter behandlet_tidspunkt type timestamptz using behandlet_tidspunkt at time zone 'Europe/Oslo',
+    alter besluttet_tidspunkt type timestamptz using besluttet_tidspunkt at time zone 'Europe/Oslo';
 
 alter table nav_ansatt
     alter skal_slettes_dato type date;
