d246335d8c5a4c0c9efa4c1d2abd483779c7933b
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-16 12:37:25 +0200

    migrer read_at til timestamptz

--- mulighetsrommet-api/src/main/resources/db/migration/V301__notifications.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V301__notifications.sql
@@ -5,3 +5,6 @@ drop type notification_type;
 
 alter table user_notification
     rename done_at to read_at;
+
+alter table user_notification
+    alter read_at type timestamptz using read_at::timestamptz at time zone 'Europe/Oslo';
