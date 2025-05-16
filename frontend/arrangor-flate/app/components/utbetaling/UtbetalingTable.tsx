be3c13959099213266739587cd73c52dcbc6b408
Sindre Moldeklev <sndrem@gmail.com>
2025-05-16 13:52:22 +0200

    Juster utbetalingstable

--- frontend/arrangor-flate/app/components/utbetaling/UtbetalingTable.tsx
+++ frontend/arrangor-flate/app/components/utbetaling/UtbetalingTable.tsx
@@ -1,18 +1,58 @@
 import { formaterNOK } from "@mr/frontend-common/utils/utils";
-import { Alert, Table } from "@navikt/ds-react";
-import React from "react";
+import { Alert, SortState, Table } from "@navikt/ds-react";
+import React, { useState } from "react";
 import { formaterDato, formaterPeriode, useOrgnrFromUrl } from "~/utils";
 import { internalNavigation } from "~/internal-navigation";
 import { LinkWithTabState } from "../LinkWithTabState";
 import { ArrFlateUtbetalingKompakt, ArrFlateUtbetalingStatus } from "api-client";
 import { UtbetalingStatusTag } from "./UtbetalingStatusTag";
+import { data } from "react-router";
 
 interface Props {
   utbetalinger: ArrFlateUtbetalingKompakt[];
 }
 
+interface ScopedSortState extends SortState {
+  orderBy: keyof ArrFlateUtbetalingKompakt;
+}
+
 export function UtbetalingTable({ utbetalinger }: Props) {
   const orgnr = useOrgnrFromUrl();
+
+  const [sort, setSort] = useState<ScopedSortState | undefined>();
+
+  const handleSort = (sortKey: ScopedSortState["orderBy"]) => {
+    setSort(
+      sort && sortKey === sort.orderBy && sort.direction === "descending"
+        ? undefined
+        : {
+            orderBy: sortKey,
+            direction:
+              sort && sortKey === sort.orderBy && sort.direction === "ascending"
+                ? "descending"
+                : "ascending",
+          },
+    );
+  };
+
+  function comparator<T>(a: T, b: T, orderBy: keyof T): number {
+    if (b[orderBy] == null || b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortedData = utbetalinger.slice().sort((a, b) => {
+    if (sort) {
+      return sort.direction === "ascending"
+        ? comparator(b, a, sort.orderBy)
+        : comparator(a, b, sort.orderBy);
+    }
+    return 1;
+  });
 
   if (utbetalinger.length === 0) {
     return (
@@ -22,25 +62,38 @@ export function UtbetalingTable({ utbetalinger }: Props) {
     );
   }
   return (
-    <Table aria-label="Utbetalinger">
+    <Table
+      aria-label="Utbetalinger"
+      zebraStripes
+      sort={sort}
+      onSortChange={(sortKey) => handleSort(sortKey as ScopedSortState["orderBy"])}
+    >
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell scope="col">Tiltakstype</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
-          <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
-          <Table.HeaderCell align="right" scope="col" className="min-w-32">
+          <Table.ColumnHeader scope="col">Tiltakstype</Table.ColumnHeader>
+          <Table.ColumnHeader scope="col">Navn</Table.ColumnHeader>
+          <Table.ColumnHeader scope="col">Periode</Table.ColumnHeader>
+          <Table.ColumnHeader scope="col" sortable sortKey="fristForGodkjenning">
+            Frist for godkjenning
+          </Table.ColumnHeader>
+          <Table.ColumnHeader
+            align="right"
+            scope="col"
+            className="min-w-32"
+            sortable
+            sortKey="belop"
+          >
             Beløp
-          </Table.HeaderCell>
-          <Table.HeaderCell scope="col">Frist for godkjenning</Table.HeaderCell>
-          <Table.HeaderCell scope="col" className="min-w-44">
+          </Table.ColumnHeader>
+          <Table.ColumnHeader scope="col" className="min-w-44" sortable sortKey="status">
             Status
-          </Table.HeaderCell>
-          <Table.HeaderCell scope="col" className="w-10"></Table.HeaderCell>
-          <Table.HeaderCell scope="col"></Table.HeaderCell>
+          </Table.ColumnHeader>
+          <Table.ColumnHeader scope="col" className="w-10"></Table.ColumnHeader>
+          <Table.ColumnHeader scope="col"></Table.ColumnHeader>
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {utbetalinger.map(
+        {sortedData.map(
           ({ id, status, fristForGodkjenning, belop, periode, gjennomforing, tiltakstype }) => {
             return (
               <React.Fragment key={id}>
@@ -48,8 +101,8 @@ export function UtbetalingTable({ utbetalinger }: Props) {
                   <Table.DataCell>{tiltakstype.navn}</Table.DataCell>
                   <Table.DataCell>{gjennomforing.navn}</Table.DataCell>
                   <Table.DataCell>{formaterPeriode(periode)}</Table.DataCell>
-                  <Table.DataCell align="right">{formaterNOK(belop)}</Table.DataCell>
                   <Table.DataCell>{formaterDato(fristForGodkjenning)}</Table.DataCell>
+                  <Table.DataCell align="right">{formaterNOK(belop)}</Table.DataCell>
                   <Table.DataCell>
                     <UtbetalingStatusTag status={status} />
                   </Table.DataCell>
