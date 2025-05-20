15fbdc38f89cb3798632d9373c4354b8261a8b61
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-20 08:50:57 +0200

    endre fra Miljø til Environment

--- frontend/arrangor-flate/app/services/dekoratør/dekorator.server.ts
+++ frontend/arrangor-flate/app/services/dekoratør/dekorator.server.ts
@@ -1,5 +1,5 @@
 import { fetchDecoratorHtml } from "@navikt/nav-dekoratoren-moduler/ssr";
-import { hentMiljø, Miljø } from "../miljø";
+import { Environment, getEnvironment } from "~/services/environment";
 
 const visDekoratørUnderUtvikling = true;
 const brukSsrDekoratørIMiljø = true;
@@ -12,18 +12,18 @@ export type Dekoratørfragmenter = {
 };
 
 export const hentSsrDekoratør = async (): Promise<Dekoratørfragmenter | null> => {
-  const miljø = hentMiljø();
+  const env = getEnvironment();
 
   try {
-    if (miljø === Miljø.Lokalt) {
+    if (env === Environment.Lokalt) {
       if (visDekoratørUnderUtvikling) {
-        return await hentDekoratør(miljø);
+        return await hentDekoratør(env);
       } else {
         return null;
       }
     } else {
       if (brukSsrDekoratørIMiljø) {
-        return await hentDekoratør(miljø);
+        return await hentDekoratør(env);
       } else {
         return null;
       }
@@ -36,9 +36,9 @@ export const hentSsrDekoratør = async (): Promise<Dekoratørfragmenter | null> 
   }
 };
 
-export const hentDekoratør = async (miljø: Miljø): Promise<Dekoratørfragmenter> => {
+export const hentDekoratør = async (env: Environment): Promise<Dekoratørfragmenter> => {
   const decorator = await fetchDecoratorHtml({
-    env: miljø === Miljø.ProdGcp ? "prod" : "dev",
+    env: env === Environment.ProdGcp ? "prod" : "dev",
     params: {
       simple: false,
       simpleFooter: true,
---
+++ frontend/arrangor-flate/app/services/environment.ts
@@ -0,0 +1,35 @@
+export enum Environment {
+  DevGcp = "dev-gcp",
+  ProdGcp = "prod-gcp",
+  Lokalt = "lokalt",
+}
+
+const erHosKlient = () => typeof document !== "undefined";
+
+export const getEnvironment = () => {
+  return erHosKlient() ? getClientEnvironment(window) : getServerEnvironment();
+};
+
+const getServerEnvironment = () => {
+  const clusterName = process.env.NAIS_CLUSTER_NAME;
+
+  if (clusterName === Environment.DevGcp) {
+    return Environment.DevGcp;
+  } else if (clusterName === Environment.ProdGcp) {
+    return Environment.ProdGcp;
+  } else {
+    return Environment.Lokalt;
+  }
+};
+
+const getClientEnvironment = (window: Window) => {
+  const { href } = window.location;
+
+  if (href.includes("dev.nav.no")) {
+    return Environment.DevGcp;
+  } else if (href.includes("nav.no")) {
+    return Environment.ProdGcp;
+  } else {
+    return Environment.Lokalt;
+  }
+};
