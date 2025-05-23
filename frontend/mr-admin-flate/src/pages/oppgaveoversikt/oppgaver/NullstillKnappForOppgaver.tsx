543a17b5edeb124ce189721f65ea038dbdee7a71
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-05-23 19:15:43 +0200

    samme styling av nullstill-knapper

--- frontend/mr-admin-flate/src/pages/oppgaveoversikt/oppgaver/NullstillKnappForOppgaver.tsx
+++ frontend/mr-admin-flate/src/pages/oppgaveoversikt/oppgaver/NullstillKnappForOppgaver.tsx
@@ -1,6 +1,5 @@
 import { OppgaverFilterType } from "@/api/atoms";
 import { NullstillFilterKnapp } from "@mr/frontend-common/components/nullstillFilterKnapp/NullstillFilterKnapp";
-import { HStack } from "@navikt/ds-react";
 
 interface Props {
   filter: OppgaverFilterType;
@@ -8,13 +7,7 @@ interface Props {
 }
 
 export function NullstillKnappForOppgaver({ filter, resetFilter }: Props) {
-  return (
-    <div className="grid grid-cols-[auto auto] h-[100%] items-center">
-      {filter.type.length > 0 || filter.regioner.length > 0 || filter.tiltakstyper.length > 0 ? (
-        <HStack gap="2">
-          <NullstillFilterKnapp onClick={() => resetFilter()} />
-        </HStack>
-      ) : null}
-    </div>
-  );
+  return filter.type.length > 0 || filter.regioner.length > 0 || filter.tiltakstyper.length > 0 ? (
+    <NullstillFilterKnapp onClick={() => resetFilter()} />
+  ) : null;
 }
