1a76a89fd4fc063a4251a35c64e1a183fbd02a74
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-06-04 07:00:27 +0200

    oppdater executionContexts til å være basert på kombinasjon av task name og task id

--- common/tasks/src/main/kotlin/no/nav/mulighetsrommet/tasks/OpenTelemetrySchedulerListener.kt
+++ common/tasks/src/main/kotlin/no/nav/mulighetsrommet/tasks/OpenTelemetrySchedulerListener.kt
@@ -20,39 +20,35 @@ import kotlin.jvm.optionals.getOrNull
 class OpenTelemetrySchedulerListener : SchedulerListener {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    private val executionContexts = ConcurrentHashMap<String, ExecutionContext>()
-
-    private data class ExecutionContext(val span: Span, val scope: Scope)
+    private val executionContexts = ConcurrentHashMap<IdentifiedTask, ExecutionContext>()
 
     override fun onExecutionStart(currentlyExecuting: CurrentlyExecuting) {
-        val executionId = currentlyExecuting.execution.id
-        val taskName = currentlyExecuting.execution.taskName
+        val task = IdentifiedTask.fromExecution(currentlyExecuting.execution)
 
         val tracer: Tracer = GlobalOpenTelemetry.getTracer("application")
 
-        val span = tracer.spanBuilder(taskName)
-            .setAttribute(AttributeKey.stringKey(Attributes.TASK_ID_ATTRIBUTE), executionId)
-            .setAttribute(AttributeKey.stringKey(Attributes.TASK_NAME_ATTRIBUTE), taskName)
+        val span = tracer.spanBuilder(task.name)
+            .setAttribute(AttributeKey.stringKey(Attributes.TASK_ID_ATTRIBUTE), task.id)
+            .setAttribute(AttributeKey.stringKey(Attributes.TASK_NAME_ATTRIBUTE), task.name)
             .startSpan()
 
         val scope = Context.current().with(span).makeCurrent()
 
-        if (executionContexts.containsKey(executionId)) {
-            log.warn("Found existing execution context for task name=$taskName id=$executionId. This should not happen.")
+        if (executionContexts.containsKey(task)) {
+            log.warn("Found existing execution context for task=$task. This should not happen.")
         }
-        executionContexts[executionId] = ExecutionContext(span, scope)
+        executionContexts[task] = ExecutionContext(span, scope)
 
-        log.info("Task started name=$taskName id=$executionId")
+        log.info("Task started task=$task")
     }
 
     override fun onExecutionComplete(executionComplete: ExecutionComplete) {
-        val executionId = executionComplete.execution.id
-        val taskName = executionComplete.execution.taskName
-        log.info("Task finished name=$taskName id=$executionId")
+        val task = IdentifiedTask.fromExecution(executionComplete.execution)
+        log.info("Task finished task=$task")
 
-        val context = executionContexts.remove(executionId)
+        val context = executionContexts.remove(task)
         if (context == null) {
-            log.warn("Expected to find an execution context for task name=$taskName id=$executionId. Execution context was not found.")
+            log.warn("Expected to find an execution context for task=$task. Execution context was not found.")
             return
         }
 
@@ -82,3 +78,14 @@ class OpenTelemetrySchedulerListener : SchedulerListener {
 
     override fun onCandidateEvent(type: SchedulerListener.CandidateEventType) = Unit
 }
+
+private data class IdentifiedTask(val name: String, val id: String) {
+    companion object {
+        fun fromExecution(execution: Execution) = IdentifiedTask(
+            name = execution.taskName,
+            id = execution.id,
+        )
+    }
+}
+
+private data class ExecutionContext(val span: Span, val scope: Scope)
