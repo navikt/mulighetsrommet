cee13ba755bfef5812f0d02e0c4a7d1d55d8ee50
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-06-23 14:58:42 +0200

    gi oss tilgang til tabeller i arena-adapter for enklere vedlikehold

---
+++ mulighetsrommet-arena-adapter/src/main/resources/db/migration/R__privileges_cloudsqliamuser.sql
@@ -0,0 +1,11 @@
+do
+$$
+    begin
+        if exists (select 1 from pg_roles where rolname = 'cloudsqliamuser') then
+            grant insert, update, delete on table topics to cloudsqliamuser;
+            grant insert, update, delete on table kafka_consumer_record to cloudsqliamuser;
+            grant insert, update, delete on table scheduled_tasks to cloudsqliamuser;
+            grant insert, update, delete on table arena_events to cloudsqliamuser;
+        end if;
+    end
+$$;
