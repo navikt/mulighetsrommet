072a1f9aedefdd416559f80f3792410b9972b488
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-06-25 15:41:14 +0200

    Endre lenke-tekst på utbetalinger som venter på veileder
    Veileder må ta stilling til endringsforslag

---
+++ frontend/arrangor-flate/app/components/utbetaling/UtbetalingTextLink.tsx
@@ -0,0 +1,54 @@
+import { Link } from "@navikt/ds-react";
+import { ArrFlateUtbetalingStatus } from "api-client";
+import { Link as ReactRouterLink } from "react-router";
+import { internalNavigation } from "~/internal-navigation";
+
+interface UtbetalingTextLinkProps {
+  status: ArrFlateUtbetalingStatus;
+  gjennomforingNavn: string;
+  utbetalingId: string;
+  orgnr: string;
+}
+
+export function UtbetalingTextLink({
+  status,
+  gjennomforingNavn,
+  utbetalingId,
+  orgnr,
+}: UtbetalingTextLinkProps) {
+  switch (status) {
+    case ArrFlateUtbetalingStatus.KLAR_FOR_GODKJENNING: {
+      return (
+        <Link
+          as={ReactRouterLink}
+          aria-label={`Start innsending for krav om utbetaling for ${gjennomforingNavn}`}
+          to={internalNavigation(orgnr).innsendingsinformasjon(utbetalingId)}
+        >
+          Start innsending
+        </Link>
+      );
+    }
+    case ArrFlateUtbetalingStatus.VENTER_PA_ENDRING: {
+      return (
+        <Link
+          as={ReactRouterLink}
+          aria-label={`Se innsending for krav om utbetaling for ${gjennomforingNavn}`}
+          to={internalNavigation(orgnr).innsendingsinformasjon(utbetalingId)}
+        >
+          Se hvorfor
+        </Link>
+      );
+    }
+    default: {
+      return (
+        <Link
+          as={ReactRouterLink}
+          aria-label={`Detaljer for krav om utbetaling for ${gjennomforingNavn}`}
+          to={internalNavigation(orgnr).detaljer(utbetalingId)}
+        >
+          Detaljer
+        </Link>
+      );
+    }
+  }
+}
