c7130276e4434828feb2aaee5d69c412318a024b
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-07-01 14:22:01 +0200

    fjern routes som ikke er i bruk

--- mulighetsrommet-tiltaksokonomi/src/main/kotlin/no/nav/tiltak/okonomi/api/Api.kt
+++ mulighetsrommet-tiltaksokonomi/src/main/kotlin/no/nav/tiltak/okonomi/api/Api.kt
@@ -4,21 +4,17 @@ import io.ktor.server.application.*
 import io.ktor.server.resources.*
 import io.ktor.server.routing.*
 import no.nav.mulighetsrommet.kafka.KafkaConsumerOrchestrator
-import no.nav.tiltak.okonomi.db.OkonomiDatabase
 import no.nav.tiltak.okonomi.service.OkonomiService
 
 const val API_BASE_PATH = "/api/v1/okonomi"
 
 fun Application.configureApi(
     kafka: KafkaConsumerOrchestrator,
-    db: OkonomiDatabase,
     okonomi: OkonomiService,
 ) = routing {
     install(Resources)
 
     maamRoutes(kafka)
 
-    bestillingRoutes(db, okonomi)
-    fakturaRoutes(db, okonomi)
     oebsRoutes(okonomi)
 }
