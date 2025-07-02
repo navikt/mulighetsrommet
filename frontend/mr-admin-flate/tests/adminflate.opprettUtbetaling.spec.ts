fd32d3cdb05bf926087ad0a8bd632f46fba01c66
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-07-02 08:00:34 +0200

    gjenbruk navigering på tvers av tester

--- frontend/mr-admin-flate/tests/adminflate.opprettUtbetaling.spec.ts
+++ frontend/mr-admin-flate/tests/adminflate.opprettUtbetaling.spec.ts
@@ -1,28 +1,28 @@
-import { expect, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.setViewportSize({ width: 1920, height: 1920 });
   await page.goto("/");
 });
 
-const korreksjonsKnappTekst = "Opprett korreksjon på utbetaling"
+const korreksjonsKnappTekst = "Opprett korreksjon på utbetaling";
 
-test.describe("Manuell utbetaling", () => {
+async function navigateToUtbetalingerSkjema(page: Page) {
+  await page.getByRole("link", { name: "Gjennomføringer" }).nth(0).click();
+  await page.locator("table").getByRole("cell").first().click();
+  await page.getByRole("tab", { name: "Utbetalinger" }).click();
+  await page.getByRole("button", { name: "Handlinger" }).click();
+  await page.getByRole("button", { name: korreksjonsKnappTekst }).click();
+}
+
+test.describe("Opprett utbetaling", () => {
   test("Kan navigere til skjema for å opprette utbetalinger", async ({ page }) => {
-    await page.getByRole("link", { name: "Gjennomføringer" }).nth(0).click();
-    await page.locator("table").getByRole("cell").first().click();
-    await page.getByRole("tab", { name: "Utbetalinger" }).click();
-    await page.getByRole("button", { name: "Handlinger" }).click();
-    await page.getByRole("button", { name: korreksjonsKnappTekst }).click();
+    await navigateToUtbetalingerSkjema(page);
     await expect(page).toHaveURL(/.*\/skjema$/);
   });
 
   test("Skjema for opprett utbetalinger validerer", async ({ page }) => {
-    await page.getByRole("link", { name: "Gjennomføringer" }).nth(0).click();
-    await page.locator("table").getByRole("cell").first().click();
-    await page.getByRole("tab", { name: "Utbetalinger" }).click();
-    await page.getByRole("button", { name: "Handlinger" }).click();
-    await page.getByRole("button", { name: korreksjonsKnappTekst }).click();
+    await navigateToUtbetalingerSkjema(page);
     await page.getByRole("button", { name: "Opprett" }).click();
     await expect(page.getByText("Du må sette startdato for perioden")).toBeVisible();
     await expect(page.getByText("Du må sette sluttdato for perioden")).toBeVisible();
@@ -31,11 +31,7 @@ test.describe("Manuell utbetaling", () => {
   });
 
   test("Skjema for opprett utbetalinger sender bruker til kostnadsfordeling", async ({ page }) => {
-    await page.getByRole("link", { name: "Gjennomføringer" }).nth(0).click();
-    await page.locator("table").getByRole("cell").first().click();
-    await page.getByRole("tab", { name: "Utbetalinger" }).click();
-    await page.getByRole("button", { name: "Handlinger" }).click();
-    await page.getByRole("button", { name: korreksjonsKnappTekst }).click();
+    await navigateToUtbetalingerSkjema(page);
     await page.getByLabel("Periodestart").fill("01.01.2025");
     await page.getByLabel("Periodeslutt").fill("31.01.2025");
     await page.getByLabel("beløp").fill("100");
