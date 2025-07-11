73e70c22b759def0b4b29e9ee3470d7bcf436dea
Benedicte Myrvoll <benemyr17@gmail.com>
2025-07-11 15:35:17 +0200

    Opprydning tailwind

--- frontend/mr-admin-flate/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
