7e71ee3fe3a83880aba0512e7f763909cb536069
Benedicte Myrvoll <benemyr17@gmail.com>
2025-07-11 15:34:49 +0200

    Fiks oppgavelink

--- frontend/mr-admin-flate/src/components/oppgaver/Oppgave.tsx
+++ frontend/mr-admin-flate/src/components/oppgaver/Oppgave.tsx
@@ -1,8 +1,9 @@
 import { formaterDato } from "@/utils/Utils";
 import { type Oppgave, OppgaveEnhet, OppgaveIconType, OppgaveType } from "@mr/api-client-v2";
 import { BankNoteIcon, HandshakeIcon, PiggybankIcon } from "@navikt/aksel-icons";
-import { BodyShort, Box, HStack, LinkCard, Tag } from "@navikt/ds-react";
+import { BodyShort, Box, LinkCard, Spacer, Tag, TagProps } from "@navikt/ds-react";
 import { ReactNode } from "react";
+import { Link } from "react-router";
 
 interface OppgaveProps {
   oppgave: Oppgave;
@@ -16,27 +17,26 @@ export function Oppgave({ oppgave }: OppgaveProps) {
         asChild
         borderRadius="12"
         padding="space-8"
-        style={{ backgroundColor: "var(--ax-bg-moderateA)" }}
+        style={{ backgroundColor: "var(--a-grayalpha-100)" }}
       >
         <LinkCard.Icon>
-          <div className="inline-flex items-center justify-center p-2 bg-gray-200 rounded-xl">
-            <OppgaveIcon type={iconType} fontSize="2rem" />
-          </div>
+          <OppgaveIcon type={iconType} fontSize="2rem" />
         </LinkCard.Icon>
       </Box>
       <LinkCard.Title>
-        <LinkCard.Anchor href={link.link}>{title}</LinkCard.Anchor>
+        <LinkCard.Anchor asChild>
+          <Link to={link.link}>{title}</Link>
+        </LinkCard.Anchor>
       </LinkCard.Title>
       <LinkCard.Description>{description}</LinkCard.Description>
-      <LinkCard.Footer className="flex justify-between items-center">
-        <HStack gap="2">
-          <OppgaveStatus
-            type={oppgave.type}
-            label={oppgave.navn}
-            icon={<OppgaveIcon type={iconType} />}
-          />
-          <OppgaveEnhetTag enhet={oppgave.enhet} />
-        </HStack>
+      <LinkCard.Footer>
+        <OppgaveStatus
+          type={oppgave.type}
+          label={oppgave.navn}
+          icon={<OppgaveIcon type={iconType} />}
+        />
+        <OppgaveEnhetTag enhet={oppgave.enhet} />
+        <Spacer />
         <BodyShort textColor="subtle" size="small">
           Opprettet {formaterDato(createdAt)}
         </BodyShort>
@@ -57,20 +57,23 @@ function OppgaveIcon(props: { type: OppgaveIconType; fontSize?: string }) {
   }
 }
 
-const POSITIVE_COLOR = "#FFD799";
-const NEGATIVE_COLOR = "#FFC2C2";
-
-const oppgaveColor: Record<OppgaveType, string> = {
-  TILSAGN_TIL_GODKJENNING: POSITIVE_COLOR,
-  TILSAGN_TIL_ANNULLERING: NEGATIVE_COLOR,
-  TILSAGN_TIL_OPPGJOR: NEGATIVE_COLOR,
-  TILSAGN_RETURNERT: NEGATIVE_COLOR,
-  UTBETALING_TIL_ATTESTERING: POSITIVE_COLOR,
-  UTBETALING_RETURNERT: NEGATIVE_COLOR,
-  UTBETALING_TIL_BEHANDLING: POSITIVE_COLOR,
-  AVTALE_MANGLER_ADMINISTRATOR: NEGATIVE_COLOR,
-  GJENNOMFORING_MANGLER_ADMINISTRATOR: NEGATIVE_COLOR,
-};
+function getOppgaveVariant(type: OppgaveType): TagProps["variant"] {
+  switch (type) {
+    case (OppgaveType.TILSAGN_TIL_ANNULLERING,
+    OppgaveType.TILSAGN_TIL_OPPGJOR,
+    OppgaveType.TILSAGN_RETURNERT,
+    OppgaveType.UTBETALING_RETURNERT,
+    OppgaveType.AVTALE_MANGLER_ADMINISTRATOR,
+    OppgaveType.GJENNOMFORING_MANGLER_ADMINISTRATOR):
+      return "warning-moderate";
+    case (OppgaveType.TILSAGN_TIL_GODKJENNING,
+    OppgaveType.UTBETALING_TIL_ATTESTERING,
+    OppgaveType.UTBETALING_TIL_BEHANDLING):
+      return "error-moderate";
+    default:
+      return "warning-moderate";
+  }
+}
 
 interface OppgaveStatusProps {
   type: OppgaveType;
@@ -79,17 +82,12 @@ interface OppgaveStatusProps {
 }
 
 function OppgaveStatus({ type, label, icon }: OppgaveStatusProps) {
-  const color = oppgaveColor[type];
+  const variant = getOppgaveVariant(type);
 
   return (
-    <div
-      style={{
-        backgroundColor: color,
-      }}
-      className="p-1 h-6 flex items-center gap-2 min-w-[230px] justify-center"
-    >
-      {icon} {label}
-    </div>
+    <Tag size="small" variant={variant} icon={icon}>
+      {label}
+    </Tag>
   );
 }
 
