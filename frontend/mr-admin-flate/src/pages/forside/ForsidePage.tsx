3096a65628ed9acdd6b91b7b90e5b960d950e7cd
Benedicte Myrvoll <benemyr17@gmail.com>
2025-07-31 15:28:08 +0200

    Sentrer forsidekort

--- frontend/mr-admin-flate/src/pages/forside/ForsidePage.tsx
+++ frontend/mr-admin-flate/src/pages/forside/ForsidePage.tsx
@@ -1,18 +1,17 @@
 import { ForsidekortListe } from "@/pages/forside/forsidekort/ForsidekortListe";
 import { BrukerNotifikasjoner } from "@/components/notifikasjoner/BrukerNotifikasjoner";
 import { HeroBanner } from "@/pages/forside/HeroBanner";
+import { VStack } from "@navikt/ds-react";
 
 export function ForsidePage() {
   return (
     <>
       <title>Nav Tiltaksadministrasjon</title>
-      <div className="bg-[var(--a-deepblue-800)] p-16 min-h-[15rem] w-full mb-6">
-        <HeroBanner />
-      </div>
-      <div className="relative flex flex-col justify-center mx-auto max-w-[1280px] px-4 lg:px-0">
+      <HeroBanner />
+      <VStack gap="4" justify="center" className="mx-auto lg:w-5xl max-w-[1280px] px-4">
         <BrukerNotifikasjoner />
         <ForsidekortListe />
-      </div>
+      </VStack>
     </>
   );
 }
--- frontend/mr-admin-flate/src/pages/forside/forsidekort/Forsidekort.tsx
+++ frontend/mr-admin-flate/src/pages/forside/forsidekort/Forsidekort.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Heading } from "@navikt/ds-react";
+import { BodyShort, Box, Heading, VStack } from "@navikt/ds-react";
 import { Link } from "react-router";
 import { kebabCase } from "../../../../../frontend-common/utils/TestUtils";
 import { ReactNode } from "react";
@@ -13,20 +13,29 @@ export interface ForsideKortProps {
 
 export function Forsidekort({ navn, ikon, url, tekst, apneINyTab = false }: ForsideKortProps) {
   return (
-    <Link
+    <Box
+      as={Link}
+      background="bg-default"
+      borderRadius="4"
       key={url}
-      className="text-text-default w-[370px] h-[350px] bg-white items-center grid grid-rows-[auto,1fr,1fr] p-12 text-center shadow-md hover:shadow-lg transition-all duration-150 ease-in-out no-underline text-black rounded"
+      className="text-text-default shadow-md hover:shadow-lg transition-all duration-150 ease-in-out"
       to={url}
       {...(apneINyTab ? { target: "_blank", rel: "noopener noreferrer" } : {})}
       data-testid={`forsidekort-${kebabCase(navn)}`}
     >
-      <span className="flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto mb-2">
-        <div className="[&>svg]:w-16 [&>svg]:h-16">{ikon}</div>
-      </span>
-      <Heading size="medium" level="3">
-        {navn}
-      </Heading>
-      {tekst ? <BodyShort className="text-gray-600">{tekst}</BodyShort> : null}
-    </Link>
+      <VStack align="center" gap="4" padding="12">
+        <span className="flex justify-center items-center w-[100px] h-[100px] rounded-full mx-auto">
+          <div className="[&>svg]:w-16 [&>svg]:h-16">{ikon}</div>
+        </span>
+        <Heading align="center" size="medium" level="3">
+          {navn}
+        </Heading>
+        {tekst ? (
+          <BodyShort align="center" className="text-gray-600">
+            {tekst}
+          </BodyShort>
+        ) : null}
+      </VStack>
+    </Box>
   );
 }
