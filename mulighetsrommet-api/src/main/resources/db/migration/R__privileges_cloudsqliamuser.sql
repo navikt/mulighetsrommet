e47e2dca55c98b73e94bbf812b60bf7e2a693375
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-08-04 18:13:47 +0200

    oppdater script til å automatisk fjerne rettigheter som ikke eksplisitt er nevnt

--- mulighetsrommet-api/src/main/resources/db/migration/R__privileges_cloudsqliamuser.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/R__privileges_cloudsqliamuser.sql
@@ -1,11 +1,24 @@
+--
+-- Dette scriptet kan benyttes til å gi deg midlertidige skriverettigheter (via gruppen cloudsqliamuser) til ønskede tabeller.
+--
+-- Hver gang scriptet kjøres vil først alle rettigheter fjernes, før de deretter gis på nytt.
+-- Det holder derfor å legge til (eller fjerne) grants til ønskede tabeller uten å eksplisitt måtte huske å kjøre `revoke` selv.
+--
 do
 $$
     begin
         if exists (select 1 from pg_roles where rolname = 'cloudsqliamuser') then
+            -- Fjern alle skriverettigheter
+            execute (
+                select string_agg('revoke insert, update, delete on table ' || quote_ident(tablename) || ' from cloudsqliamuser;', E'\n')
+                from pg_tables
+                where schemaname = 'public'
+            );
+
+            -- Deretter gir vi skriverettigheter til ønskede tabeller
             grant insert, update, delete on table topics to cloudsqliamuser;
             grant insert, update, delete on table kafka_consumer_record to cloudsqliamuser;
             grant insert, update, delete on table scheduled_tasks to cloudsqliamuser;
-            grant insert, update, delete on table del_med_bruker to cloudsqliamuser;
         end if;
     end
 $$;
