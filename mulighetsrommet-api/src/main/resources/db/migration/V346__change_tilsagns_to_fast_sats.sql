1ac7387a9f5d40bd72541256adcfc09d398e5a3e
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-08-21 10:09:29 +0200

    Oppdater migrering til fast sats beregningstype

--- mulighetsrommet-api/src/main/resources/db/migration/V346__change_tilsagns_to_fast_sats.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V346__change_tilsagns_to_fast_sats.sql
@@ -6,5 +6,4 @@ inner join avtale a on g.avtale_id = a.id
 where
     g.id = t.gjennomforing_id
   and  a.prismodell = 'FORHANDSGODKJENT_PRIS_PER_MANEDSVERK'
-  and t.beregning_type = 'PRIS_PER_MANEDSVERK'
-  and a.id = 'e478ce1f-1ab9-466d-9314-50bb3f3be745';
+  and t.beregning_type = 'PRIS_PER_MANEDSVERK';
