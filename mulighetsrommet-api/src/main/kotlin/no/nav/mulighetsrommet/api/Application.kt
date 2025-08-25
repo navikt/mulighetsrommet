518aaa07076911d5556e6a90000b89ef87d423d4
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-08-25 07:57:11 +0200

    trekk ut openapi-config til egen plugin

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/Application.kt
@@ -1,24 +1,15 @@
 package no.nav.mulighetsrommet.api
 
 import com.github.kagkarlsson.scheduler.Scheduler
-import io.github.smiley4.ktoropenapi.OpenApi
-import io.github.smiley4.ktoropenapi.config.OutputFormat
-import io.github.smiley4.ktoropenapi.config.SchemaGenerator
-import io.github.smiley4.ktoropenapi.config.SchemaOverwriteModule
-import io.github.smiley4.schemakenerator.swagger.data.RefType
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
-import io.swagger.v3.oas.models.media.Schema
-import kotlinx.serialization.json.Json
 import no.nav.common.job.leader_election.ShedLockLeaderElectionClient
 import no.nav.common.kafka.producer.feilhandtering.KafkaProducerRecordProcessor
 import no.nav.mulighetsrommet.api.plugins.*
-import no.nav.mulighetsrommet.api.routes.OpenApiSpec
 import no.nav.mulighetsrommet.api.routes.apiRoutes
-import no.nav.mulighetsrommet.clamav.Vedlegg
 import no.nav.mulighetsrommet.database.Database
 import no.nav.mulighetsrommet.database.FlywayMigrationManager
 import no.nav.mulighetsrommet.env.NaisEnv
@@ -28,8 +19,6 @@ import no.nav.mulighetsrommet.ktor.plugins.configureMetrics
 import no.nav.mulighetsrommet.ktor.plugins.configureMonitoring
 import no.nav.mulighetsrommet.ktor.plugins.configureStatusPages
 import no.nav.mulighetsrommet.metrics.Metrics
-import no.nav.mulighetsrommet.model.PortableTextTypedObject
-import no.nav.mulighetsrommet.model.ProblemDetail
 import org.koin.ktor.ext.inject
 import kotlin.time.Duration.Companion.seconds
 
@@ -71,169 +60,7 @@ fun Application.configure(config: AppConfig) {
     configureMonitoring({ db.isHealthy() })
     configureSerialization()
     configureStatusPages()
-
-    install(OpenApi) {
-        outputFormat = OutputFormat.YAML
-
-        pathFilter = { _, urlParts ->
-            val url = urlParts.joinToString("/", prefix = "/")
-            OpenApiSpec.match(url) != null
-        }
-
-        specAssigner = { url, _ ->
-            OpenApiSpec.match(url)?.specName ?: throw IllegalStateException("Failed to resolve OpenApiSpec for $url")
-        }
-
-        schemas {
-            generator = SchemaGenerator.kotlinx(
-                Json {
-                    /**
-                     * Hvis vi setter denne til true så vil generert skjema for nullable felter ha "null" som et valg,
-                     * f.eks. allOf: ["null", "string"] og property blir samtidig markert som required.
-                     */
-                    explicitNulls = true
-                    /**
-                     * Gjør at felter som har en default-verdi i Kotlin blir markert som required i skjemaet.
-                     */
-                    encodeDefaults = true
-                },
-            ) {
-                /**
-                 * Gjør at generert skjema ikke har "null" som et valg for nullable felter - altså at property
-                 * ikke eksplisitt kan settes til null, men må tas ut av f.eks. en request.
-                 *
-                 * Default er true, noe som gjør at skjema blir generert med både f.eks. allOf: ["null", "string"]
-                 * og at property ikke er required.
-                 */
-                //  explicitNullTypes = false
-
-                referencePath = RefType.SIMPLE
-                title = null
-
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "UUIDCustom",
-                        schema = {
-                            Schema<Any>().also {
-                                it.types = setOf("string")
-                                it.format = "uuid"
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "LocalDateTime",
-                        schema = {
-                            Schema<Any>().also {
-                                it.types = setOf("string")
-                                it.format = "date-time"
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "LocalDate",
-                        schema = {
-                            Schema<Any>().also {
-                                it.types = setOf("string")
-                                it.format = "date"
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "kotlin.ByteArray",
-                        schema = {
-                            Schema<Any>().also {
-                                it.types = setOf("string")
-                                it.format = "binary"
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = Vedlegg::class.qualifiedName!!,
-                        schema = {
-                            Schema<Vedlegg>().apply {
-                                types = setOf("string")
-                                format = "binary"
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "PortableTextTypedObject",
-                        schema = {
-                            Schema<PortableTextTypedObject>().apply {
-                                type = "object"
-                                addProperty(
-                                    "_type",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                addProperty(
-                                    "_key",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                required = listOf("_type")
-                                additionalProperties = Schema<Any>()
-                            }
-                        },
-                    ),
-                )
-                overwrite(
-                    SchemaOverwriteModule(
-                        identifier = "ProblemDetail",
-                        schema = {
-                            Schema<ProblemDetail>().apply {
-                                type = "object"
-                                addProperty(
-                                    "type",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                addProperty(
-                                    "title",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                addProperty(
-                                    "status",
-                                    Schema<String>().apply {
-                                        types = setOf("integer")
-                                    },
-                                )
-                                addProperty(
-                                    "detail",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                addProperty(
-                                    "instance",
-                                    Schema<String>().apply {
-                                        types = setOf("string")
-                                    },
-                                )
-                                required = listOf("type", "title", "status", "detail")
-                                additionalProperties = Schema<Any>()
-                            }
-                        },
-                    ),
-                )
-            }
-        }
-    }
+    configureOpenApiGenerator()
 
     FlywayMigrationManager(config.flyway).migrate(db)
 
---
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/OpenApiGenerator.kt
@@ -0,0 +1,209 @@
+package no.nav.mulighetsrommet.api.plugins
+
+import io.github.smiley4.ktoropenapi.OpenApi
+import io.github.smiley4.ktoropenapi.config.OutputFormat
+import io.github.smiley4.ktoropenapi.config.SchemaOverwriteModule
+import io.github.smiley4.schemakenerator.core.CoreSteps.handleNameAnnotation
+import io.github.smiley4.schemakenerator.core.data.TypeData
+import io.github.smiley4.schemakenerator.core.data.TypeId
+import io.github.smiley4.schemakenerator.serialization.SerializationSteps.addJsonClassDiscriminatorProperty
+import io.github.smiley4.schemakenerator.serialization.SerializationSteps.analyzeTypeUsingKotlinxSerialization
+import io.github.smiley4.schemakenerator.swagger.SwaggerSteps
+import io.github.smiley4.schemakenerator.swagger.SwaggerSteps.compileReferencingRoot
+import io.github.smiley4.schemakenerator.swagger.SwaggerSteps.generateSwaggerSchema
+import io.github.smiley4.schemakenerator.swagger.SwaggerSteps.handleSchemaAnnotations
+import io.github.smiley4.schemakenerator.swagger.SwaggerSteps.mergePropertyAttributesIntoType
+import io.github.smiley4.schemakenerator.swagger.TitleBuilder
+import io.ktor.server.application.*
+import io.swagger.v3.oas.models.media.Schema
+import no.nav.mulighetsrommet.api.routes.OpenApiSpec
+import no.nav.mulighetsrommet.clamav.Vedlegg
+import no.nav.mulighetsrommet.model.PortableTextTypedObject
+import no.nav.mulighetsrommet.model.ProblemDetail
+
+fun Application.configureOpenApiGenerator() {
+    install(OpenApi) {
+        outputFormat = OutputFormat.YAML
+
+        pathFilter = { _, urlParts ->
+            val url = urlParts.joinToString("/", prefix = "/")
+            OpenApiSpec.match(url) != null
+        }
+
+        specAssigner = { url, _ ->
+            OpenApiSpec.match(url)?.specName
+                ?: throw IllegalStateException("Failed to resolve OpenApiSpec for $url")
+        }
+
+        schemas {
+            /**
+             * Denne implementasjonen er basert på implementasjonen til [io.github.smiley4.ktoropenapi.config.SchemaGenerator.kotlinx].
+             * Noen enddringer har blitt gjort fra "standard" implementasjon, samt noen ting er verdt å være klar over
+             * ifm. generering av openapi-dokumentasjon:
+             * - Noen steg har blitt fjernet da de uansett ikke er i bruk i dette prosjektet (f.eks. prosessering av noen annotasjoner, eller generering av titles)
+             * - Modellnavn utledes basert på en egen [schemaPathBuilder] slik at path til nøstede klasser blir med i typenavnet, men ikke selve package path'en
+             */
+            generator = { type ->
+                type
+                    .analyzeTypeUsingKotlinxSerialization {
+                        customModules.addAll(customSchemaOverwrites)
+                    }
+                    .addJsonClassDiscriminatorProperty()
+                    .handleNameAnnotation()
+                    .generateSwaggerSchema {
+                        /**
+                         * Optional constructor-felter blir required i generert skjema hvis selve typen ikke er nullable
+                         */
+                        optionals = SwaggerSteps.RequiredHandling.REQUIRED
+
+                        /**
+                         * Nullable felter blir required i generert skjema (men fortsatt nullable)
+                         */
+                        nullables = SwaggerSteps.RequiredHandling.REQUIRED
+
+                        customModules.addAll(customSchemaOverwrites)
+                    }
+                    .handleSchemaAnnotations()
+                    .mergePropertyAttributesIntoType()
+                    .compileReferencingRoot(
+                        /**
+                         * Sørger for at "null" blir en del av gyldige typer for nullable felter i generert json schema
+                         */
+                        explicitNullTypes = true,
+                        builder = schemaPathBuilder,
+                    )
+            }
+        }
+    }
+}
+
+/**
+ * Standardnavn for genererte modeller er basert på qualified name, f.eks. `no.nav.MyClass`.
+ * For å unngå unødvendig verbose typer i generert OpenAPI, fjerner vi package path,
+ * men beholder navn på nøstede klasser.
+ *
+ * Eksempler:
+ * - `no.nav.MyClass` blir `MyClass`
+ * - `no.nav.MyClass.InnerClass` blir `MyClass.InnerClass`
+ */
+private val schemaPathBuilder = { type: TypeData, types: Map<TypeId, TypeData> ->
+    val fullPath = TitleBuilder.BUILDER_OPENAPI_FULL(type, types)
+    fullPath
+        .split(".")
+        .filter { !it.toCharArray().first().isLowerCase() && it != "Companion" }
+        .joinToString(".")
+        .ifEmpty { fullPath }
+}
+
+/**
+ * Noen modeller trenger litt hjelp til å representeres riktig i OpenAPI.
+ */
+private val customSchemaOverwrites: List<SchemaOverwriteModule> = listOf(
+    SchemaOverwriteModule(
+        identifier = "UUIDCustom",
+        schema = {
+            Schema<Any>().also {
+                it.types = setOf("string")
+                it.format = "uuid"
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = "LocalDateTime",
+        schema = {
+            Schema<Any>().also {
+                it.types = setOf("string")
+                it.format = "date-time"
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = "LocalDate",
+        schema = {
+            Schema<Any>().also {
+                it.types = setOf("string")
+                it.format = "date"
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = "kotlin.ByteArray",
+        schema = {
+            Schema<Any>().also {
+                it.types = setOf("string")
+                it.format = "binary"
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = Vedlegg::class.qualifiedName!!,
+        schema = {
+            Schema<Vedlegg>().apply {
+                types = setOf("string")
+                format = "binary"
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = "PortableTextTypedObject",
+        schema = {
+            Schema<PortableTextTypedObject>().apply {
+                type = "object"
+                addProperty(
+                    "_type",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                addProperty(
+                    "_key",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                required = listOf("_type")
+                additionalProperties = Schema<Any>()
+            }
+        },
+    ),
+    SchemaOverwriteModule(
+        identifier = "ProblemDetail",
+        schema = {
+            Schema<ProblemDetail>().apply {
+                type = "object"
+                addProperty(
+                    "type",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                addProperty(
+                    "title",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                addProperty(
+                    "status",
+                    Schema<String>().apply {
+                        types = setOf("integer")
+                    },
+                )
+                addProperty(
+                    "detail",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                addProperty(
+                    "instance",
+                    Schema<String>().apply {
+                        types = setOf("string")
+                    },
+                )
+                required = listOf("type", "title", "status", "detail")
+                additionalProperties = Schema<Any>()
+            }
+        },
+    ),
+)
