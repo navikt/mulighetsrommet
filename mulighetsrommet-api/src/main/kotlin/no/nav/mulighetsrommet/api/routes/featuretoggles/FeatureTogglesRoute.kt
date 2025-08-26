3800cfbc2bc869eb81dbf3027eb3e93c0a7a42a0
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2025-08-26 09:49:20 +0200

    fix: handle empty tiltakskode in featuretoggle routes

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/featuretoggles/FeatureTogglesRoute.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/featuretoggles/FeatureTogglesRoute.kt
@@ -8,7 +8,6 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import no.nav.mulighetsrommet.api.plugins.getNavIdent
-import no.nav.mulighetsrommet.api.veilederflate.routes.NavVeilederDto
 import no.nav.mulighetsrommet.model.ProblemDetail
 import no.nav.mulighetsrommet.model.Tiltakskode
 import no.nav.mulighetsrommet.unleash.FeatureToggle
@@ -43,6 +42,7 @@ fun Route.featureTogglesRoute() {
         }) {
             val feature: FeatureToggle by call.parameters
             val tiltakskoder = call.parameters.getAll("tiltakskoder")
+                ?.filter { it.isNotBlank() }
                 ?.map { Tiltakskode.valueOf(it) }
                 ?: emptyList()
 
