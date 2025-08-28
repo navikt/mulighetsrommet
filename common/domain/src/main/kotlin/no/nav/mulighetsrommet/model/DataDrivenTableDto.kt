37a9c693752a2401e04daf70406d831c8643b17f
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-08-28 15:55:40 +0200

    endre table til å benytte mer strukturerte typer celler

--- common/domain/src/main/kotlin/no/nav/mulighetsrommet/model/DataDrivenTableDto.kt
+++ common/domain/src/main/kotlin/no/nav/mulighetsrommet/model/DataDrivenTableDto.kt
@@ -2,12 +2,11 @@ package no.nav.mulighetsrommet.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonElement
 
 @Serializable
 class DataDrivenTableDto(
     val columns: List<Column>,
-    val rows: List<Map<String, JsonElement>>,
+    val rows: List<Row>,
 ) {
     @Serializable
     data class Column(
@@ -15,7 +14,6 @@ class DataDrivenTableDto(
         val label: String,
         val sortable: Boolean,
         val align: Align,
-        val format: Format?,
     ) {
         @Serializable
         enum class Align {
@@ -25,11 +23,61 @@ class DataDrivenTableDto(
             @SerialName("right")
             RIGHT,
         }
+    }
 
-        @Serializable
+    @Serializable
+    data class Row(
+        val cells: List<Cell>,
+    )
+
+    @Serializable
+    data class Cell(
+        val key: String,
+        val value: DataElement?,
+    )
+}
+
+@Serializable
+sealed class DataElement {
+
+    @Serializable
+    @SerialName("text")
+    data class Text(
+        val value: String,
+        val format: Format? = null,
+    ) : DataElement() {
         enum class Format {
             DATE,
             NOK,
+            NUMBER,
         }
     }
+
+    @Serializable
+    @SerialName("status")
+    data class Status(
+        val value: String,
+        val variant: Variant,
+    ) : DataElement() {
+        enum class Variant {
+            NEUTRAL,
+            SUCCESS,
+            WARNING,
+            ERROR,
+        }
+    }
+
+    @Serializable
+    @SerialName("link")
+    data class Link(
+        val text: String,
+        val href: String,
+    ) : DataElement()
+
+    @Serializable
+    @SerialName("periode")
+    data class Periode(
+        val start: String,
+        val slutt: String,
+    ) : DataElement()
 }
