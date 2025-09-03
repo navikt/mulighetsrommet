c422288aa3486cc22f471e274bf4cd7ea505ec31
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2025-09-03 15:27:01 +0200

    Bruk beregningType fra default i TilsagnForm

--- frontend/mr-admin-flate/src/pages/gjennomforing/tilsagn/opprett/OpprettTilsagnFormPage.tsx
+++ frontend/mr-admin-flate/src/pages/gjennomforing/tilsagn/opprett/OpprettTilsagnFormPage.tsx
@@ -1,10 +1,8 @@
 import { TilsagnFormContainer } from "@/components/tilsagn/TilsagnFormContainer";
 import { TilsagnBeregningType, TilsagnType } from "@tiltaksadministrasjon/api-client";
 import { useSearchParams } from "react-router";
-import { usePotentialAvtale } from "@/api/avtaler/useAvtale";
 import { useAdminGjennomforingById } from "@/api/gjennomforing/useAdminGjennomforingById";
 import { useTilsagnDefaults } from "./opprettTilsagnLoader";
-import { Laster } from "@/components/laster/Laster";
 import { useRequiredParams } from "@/hooks/useRequiredParams";
 
 function useHentData(gjennomforingId: string) {
@@ -15,7 +13,6 @@ function useHentData(gjennomforingId: string) {
   const kostnadssted = searchParams.get("kostnadssted");
 
   const { data: gjennomforing } = useAdminGjennomforingById(gjennomforingId);
-  const { data: avtale } = usePotentialAvtale(gjennomforing.avtaleId);
   const { data: defaults } = useTilsagnDefaults({
     id: null,
     gjennomforingId,
@@ -34,16 +31,12 @@ function useHentData(gjennomforingId: string) {
     kommentar: null,
   });
 
-  return { gjennomforing, avtale, defaults };
+  return { gjennomforing, defaults };
 }
 
 export function OpprettTilsagnFormPage() {
   const { gjennomforingId } = useRequiredParams(["gjennomforingId"]);
-  const { gjennomforing, avtale, defaults } = useHentData(gjennomforingId);
+  const { gjennomforing, defaults } = useHentData(gjennomforingId);
 
-  if (!avtale) {
-    return <Laster tekst="Laster data..." />;
-  }
-
-  return <TilsagnFormContainer avtale={avtale} gjennomforing={gjennomforing} defaults={defaults} />;
+  return <TilsagnFormContainer gjennomforing={gjennomforing} defaults={defaults} />;
 }
--- frontend/mr-admin-flate/src/pages/gjennomforing/tilsagn/rediger/RedigerTilsagnFormPage.tsx
+++ frontend/mr-admin-flate/src/pages/gjennomforing/tilsagn/rediger/RedigerTilsagnFormPage.tsx
@@ -1,35 +1,28 @@
 import { TilsagnFormContainer } from "@/components/tilsagn/TilsagnFormContainer";
-import { usePotentialAvtale } from "@/api/avtaler/useAvtale";
 import { useAdminGjennomforingById } from "@/api/gjennomforing/useAdminGjennomforingById";
 import { useTilsagn, useTilsagnRequest } from "../detaljer/tilsagnDetaljerLoader";
-import { Laster } from "@/components/laster/Laster";
 import { ToTrinnsOpprettelsesForklaring } from "../ToTrinnsOpprettelseForklaring";
 import { useRequiredParams } from "@/hooks/useRequiredParams";
 
 function useRedigerTilsagnFormData(gjennomforingId: string, tilsagnId: string) {
   const { data: gjennomforing } = useAdminGjennomforingById(gjennomforingId);
   const { data: tilsagnDetaljer } = useTilsagn(tilsagnId);
-  const { data: avtale } = usePotentialAvtale(gjennomforing.avtaleId);
   const { data: defaults } = useTilsagnRequest(tilsagnId);
-  return { avtale, gjennomforing, defaults, opprettelse: tilsagnDetaljer.opprettelse };
+  return { gjennomforing, defaults, opprettelse: tilsagnDetaljer.opprettelse };
 }
 
 export function RedigerTilsagnFormPage() {
   const { gjennomforingId, tilsagnId } = useRequiredParams(["gjennomforingId", "tilsagnId"]);
 
-  const { avtale, gjennomforing, defaults, opprettelse } = useRedigerTilsagnFormData(
+  const { gjennomforing, defaults, opprettelse } = useRedigerTilsagnFormData(
     gjennomforingId,
     tilsagnId,
   );
 
-  if (!avtale) {
-    return <Laster tekst="Laster data..." />;
-  }
-
   return (
     <>
       <ToTrinnsOpprettelsesForklaring opprettelse={opprettelse} />
-      <TilsagnFormContainer avtale={avtale} gjennomforing={gjennomforing} defaults={defaults} />
+      <TilsagnFormContainer gjennomforing={gjennomforing} defaults={defaults} />
     </>
   );
 }
