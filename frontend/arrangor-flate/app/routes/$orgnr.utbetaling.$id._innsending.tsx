3ad435b07b58552da35b77f4fd1ac1b6391ae634
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-09-12 13:48:19 +0200

    Stegindikator på toppen - innsending

--- frontend/arrangor-flate/app/routes/$orgnr.utbetaling.$id._innsending.tsx
+++ frontend/arrangor-flate/app/routes/$orgnr.utbetaling.$id._innsending.tsx
@@ -1,4 +1,4 @@
-import { Heading, Hide, HStack, Link, Stepper, VStack } from "@navikt/ds-react";
+import { Box, Hide, HStack, Link, Stepper, VStack } from "@navikt/ds-react";
 import { Outlet, useLocation } from "react-router";
 import { Link as ReactRouterLink } from "react-router";
 import { ChevronLeftIcon } from "@navikt/aksel-icons";
@@ -30,32 +30,32 @@ export default function UtbetalingLayout() {
   }, [step]);
 
   return (
-    <VStack gap="4" width="100%" className="bg-bg-subtle flex-1 px-10 pt-4 pb-10">
-      <Link as={ReactRouterLink} to={pathByOrgnr(orgnr).utbetalinger} className="max-w-max">
-        <ChevronLeftIcon /> Tilbake til oversikt
-      </Link>
-      <HStack gap="10" width="100%" justify="center" className="xl:max-w-[1920px] xl:mx-auto">
+    <Box width="100%" className="bg-bg-subtle flex-1 px-10 pt-4 pb-10">
+      <VStack gap="4" justify="center" className="xl:max-w-[1920px] xl:mx-auto">
+        <Link as={ReactRouterLink} to={pathByOrgnr(orgnr).utbetalinger} className="max-w-max">
+          <ChevronLeftIcon /> Tilbake til oversikt
+        </Link>
+        <Hide below="sm">
+          <Stepper
+            aria-labelledby="stepper-heading"
+            aria-label="Steg"
+            activeStep={activeStep}
+            onStepChange={setActiveStep}
+            orientation="horizontal"
+          >
+            <Stepper.Step interactive={false} completed={activeStep > 1}>
+              Innsendingsinformasjon
+            </Stepper.Step>
+            <Stepper.Step interactive={false} completed={activeStep > 2}>
+              Beregning
+            </Stepper.Step>
+            <Stepper.Step interactive={false}>Oppsummering</Stepper.Step>
+          </Stepper>
+        </Hide>
         <VStack padding="8" flexGrow="2" className="bg-bg-default rounded-lg">
           <Outlet />
         </VStack>
-        <Hide below="xl">
-          <VStack flexGrow="1">
-            <Heading size="medium" spacing level="2" id="stepper-heading">
-              Steg
-            </Heading>
-            <Stepper
-              aria-labelledby="stepper-heading"
-              activeStep={activeStep}
-              onStepChange={setActiveStep}
-              interactive={false}
-            >
-              <Stepper.Step>Innsendingsinformasjon</Stepper.Step>
-              <Stepper.Step>Beregning</Stepper.Step>
-              <Stepper.Step>Oppsummering</Stepper.Step>
-            </Stepper>
-          </VStack>
-        </Hide>
-      </HStack>
-    </VStack>
+      </VStack>
+    </Box>
   );
 }
