586804cffb2e3e72ea8855f546eec24846738abd
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-09-23 10:50:06 +0200

    implementer linting for frontend-common

--- frontend/frontend-common/components/skeleton/ListSkeleton.tsx
+++ frontend/frontend-common/components/skeleton/ListSkeleton.tsx
@@ -2,14 +2,14 @@ import { Skeleton, VStack } from "@navikt/ds-react";
 
 export function ListSkeleton() {
   return (
-      <div>
-        <VStack style={{ margin: ".5rem" }} gap="2">
-          <Skeleton height={60} variant="rounded" />
-          <Skeleton height={60} variant="rounded" />
-          <Skeleton height={60} variant="rounded" />
-          <Skeleton height={60} variant="rounded" />
-          <Skeleton height={60} variant="rounded" />
-        </VStack>
-      </div>
+    <div>
+      <VStack style={{ margin: ".5rem" }} gap="2">
+        <Skeleton height={60} variant="rounded" />
+        <Skeleton height={60} variant="rounded" />
+        <Skeleton height={60} variant="rounded" />
+        <Skeleton height={60} variant="rounded" />
+        <Skeleton height={60} variant="rounded" />
+      </VStack>
+    </div>
   );
 }
