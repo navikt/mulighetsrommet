d1a7446d4aeaeb2d3b707ffdee81c96601718733
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2025-09-26 08:28:21 +0200

    add feature toggle prismodell hele uker

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/unleash/UnleashService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/unleash/UnleashService.kt
@@ -10,6 +10,7 @@ import no.nav.mulighetsrommet.unleash.strategies.ByTiltakskodeStrategy
 
 enum class FeatureToggle(val key: String) {
     MULIGHETSROMMET_TILTAKSTYPE_MIGRERING_TILSAGN("mulighetsrommet.tiltakstype.migrering.tilsagn"),
+    MULIGHETSROMMET_PRISMODELL_HELE_UKER("mulighetsrommet.okonomi.prismodell.hele-uker"),
     MULIGHETSROMMET_TILTAKSTYPE_MIGRERING_UTBETALING("mulighetsrommet.tiltakstype.migrering.okonomi"),
     ARRANGORFLATE_OPPRETT_UTBETALING_ANNEN_AVTALT_PPRIS("arrangorflate.utbetaling.opprett-utbetaling.annen-avtalt-ppris"),
 }
