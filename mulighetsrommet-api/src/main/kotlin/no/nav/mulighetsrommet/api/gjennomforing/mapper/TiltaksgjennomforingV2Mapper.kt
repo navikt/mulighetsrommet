bc658bbf1c8ca63ed63e53eae52117b2c0618c6e
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-10-03 17:57:35 +0200

    implementer v2-endepunkt for gjennomføringer

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/gjennomforing/mapper/TiltaksgjennomforingV2Mapper.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/gjennomforing/mapper/TiltaksgjennomforingV2Mapper.kt
@@ -1,9 +1,34 @@
 package no.nav.mulighetsrommet.api.gjennomforing.mapper
 
 import no.nav.mulighetsrommet.api.gjennomforing.model.Enkeltplass
+import no.nav.mulighetsrommet.api.gjennomforing.model.Gjennomforing
 import no.nav.mulighetsrommet.model.TiltaksgjennomforingV2Dto
+import java.time.ZoneId
 
 object TiltaksgjennomforingV2Mapper {
+    fun fromGruppe(gruppe: Gjennomforing) = TiltaksgjennomforingV2Dto(
+        tiltakstype = TiltaksgjennomforingV2Dto.Tiltakstype(
+            arenakode = gruppe.tiltakstype.tiltakskode.arenakode,
+            tiltakskode = gruppe.tiltakstype.tiltakskode,
+        ),
+        arrangor = TiltaksgjennomforingV2Dto.Arrangor(
+            organisasjonsnummer = gruppe.arrangor.organisasjonsnummer,
+        ),
+        gjennomforing = TiltaksgjennomforingV2Dto.Gruppe(
+            id = gruppe.id,
+            opprettetTidspunkt = gruppe.opprettetTidspunkt.atZone(ZoneId.systemDefault()).toInstant(),
+            oppdatertTidspunkt = gruppe.oppdatertTidspunkt.atZone(ZoneId.systemDefault()).toInstant(),
+            navn = gruppe.navn,
+            startDato = gruppe.startDato,
+            sluttDato = gruppe.sluttDato,
+            status = gruppe.status.type,
+            oppstart = gruppe.oppstart,
+            tilgjengeligForArrangorFraOgMedDato = gruppe.tilgjengeligForArrangorDato,
+            apentForPamelding = gruppe.apentForPamelding,
+            antallPlasser = gruppe.antallPlasser,
+        ),
+    )
+
     fun fromEnkeltplass(enkeltplass: Enkeltplass) = TiltaksgjennomforingV2Dto(
         tiltakstype = TiltaksgjennomforingV2Dto.Tiltakstype(
             arenakode = enkeltplass.tiltakstype.tiltakskode.arenakode,
