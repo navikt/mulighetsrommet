f99fad95f740eb84ed52ab0b1f7f17446453cec4
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-10-03 17:57:35 +0200

    implementer v2-endepunkt for gjennomføringer

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/gjennomforing/api/GjennomforingPublicRoutes.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/gjennomforing/api/GjennomforingPublicRoutes.kt
@@ -5,103 +5,167 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import no.nav.mulighetsrommet.api.ApiDatabase
+import no.nav.mulighetsrommet.api.QueryContext
 import no.nav.mulighetsrommet.api.arenaadapter.ArenaAdapterClient
 import no.nav.mulighetsrommet.api.gjennomforing.mapper.TiltaksgjennomforingV1Mapper
+import no.nav.mulighetsrommet.api.gjennomforing.mapper.TiltaksgjennomforingV2Mapper
 import no.nav.mulighetsrommet.api.gjennomforing.service.GjennomforingService
 import no.nav.mulighetsrommet.api.plugins.pathParameterUuid
 import no.nav.mulighetsrommet.model.ExchangeArenaIdForIdResponse
 import no.nav.mulighetsrommet.model.TiltaksgjennomforingArenaDataDto
 import no.nav.mulighetsrommet.model.TiltaksgjennomforingV1Dto
+import no.nav.mulighetsrommet.model.TiltaksgjennomforingV2Dto
 import org.koin.ktor.ext.inject
 import java.util.*
 
 fun Route.gjennomforingPublicRoutes() {
-    val gjennomforingService: GjennomforingService by inject()
-    val arenaAdapterService: ArenaAdapterClient by inject()
+    route("/v2/tiltaksgjennomforinger") {
+        getGjennomforingV2Route()
+    }
 
     route("/v1/tiltaksgjennomforinger") {
-        get("{id}", {
-            tags = setOf("Tiltaksgjennomforing")
-            operationId = "getTiltaksgjennomforing"
-            request {
-                pathParameterUuid("id")
-            }
-            response {
-                code(HttpStatusCode.OK) {
-                    description = "Gjennomføringen"
-                    body<TiltaksgjennomforingV1Dto>()
-                }
-                code(HttpStatusCode.NotFound) {
-                    description = "Gjennomføringen ble ikke funnet"
-                }
-            }
-        }) {
-            val id: UUID by call.parameters
-
-            val result = gjennomforingService.get(id)
-                ?.let { TiltaksgjennomforingV1Mapper.fromGjennomforing(it) }
-                ?: return@get call.respond(HttpStatusCode.NotFound, "Ingen tiltaksgjennomføring med id=$id")
-
-            call.respond(result)
-        }
-
-        get("id/{arenaId}", {
-            tags = setOf("Tiltaksgjennomforing")
-            operationId = "getTiltaksgjennomforingId"
-            request {
-                pathParameter<String>("arenaId")
-            }
-            response {
-                code(HttpStatusCode.OK) {
-                    description = "Id til tiltaksgjennomføringen"
-                    body<ExchangeArenaIdForIdResponse>()
-                }
-                code(HttpStatusCode.NotFound) {
-                    description = "Gjennomføringen ble ikke funnet"
-                }
-            }
-        }) {
-            val arenaId: String by call.parameters
-
-            val response = arenaAdapterService.exchangeTiltaksgjennomforingArenaIdForId(arenaId)
-                ?: return@get call.respond(
-                    HttpStatusCode.NotFound,
-                    "Det finnes ingen tiltaksgjennomføring med arenaId=$arenaId",
-                )
-
-            call.respond(response)
-        }
-
-        get("arenadata/{id}", {
-            tags = setOf("Tiltaksgjennomforing")
-            operationId = "getTiltaksgjennomforingArenadata"
-            request {
-                pathParameterUuid("id")
-            }
-            response {
-                code(HttpStatusCode.OK) {
-                    description = "Arenadata for tiltaksgjennføringen"
-                    body<TiltaksgjennomforingArenaDataDto>()
-                }
-                code(HttpStatusCode.NotFound) {
-                    description = "Gjennomføringen ble ikke funnet"
-                }
-            }
-        }) {
-            val id = call.parameters.getOrFail<UUID>("id")
-
-            val gjennomforing = gjennomforingService.get(id)
-                ?: return@get call.respond(HttpStatusCode.NotFound)
-
-            val arenaData = gjennomforing.tiltaksnummer?.let { toArenaDataDto(it) }
-                ?: return@get call.respond(HttpStatusCode.NoContent)
-
-            call.respond(HttpStatusCode.OK, arenaData)
-        }
+        getGjennomforingV1Route()
+        getGjennomforingIdRoute()
+        getGjennomforingArenaDataRoute()
     }
 }
 
-fun toArenaDataDto(tiltaksnummer: String) = TiltaksgjennomforingArenaDataDto(
-    opprettetAar = tiltaksnummer.split("#").first().toInt(),
-    lopenr = tiltaksnummer.split("#")[1].toInt(),
-)
+private fun Route.getGjennomforingV2Route() {
+    val db: ApiDatabase by inject()
+
+    suspend fun QueryContext.getTiltaksgjennomforingV2(id: UUID): TiltaksgjennomforingV2Dto? = coroutineScope {
+        val gruppe = async {
+            queries.gjennomforing.get(id)?.let(TiltaksgjennomforingV2Mapper::fromGruppe)
+        }
+
+        val enkeltplass = async {
+            queries.enkeltplass.get(id)?.let(TiltaksgjennomforingV2Mapper::fromEnkeltplass)
+        }
+
+        listOf(gruppe, enkeltplass).awaitAll().firstOrNull { it != null }
+    }
+
+    get("{id}", {
+        tags = setOf("Tiltaksgjennomforing")
+        operationId = "getTiltaksgjennomforingV2"
+        request {
+            pathParameterUuid("id")
+        }
+        response {
+            code(HttpStatusCode.OK) {
+                description = "Gjennomføringen"
+                body<TiltaksgjennomforingV2Dto>()
+            }
+            code(HttpStatusCode.NotFound) {
+                description = "Gjennomføringen ble ikke funnet"
+            }
+        }
+    }) {
+        val id: UUID by call.parameters
+
+        val tiltaksgjennomforing = db.session { getTiltaksgjennomforingV2(id) }
+            ?: return@get call.respond(HttpStatusCode.NotFound, "Ingen tiltaksgjennomføring med id=$id")
+
+        call.respond(tiltaksgjennomforing)
+    }
+}
+
+private fun Route.getGjennomforingV1Route() {
+    val gjennomforingService: GjennomforingService by inject()
+
+    get("{id}", {
+        tags = setOf("Tiltaksgjennomforing")
+        operationId = "getTiltaksgjennomforingV1"
+        request {
+            pathParameterUuid("id")
+        }
+        response {
+            code(HttpStatusCode.OK) {
+                description = "Gjennomføringen"
+                body<TiltaksgjennomforingV1Dto>()
+            }
+            code(HttpStatusCode.NotFound) {
+                description = "Gjennomføringen ble ikke funnet"
+            }
+        }
+    }) {
+        val id: UUID by call.parameters
+
+        val result = gjennomforingService.get(id)
+            ?.let { TiltaksgjennomforingV1Mapper.fromGjennomforing(it) }
+            ?: return@get call.respond(HttpStatusCode.NotFound, "Ingen tiltaksgjennomføring med id=$id")
+
+        call.respond(result)
+    }
+}
+
+private fun Route.getGjennomforingIdRoute() {
+    val arenaAdapterService: ArenaAdapterClient by inject()
+
+    get("id/{arenaId}", {
+        tags = setOf("Tiltaksgjennomforing")
+        operationId = "getTiltaksgjennomforingId"
+        request {
+            pathParameter<String>("arenaId")
+        }
+        response {
+            code(HttpStatusCode.OK) {
+                description = "Id til tiltaksgjennomføringen"
+                body<ExchangeArenaIdForIdResponse>()
+            }
+            code(HttpStatusCode.NotFound) {
+                description = "Gjennomføringen ble ikke funnet"
+            }
+        }
+    }) {
+        val arenaId: String by call.parameters
+
+        val response = arenaAdapterService.exchangeTiltaksgjennomforingArenaIdForId(arenaId)
+            ?: return@get call.respond(
+                HttpStatusCode.NotFound,
+                "Det finnes ingen tiltaksgjennomføring med arenaId=$arenaId",
+            )
+
+        call.respond(response)
+    }
+}
+
+private fun Route.getGjennomforingArenaDataRoute() {
+    val gjennomforingService: GjennomforingService by inject()
+
+    get("arenadata/{id}", {
+        tags = setOf("Tiltaksgjennomforing")
+        operationId = "getTiltaksgjennomforingArenadata"
+        request {
+            pathParameterUuid("id")
+        }
+        response {
+            code(HttpStatusCode.OK) {
+                description = "Arenadata for tiltaksgjennføringen"
+                body<TiltaksgjennomforingArenaDataDto>()
+            }
+            code(HttpStatusCode.NotFound) {
+                description = "Gjennomføringen ble ikke funnet"
+            }
+        }
+    }) {
+        val id: UUID by call.parameters
+
+        val gjennomforing = gjennomforingService.get(id)
+            ?: return@get call.respond(HttpStatusCode.NotFound)
+
+        val arenaData = gjennomforing.tiltaksnummer?.let { toArenaDataDto(it) }
+            ?: return@get call.respond(HttpStatusCode.NoContent)
+
+        call.respond(HttpStatusCode.OK, arenaData)
+    }
+}
+
+fun toArenaDataDto(tiltaksnummer: String): TiltaksgjennomforingArenaDataDto {
+    val (aar, lopenr) = tiltaksnummer.split("#")
+    return TiltaksgjennomforingArenaDataDto(opprettetAar = aar.toInt(), lopenr = lopenr.toInt())
+}
