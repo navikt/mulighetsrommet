78f1ac500670c37327aa06e0a2ee7e7bc72e462c
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-10-07 10:43:43 +0200

    fjern ubrukt fil

--- frontend/mr-admin-flate/src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import "jest-environment-jsdom";
-
-// Mock ResizeObserver which might be needed for some UI components
-global.ResizeObserver = class ResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-};
