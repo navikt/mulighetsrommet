cc0569e83d1f125f7125d5c380d52e7c6a8b8b55
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-10-31 08:50:33 +0100

    Litt oppsett av testdata

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/utbetaling/ArrangorflateOpprettKravRoutesTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/utbetaling/ArrangorflateOpprettKravRoutesTest.kt
@@ -2,6 +2,7 @@ package no.nav.mulighetsrommet.api.utbetaling
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
@@ -10,29 +11,55 @@ import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import no.nav.mulighetsrommet.api.OkonomiConfig
 import no.nav.mulighetsrommet.api.arrangorflate.api.GjennomforingerTableResponse
+import no.nav.mulighetsrommet.api.arrangorflate.api.OpprettKravInnsendingsInformasjon
+import no.nav.mulighetsrommet.api.arrangorflate.api.OpprettKravInnsendingsInformasjon.DatoVelger
 import no.nav.mulighetsrommet.api.databaseConfig
+import no.nav.mulighetsrommet.api.fixtures.*
+import no.nav.mulighetsrommet.api.tilsagn.model.TilsagnStatus
+import no.nav.mulighetsrommet.api.utbetaling.ArrangorflateTestUtils.hovedenhet
 import no.nav.mulighetsrommet.api.withTestApplication
 import no.nav.mulighetsrommet.database.kotest.extensions.ApiDatabaseTestListener
 import no.nav.mulighetsrommet.model.Periode
 import no.nav.mulighetsrommet.model.Tiltakskode
 import no.nav.security.mock.oauth2.MockOAuth2Server
+import org.junit.jupiter.api.fail
 import java.time.LocalDate
 
 class ArrangorflateOpprettKravRoutesTest : FunSpec({
     val database = extension(ApiDatabaseTestListener(databaseConfig))
 
-    val deltaker = ArrangorflateTestUtils.createTestDeltaker()
-    val tilsagn = ArrangorflateTestUtils.createTestTilsagn()
-
-    val domain = ArrangorflateTestUtils.createTestDomain(
-        deltaker = deltaker,
-        tilsagn = tilsagn,
-    )
-
-    val oauth = MockOAuth2Server()
     val identMedTilgang = ArrangorflateTestUtils.identMedTilgang
     val underenhet = ArrangorflateTestUtils.underenhet
     val orgnr = underenhet.organisasjonsnummer.value
+
+    val deltaker = ArrangorflateTestUtils.createTestDeltaker()
+    val tilsagn = ArrangorflateTestUtils.createTestTilsagn()
+    val aftGjennomforing = GjennomforingFixtures.AFT1
+    val oppfolgingGjennomforing = GjennomforingFixtures.Oppfolging1
+    val arrGjennomforing = GjennomforingFixtures.ArbeidsrettetRehabilitering
+
+    val domain = MulighetsrommetTestDomain(
+        navEnheter = listOf(NavEnhetFixtures.Innlandet, NavEnhetFixtures.Gjovik),
+        ansatte = listOf(NavAnsattFixture.DonaldDuck, NavAnsattFixture.MikkeMus),
+        tiltakstyper = listOf(TiltakstypeFixtures.AFT, TiltakstypeFixtures.Oppfolging, TiltakstypeFixtures.ArbeidsrettetRehabilitering),
+        deltakere = listOf(deltaker),
+        arrangorer = listOf(hovedenhet, ArrangorflateTestUtils.underenhet),
+        tilsagn = listOf(tilsagn),
+        gjennomforinger = listOf(
+            aftGjennomforing,
+            oppfolgingGjennomforing,
+            arrGjennomforing,
+        ),
+        avtaler = listOf(
+            AvtaleFixtures.AFT, // Forhåndsgodkjent
+            AvtaleFixtures.oppfolging, // Avtalt pris per time oppfølging
+            AvtaleFixtures.ARR, // Annen avtalt pris
+        ),
+    ) {
+        setTilsagnStatus(tilsagn, TilsagnStatus.GODKJENT)
+    }
+
+    val oauth = MockOAuth2Server()
 
     beforeSpec {
         oauth.start()
@@ -130,7 +157,10 @@ class ArrangorflateOpprettKravRoutesTest : FunSpec({
                                     "vedlegg",
                                     fil,
                                     Headers.build {
-                                        append(HttpHeaders.ContentDisposition, "form-data; name=\"file\"; filename=\"$filnavn\"")
+                                        append(
+                                            HttpHeaders.ContentDisposition,
+                                            "form-data; name=\"file\"; filename=\"$filnavn\"",
+                                        )
                                         append(HttpHeaders.ContentType, ContentType.Application.Pdf.toString())
                                     },
                                 )
@@ -140,6 +170,75 @@ class ArrangorflateOpprettKravRoutesTest : FunSpec({
                 }
 
             response.status shouldBe HttpStatusCode.Forbidden
+        }
+    }
+
+    test("Timespris får liste av tilgjengelige perioder") {
+        withTestApplication(ArrangorflateTestUtils.appConfig(oauth)) {
+            val client = createClient {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }
+            val response =
+                client.get("/api/arrangorflate/arrangor/$orgnr/gjennomforing/${oppfolgingGjennomforing.id}/opprett-krav/innsendingsinformasjon") {
+                    bearerAuth(oauth.issueToken(claims = mapOf("pid" to identMedTilgang.value)).serialize())
+                }
+
+            response.status shouldBe HttpStatusCode.OK
+            val data = response.body<OpprettKravInnsendingsInformasjon>()
+            when (data.datoVelger) {
+                is DatoVelger.DatoSelect ->
+                    data.datoVelger.periodeForslag.isNotEmpty()
+                is DatoVelger.DatoRange ->
+                    fail { "Skal vise en liste av perioder for timespris innsending" }
+            }
+        }
+    }
+
+    test("Annen avtalt pris skal kunne velge fritt i datovelger") {
+        withTestApplication(ArrangorflateTestUtils.appConfig(oauth)) {
+            val client = createClient {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }
+            val response =
+                client.get("/api/arrangorflate/arrangor/$orgnr/gjennomforing/${arrGjennomforing.id}/opprett-krav/innsendingsinformasjon") {
+                    bearerAuth(oauth.issueToken(claims = mapOf("pid" to identMedTilgang.value)).serialize())
+                }
+
+            response.status shouldBe HttpStatusCode.OK
+            val data = response.body<OpprettKravInnsendingsInformasjon>()
+            when (data.datoVelger) {
+                is DatoVelger.DatoSelect ->
+                    fail { "Skal vise en liste av perioder for timespris innsending" }
+                is DatoVelger.DatoRange ->
+                    data.datoVelger.maksSluttdato shouldBe null
+            }
+        }
+    }
+
+    test("Investeringskrav skal bare kunne velge fra forrige utbetalingsperiode") {
+        withTestApplication(ArrangorflateTestUtils.appConfig(oauth)) {
+            val client = createClient {
+                install(ContentNegotiation) {
+                    json()
+                }
+            }
+            val response =
+                client.get("/api/arrangorflate/arrangor/$orgnr/gjennomforing/${aftGjennomforing.id}/opprett-krav/innsendingsinformasjon") {
+                    bearerAuth(oauth.issueToken(claims = mapOf("pid" to identMedTilgang.value)).serialize())
+                }
+
+            response.status shouldBe HttpStatusCode.OK
+            val data = response.body<OpprettKravInnsendingsInformasjon>()
+            when (data.datoVelger) {
+                is DatoVelger.DatoSelect ->
+                    fail { "Skal vise en liste av perioder for timespris innsending" }
+                is DatoVelger.DatoRange ->
+                    data.datoVelger.maksSluttdato shouldBe LocalDate.now().withDayOfMonth(1).minusDays(1) // Siste dag forrige måned
+            }
         }
     }
 })
