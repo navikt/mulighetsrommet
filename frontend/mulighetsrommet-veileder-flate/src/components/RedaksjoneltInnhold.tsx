c5a2ab3a00e0b0c519bd338e902b9089e1de876d
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-11-04 13:38:39 +0100

    Legg til slate konvertering i veilederflate

--- frontend/mulighetsrommet-veileder-flate/src/components/RedaksjoneltInnhold.tsx
+++ frontend/mulighetsrommet-veileder-flate/src/components/RedaksjoneltInnhold.tsx
@@ -1,5 +1,6 @@
-import { PortableText, PortableTextReactComponents } from "@portabletext/react";
+import { PortableText, PortableTextReactComponents, PortableTextBlock } from "@portabletext/react";
 import { Alert, BodyLong, GuidePanel, List } from "@navikt/ds-react";
+import { PortableTextTypedObject } from "@api-client";
 
 interface ImageProp {
   value: { asset: { url: string }; altText: string };
@@ -72,5 +73,71 @@ interface RedaksjoneltinnholdProps {
 }
 
 export function RedaksjoneltInnhold({ value }: RedaksjoneltinnholdProps) {
-  return <PortableText value={value} components={portableTextComponent} />;
+  const convertedValue = convertSlateToPortableText(value) as unknown as PortableTextBlock[];
+  return <PortableText value={convertedValue} components={portableTextComponent} />;
+}
+
+function getOrAddKey(obj: { _key?: string | null }): string {
+  return obj._key ?? crypto.randomUUID().slice(0, 8);
+}
+
+function convertSlateBlock(block: PortableTextTypedObject): PortableTextTypedObject {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (block._key !== null) {
+    // not from slate
+    return block;
+  }
+
+  const newBlock: PortableTextTypedObject = { ...block };
+  if (block._type === "block") {
+    newBlock["style"] = "normal";
+    // required to display anything
+    newBlock._key = getOrAddKey(newBlock);
+    if ("listItem" in newBlock) {
+      // indent existing lists
+      newBlock["level"] = 1;
+    }
+    // Fix existing links
+    if ("markDefs" in newBlock && Array.isArray(newBlock.markDefs)) {
+      newBlock.markDefs = newBlock["markDefs"] ?? [];
+      const linkMarkDefIndex = ((newBlock.markDefs || []) as PortableTextTypedObject[]).findIndex(
+        (obj) => obj._type === "link",
+      );
+      if (linkMarkDefIndex > -1) {
+        const linkMarkDef = (newBlock.markDefs as PortableTextTypedObject[])[linkMarkDefIndex];
+        const newKey = getOrAddKey({ _key: null });
+        newBlock.children = (newBlock.children as PortableTextTypedObject[]).map((child) => {
+          if ("marks" in child) {
+            child.marks = (child.marks as string[]).map((mark) => {
+              if (mark === linkMarkDef._key) {
+                return newKey;
+              }
+              return mark;
+            });
+          }
+          child._key = getOrAddKey(child);
+          return child;
+        });
+        (newBlock.markDefs as PortableTextTypedObject[])[linkMarkDefIndex] = {
+          ...linkMarkDef,
+          _key: newKey,
+        };
+      }
+    }
+  }
+  return newBlock;
+}
+
+// PortableText editor requires _key to not be null in certain blocks
+// Only attemt to fix block if null-key is detected
+function convertSlateToPortableText(
+  slateData: PortableTextTypedObject[] | undefined | null,
+): PortableTextTypedObject[] | null {
+  if (!slateData) {
+    return null;
+  }
+  if (!slateData.length) {
+    return [];
+  }
+  return slateData.map(convertSlateBlock);
 }
