68b1c612fe37fff20949bf7d60100c251f240b07
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-11-17 10:26:42 +0100

    sentraliser tester relatert til database queries

---
+++ mulighetsrommet-tiltakshistorikk/src/test/kotlin/no/nav/tiltak/historikk/db/TiltakshistorikkDatabaseTest.kt
@@ -0,0 +1,386 @@
+package no.nav.tiltak.historikk.db
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import no.nav.amt.model.AmtDeltakerV1Dto
+import no.nav.mulighetsrommet.arena.ArenaDeltakerDbo
+import no.nav.mulighetsrommet.database.kotest.extensions.FlywayDatabaseTestListener
+import no.nav.mulighetsrommet.model.*
+import no.nav.tiltak.historikk.TestFixtures
+import no.nav.tiltak.historikk.TiltakshistorikkV1Dto
+import no.nav.tiltak.historikk.databaseConfig
+import no.nav.tiltak.historikk.db.queries.VirksomhetDbo
+import no.nav.tiltak.historikk.kafka.consumers.toGjennomforingDbo
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+class TiltakshistorikkDatabaseTest : FunSpec({
+    val database = extension(FlywayDatabaseTestListener(databaseConfig))
+
+    context("Virksomhet") {
+        val db = TiltakshistorikkDatabase(database.db)
+
+        test("oppretter, henter og sletter virksomhet") {
+            val virksomhet = VirksomhetDbo(
+                organisasjonsnummer = Organisasjonsnummer("123456789"),
+                overordnetEnhetOrganisasjonsnummer = Organisasjonsnummer("987654321"),
+                navn = "Test Virksomhet",
+                organisasjonsform = "AS",
+                slettetDato = null,
+            )
+
+            db.session {
+                queries.virksomhet.upsert(virksomhet)
+
+                val hentet = queries.virksomhet.get(virksomhet.organisasjonsnummer)
+                hentet shouldBe virksomhet
+
+                queries.virksomhet.delete(virksomhet.organisasjonsnummer)
+
+                val slettet = queries.virksomhet.get(virksomhet.organisasjonsnummer)
+                slettet.shouldBeNull()
+            }
+        }
+
+        test("oppdaterer eksisterende virksomhet ved upsert") {
+            val virksomhet = VirksomhetDbo(
+                organisasjonsnummer = Organisasjonsnummer("111222333"),
+                overordnetEnhetOrganisasjonsnummer = null,
+                navn = "Original Navn",
+                organisasjonsform = "AS",
+                slettetDato = null,
+            )
+
+            db.session {
+                queries.virksomhet.upsert(virksomhet)
+
+                val updated = virksomhet.copy(navn = "Oppdatert Navn", organisasjonsform = "ASA")
+                queries.virksomhet.upsert(updated)
+
+                val hentet = queries.virksomhet.get(virksomhet.organisasjonsnummer)
+                hentet shouldBe updated
+            }
+        }
+    }
+
+    context("Arena deltaker") {
+        val arbeidstreningArenaDeltakelse = ArenaDeltakerDbo(
+            id = UUID.randomUUID(),
+            norskIdent = NorskIdent("12345678910"),
+            arenaTiltakskode = "ARBTREN",
+            status = ArenaDeltakerStatus.GJENNOMFORES,
+            startDato = LocalDateTime.of(2024, 1, 1, 0, 0, 0),
+            sluttDato = LocalDateTime.of(2024, 1, 31, 0, 0, 0),
+            beskrivelse = "Arbeidstrening hos Fretex",
+            arrangorOrganisasjonsnummer = Organisasjonsnummer("123123123"),
+            registrertIArenaDato = LocalDateTime.of(2024, 1, 1, 0, 0, 0),
+        )
+
+        val mentorArenaDeltakelse = ArenaDeltakerDbo(
+            id = UUID.randomUUID(),
+            norskIdent = NorskIdent("12345678910"),
+            arenaTiltakskode = "MENTOR",
+            status = ArenaDeltakerStatus.GJENNOMFORES,
+            startDato = LocalDateTime.of(2002, 2, 1, 0, 0, 0),
+            sluttDato = LocalDateTime.of(2002, 2, 1, 0, 0, 0),
+            beskrivelse = "Mentortiltak hos Joblearn",
+            arrangorOrganisasjonsnummer = Organisasjonsnummer("123123123"),
+            registrertIArenaDato = LocalDateTime.of(2002, 1, 1, 0, 0, 0),
+        )
+
+        var db = TiltakshistorikkDatabase(database.db)
+
+        beforeAny {
+            db.session {
+                queries.virksomhet.upsert(TestFixtures.virksomhet)
+            }
+        }
+
+        test("oppretter, henter og sletter Arena-deltakere") {
+            db.session {
+                queries.arenaDeltaker.upsertArenaDeltaker(arbeidstreningArenaDeltakelse)
+                queries.arenaDeltaker.upsertArenaDeltaker(mentorArenaDeltakelse)
+
+                queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(NorskIdent("12345678910")),
+                    maxAgeYears = null,
+                ) shouldContainExactlyInAnyOrder listOf(
+                    TiltakshistorikkV1Dto.ArenaDeltakelse(
+                        id = mentorArenaDeltakelse.id,
+                        norskIdent = NorskIdent("12345678910"),
+                        arenaTiltakskode = "MENTOR",
+                        startDato = LocalDate.of(2002, 2, 1),
+                        sluttDato = LocalDate.of(2002, 2, 1),
+                        status = ArenaDeltakerStatus.GJENNOMFORES,
+                        beskrivelse = "Mentortiltak hos Joblearn",
+                        arrangor = TiltakshistorikkV1Dto.Arrangor(Organisasjonsnummer("123123123")),
+                    ),
+                    TiltakshistorikkV1Dto.ArenaDeltakelse(
+                        id = arbeidstreningArenaDeltakelse.id,
+                        norskIdent = NorskIdent("12345678910"),
+                        arenaTiltakskode = "ARBTREN",
+                        status = ArenaDeltakerStatus.GJENNOMFORES,
+                        startDato = LocalDate.of(2024, 1, 1),
+                        sluttDato = LocalDate.of(2024, 1, 31),
+                        beskrivelse = "Arbeidstrening hos Fretex",
+                        arrangor = TiltakshistorikkV1Dto.Arrangor(Organisasjonsnummer("123123123")),
+                    ),
+                )
+
+                queries.arenaDeltaker.deleteArenaDeltaker(mentorArenaDeltakelse.id)
+
+                queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(NorskIdent("12345678910")),
+                    maxAgeYears = null,
+                ) shouldBe listOf(
+                    TiltakshistorikkV1Dto.ArenaDeltakelse(
+                        id = arbeidstreningArenaDeltakelse.id,
+                        norskIdent = NorskIdent("12345678910"),
+                        arenaTiltakskode = "ARBTREN",
+                        status = ArenaDeltakerStatus.GJENNOMFORES,
+                        startDato = LocalDate.of(2024, 1, 1),
+                        sluttDato = LocalDate.of(2024, 1, 31),
+                        beskrivelse = "Arbeidstrening hos Fretex",
+                        arrangor = TiltakshistorikkV1Dto.Arrangor(Organisasjonsnummer("123123123")),
+                    ),
+                )
+
+                queries.arenaDeltaker.deleteArenaDeltaker(arbeidstreningArenaDeltakelse.id)
+
+                queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(NorskIdent("12345678910")),
+                    maxAgeYears = null,
+                ).shouldBeEmpty()
+            }
+        }
+
+        test("filtrerer Arena-deltakere basert på maxAgeYears") {
+            val mentorArenaDeltakelseUtenSlutt = ArenaDeltakerDbo(
+                id = UUID.randomUUID(),
+                norskIdent = NorskIdent("12345678910"),
+                arenaTiltakskode = "MENTOR",
+                status = ArenaDeltakerStatus.GJENNOMFORES,
+                startDato = LocalDateTime.of(2002, 2, 1, 0, 0, 0),
+                sluttDato = null,
+                beskrivelse = "Mentortiltak hos Joblearn",
+                arrangorOrganisasjonsnummer = Organisasjonsnummer("123123123"),
+                registrertIArenaDato = LocalDateTime.of(2002, 1, 1, 0, 0, 0),
+            )
+
+            db.transaction {
+                queries.arenaDeltaker.upsertArenaDeltaker(arbeidstreningArenaDeltakelse)
+                queries.arenaDeltaker.upsertArenaDeltaker(mentorArenaDeltakelse)
+                queries.arenaDeltaker.upsertArenaDeltaker(mentorArenaDeltakelseUtenSlutt)
+
+                queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(arbeidstreningArenaDeltakelse.norskIdent),
+                    maxAgeYears = 5,
+                ).map { it.id } shouldContainExactlyInAnyOrder listOf(arbeidstreningArenaDeltakelse.id)
+
+                queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(arbeidstreningArenaDeltakelse.norskIdent),
+                    maxAgeYears = null,
+                ).map { it.id } shouldContainExactlyInAnyOrder listOf(
+                    arbeidstreningArenaDeltakelse.id,
+                    mentorArenaDeltakelse.id,
+                    mentorArenaDeltakelseUtenSlutt.id,
+                )
+            }
+        }
+    }
+
+    context("Komet deltaker") {
+        val gruppeAmo = TestFixtures.gjennomforingGruppe
+        val amtDeltaker = TestFixtures.amtDeltaker
+
+        val db = TiltakshistorikkDatabase(database.db)
+
+        beforeAny {
+            db.session {
+                queries.virksomhet.upsert(TestFixtures.virksomhet)
+                queries.gjennomforing.upsert(toGjennomforingDbo(gruppeAmo))
+            }
+        }
+
+        test("oppretter og henter Komet-deltaker med tilhørende gjennomføring") {
+            db.transaction {
+                queries.kometDeltaker.upsertKometDeltaker(amtDeltaker)
+
+                queries.kometDeltaker.getKometHistorikk(
+                    identer = listOf(NorskIdent(amtDeltaker.personIdent)),
+                    maxAgeYears = null,
+                ) shouldBe listOf(
+                    TiltakshistorikkV1Dto.GruppetiltakDeltakelse(
+                        id = amtDeltaker.id,
+                        norskIdent = NorskIdent("10101010100"),
+                        startDato = null,
+                        sluttDato = null,
+                        status = DeltakerStatus(
+                            type = DeltakerStatusType.VENTER_PA_OPPSTART,
+                            aarsak = null,
+                            opprettetDato = LocalDateTime.of(2022, 1, 1, 0, 0),
+                        ),
+                        tiltakstype = TiltakshistorikkV1Dto.GruppetiltakDeltakelse.Tiltakstype(
+                            tiltakskode = gruppeAmo.tiltakskode,
+                            navn = null,
+                        ),
+                        gjennomforing = TiltakshistorikkV1Dto.Gjennomforing(
+                            id = gruppeAmo.id,
+                            navn = gruppeAmo.navn,
+                            tiltakskode = gruppeAmo.tiltakskode,
+                        ),
+                        arrangor = TiltakshistorikkV1Dto.Arrangor(Organisasjonsnummer("123123123")),
+                    ),
+                )
+            }
+        }
+
+        test("filtrerer Komet-deltakere basert på maxAgeYears") {
+            db.transaction {
+                val amtDeltakerReg2005 = AmtDeltakerV1Dto(
+                    id = UUID.randomUUID(),
+                    gjennomforingId = gruppeAmo.id,
+                    personIdent = "10101010100",
+                    startDato = null,
+                    sluttDato = null,
+                    status = DeltakerStatus(
+                        type = DeltakerStatusType.VENTER_PA_OPPSTART,
+                        aarsak = null,
+                        opprettetDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    ),
+                    registrertDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    endretDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    dagerPerUke = 2.5f,
+                    prosentStilling = null,
+                    deltakelsesmengder = listOf(),
+                )
+                val amtDeltakerReg2005Slutt2024 = AmtDeltakerV1Dto(
+                    id = UUID.randomUUID(),
+                    gjennomforingId = gruppeAmo.id,
+                    personIdent = "10101010100",
+                    startDato = null,
+                    sluttDato = LocalDate.of(2024, 1, 1),
+                    status = DeltakerStatus(
+                        type = DeltakerStatusType.VENTER_PA_OPPSTART,
+                        aarsak = null,
+                        opprettetDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    ),
+                    registrertDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    endretDato = LocalDateTime.of(2005, 3, 1, 0, 0, 0),
+                    dagerPerUke = 2.5f,
+                    prosentStilling = null,
+                    deltakelsesmengder = listOf(),
+                )
+
+                queries.kometDeltaker.upsertKometDeltaker(amtDeltaker)
+                queries.kometDeltaker.upsertKometDeltaker(amtDeltakerReg2005)
+                queries.kometDeltaker.upsertKometDeltaker(amtDeltakerReg2005Slutt2024)
+
+                queries.kometDeltaker.getKometHistorikk(
+                    identer = listOf(NorskIdent(amtDeltaker.personIdent)),
+                    maxAgeYears = null,
+                ).map { it.id } shouldContainExactlyInAnyOrder listOf(
+                    amtDeltaker.id,
+                    amtDeltakerReg2005.id,
+                    amtDeltakerReg2005Slutt2024.id,
+                )
+
+                queries.kometDeltaker.getKometHistorikk(
+                    identer = listOf(NorskIdent(amtDeltaker.personIdent)),
+                    maxAgeYears = 5,
+                ).map { it.id } shouldContainExactlyInAnyOrder listOf(
+                    amtDeltaker.id,
+                    amtDeltakerReg2005Slutt2024.id,
+                )
+            }
+        }
+
+        test("sletter Komet-deltaker") {
+            db.transaction {
+                val testDeltaker = AmtDeltakerV1Dto(
+                    id = UUID.randomUUID(),
+                    gjennomforingId = gruppeAmo.id,
+                    personIdent = "99999999999",
+                    startDato = null,
+                    sluttDato = null,
+                    status = DeltakerStatus(
+                        type = DeltakerStatusType.VENTER_PA_OPPSTART,
+                        aarsak = null,
+                        opprettetDato = LocalDateTime.of(2022, 1, 1, 0, 0),
+                    ),
+                    registrertDato = LocalDateTime.of(2022, 1, 1, 0, 0),
+                    endretDato = LocalDateTime.of(2022, 1, 1, 0, 0),
+                    dagerPerUke = 2.5f,
+                    prosentStilling = null,
+                    deltakelsesmengder = listOf(),
+                )
+
+                queries.kometDeltaker.upsertKometDeltaker(testDeltaker)
+
+                queries.kometDeltaker.getKometHistorikk(
+                    identer = listOf(NorskIdent(testDeltaker.personIdent)),
+                    maxAgeYears = null,
+                ).map { it.id } shouldBe listOf(testDeltaker.id)
+
+                queries.kometDeltaker.deleteKometDeltaker(testDeltaker.id)
+
+                queries.kometDeltaker.getKometHistorikk(
+                    identer = listOf(NorskIdent(testDeltaker.personIdent)),
+                    maxAgeYears = null,
+                ).shouldBeEmpty()
+            }
+        }
+    }
+
+    context("Flere identiteter") {
+        var db = TiltakshistorikkDatabase(database.db)
+
+        beforeAny {
+            db.session {
+                queries.virksomhet.upsert(TestFixtures.virksomhet)
+            }
+        }
+
+        test("henter Arena-historikk for flere identiteter") {
+            db.transaction {
+                val deltaker1 = ArenaDeltakerDbo(
+                    id = UUID.randomUUID(),
+                    norskIdent = NorskIdent("11111111111"),
+                    arenaTiltakskode = "ARBTREN",
+                    status = ArenaDeltakerStatus.GJENNOMFORES,
+                    startDato = LocalDateTime.of(2020, 1, 1, 0, 0, 0),
+                    sluttDato = LocalDateTime.of(2020, 6, 1, 0, 0, 0),
+                    beskrivelse = "Første deltakelse",
+                    arrangorOrganisasjonsnummer = Organisasjonsnummer("123123123"),
+                    registrertIArenaDato = LocalDateTime.of(2020, 1, 1, 0, 0, 0),
+                )
+
+                val deltaker2 = ArenaDeltakerDbo(
+                    id = UUID.randomUUID(),
+                    norskIdent = NorskIdent("22222222222"),
+                    arenaTiltakskode = "MENTOR",
+                    status = ArenaDeltakerStatus.DELTAKELSE_AVBRUTT,
+                    startDato = LocalDateTime.of(2021, 1, 1, 0, 0, 0),
+                    sluttDato = LocalDateTime.of(2021, 3, 1, 0, 0, 0),
+                    beskrivelse = "Andre deltakelse",
+                    arrangorOrganisasjonsnummer = Organisasjonsnummer("123123123"),
+                    registrertIArenaDato = LocalDateTime.of(2021, 1, 1, 0, 0, 0),
+                )
+
+                queries.arenaDeltaker.upsertArenaDeltaker(deltaker1)
+                queries.arenaDeltaker.upsertArenaDeltaker(deltaker2)
+
+                val historikk = queries.arenaDeltaker.getArenaHistorikk(
+                    identer = listOf(NorskIdent("11111111111"), NorskIdent("22222222222")),
+                    maxAgeYears = null,
+                )
+
+                historikk.map { it.id } shouldContainExactlyInAnyOrder listOf(deltaker1.id, deltaker2.id)
+            }
+        }
+    }
+})
