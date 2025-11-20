2737261d6c6102d9e8aeb1dbf700837328e7ef49
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-11-20 13:04:36 +0100

    Kommenter status typene for bestillinger og faktura

--- common/tiltaksokonomi-client/src/main/kotlin/no/nav/tiltak/okonomi/FakturaStatus.kt
+++ common/tiltaksokonomi-client/src/main/kotlin/no/nav/tiltak/okonomi/FakturaStatus.kt
@@ -13,9 +13,28 @@ data class FakturaStatus(
 )
 
 enum class FakturaStatusType {
+    /**
+     * Sendt til OeBS, ikke mottatt kvittering enda
+     */
     SENDT,
+
+    /**
+     * Fakturaen er prosessert ok i OeBS, men ikke sendt til banken
+     */
     IKKE_BETALT,
+
+    /**
+     * Noe av beløpet er sendt til banken (visstnok lite brukt)
+     */
     DELVIS_BETALT,
+
+    /**
+     * Betyr at hele beløpet er sendt til banken
+     */
     FULLT_BETALT,
+
+    /**
+     * Krever manuell oppfølging
+     */
     FEILET,
 }
