b961d49be367fb6d416f975cd5460f098658f17e
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-11-20 17:00:18 +0100

    fjern foreign keys til tiltakstypene finnes

--- mulighetsrommet-tiltakshistorikk/src/main/resources/db/migration/V20__tiltakstype.sql
+++ mulighetsrommet-tiltakshistorikk/src/main/resources/db/migration/V20__tiltakstype.sql
@@ -13,13 +13,3 @@ create trigger set_timestamp
     on tiltakstype
     for each row
 execute procedure trigger_set_timestamp();
-
-alter table arena_gjennomforing
-    add constraint fk_arena_tiltakskode foreign key (arena_tiltakskode) references tiltakstype (arena_tiltakskode);
-
-create index idx_arena_gjennomforing_arena_tiltakskode on arena_gjennomforing (arena_tiltakskode);
-
-alter table gjennomforing
-    add constraint fk_tiltakskode foreign key (tiltakskode) references tiltakstype (tiltakskode);
-
-create index idx_gjennomforing_tiltakskode on gjennomforing (tiltakskode);
