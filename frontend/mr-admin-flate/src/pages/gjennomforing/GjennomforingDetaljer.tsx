c0cd8a0b71cc623f33b6f012e31b1d88c77a4232
Benedicte Myrvoll <benemyr17@gmail.com>
2025-11-24 10:48:33 +0100

    Strukturer gjennomføringdetaljer likt  som avtaledetaljer

--- frontend/mr-admin-flate/src/pages/gjennomforing/GjennomforingDetaljer.tsx
+++ frontend/mr-admin-flate/src/pages/gjennomforing/GjennomforingDetaljer.tsx
@@ -3,8 +3,7 @@ import { getDisplayName } from "@/api/enhet/helpers";
 import { useGjennomforing } from "@/api/gjennomforing/useGjennomforing";
 import { usePollTiltaksnummer } from "@/api/gjennomforing/usePollTiltaksnummer";
 import { AmoKategoriseringDetaljer } from "@/components/amoKategorisering/AmoKategoriseringDetaljer";
-import { Bolk } from "@/components/detaljside/Bolk";
-import { Metadata, Separator } from "@/components/detaljside/Metadata";
+import { Separator } from "@/components/detaljside/Metadata";
 import { NokkeltallDeltakere } from "@/components/gjennomforing/NokkeltallDeltakere";
 import { StengtHosArrangorTable } from "@/components/gjennomforing/stengt/StengtHosArrangorTable";
 import { TiltakTilgjengeligForArrangor } from "@/components/gjennomforing/TilgjengeligTiltakForArrangor";
@@ -18,10 +17,14 @@ import { formatertVentetid, isKursTiltak } from "@/utils/Utils";
 import { Lenke } from "@mr/frontend-common/components/lenke/Lenke";
 import { NOM_ANSATT_SIDE } from "@mr/frontend-common/constants";
 import { formaterDato } from "@mr/frontend-common/utils/date";
-import { BodyShort, HelpText, HStack, Tag, VStack } from "@navikt/ds-react";
+import { HelpText, HStack, Tag, VStack } from "@navikt/ds-react";
 import { Link } from "react-router";
 import { GjennomforingPageLayout } from "./GjennomforingPageLayout";
 import { GjennomforingOppstartstype } from "@tiltaksadministrasjon/api-client";
+import {
+  Definisjonsliste,
+  Definition,
+} from "@mr/frontend-common/components/definisjonsliste/Definisjonsliste";
 
 export function GjennomforingDetaljer() {
   const { gjennomforingId } = useRequiredParams(["gjennomforingId"]);
@@ -29,6 +32,7 @@ export function GjennomforingDetaljer() {
   const { data: avtale } = usePotentialAvtale(gjennomforing.avtaleId);
 
   const {
+    navn,
     tiltakstype,
     tiltaksnummer,
     startDato,
@@ -46,192 +50,165 @@ export function GjennomforingDetaljer() {
     utdanningslop,
   } = gjennomforing;
 
+  const avtaleMeta: Definition[] = [
+    {
+      key: gjennomforingTekster.avtaleLabel,
+      value: avtale ? (
+        <Link to={`/avtaler/${avtale.id}`}>
+          {avtale.navn} {avtale.avtalenummer ?? null}
+        </Link>
+      ) : (
+        gjennomforingTekster.ingenAvtaleForGjennomforingenLabel
+      ),
+    },
+    {
+      key: "Avtaleperiode",
+      value:
+        avtale &&
+        `${formaterDato(avtale.startDato)} -
+                        ${formaterDato(avtale.sluttDato) ?? ""}`,
+    },
+  ];
+
+  const gjennomforingMeta: Definition[] = [
+    { key: gjennomforingTekster.tiltaksnavnLabel, value: navn },
+    {
+      key: gjennomforingTekster.tiltakstypeLabel,
+      value: tiltakstype.navn,
+    },
+    {
+      key: gjennomforingTekster.oppstartstypeLabel,
+      value: oppstart === GjennomforingOppstartstype.FELLES ? "Felles" : "Løpende oppstart",
+    },
+    {
+      key: gjennomforingTekster.tiltaksnummerLabel,
+      value: tiltaksnummer ?? <HentTiltaksnummer id={gjennomforing.id} />,
+    },
+  ];
+
+  const varighet: Definition[] = [
+    { key: gjennomforingTekster.startdatoLabel, value: formaterDato(startDato) },
+    { key: gjennomforingTekster.sluttdatoLabel, value: formaterDato(sluttDato) ?? "" },
+    { key: gjennomforingTekster.antallPlasserLabel, value: antallPlasser },
+    { key: gjennomforingTekster.apentForPameldingLabel, value: apentForPamelding ? "Ja" : "Nei" },
+    ...(isKursTiltak(tiltakstype.tiltakskode)
+      ? [{ key: gjennomforingTekster.deltidsprosentLabel, value: deltidsprosent }]
+      : []),
+    ...(gjennomforing.estimertVentetid
+      ? [
+          {
+            key: gjennomforingTekster.estimertVentetidLabel,
+            value: formatertVentetid(
+              gjennomforing.estimertVentetid.verdi,
+              gjennomforing.estimertVentetid.enhet,
+            ),
+          },
+        ]
+      : []),
+  ];
+  const administratorMeta: Definition[] = [
+    {
+      key: gjennomforingTekster.administratorerForGjennomforingenLabel,
+      value: administratorer.length ? (
+        <ul>
+          {administratorer.map((admin) => {
+            return (
+              <li key={admin.navIdent}>
+                <Lenke to={`${NOM_ANSATT_SIDE}${admin.navIdent}`} isExternal>
+                  {`${admin.navn} - ${admin.navIdent}`}{" "}
+                </Lenke>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        gjennomforingTekster.ingenAdministratorerSattForGjennomforingenLabel
+      ),
+    },
+    {
+      key: gjennomforingTekster.ansvarligEnhetFraArenaLabel,
+      value: arenaAnsvarligEnhet ? (
+        <HStack gap="2">
+          {getDisplayName(arenaAnsvarligEnhet)}
+          <HelpText title="Hva betyr feltet 'Ansvarlig enhet fra Arena'?">
+            Ansvarlig enhet fra Arena blir satt i Arena basert på tiltaksansvarlig sin enhet når man
+            oppretter tiltak i Arena.
+          </HelpText>
+        </HStack>
+      ) : (
+        "-"
+      ),
+    },
+  ];
+  const arrangorMeta: Definition[] = [
+    ...(avtale?.arrangor
+      ? [
+          {
+            key: gjennomforingTekster.tiltaksarrangorHovedenhetLabel,
+            value: (
+              <Link to={`/arrangorer/${avtale.arrangor.id}`}>
+                {avtale.arrangor.navn} - {avtale.arrangor.organisasjonsnummer}
+              </Link>
+            ),
+          },
+        ]
+      : []),
+    {
+      key: gjennomforingTekster.tiltaksarrangorUnderenhetLabel,
+      value: `${arrangor.navn} - ${arrangor.organisasjonsnummer}`,
+    },
+    {
+      key: gjennomforingTekster.kontaktpersonerHosTiltaksarrangorLabel,
+      value:
+        arrangor.kontaktpersoner.length > 0
+          ? arrangor.kontaktpersoner.map((kontaktperson) => (
+              <ArrangorKontaktpersonDetaljer key={kontaktperson.id} kontaktperson={kontaktperson} />
+            ))
+          : "-",
+    },
+  ];
+
+  const stedMeta: Definition[] = [
+    { key: gjennomforingTekster.stedForGjennomforingLabel, value: stedForGjennomforing },
+    { key: gjennomforingTekster.oppmoteStedLabel, value: oppmoteSted },
+  ];
+
   return (
     <GjennomforingPageLayout>
       <TwoColumnGrid separator>
         <VStack justify={"space-between"}>
-          <Bolk aria-label="Tiltaksnavn og tiltaksnummer" data-testid="tiltaksnavn">
-            <Metadata header={gjennomforingTekster.tiltaksnavnLabel} value={gjennomforing.navn} />
-            <Metadata
-              header={gjennomforingTekster.tiltaksnummerLabel}
-              value={tiltaksnummer ?? <HentTiltaksnummer id={gjennomforing.id} />}
-            />
-          </Bolk>
-
-          <Bolk aria-label="Tiltakstype og avtaletype">
-            <Metadata
-              header={gjennomforingTekster.avtaleLabel}
-              value={
-                avtale?.id ? (
-                  <>
-                    <Link to={`/avtaler/${avtale.id}`}>
-                      {avtale.navn} {avtale.avtalenummer ? ` - ${avtale.avtalenummer}` : null}
-                    </Link>{" "}
-                    <BodyShort>
-                      <small>
-                        Avtalens periode: {formaterDato(avtale.startDato)} -{" "}
-                        {avtale.sluttDato ? formaterDato(avtale.sluttDato) : ""}
-                      </small>
-                    </BodyShort>
-                  </>
-                ) : (
-                  gjennomforingTekster.ingenAvtaleForGjennomforingenLabel
-                )
-              }
-            />
-            <Metadata header={gjennomforingTekster.tiltakstypeLabel} value={tiltakstype.navn} />
-          </Bolk>
+          <Definisjonsliste title="Gjennomføring" definitions={gjennomforingMeta} />
           <Separator />
+          <Definisjonsliste title="Avtale" definitions={avtaleMeta} />
+          <Separator />
+          <Definisjonsliste title="Varighet og påmelding" definitions={varighet} />
           {amoKategorisering && (
             <>
-              <AmoKategoriseringDetaljer amoKategorisering={amoKategorisering} />
               <Separator />
+              <AmoKategoriseringDetaljer amoKategorisering={amoKategorisering} />
             </>
           )}
           {utdanningslop && <UtdanningslopDetaljer utdanningslop={utdanningslop} />}
-          <Bolk aria-label={gjennomforingTekster.oppstartstypeLabel}>
-            <Metadata
-              header={gjennomforingTekster.oppstartstypeLabel}
-              value={oppstart === GjennomforingOppstartstype.FELLES ? "Felles" : "Løpende oppstart"}
-            />
-          </Bolk>
-          <Bolk aria-label="Start- og sluttdato">
-            <Metadata
-              header={gjennomforingTekster.startdatoLabel}
-              value={formaterDato(startDato)}
-            />
-            <Metadata
-              header={gjennomforingTekster.sluttdatoLabel}
-              value={sluttDato ? formaterDato(sluttDato) : "-"}
-            />
-          </Bolk>
+        </VStack>
+        <VStack>
+          <Definisjonsliste title="Administrator" definitions={administratorMeta} />
+          <Separator />
+          <Definisjonsliste title="Arrangør" definitions={arrangorMeta} columns={1} />
+          {(stedForGjennomforing || oppmoteSted) && (
+            <>
+              <Separator />
+              <Definisjonsliste title="Sted" definitions={stedMeta} />
+            </>
+          )}
           {gjennomforing.stengt.length !== 0 && (
             <StengtHosArrangorTable gjennomforing={gjennomforing} readOnly />
           )}
-
-          <Bolk>
-            <Metadata header={gjennomforingTekster.antallPlasserLabel} value={antallPlasser} />
-            {isKursTiltak(tiltakstype.tiltakskode) && (
-              <Metadata header={gjennomforingTekster.deltidsprosentLabel} value={deltidsprosent} />
-            )}
-          </Bolk>
-
-          <Separator />
-          <Bolk aria-label={gjennomforingTekster.apentForPameldingLabel}>
-            <Metadata
-              header={gjennomforingTekster.apentForPameldingLabel}
-              value={apentForPamelding ? "Ja" : "Nei"}
-            />
-          </Bolk>
-
-          <Separator />
-
-          {gjennomforing.estimertVentetid ? (
-            <>
-              <Bolk aria-label={gjennomforingTekster.estimertVentetidLabel}>
-                <Metadata
-                  header={gjennomforingTekster.estimertVentetidLabel}
-                  value={formatertVentetid(
-                    gjennomforing.estimertVentetid.verdi,
-                    gjennomforing.estimertVentetid.enhet,
-                  )}
-                />
-              </Bolk>
-              <Separator />
-            </>
-          ) : null}
-
-          <Bolk aria-label={gjennomforingTekster.administratorerForGjennomforingenLabel}>
-            <Metadata
-              header={gjennomforingTekster.administratorerForGjennomforingenLabel}
-              value={
-                administratorer.length ? (
-                  <ul>
-                    {administratorer.map((admin) => {
-                      return (
-                        <li key={admin.navIdent}>
-                          <Lenke
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            to={`${NOM_ANSATT_SIDE}${admin.navIdent}`}
-                            isExternal
-                            className="flex gap-1.5"
-                          >
-                            {`${admin.navn} - ${admin.navIdent}`}{" "}
-                          </Lenke>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                ) : (
-                  gjennomforingTekster.ingenAdministratorerSattForGjennomforingenLabel
-                )
-              }
-            />
-          </Bolk>
         </VStack>
-        <VStack>
-          {arenaAnsvarligEnhet ? (
-            <Bolk>
-              <div style={{ display: "flex", gap: "1rem" }}>
-                <Metadata
-                  header={gjennomforingTekster.ansvarligEnhetFraArenaLabel}
-                  value={getDisplayName(arenaAnsvarligEnhet)}
-                />
-                <HelpText title="Hva betyr feltet 'Ansvarlig enhet fra Arena'?">
-                  Ansvarlig enhet fra Arena blir satt i Arena basert på tiltaksansvarlig sin enhet
-                  når man oppretter tiltak i Arena.
-                </HelpText>
-              </div>
-            </Bolk>
-          ) : null}
-          <Separator />
-          <VStack gap="5">
-            {avtale?.arrangor ? (
-              <Metadata
-                header={gjennomforingTekster.tiltaksarrangorHovedenhetLabel}
-                value={
-                  <Link to={`/arrangorer/${avtale.arrangor.id}`}>
-                    {avtale.arrangor.navn} - {avtale.arrangor.organisasjonsnummer}
-                  </Link>
-                }
-              />
-            ) : null}
-            <Metadata
-              header={gjennomforingTekster.tiltaksarrangorUnderenhetLabel}
-              value={`${arrangor.navn} - ${arrangor.organisasjonsnummer}`}
-            />
-            {arrangor.kontaktpersoner.length > 0 && (
-              <Metadata
-                header={gjennomforingTekster.kontaktpersonerHosTiltaksarrangorLabel}
-                value={
-                  <VStack>
-                    {arrangor.kontaktpersoner.map((kontaktperson) => (
-                      <ArrangorKontaktpersonDetaljer
-                        key={kontaktperson.id}
-                        kontaktperson={kontaktperson}
-                      />
-                    ))}
-                  </VStack>
-                }
-              />
-            )}
-            {(stedForGjennomforing || oppmoteSted) && (
-              <>
-                <Separator />
-                <Metadata
-                  header={gjennomforingTekster.stedForGjennomforingLabel}
-                  value={stedForGjennomforing}
-                />
-                <Metadata header={gjennomforingTekster.oppmoteStedLabel} value={oppmoteSted} />
-              </>
-            )}
-          </VStack>
-          {new Date() < new Date(gjennomforing.startDato) && (
-            <TiltakTilgjengeligForArrangor gjennomforing={gjennomforing} />
-          )}
-        </VStack>
+        {new Date() < new Date(gjennomforing.startDato) && (
+          <TiltakTilgjengeligForArrangor gjennomforing={gjennomforing} />
+        )}
       </TwoColumnGrid>
+      <Separator />
       <NokkeltallDeltakere gjennomforingId={gjennomforing.id} />
     </GjennomforingPageLayout>
   );
