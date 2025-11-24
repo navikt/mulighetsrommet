6600412e2d6318d68a17c76dda9003a98e0e793c
Benedicte Myrvoll <benemyr17@gmail.com>
2025-11-24 13:39:23 +0100

    Fjern ubrukt css

--- frontend/mr-admin-flate/src/layouts/HeaderBanner.tsx
+++ frontend/mr-admin-flate/src/layouts/HeaderBanner.tsx
@@ -4,13 +4,12 @@ import { kebabCase } from "@mr/frontend-common/utils/TestUtils";
 
 interface HeaderBannerProps {
   heading: string;
-  harUndermeny?: boolean;
   ikon?: ReactNode;
 }
 
-export function HeaderBanner({ heading, harUndermeny = false, ikon }: HeaderBannerProps) {
+export function HeaderBanner({ heading, ikon }: HeaderBannerProps) {
   return (
-    <div className={`bg-white p-2 ${!harUndermeny && "border-b-border-divider"}`}>
+    <div className="bg-white p-2">
       <HStack align="center" justify="start" gap="2" wrap>
         {ikon ? <span>{ikon}</span> : null}
         <Heading level="2" size="large" data-testid={`header_${kebabCase(heading)}`}>
