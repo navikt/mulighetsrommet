e11120e90f4d7fc31095df28d6e40513a98e1092
Sondre Lefsaker <sondre.lefsaker@nav.no>
2025-11-26 09:00:28 +0100

    fjern gamle views

---
+++ mulighetsrommet-api/src/main/resources/db/migration/V384__drop_old_views.sql
@@ -0,0 +1,8 @@
+drop view if exists avtale_admin_dto_view;
+drop view if exists gjennomforing_admin_dto_view;
+drop view if exists tilsagn_admin_dto_view;
+drop view if exists tiltakstype_admin_dto_view;
+drop view if exists utbetaling_dto_view;
+drop view if exists veilederflate_tiltak_view;
+drop view if exists view_gjennomforing_enkeltplass_admin;
+drop view if exists view_nav_ansatt_dto;
