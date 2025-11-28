72bf95519766aa43911ef305f2fc8a47f44bbc0a
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2025-11-28 08:07:44 +0100

    fix: include frontend-common in Tailwind content paths arrangor-flate

--- frontend/arrangor-flate/tailwind.config.ts
+++ frontend/arrangor-flate/tailwind.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "tailwindcss";
+import path from "path";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
+    "../frontend-common/**/*.{js,jsx,ts,tsx}",
+    path.join(
+      path.dirname(require.resolve("@mr/frontend-common/package.json")),
+      "**/*.{js,jsx,ts,tsx}",
+    ),
+  ],
   theme: {
     extend: {},
   },
