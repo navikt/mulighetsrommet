a8de6613fc24d94e6a9456e4082f8a290f1d0f93
Ovin Teige <Ovin.Eilertsen.Teige@nav.no>
2025-12-01 10:56:31 +0100

    Legg til test for timespris på dokument generering

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/utbetaling/mapper/UbetalingToPdfDocumentContentMapperTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/utbetaling/mapper/UbetalingToPdfDocumentContentMapperTest.kt
@@ -9,6 +9,7 @@ import no.nav.mulighetsrommet.api.clients.amtDeltaker.DeltakerPersonalia
 import no.nav.mulighetsrommet.api.clients.pdl.PdlGradering
 import no.nav.mulighetsrommet.api.pdfgen.PdfDocumentContent
 import no.nav.mulighetsrommet.api.utbetaling.model.DeltakelseDeltakelsesprosentPerioder
+import no.nav.mulighetsrommet.api.utbetaling.model.DeltakelsePeriode
 import no.nav.mulighetsrommet.api.utbetaling.model.DeltakelsesprosentPeriode
 import no.nav.mulighetsrommet.api.utbetaling.model.DelutbetalingStatus
 import no.nav.mulighetsrommet.api.utbetaling.model.SatsPeriode
@@ -17,6 +18,7 @@ import no.nav.mulighetsrommet.api.utbetaling.model.Utbetaling
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningFastSatsPerTiltaksplassPerManed
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningOutputDeltakelse
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningOutputDeltakelse.BeregnetPeriode
+import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningPrisPerTimeOppfolging
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingStatusType
 import no.nav.mulighetsrommet.model.*
 import no.nav.mulighetsrommet.model.NorskIdent
@@ -36,7 +38,7 @@ class UbetalingToPdfDocumentContentMapperTest : FunSpec({
     val deltaker2Id = UUID.randomUUID()
     val deltaker3Id = UUID.randomUUID()
 
-    val utbetaling = Utbetaling(
+    val utbetalingFastSats = Utbetaling(
         id = UUID.randomUUID(),
         status = UtbetalingStatusType.FERDIG_BEHANDLET,
         godkjentAvArrangorTidspunkt = LocalDate.of(2025, 1, 2).atStartOfDay(),
@@ -144,6 +146,64 @@ class UbetalingToPdfDocumentContentMapperTest : FunSpec({
         tilskuddstype = Tilskuddstype.TILTAK_DRIFTSTILSKUDD,
     )
 
+    val utbetalingPrisPerTimeOppfolging = Utbetaling(
+        id = UUID.randomUUID(),
+        status = UtbetalingStatusType.FERDIG_BEHANDLET,
+        godkjentAvArrangorTidspunkt = LocalDate.of(2025, 1, 2).atStartOfDay(),
+        createdAt = LocalDate.of(2025, 1, 1).atStartOfDay(),
+        tiltakstype = Utbetaling.Tiltakstype("Oppfolging", Tiltakskode.OPPFOLGING),
+        gjennomforing = Utbetaling.Gjennomforing(
+            id = UUID.randomUUID(),
+            lopenummer = Tiltaksnummer("2025/10000"),
+            navn = "Oppfolging hos Nav",
+        ),
+        arrangor = Utbetaling.Arrangor(
+            id = UUID.randomUUID(),
+            organisasjonsnummer = Organisasjonsnummer("123456789"),
+            navn = "Nav",
+            slettet = false,
+        ),
+        beregning = UtbetalingBeregningPrisPerTimeOppfolging(
+            input = UtbetalingBeregningPrisPerTimeOppfolging.Input(
+                satser = setOf(SatsPeriode(Periode.forMonthOf(LocalDate.of(2025, 1, 1)), 34)),
+                stengt = setOf(
+                    StengtPeriode(
+                        periode = Periode(LocalDate.of(2025, 1, 7), LocalDate.of(2025, 1, 14)),
+                        beskrivelse = "Stengt for ferie",
+                    ),
+                ),
+                deltakelser = setOf(
+                    DeltakelsePeriode(
+                        deltakelseId = deltaker1Id,
+                        periode = Periode.forMonthOf(LocalDate.of(2025, 1, 1)),
+                    ),
+                    DeltakelsePeriode(
+                        deltakelseId = deltaker2Id,
+                        periode = Periode.forMonthOf(LocalDate.of(2025, 1, 1)),
+                    ),
+                    DeltakelsePeriode(
+                        deltakelseId = deltaker3Id,
+                        periode = Periode.forMonthOf(LocalDate.of(2025, 1, 1)),
+                    ),
+                ),
+                belop = 100,
+            ),
+            output = UtbetalingBeregningPrisPerTimeOppfolging.Output(
+                belop = 100,
+            ),
+        ),
+        betalingsinformasjon = Utbetaling.Betalingsinformasjon(
+            kontonummer = Kontonummer("12345678901"),
+            kid = null,
+        ),
+        periode = Periode.forMonthOf(LocalDate.of(2025, 1, 1)),
+        innsender = Arrangor,
+        journalpostId = null,
+        beskrivelse = null,
+        begrunnelseMindreBetalt = null,
+        tilskuddstype = Tilskuddstype.TILTAK_DRIFTSTILSKUDD,
+    )
+
     val personalia = mapOf(
         deltaker1Id to DeltakerPersonalia(
             navn = "Ola Skjermet",
@@ -194,21 +254,39 @@ class UbetalingToPdfDocumentContentMapperTest : FunSpec({
         ),
     )
 
-    test("pdf-content for utbetalingsdetaljer til arrangør") {
-        val pdfContent = UbetalingToPdfDocumentContentMapper.toUtbetalingsdetaljerPdfContent(utbetaling, linjer)
+    context("pdf-content for utbetalingsdetaljer til arrangør") {
+        test("fast sats per tiltaksplass per maned") {
+            val pdfContent =
+                UbetalingToPdfDocumentContentMapper.toUtbetalingsdetaljerPdfContent(utbetalingFastSats, linjer)
 
-        jsonPrettyPrint.encodeToString<PdfDocumentContent>(pdfContent) shouldBe expectedUtbetalingsdetaljerContent
+            jsonPrettyPrint.encodeToString<PdfDocumentContent>(pdfContent) shouldBe expectedUtbetalingsdetaljerFastSatsContent
+        }
+
+        test("avtalt pris per time oppfølging per deltaker") {
+            val pdfContent =
+                UbetalingToPdfDocumentContentMapper.toUtbetalingsdetaljerPdfContent(utbetalingPrisPerTimeOppfolging, linjer)
+
+            jsonPrettyPrint.encodeToString<PdfDocumentContent>(pdfContent) shouldBe expectedUtbetalingsdetaljerTimesPrisContent
+        }
     }
 
-    test("pdf-content for journalpost av innsending fra arrangør") {
-        val pdfContent = UbetalingToPdfDocumentContentMapper.toJournalpostPdfContent(utbetaling, personalia)
+    context("pdf-content for journalpost av innsending fra arrangør") {
+        test("fast sats per tiltaksplass per maned") {
+            val pdfContent = UbetalingToPdfDocumentContentMapper.toJournalpostPdfContent(utbetalingFastSats, personalia)
 
-        jsonPrettyPrint.encodeToString(pdfContent) shouldBe expectedJournalpostContent
+            jsonPrettyPrint.encodeToString<PdfDocumentContent>(pdfContent) shouldBe expectedJournalpostFastSatsContent
+        }
+        test("avtalt pris per time oppfølging per deltaker") {
+            val pdfContent =
+                UbetalingToPdfDocumentContentMapper.toJournalpostPdfContent(utbetalingPrisPerTimeOppfolging, personalia)
+
+            jsonPrettyPrint.encodeToString<PdfDocumentContent>(pdfContent) shouldBe expectedJournalpostTimesPrisContent
+        }
     }
 })
 
 @Language("JSON")
-private val expectedUtbetalingsdetaljerContent = """
+private val expectedUtbetalingsdetaljerFastSatsContent = """
 {
   "title": "Utbetalingsdetaljer",
   "subject": "Utbetaling til Nav",
@@ -396,7 +474,191 @@ private val expectedUtbetalingsdetaljerContent = """
 """.trimIndent()
 
 @Language("JSON")
-private val expectedJournalpostContent = """
+private val expectedUtbetalingsdetaljerTimesPrisContent = """
+    {
+      "title": "Utbetalingsdetaljer",
+      "subject": "Utbetaling til Nav",
+      "description": "Detaljer om utbetaling for gjennomføring av Oppfolging",
+      "author": "Nav",
+      "sections": [
+        {
+          "title": {
+            "text": "Detaljer om utbetaling",
+            "level": 1
+          }
+        },
+        {
+          "title": {
+            "text": "Innsending",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Arrangør",
+                  "value": "Nav (123456789)"
+                },
+                {
+                  "label": "Dato innsendt av arrangør",
+                  "value": "02.01.2025"
+                },
+                {
+                  "label": "Tiltakstype",
+                  "value": "Oppfolging"
+                },
+                {
+                  "label": "Løpenummer",
+                  "value": "2025/10000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Utbetaling",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Utbetalingsperiode",
+                  "value": "01.01.2025 - 31.01.2025"
+                }
+              ]
+            },
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Avtalt pris per time oppfølging",
+                  "value": "34",
+                  "format": "NOK"
+                }
+              ]
+            },
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Beløp",
+                  "value": "100",
+                  "format": "NOK"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Betalingsinformasjon",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Kontonummer",
+                  "value": "12345678901"
+                },
+                {
+                  "label": "KID-nummer",
+                  "value": null
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Utbetalingsstatus",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Status",
+                  "value": "Overført til utbetaling",
+                  "format": "STATUS_SUCCESS"
+                },
+                {
+                  "label": "Godkjent beløp til utbetaling",
+                  "value": "100",
+                  "format": "NOK"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Tilsagn som er brukt til utbetaling",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Tilsagn",
+                  "value": "A-1-1"
+                },
+                {
+                  "label": "Beløp til utbetaling",
+                  "value": "99",
+                  "format": "NOK"
+                },
+                {
+                  "label": "Status",
+                  "value": "Overført til utbetaling",
+                  "format": "STATUS_SUCCESS"
+                },
+                {
+                  "label": "Status endret",
+                  "value": "2025-01-03T00:00",
+                  "format": "DATE"
+                }
+              ]
+            },
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Tilsagn",
+                  "value": "A-1-2"
+                },
+                {
+                  "label": "Beløp til utbetaling",
+                  "value": "1",
+                  "format": "NOK"
+                },
+                {
+                  "label": "Status",
+                  "value": "Overført til utbetaling",
+                  "format": "STATUS_SUCCESS"
+                },
+                {
+                  "label": "Status endret",
+                  "value": "2025-01-03T00:00",
+                  "format": "DATE"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+""".trimIndent()
+
+@Language("JSON")
+private val expectedJournalpostFastSatsContent = """
 {
   "title": "Utbetaling",
   "subject": "Krav om utbetaling fra Nav",
@@ -700,4 +962,204 @@ private val expectedJournalpostContent = """
     }
   ]
 }
+""".trimIndent()
+
+@Language("JSON")
+private val expectedJournalpostTimesPrisContent = """
+    {
+      "title": "Utbetaling",
+      "subject": "Krav om utbetaling fra Nav",
+      "description": "Krav om utbetaling fra Nav",
+      "author": "Tiltaksadministrasjon",
+      "sections": [
+        {
+          "title": {
+            "text": "Innsendt krav om utbetaling",
+            "level": 1
+          }
+        },
+        {
+          "title": {
+            "text": "Innsending",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Arrangør",
+                  "value": "Nav (123456789)"
+                },
+                {
+                  "label": "Dato innsendt av arrangør",
+                  "value": "02.01.2025"
+                },
+                {
+                  "label": "Tiltakstype",
+                  "value": "Oppfolging"
+                },
+                {
+                  "label": "Løpenummer",
+                  "value": "2025/10000"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Utbetaling",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Utbetalingsperiode",
+                  "value": "01.01.2025 - 31.01.2025"
+                }
+              ]
+            },
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Avtalt pris per time oppfølging",
+                  "value": "34",
+                  "format": "NOK"
+                }
+              ]
+            },
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Beløp",
+                  "value": "100",
+                  "format": "NOK"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Betalingsinformasjon",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "description-list",
+              "entries": [
+                {
+                  "label": "Kontonummer",
+                  "value": "12345678901"
+                },
+                {
+                  "label": "KID-nummer",
+                  "value": null
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Stengt hos arrangør",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "item-list",
+              "description": "Det er registrert stengt hos arrangør i følgende perioder:",
+              "items": [
+                "07.01.2025 - 13.01.2025: Stengt for ferie"
+              ]
+            }
+          ]
+        },
+        {
+          "title": {
+            "text": "Deltakerperioder",
+            "level": 2
+          },
+          "blocks": [
+            {
+              "type": "table",
+              "table": {
+                "columns": [
+                  {
+                    "title": "Navn"
+                  },
+                  {
+                    "title": "Fødselsnr.",
+                    "align": "RIGHT"
+                  },
+                  {
+                    "title": "Startdato i perioden",
+                    "align": "RIGHT"
+                  },
+                  {
+                    "title": "Sluttdato i perioden",
+                    "align": "RIGHT"
+                  }
+                ],
+                "rows": [
+                  {
+                    "cells": [
+                      {
+                        "value": "Skjermet"
+                      },
+                      {
+                        "value": null
+                      },
+                      {
+                        "value": "01.01.2025"
+                      },
+                      {
+                        "value": "31.01.2025"
+                      }
+                    ]
+                  },
+                  {
+                    "cells": [
+                      {
+                        "value": "Ola Nordmann"
+                      },
+                      {
+                        "value": "01010199999"
+                      },
+                      {
+                        "value": "01.01.2025"
+                      },
+                      {
+                        "value": "31.01.2025"
+                      }
+                    ]
+                  },
+                  {
+                    "cells": [
+                      {
+                        "value": "Kari Nordmann"
+                      },
+                      {
+                        "value": "01010199998"
+                      },
+                      {
+                        "value": "01.01.2025"
+                      },
+                      {
+                        "value": "31.01.2025"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    }
 """.trimIndent()
