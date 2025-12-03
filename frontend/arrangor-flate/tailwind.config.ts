72d73ccd659f8854103959cc6033777ee12c8f8c
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2025-12-03 10:12:26 +0100

    Ta med tailwind klasser fra frontend-common i admin-flate

--- frontend/arrangor-flate/tailwind.config.ts
+++ frontend/arrangor-flate/tailwind.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 export default {
   content: [
     "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
-    "../frontend-common/**/*.{js,jsx,ts,tsx}",
     path.join(
       path.dirname(require.resolve("@mr/frontend-common/package.json")),
       "**/*.{js,jsx,ts,tsx}",
