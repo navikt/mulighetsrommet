8a719c9f942f307aefbb872f8d39aeb56355a50b
ovin-nav <ovin.eilertsen.teige@nav.no>
2025-12-09 12:11:54 +0100

    Ikke bruk expandable row når vi ikke har expandable content

--- frontend/frontend-common/components/datadriven/DataDrivenTable.tsx
+++ frontend/frontend-common/components/datadriven/DataDrivenTable.tsx
@@ -1,5 +1,10 @@
 import { Table, TableProps } from "@navikt/ds-react";
-import { DataDrivenTableDto, DataDrivenTableDtoRow, DataElement } from "./types";
+import {
+  DataDrivenTableDto,
+  DataDrivenTableDtoColumn,
+  DataDrivenTableDtoRow,
+  DataElement,
+} from "./types";
 import { DataDrivenTimeline } from "./DataDrivenTimeline";
 import { useSortableData } from "../../hooks/useSortableData";
 import { compareDataElements } from "./util";
@@ -19,6 +24,8 @@ export function DataDrivenTable({ data, className, size, ...rest }: Props) {
     compareDataElements,
   );
 
+  const hasExpandableContent = data.rows.some((row) => !!row.content);
+
   return (
     <Table sort={sort} onSortChange={toggleSort} className={className} size={size} {...rest}>
       <Table.Header>
@@ -36,25 +43,47 @@ export function DataDrivenTable({ data, className, size, ...rest }: Props) {
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {sortedData.map((row: DataDrivenTableDtoRow, index: number) => (
-          <Table.ExpandableRow
-            expansionDisabled={!row.content}
-            togglePlacement="right"
-            key={index}
-            content={row.content && <DataDrivenTimeline data={row.content} />}
-          >
-            {data.columns.map((col) => {
-              const cells: Record<string, DataElement | null> = row.cells;
-              const cell = cells[col.key];
-              return (
-                <Table.DataCell key={col.key} align={col.align}>
-                  {cell ? getDataElement(cell) : null}
-                </Table.DataCell>
-              );
-            })}
-          </Table.ExpandableRow>
-        ))}
+        {sortedData.map((row: DataDrivenTableDtoRow, index: number) => {
+          if (hasExpandableContent) {
+            return (
+              <Table.ExpandableRow
+                expansionDisabled={!row.content}
+                togglePlacement="right"
+                key={index}
+                content={row.content && <DataDrivenTimeline data={row.content} />}
+              >
+                <RowContent columns={data.columns} row={row} />
+              </Table.ExpandableRow>
+            );
+          } else {
+            return (<Table.Row
+            key={index}>
+                <RowContent columns={data.columns} row={row} />
+            </Table.Row>)
+          }
+        })}
       </Table.Body>
     </Table>
+  );
+}
+
+interface RowContentProps {
+  columns: DataDrivenTableDtoColumn[];
+  row: DataDrivenTableDtoRow;
+}
+
+function RowContent({ columns, row }: RowContentProps) {
+  return (
+    <>
+      {columns.map((col) => {
+        const cells: Record<string, DataElement | null> = row.cells;
+        const cell = cells[col.key];
+        return (
+          <Table.DataCell key={col.key} align={col.align}>
+            {cell ? getDataElement(cell) : null}
+          </Table.DataCell>
+        );
+      })}
+    </>
   );
 }
