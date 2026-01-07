5547a7307e145408dafb454e1d546562ae5a6be5
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-01-07 14:39:38 +0100

    Midlertidig aktiver avtale så innlandet får registrert gjennomføring

---
+++ mulighetsrommet-api/src/main/resources/db/migration/V399__aktiver_avtale.sql
@@ -0,0 +1,3 @@
+-- Åpner avtale Mat og service | T4 | Gudbrandsdalen, som ble avsluttet 3. jan 2026 fordi
+-- innlandet ikke rakk og lage gjennomføring som skulle ha startet før denne slutten.
+update avtale set status = 'AKTIV' where id = 'c72840fd-824d-4672-9569-65eed21ee315'
