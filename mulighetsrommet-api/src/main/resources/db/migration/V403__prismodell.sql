9740e222032316dd4bc343b67003311452bb4cbe
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-01-15 14:43:05 +0100

    legg til timestamps i prismodell-tabeller

--- mulighetsrommet-api/src/main/resources/db/migration/V403__prismodell.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V403__prismodell.sql
@@ -9,12 +9,31 @@ alter table avtale_prismodell
 alter table avtale_prismodell
     rename to prismodell;
 
+alter table prismodell
+    add created_at timestamp default now(),
+    add updated_at timestamp default now();
+
+create trigger set_timestamp
+    before update
+    on prismodell
+    for each row
+execute procedure trigger_set_timestamp();
+
+
 create table avtale_prismodell
 (
     avtale_id     uuid not null references avtale (id),
     prismodell_id uuid not null references prismodell (id),
+    created_at    timestamp default now(),
+    updated_at    timestamp default now(),
     primary key (avtale_id, prismodell_id)
 );
+
+create trigger set_timestamp
+    before update
+    on avtale_prismodell
+    for each row
+execute procedure trigger_set_timestamp();
 
 insert into avtale_prismodell(avtale_id, prismodell_id)
 select avtale_id, id as prismodell_id
