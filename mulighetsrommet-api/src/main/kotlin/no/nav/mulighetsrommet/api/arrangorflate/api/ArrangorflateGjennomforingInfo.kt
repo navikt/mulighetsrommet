7620b9eb24b63aaabe3769ffcf3296baf9454fd1
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-01-26 10:10:42 +0100

    fjern ubrukt modell

---
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/arrangorflate/api/ArrangorflateGjennomforingInfo.kt
@@ -0,0 +1,14 @@
+package no.nav.mulighetsrommet.api.arrangorflate.api
+
+import kotlinx.serialization.Serializable
+import no.nav.mulighetsrommet.model.Tiltaksnummer
+import no.nav.mulighetsrommet.serializers.UUIDSerializer
+import java.util.UUID
+
+@Serializable
+data class ArrangorflateGjennomforingInfo(
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
+    val navn: String,
+    val lopenummer: Tiltaksnummer,
+)
