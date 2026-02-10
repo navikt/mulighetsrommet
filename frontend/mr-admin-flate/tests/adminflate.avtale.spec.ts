605eab3ce5547998d9a90d8af490d1fe03dec17e
Benedicte Myrvoll <benemyr17@gmail.com>
2026-02-10 14:32:25 +0100

    Fiks selectors

--- frontend/mr-admin-flate/tests/adminflate.avtale.spec.ts
+++ frontend/mr-admin-flate/tests/adminflate.avtale.spec.ts
@@ -19,7 +19,7 @@ async function fyllInnForhandsgodkjentAvtaleDetaljer(page: Page) {
   await page.locator('[name="detaljer.navn"]').fill("Testavtale fra Playwright");
   await page.getByLabel("tiltakstype").selectOption({ value: "ARBEIDSFORBEREDENDE_TRENING" });
   await page.keyboard.press("Enter");
-  await page.fill('.navds-form-field:has(label:text("Startdato")) input', "01.02.2025");
+  await page.getByLabel("Startdato").fill("01.02.2025");
   await page.getByRole("button", { name: "Neste" }).click();
 }
 
@@ -35,14 +35,15 @@ async function fyllInnPersonopplysninger(page: Page) {
 }
 
 async function fyllInnNavRegioner(page: Page) {
-  await page.locator('button:has-text("Opprett avtale")').click();
-  const errorMessages = await page.locator(".navds-error-summary__list li").allTextContents();
-  expect(errorMessages).toContain("Du må velge minst én region");
+  await page.locator("text=Opprett avtale").click();
+  await expect(page.locator(".aksel-error-summary__item")).toContainText(
+    "Du må velge minst én region",
+  );
 
   await page.click("input#navRegioner");
   await page.keyboard.press("Enter");
 
   await page.click("input#navKontorer");
   await page.keyboard.press("Enter");
-  await page.locator('button:has-text("Opprett avtale")').click();
+  await page.locator("text=Opprett avtale").click();
 }
