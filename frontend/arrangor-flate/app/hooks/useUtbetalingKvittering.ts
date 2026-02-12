acbc55b3803c5cf868be001595305a166084fb68
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-02-12 14:00:22 +0100

    Arrflate: Bruk eget endepunkt for kvittering

---
+++ frontend/arrangor-flate/app/hooks/useUtbetalingKvittering.ts
@@ -0,0 +1,21 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ArrangorflateService } from "api-client";
+import { queryClient } from "~/api/client";
+import { queryKeys } from "~/api/queryKeys";
+
+export function useUtbetalingsKvittering(id: string) {
+  return useSuspenseQuery({
+    queryKey: queryKeys.utbetalingKvittering(id),
+    queryFn: async () => {
+      const result = await ArrangorflateService.getUtbetalingsKvittering({
+        path: { id },
+        client: queryClient,
+      });
+      if (result.error) {
+        throw result.error;
+      }
+      return result.data;
+    },
+    retry: false,
+  });
+}
