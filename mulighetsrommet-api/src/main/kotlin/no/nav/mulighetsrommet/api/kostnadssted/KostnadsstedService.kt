d56482b6af71b95e658f3c4638a3ba7ae11382d9
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-02-23 13:23:01 +0100

    fjern ubrukt funksjon

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/kostnadssted/KostnadsstedService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/kostnadssted/KostnadsstedService.kt
@@ -1,15 +1,10 @@
 package no.nav.mulighetsrommet.api.kostnadssted
 
 import no.nav.mulighetsrommet.api.ApiDatabase
-import no.nav.mulighetsrommet.model.NavEnhetNummer
 
 class KostnadsstedService(
     private val db: ApiDatabase,
 ) {
-    fun hentKostnadssted(regioner: List<NavEnhetNummer>): List<Kostnadssted> = db.session {
-        queries.kostnadssted.getAll(regioner)
-    }
-
     fun hentKostnadssteder(): List<RegionKostnadssteder> = db.session {
         val kostnadssteder = queries.kostnadssted.getAll()
         return RegionKostnadssteder.fromKostnadssteder(kostnadssteder)
