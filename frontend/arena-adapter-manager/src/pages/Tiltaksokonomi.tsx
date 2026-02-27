f6a129704f43fc80d3d35f088e30ca3baf1d7cd7
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-02-27 14:57:55 +0100

    Vis failed records tiltaksokonomi

--- frontend/arena-adapter-manager/src/pages/Tiltaksokonomi.tsx
+++ frontend/arena-adapter-manager/src/pages/Tiltaksokonomi.tsx
@@ -1,10 +1,12 @@
 import TopicOverview from "../sections/TopicOverview";
 import { ApiBase } from "../core/api.tsx";
+import FailedKafkaConsumerRecordsOverview from "../sections/FailedKafkaConsumerRecordsOverview.tsx";
 
 export function Tiltaksokonomi() {
   return (
     <>
       <TopicOverview base={ApiBase.TILTAKSOKONOMI} />
+      <FailedKafkaConsumerRecordsOverview base={ApiBase.TILTAKSOKONOMI} />
     </>
   );
 }
--- mulighetsrommet-tiltaksokonomi/src/main/kotlin/no/nav/tiltak/okonomi/api/Api.kt
+++ mulighetsrommet-tiltaksokonomi/src/main/kotlin/no/nav/tiltak/okonomi/api/Api.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.resources.Resources
 import io.ktor.server.routing.routing
+import no.nav.mulighetsrommet.database.Database
 import no.nav.mulighetsrommet.kafka.KafkaConsumerOrchestrator
 import no.nav.tiltak.okonomi.service.OkonomiService
 
@@ -12,10 +13,11 @@ const val API_BASE_PATH = "/api/v1/okonomi"
 fun Application.configureApi(
     kafka: KafkaConsumerOrchestrator,
     okonomi: OkonomiService,
+    db: Database,
 ) = routing {
     install(Resources)
 
-    maamRoutes(kafka)
+    maamRoutes(kafka, db)
 
     oebsRoutes(okonomi)
 }
