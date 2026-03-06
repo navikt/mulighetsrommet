f0f00d73d3c47a2de003e0e057f6c1b3050efed9
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-03-06 19:43:16 +0100

    trekk beregning ut i egen metode

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/arrangorflate/service/ArrangorflateUtbetalingService.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/arrangorflate/service/ArrangorflateUtbetalingService.kt
@@ -1,8 +1,10 @@
 package no.nav.mulighetsrommet.api.arrangorflate.service
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.core.left
 import arrow.core.nel
+import arrow.core.right
 import no.nav.mulighetsrommet.api.ApiDatabase
 import no.nav.mulighetsrommet.api.QueryContext
 import no.nav.mulighetsrommet.api.arrangorflate.api.OpprettKravUtbetalingRequest
@@ -13,10 +15,10 @@ import no.nav.mulighetsrommet.api.gjennomforing.model.GjennomforingAvtale
 import no.nav.mulighetsrommet.api.responses.FieldError
 import no.nav.mulighetsrommet.api.utbetaling.model.OpprettUtbetaling
 import no.nav.mulighetsrommet.api.utbetaling.model.Utbetaling
+import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregning
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningFri
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingBeregningPrisPerTimeOppfolging
 import no.nav.mulighetsrommet.api.utbetaling.model.UtbetalingInputHelper
-import no.nav.mulighetsrommet.api.utbetaling.service.TidligstTidspunktForUtbetalingCalculator
 import no.nav.mulighetsrommet.api.utbetaling.service.UtbetalingService
 import no.nav.mulighetsrommet.model.Arrangor
 import no.nav.mulighetsrommet.model.Periode
@@ -27,38 +29,13 @@ class ArrangorflateUtbetalingService(
     private val db: ApiDatabase,
     private val utbetalingService: UtbetalingService,
 ) {
-    data class Config(
-        val tidligstTidspunktForUtbetaling: TidligstTidspunktForUtbetalingCalculator,
-    )
-
     suspend fun opprettUtbetaling(
         opprett: ArrangorflateOpprettUtbetaling,
-    ): Either<List<FieldError>, Utbetaling> = db.session {
-        val gjennomforing = queries.gjennomforing.getGjennomforingAvtaleOrError(opprett.gjennomforingId)
-
-        val (tilskuddstype, beregning) = when (gjennomforing.prismodell) {
-            is Prismodell.ForhandsgodkjentPrisPerManedsverk,
-            -> Tilskuddstype.TILTAK_INVESTERINGER to UtbetalingBeregningFri.belop(opprett.pris)
-
-            is Prismodell.AnnenAvtaltPris,
-            -> Tilskuddstype.TILTAK_DRIFTSTILSKUDD to UtbetalingBeregningFri.belop(opprett.pris)
-
-            is Prismodell.AvtaltPrisPerTimeOppfolgingPerDeltaker,
-            -> Tilskuddstype.TILTAK_DRIFTSTILSKUDD to getBeregningPrisPerTimeOppfolging(opprett, gjennomforing)
-
-            is Prismodell.AvtaltPrisPerManedsverk,
-            is Prismodell.AvtaltPrisPerUkesverk,
-            is Prismodell.AvtaltPrisPerHeleUkesverk,
-            -> return FieldError.of(
-                "Kan ikke opprette utbetaling for denne tiltaksgjennomføringen",
-                OpprettKravUtbetalingRequest::tilsagnId,
-            ).nel().left()
-        }
-
-        return utbetalingService.opprettUtbetaling(
-            opprett = OpprettUtbetaling(
+    ): Either<List<FieldError>, Utbetaling> {
+        return beregnUtbetaling(opprett).flatMap { (tilskuddstype, beregning) ->
+            val utbetaling = OpprettUtbetaling(
                 id = UUID.randomUUID(),
-                gjennomforingId = gjennomforing.id,
+                gjennomforingId = opprett.gjennomforingId,
                 periode = opprett.periode,
                 beregning = beregning,
                 kid = opprett.kidNummer,
@@ -68,14 +45,41 @@ class ArrangorflateUtbetalingService(
                 kommentar = null,
                 korreksjonGjelderUtbetalingId = null,
                 korreksjonBegrunnelse = null,
-            ),
-            agent = Arrangor,
-        )
+            )
+            utbetalingService.opprettUtbetaling(utbetaling, Arrangor)
+        }
     }
 
     fun getAvtaltPrisPerTimeOppfolgingData(gjennomforingId: UUID, periode: Periode): AvtaltPrisPerTimeOppfolgingData = db.session {
         val gjennomforing = queries.gjennomforing.getGjennomforingAvtaleOrError(gjennomforingId)
         return getAvtaltPrisPerTimeOppfolgingData(gjennomforing, periode)
+    }
+
+    private fun beregnUtbetaling(
+        opprett: ArrangorflateOpprettUtbetaling,
+    ): Either<List<FieldError>, Pair<Tilskuddstype, UtbetalingBeregning>> = db.session {
+        val gjennomforing = queries.gjennomforing.getGjennomforingAvtaleOrError(opprett.gjennomforingId)
+        return when (gjennomforing.prismodell) {
+            is Prismodell.ForhandsgodkjentPrisPerManedsverk,
+            -> Pair(Tilskuddstype.TILTAK_INVESTERINGER, UtbetalingBeregningFri.belop(opprett.pris)).right()
+
+            is Prismodell.AnnenAvtaltPris,
+            -> Pair(Tilskuddstype.TILTAK_DRIFTSTILSKUDD, UtbetalingBeregningFri.belop(opprett.pris)).right()
+
+            is Prismodell.AvtaltPrisPerTimeOppfolgingPerDeltaker,
+            -> Pair(
+                Tilskuddstype.TILTAK_DRIFTSTILSKUDD,
+                getBeregningPrisPerTimeOppfolging(opprett, gjennomforing),
+            ).right()
+
+            is Prismodell.AvtaltPrisPerManedsverk,
+            is Prismodell.AvtaltPrisPerUkesverk,
+            is Prismodell.AvtaltPrisPerHeleUkesverk,
+            -> FieldError.of(
+                "Kan ikke opprette utbetaling for denne tiltaksgjennomføringen",
+                OpprettKravUtbetalingRequest::tilsagnId,
+            ).nel().left()
+        }
     }
 
     private fun QueryContext.getBeregningPrisPerTimeOppfolging(
