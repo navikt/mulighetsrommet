7b2ac85877c58056cb7f40e55b0288c38fc3ab0d
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-03-10 09:40:52 +0100

    Bare skjul kurstype for studiespes tiltakstype

--- frontend/mr-admin-flate/src/pages/avtaler/AvtaleDetaljer.tsx
+++ frontend/mr-admin-flate/src/pages/avtaler/AvtaleDetaljer.tsx
@@ -6,7 +6,7 @@ import { UtdanningslopDetaljer } from "@/components/utdanning/UtdanningslopDetal
 import { TwoColumnGrid } from "@/layouts/TwoColumGrid";
 import { ArrangorKontaktpersonDetaljer } from "@/pages/arrangor/ArrangorKontaktpersonDetaljer";
 import { avtaletypeTilTekst } from "@/utils/Utils";
-import { AmoKurstype, Avtaletype } from "@tiltaksadministrasjon/api-client";
+import { AmoKurstype, Avtaletype, Tiltakskode } from "@tiltaksadministrasjon/api-client";
 import { Lenke } from "@mr/frontend-common/components/lenke/Lenke";
 import {
   Definisjonsliste,
@@ -43,7 +43,8 @@ export function AvtaleDetaljer() {
   } = avtale;
 
   const visAmoKategorisering =
-    amoKategorisering && amoKategorisering.kurstype !== AmoKurstype.STUDIESPESIALISERING;
+    amoKategorisering &&
+    !kursOgTiltakErStudiespesialisering(amoKategorisering.kurstype, tiltakstype.tiltakskode);
 
   const avtaleMeta: Definition[] = [
     { key: avtaletekster.avtalenavnLabel, value: navn },
@@ -164,5 +165,11 @@ export function AvtaleDetaljer() {
         )}
       </DetaljerLayout>
     </TwoColumnGrid>
+  );
+}
+
+function kursOgTiltakErStudiespesialisering(amo: AmoKurstype | null, tiltakskode: Tiltakskode) {
+  return (
+    amo === AmoKurstype.STUDIESPESIALISERING && tiltakskode === Tiltakskode.STUDIESPESIALISERING
   );
 }
