cd0576174e900f8f23ad1b6d4a15ba979fe67cd8
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-03-13 14:17:47 +0100

    Legg til tilskudd tabell
    Var nevnt en tanke om å gå vekk fra postgresql type objekter

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/helved/HelVedUtbetaling.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/clients/helved/HelVedUtbetaling.kt
@@ -34,7 +34,8 @@ data class HelVedUtbetaling(
     /** Utbetalingsperiode */
     val periode: Periode,
     /** Utbetalingslinjer som inngår i utbetalingen */
-    val linjer: List<UtbetalingLinje>,
+    val belop: Int,
+    val tilskuddstype: Tilskuddstype,
     /** Saksbehandleren som har opprettet utbetalingen */
     val saksbehandler: NavIdent,
     /** Beslutteren som har attestert utbetalingen */
@@ -51,7 +52,7 @@ data class HelVedUtbetaling(
         // Kan kanskje flytte dette inn i en validator
         require(1 <= sakId.length && sakId.length <= 25) { "På grunn av begrensninger i OS/UR kan ikke sakId være lengre enn 25 tegn" }
         require(1 <= behandlingId.length && behandlingId.length <= 30) { "På grunn av begrensninger i OS/UR kan ikke denne være lengre enn 30 tegn" }
-        require(linjer.all { it.belop > 0 }) { "Beløp kan ikke være negativt eller 0" }
+        require(belop > 0 ) { "Beløp kan ikke være negativt eller 0" }
         require(periode.fom.year == periode.tom.year) { "Utbetalingsperioden må være innen samme år" }
         require(periode.fom <= periode.tom) { "Fom-dato på en periode må være før eller lik tom-dato" }
     }
@@ -61,7 +62,7 @@ data class HelVedUtbetaling(
         @Serializable(with = LocalDateSerializer::class)
         val fom: LocalDate,
         @Serializable(with = LocalDateSerializer::class)
-        val tom: LocalDate
+        val tom: LocalDate,
     )
 
     /** Se Tiltaksforskriften § 7-2 */
@@ -85,12 +86,6 @@ data class HelVedUtbetaling(
         /** § 7-2 f */
         HOYERE_UTDANNING,
     }
-
-    @Serializable
-    data class UtbetalingLinje(
-        val belop: Int,
-        val tilskuddstype: Tilskuddstype,
-    )
 
     /** Se Tiltaksforskriften § 7-5 */
     @Serializable
---
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/tilskudd/TilskuddQueries.kt
@@ -0,0 +1,34 @@
+package no.nav.mulighetsrommet.api.tilskudd
+
+import kotliquery.Row
+import kotliquery.Session
+import kotliquery.queryOf
+import no.nav.mulighetsrommet.database.requireSingle
+import org.intellij.lang.annotations.Language
+import java.util.UUID
+
+class TilskuddQueries(private val session: Session) {
+    fun getOrError(id: UUID): Tilskudd {
+        @Language("PostgreSQL")
+        val query = """
+            select * from tilskudd
+            where id = :id
+            """.trimIndent()
+
+        return session.requireSingle(queryOf(query, id)) { it.toTilskudd()};
+    }
+
+    fun getAll(): List<Tilskudd> {
+        @Language("PostgreSQL")
+        val query = """
+            select * from tilskudd
+            """.trimIndent()
+
+        return session.list(queryOf(query)) { it.toTilskudd() }
+    }
+
+    fun Row.toTilskudd(): Tilskudd = Tilskudd(
+        id = uuid("id"),
+        navn = string("navn"),
+    )
+}
