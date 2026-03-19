d01ad4dc436f8e1ebcaaa380e9fb0865ddf1b56d
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-03-19 13:20:34 +0100

    legg til kolonne for updated_at

--- mulighetsrommet-api/src/main/resources/db/migration/V448__tilsagn_updated_at.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V448__tilsagn_updated_at.sql
@@ -2,3 +2,12 @@ drop view if exists view_tilsagn;
 
 alter table tilsagn
     rename type to tilsagn_type;
+
+alter table tilsagn
+    add updated_at timestamptz default now();
+
+create trigger set_timestamp
+    before update
+    on tilsagn
+    for each row
+execute procedure trigger_set_timestamp();
