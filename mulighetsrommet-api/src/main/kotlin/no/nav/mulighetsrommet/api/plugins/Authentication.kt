50387ab981aadfb37369399e785b3030b7e87777
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-03-31 15:44:51 +0200

    fix: getAccessToken

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Authentication.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/plugins/Authentication.kt
@@ -1,6 +1,7 @@
 package no.nav.mulighetsrommet.api.plugins
 
 import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.interfaces.Payload
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
@@ -19,7 +20,7 @@ import io.ktor.server.auth.principal
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.RoutingContext
 import no.nav.mulighetsrommet.api.AuthConfig
-import no.nav.mulighetsrommet.api.navansatt.service.NavAnsattPrincipal
+import no.nav.mulighetsrommet.api.navansatt.service.NavAnsattMedRollerPrincipal
 import no.nav.mulighetsrommet.api.navansatt.service.NavAnsattPrincipalService
 import no.nav.mulighetsrommet.ktor.exception.StatusException
 import no.nav.mulighetsrommet.ktor.extensions.getAccessToken
@@ -119,9 +120,15 @@ fun Application.configureAuthentication(
             }
 
             validate { credentials ->
-                credentials["NAVident"] ?: return@validate null
+                val navIdent = credentials["NAVident"]?.let { NavIdent(it) } ?: run {
+                    application.log.warn("'NAVident' mangler i JWT credentials")
+                    return@validate null
+                }
 
-                JWTPrincipal(credentials.payload)
+                NavAnsattUtenRollerPrincipal(
+                    navIdent = navIdent,
+                    payload = credentials.payload,
+                )
             }
         }
 
@@ -176,10 +183,21 @@ fun Application.configureAuthentication(
 
 data class ArrangorflatePrincipal(val norskIdent: NorskIdent, val principal: JWTPrincipal)
 
+class NavAnsattUtenRollerPrincipal(
+    val navIdent: NavIdent,
+    payload: Payload,
+) : JWTPayloadHolder(payload)
+
 fun ApplicationCall.getAccessType(): AccessType = when (val principal = principal<Any>()) {
     is ArrangorflatePrincipal -> AccessType.OBO.TokenX(getAccessToken())
-    is NavAnsattPrincipal -> AccessType.OBO.AzureAd(getAccessToken())
+
+    is NavAnsattMedRollerPrincipal,
+    is NavAnsattUtenRollerPrincipal,
+    -> AccessType.OBO.AzureAd(getAccessToken())
+
     is JWTPrincipal -> AccessType.M2M
+
     null -> throw IllegalStateException("No principal found, is the route authenticated?")
+
     else -> throw IllegalStateException("Unknown principal type: ${principal::class}")
 }
