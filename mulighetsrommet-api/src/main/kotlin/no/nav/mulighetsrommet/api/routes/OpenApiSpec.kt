bb7b4c677d433b143b1a67ae470f0148ffb85f8c
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-04-14 12:12:51 +0200

    bump api version

--- mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/OpenApiSpec.kt
+++ mulighetsrommet-api/src/main/kotlin/no/nav/mulighetsrommet/api/routes/OpenApiSpec.kt
@@ -6,7 +6,7 @@ enum class OpenApiSpec(
     val version: Int,
 ) {
     PUBLIC("/api/v\\d(?!/intern)".toRegex(), "public", 1),
-    TILTAKSADMINISTRASJON("/api/tiltaksadministrasjon".toRegex(), "tiltaksadministrasjon", 8),
+    TILTAKSADMINISTRASJON("/api/tiltaksadministrasjon".toRegex(), "tiltaksadministrasjon", 9),
     VEILEDERFLATE("/api/veilederflate".toRegex(), "veilederflate", 1),
     ARRANGORFLATE("/api/arrangorflate".toRegex(), "arrangorflate", 1),
     ;
