ef8f6f738469bb7d72534c985ea901d200fc7fbf
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-04-14 11:02:01 +0200

    gjenbruk zod-schema for faneinnhold

--- frontend/mr-admin-flate/src/schemas/tiltakstypeRedaksjoneltInnhold.ts
+++ frontend/mr-admin-flate/src/schemas/tiltakstypeRedaksjoneltInnhold.ts
@@ -1,30 +1,9 @@
 import z from "zod";
+import { FaneinnholdSchema } from "@/components/redaksjoneltInnhold/FaneinnholdSchema";
 
 export const TiltakstypeRedaksjoneltInnholdSchema = z.object({
   beskrivelse: z.string().nullish(),
-  faneinnhold: z
-    .object({
-      forHvemInfoboks: z.string().nullish(),
-      forHvem: z.any().nullish(),
-      detaljerOgInnholdInfoboks: z.string().nullish(),
-      detaljerOgInnhold: z.any().nullish(),
-      pameldingOgVarighetInfoboks: z.string().nullish(),
-      pameldingOgVarighet: z.any().nullish(),
-      kontaktinfo: z.any().nullish(),
-      kontaktinfoInfoboks: z.string().nullish(),
-      lenker: z
-        .object({
-          lenkenavn: z.string().min(3, { error: "Lenkenavnet må være minst 3 tegn" }),
-          lenke: z.url({ error: "Du må oppgi en gyldig url" }),
-          visKunForVeileder: z.boolean().default(false),
-          apneINyFane: z.boolean().default(false),
-        })
-        .array()
-        .nullable()
-        .optional(),
-      delMedBruker: z.string().nullish(),
-    })
-    .nullish(),
+  faneinnhold: FaneinnholdSchema.nullish(),
   faglenker: z.array(z.object({ id: z.uuid() })),
   kanKombineresMed: z.array(z.uuid()),
 });
