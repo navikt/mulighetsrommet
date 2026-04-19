3fbb9c757396256cea0545bf71e3b2e3523fc73b
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-04-19 10:54:41 +0200

    emptystate for tilskuddsbehandlinger

--- frontend/mr-admin-flate/src/pages/tilskudd-behandling/TilskuddBehandlingerPage.tsx
+++ frontend/mr-admin-flate/src/pages/tilskudd-behandling/TilskuddBehandlingerPage.tsx
@@ -2,7 +2,7 @@ import { useTilskuddBehandlinger } from "@/api/tilskudd-behandling/useTilskuddBe
 import { Handlinger } from "@/components/handlinger/Handlinger";
 import { useRequiredParams } from "@/hooks/useRequiredParams";
 import { KnapperadContainer } from "@/layouts/KnapperadContainer";
-import { ActionMenu, Table } from "@navikt/ds-react";
+import { ActionMenu, Alert, Table } from "@navikt/ds-react";
 import { Link } from "react-router";
 
 export function TilskuddBehandlingerPage() {
@@ -18,26 +18,33 @@ export function TilskuddBehandlingerPage() {
           </ActionMenu.Item>
         </Handlinger>
       </KnapperadContainer>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Søknadsdato</Table.HeaderCell>
-            <Table.HeaderCell>Periode</Table.HeaderCell>
-            <Table.HeaderCell>Kostnadssted</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {behandlinger.map((b) => (
-            <Table.Row key={b.id}>
-              <Table.DataCell>{b.soknadDato}</Table.DataCell>
-              <Table.DataCell>
-                {b.periode.start} – {b.periode.slutt}
-              </Table.DataCell>
-              <Table.DataCell>{b.kostnadssted}</Table.DataCell>
+      {behandlinger.length === 0 && (
+        <Alert variant="info" className="mt-4">
+          Det finnes ingen tilskuddsbehandlinger for dette tiltaket
+        </Alert>
+      )}
+      {behandlinger.length > 0 && (
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Søknadsdato</Table.HeaderCell>
+              <Table.HeaderCell>Periode</Table.HeaderCell>
+              <Table.HeaderCell>Kostnadssted</Table.HeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+          </Table.Header>
+          <Table.Body>
+            {behandlinger.map((b) => (
+              <Table.Row key={b.id}>
+                <Table.DataCell>{b.soknadDato}</Table.DataCell>
+                <Table.DataCell>
+                  {b.periode.start} – {b.periode.slutt}
+                </Table.DataCell>
+                <Table.DataCell>{b.kostnadssted}</Table.DataCell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      )}
     </>
   );
 }
