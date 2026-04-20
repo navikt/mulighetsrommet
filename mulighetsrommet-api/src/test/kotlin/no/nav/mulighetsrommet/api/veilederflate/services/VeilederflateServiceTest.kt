2243d9ee69b231fcf7c43c664e626dd02dc5c250
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-04-20 11:05:54 +0200

    fjern funksjonalitet for å hente tiltakstyper fra sanity

--- mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/veilederflate/services/VeilederflateServiceTest.kt
+++ mulighetsrommet-api/src/test/kotlin/no/nav/mulighetsrommet/api/veilederflate/services/VeilederflateServiceTest.kt
@@ -133,11 +133,6 @@ class VeilederflateServiceTest : FunSpec({
     }
 
     val sanityService: SanityService = mockk(relaxed = true)
-    coEvery { sanityService.getTiltakstyper() } returns listOf(
-        tiltakstypeOppfolging,
-        tiltakstypeEnkelAmo,
-        tiltakstypeArbeidstrening,
-    )
     coEvery { sanityService.getAllTiltak(any(), any()) } returns listOf(
         tiltakEnkelAmo,
         tiltakArbeidstrening1,
@@ -314,9 +309,7 @@ class VeilederflateServiceTest : FunSpec({
     }
 
     context("redaksjonelt innhold for tiltakstype hentes fra riktig kilde") {
-        val sanityBeskrivelse = "Beskrivelse fra Sanity"
         val dbBeskrivelse = "Beskrivelse fra databasen"
-        val sanityFaneinnhold = Faneinnhold(forHvemInfoboks = "Sanity faneinnhold")
         val dbFaneinnhold = Faneinnhold(forHvemInfoboks = "DB faneinnhold")
 
         beforeEach {
@@ -327,14 +320,6 @@ class VeilederflateServiceTest : FunSpec({
                     dbFaneinnhold,
                 )
             }
-            coEvery { sanityService.getTiltakstyper() } returns listOf(
-                tiltakstypeOppfolging.copy(
-                    beskrivelse = sanityBeskrivelse,
-                    faneinnhold = sanityFaneinnhold,
-                ),
-                tiltakstypeEnkelAmo,
-                tiltakstypeArbeidstrening,
-            )
         }
 
         afterEach {
