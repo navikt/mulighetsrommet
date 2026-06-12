5b66f80a8f6d100c5d4ddd3e0a2820bf36421891
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-06-12 08:47:17 +0200

    Bruk id for avtale som PK for opplaring kategorisering rundt utdanning

--- mulighetsrommet-api/src/main/resources/db/migration/V496__fyll_kategorising_utdanningprogram.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V496__fyll_kategorising_utdanningprogram.sql
@@ -11,25 +11,24 @@ with unike_gjennomforing_utdanningsprogram as (select distinct gu.gjennomforing_
                                from src
                                         inner join gjennomforing_utdanningsprogram gu
                                                    on src.gjennomforing_id = gu.gjennomforing_id),
-     ins_kategorisering_utdanning as (
-         insert into opplaring_kategorisering (id, utdanningsprogram_id)
-             select src.new_id,
-                    src.utdanningsprogram_id
-             from src
-             on conflict do nothing)
-insert into opplaring_kategorisering_utdanning (opplaring_kategorisering_id, utdanning_id)
+ins_utdanning as (insert into opplaring_kategorisering_utdanning (opplaring_kategorisering_id, utdanning_id)
     select num.new_id,
            num.utdanning_id
     from new_utdanning_mapping num
-on conflict  do nothing;
-
+on conflict  do nothing)
+insert into opplaring_kategorisering (id, utdanningsprogram_id)
+             select src.new_id,
+                    src.utdanningsprogram_id
+             from src
+             on conflict (id) do update set
+                utdanningsprogram_id = excluded.utdanningsprogram_id;
 -- avtale utdanning til kategoriseringstabell
 with unike_avtale_utdanningsprogram as (select distinct gu.avtale_id,
                                                                gu.utdanningsprogram_id
                                                from avtale_utdanningsprogram gu),
      src as (select ugu.avtale_id,
                     ugu.utdanningsprogram_id,
-                    gen_random_uuid() as new_id
+                    ugu.avtale_id as new_id
              from unike_avtale_utdanningsprogram ugu),
      new_utdanning_mapping as (select src.new_id,
                                       au.utdanning_id
@@ -37,13 +36,15 @@ with unike_avtale_utdanningsprogram as (select distinct gu.avtale_id,
                                         inner join avtale_utdanningsprogram au
                                                    on src.avtale_id = au.avtale_id),
      ins_kategorisering_utdanning as (
-         insert into opplaring_kategorisering (id, utdanningsprogram_id)
-             select src.new_id,
-                    src.utdanningsprogram_id
-             from src
-             on conflict do nothing)
- insert into opplaring_kategorisering_utdanning (opplaring_kategorisering_id, utdanning_id)
- select num.new_id,
-        num.utdanning_id
- from new_utdanning_mapping num
- on conflict  do nothing;
+         insert into opplaring_kategorisering_utdanning (opplaring_kategorisering_id, utdanning_id)
+             select num.new_id,
+                    num.utdanning_id
+             from new_utdanning_mapping num
+             on conflict  do nothing
+         )
+insert into opplaring_kategorisering (id, utdanningsprogram_id)
+select src.new_id,
+       src.utdanningsprogram_id
+from src
+on conflict (id) do update set
+    utdanningsprogram_id = excluded.utdanningsprogram_id;
