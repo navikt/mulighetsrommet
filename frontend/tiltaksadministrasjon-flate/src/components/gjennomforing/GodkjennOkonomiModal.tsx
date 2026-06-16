9daa7f50641a2f7fb8a90293af0d7ad6f34dbddb
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-06-16 10:42:20 +0200

    Nytt design sett på vent/godkjenn okonomi

--- frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/GodkjennOkonomiModal.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/GodkjennOkonomiModal.tsx
@@ -1,13 +1,17 @@
 import { useGodkjennGjennomforingOkonomi } from "@/api/gjennomforing/useGodkjennGjennomforingOkonomi";
-import { BodyShort, Button, Modal } from "@navikt/ds-react";
+import { Button, InfoCard, Modal, VStack } from "@navikt/ds-react";
+import { Betalingsbetingelser } from "../tilskudd-behandling/Betalingsbetingelser";
+import { PrismodellDto } from "@tiltaksadministrasjon/api-client";
+import { InformationSquareIcon } from "@navikt/aksel-icons";
 
 interface Props {
   open: boolean;
   setOpen: (open: boolean) => void;
   gjennomforingId: string;
+  prismodell: PrismodellDto;
 }
 
-export function GodkjennOkonomiModal({ open, setOpen, gjennomforingId }: Props) {
+export function GodkjennOkonomiModal({ open, setOpen, gjennomforingId, prismodell }: Props) {
   const godkjennMutation = useGodkjennGjennomforingOkonomi();
 
   function close() {
@@ -21,15 +25,26 @@ export function GodkjennOkonomiModal({ open, setOpen, gjennomforingId }: Props) 
   return (
     <Modal open={open} onClose={close} header={{ heading: "Godkjenn enkeltplass" }} width="medium">
       <Modal.Body>
-        <BodyShort>
-          Bekreft at du har gjennomgått og godkjenner økonomien for gjennomføringen.
-        </BodyShort>
+        <VStack gap="space-8">
+          <InfoCard data-color="info">
+            <InfoCard.Header icon={<InformationSquareIcon aria-hidden />}>
+              <InfoCard.Title>
+                Du er i ferd med å godkjenne pris og betalingsbetingelser som er beskrevet under
+              </InfoCard.Title>
+            </InfoCard.Header>
+            <InfoCard.Content>
+              Når du godkjenner økonomien, fattes det automatisk vedtak om tiltaksplass. Vedtaket
+              inneholder informasjon om innhold, pris og betalingsbetingelser.
+            </InfoCard.Content>
+          </InfoCard>
+          <Betalingsbetingelser prisbetingelser={prismodell.prisbetingelser} />
+        </VStack>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={godkjenn} loading={godkjennMutation.isPending}>
-          Godkjenn enkeltplass
+        <Button size="small" onClick={godkjenn} loading={godkjennMutation.isPending}>
+          Ja, godkjenn enkeltplass
         </Button>
-        <Button variant="secondary" onClick={close}>
+        <Button size="small" variant="secondary" onClick={close}>
           Avbryt
         </Button>
       </Modal.Footer>
