bf931d2165ca00c53a5545d7431333e78c248241
Benedicte Helen Gangstø <benedicte.gangstoe@proton.me>
2026-06-16 14:35:49 +0200

    Journalfør vedtaksbrev etter attestering av tilskuddsbehandling

--- mulighetsrommet-api/src/main/resources/db/migration/V487__tilskudd_vedtak_journalpost.sql
+++
@@ -1,6 +0,0 @@
-drop view if exists view_tilskudd_behandling;
-
-alter table tilskudd_behandling add column vedtak_journalpost_id text;
-alter table tilskudd_behandling add column vedtak_journalpost_distribuering_id text;
-alter table tilskudd_behandling add column vedtak_journalfort_tidspunkt  timestamptz;
-alter table tilskudd_behandling add column vedtak_distribuert_tidspunkt  timestamptz;
