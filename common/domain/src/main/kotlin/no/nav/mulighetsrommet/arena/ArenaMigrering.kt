fe24c0b30c5b601674ac7759c1cfed40bc6d70aa
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-06-24 11:52:58 +0200

    ikke les inn enkeltplasser fra Arena som type ENKELTPLASS
    Vi finner en ny løsning på hvordan vi skal lese inn disse tiltakene
    etter hvert som vi nærmer oss migrering.

--- common/domain/src/main/kotlin/no/nav/mulighetsrommet/arena/ArenaMigrering.kt
+++ common/domain/src/main/kotlin/no/nav/mulighetsrommet/arena/ArenaMigrering.kt
@@ -6,7 +6,5 @@ import java.time.format.DateTimeFormatter
 object ArenaMigrering {
     val TiltaksgjennomforingSluttDatoCutoffDate: LocalDate = LocalDate.of(2023, 1, 1)
 
-    val EnkeltplassSluttDatoCutoffDate: LocalDate = LocalDate.of(2026, 1, 1)
-
     val ArenaTimestampFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 }
