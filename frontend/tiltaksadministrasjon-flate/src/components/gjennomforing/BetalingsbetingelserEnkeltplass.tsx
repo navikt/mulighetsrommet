949a58045c1dea3ab91d92ded68d9eaead8e834b
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-07-01 14:31:57 +0200

    Enkeltplass: Oppdater tekster i detalje-visning

--- frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/BetalingsbetingelserEnkeltplass.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/BetalingsbetingelserEnkeltplass.tsx
@@ -28,10 +28,10 @@ export function BetalingsbetingelserEnkeltplass({ prismodell }: Props) {
 function BetalingsbetingelserAnskaffelse({ prismodell }: Props) {
   return (
     <VStack gap="space-8">
-      <Heading size="small">Betalingsbetingelser</Heading>
+      <Heading size="small">Pris- og betalingsbetingelser</Heading>
       <Heading size="xsmall">Anskaffelse</Heading>
       <BodyShort textColor="subtle">Nav har avtalt å betale leverandøren direkte</BodyShort>
-      <Heading size="xsmall">Estimert totalbeløp for anskaffelsen</Heading>
+      <Heading size="xsmall">Totalbeløp for anskaffelsen</Heading>
       <BodyShort textColor="subtle">
         {prismodell.totalBelop ? formaterValuta(prismodell.totalBelop, Valuta.NOK) : "-"}
       </BodyShort>
@@ -42,12 +42,13 @@ function BetalingsbetingelserAnskaffelse({ prismodell }: Props) {
 function BetalingsbetingelserTilskudd({ prismodell }: Props) {
   return (
     <VStack gap="space-8">
-      <Heading size="small">Betalingsbetingelser</Heading>
+      <Heading size="small">Pris- og betalingsbetingelser</Heading>
       <Heading size="xsmall">Tilskudd til en tilgjengelig studie- eller skoleplass</Heading>
-      <BodyShort textColor="subtle">
-        Utbetales i etterkant basert på dokumenterte utgifter
-      </BodyShort>
+      <BodyShort textColor="subtle">Utbetales basert på dokumenterte utgifter</BodyShort>
       <Heading size="xsmall">Aktuelle tilskuddstyper</Heading>
+      <BodyShort textColor="subtle" spacing={true}>
+        Ved flere semester er den estimerte totalsummen oppgitt
+      </BodyShort>
       <List size="small" as="ul">
         {prismodell.tilskudd.map((t) => (
           <List.Item key={t.type}>
@@ -85,12 +86,14 @@ function BetalingsbetingelserTilskudd({ prismodell }: Props) {
 function BetalingsbetingelserIngenKostnader({ prismodell }: Props) {
   return (
     <VStack gap="space-8">
-      <Heading size="small">Betalingsbetingelser</Heading>
+      <Heading size="small">Pris- og betalingsbetingelser</Heading>
       <Heading size="xsmall">Ingen kostnader</Heading>
       <BodyShort textColor="subtle">Ikke aktuelt med betaling eller refusjon fra Nav</BodyShort>
       {prismodell.aarsak && (
         <>
-          <Heading size="xsmall">Årsak</Heading>
+          <Heading size="xsmall">
+            Årsaken til at det ikke er aktuelt med betaling eller refusjon fra Nav
+          </Heading>
           <BodyShort textColor="subtle">
             {ingenKostnaderAarsakToString(prismodell.aarsak)}
           </BodyShort>
