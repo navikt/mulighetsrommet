dcbde45c3758817d4ac2076700f96b25568b9760
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-07-01 10:30:53 +0200

    legg til mock for endepunkt

--- frontend/tiltaksadministrasjon-flate/src/mocks/endpoints/oppgaverHandlers.ts
+++ frontend/tiltaksadministrasjon-flate/src/mocks/endpoints/oppgaverHandlers.ts
@@ -1,8 +1,14 @@
 import { http, HttpResponse, PathParams } from "msw";
-import { Oppgave } from "@tiltaksadministrasjon/api-client";
+import { Oppgave, OppgaveType, OppgaveTypeDto } from "@tiltaksadministrasjon/api-client";
 import { mockOppgaver } from "../fixtures/mock_oppgaver";
 
 export const oppgaverHandlers = [
+  http.get<PathParams, OppgaveTypeDto[]>("*/api/tiltaksadministrasjon/oppgaver/oppgavetyper", () =>
+    HttpResponse.json([
+      { navn: "Tilsagn til godkjenning", type: OppgaveType.TILSAGN_TIL_GODKJENNING },
+      { navn: "Utbetaling til behandling", type: OppgaveType.UTBETALING_TIL_BEHANDLING },
+    ]),
+  ),
   http.post<PathParams, Oppgave[]>("*/api/tiltaksadministrasjon/oppgaver", () =>
     HttpResponse.json(mockOppgaver),
   ),
