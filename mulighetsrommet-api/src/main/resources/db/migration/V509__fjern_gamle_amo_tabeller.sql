6d5e3ff50aba05f5d64acfb71096c122eb9ee2f8
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-07-02 11:39:46 +0200

    Fjern gamle amo tabeller

---
+++ mulighetsrommet-api/src/main/resources/db/migration/V509__fjern_gamle_amo_tabeller.sql
@@ -0,0 +1,6 @@
+drop table if exists avtale_amo_kategorisering,
+    avtale_amo_kategorisering_forerkort,
+    avtale_amo_kategorisering_sertifisering,
+    gjennomforing_amo_kategorisering,
+    gjennomforing_amo_kategorisering_forerkort,
+    gjennomforing_amo_kategorisering_sertifisering;
