6bd3215cedcfa5de1c701977ace192eb3c1ce7a5
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-07-02 11:56:28 +0200

    Rename opplaring kategorisering

--- mulighetsrommet-api/src/main/resources/db/migration/V509__fjern_gamle_amo_tabeller.sql
+++ mulighetsrommet-api/src/main/resources/db/migration/V509__fjern_gamle_amo_tabeller.sql
@@ -1,6 +1,12 @@
+drop view if exists view_avtale, view_gjennomforing_avtale_detaljer, view_opplaring_kategorisering;
+
 drop table if exists avtale_amo_kategorisering,
     avtale_amo_kategorisering_forerkort,
     avtale_amo_kategorisering_sertifisering,
     gjennomforing_amo_kategorisering,
     gjennomforing_amo_kategorisering_forerkort,
     gjennomforing_amo_kategorisering_sertifisering;
+
+alter table if exists amo_sertifisering rename to opplaring_sertifisering;
+alter table if exists opplaring_kategorisering rename constraint amo_sertifisering_pkey to opplaring_sertifisering_pkey;
+alter index if exists amo_sertifisering_pkey rename to opplaring_sertifisering_pkey;
