3d692d27a50bc57e6dfe3426409f93af9fcff557
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-07-16 12:24:15 +0200

    endre navn til arbeidsmarkedstiltak-flate

--- frontend/arbeidsmarkedstiltak-flate/src/constants.ts
+++ frontend/arbeidsmarkedstiltak-flate/src/constants.ts
@@ -1,6 +1,6 @@
 import { isProduction } from "@/environment";
 
-export const APPLICATION_NAME = "mulighetsrommet-veileder-flate";
+export const APPLICATION_NAME = "arbeidsmarkedstiltak-flate";
 export const APPLICATION_WEB_COMPONENT_NAME = "mulighetsrommet-arbeidsmarkedstiltak";
 export const PORTEN_URL = "https://jira.adeo.no/plugins/servlet/desk/portal/541/create/1401";
 
