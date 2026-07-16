e564acf8849eab2a1380040fb76ce623008d00af
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-07-16 10:39:56 +0200

    Bruk varsel modal for sletting av korreksjon

--- frontend/tiltaksadministrasjon-flate/src/components/utbetaling/SlettKorreksjonModal.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/utbetaling/SlettKorreksjonModal.tsx
@@ -1,5 +1,6 @@
 import { useSlettKorreksjon } from "@/api/utbetaling/mutations";
-import { BodyShort, Button, Heading, HStack, Modal } from "@navikt/ds-react";
+import { VarselModal } from "@mr/frontend-common/components/varsel/VarselModal";
+import { BodyShort, Button } from "@navikt/ds-react";
 import { useNavigate } from "react-router";
 
 interface SlettKorreksjonModalProps {
@@ -20,35 +21,29 @@ export function SlettKorreksjonModal({ utbetalingId, open, onClose }: SlettKorre
       },
     );
   }
-
   return (
-    <Modal onClose={onClose} closeOnBackdropClick aria-label="modal" open={open}>
-      <Modal.Header closeButton={false}>
-        <Heading align="start" size="medium">
-          Slett utbetaling
-        </Heading>
-      </Modal.Header>
-      <Modal.Body>
+    <VarselModal
+      headingIconType="warning"
+      headingText="Slett utbetaling"
+      open={open}
+      handleClose={() => onClose()}
+      body={
         <BodyShort>
           Du er i ferd med å slette en korrigeringsutbetaling. Dette vil fjerne den valgte
           ubetalingen fra løsningen. Er du sikker på at du vil fortsette?
         </BodyShort>
-      </Modal.Body>
-      <Modal.Footer>
-        <HStack gap="space-16">
-          <Button type="button" variant="secondary" onClick={onClose}>
-            Nei, takk
-          </Button>
-          <Button
-            data-color="danger"
-            title="Slett utbetaling"
-            variant="primary"
-            onClick={slettKorreksjon}
-          >
-            Ja, jeg vil slette utbetalingen
-          </Button>
-        </HStack>
-      </Modal.Footer>
-    </Modal>
+      }
+      primaryButton={
+        <Button
+          data-color="danger"
+          title="Slett utbetaling"
+          variant="primary"
+          onClick={slettKorreksjon}
+        >
+          Ja, jeg vil slette utbetalingen
+        </Button>
+      }
+      secondaryButton
+    />
   );
 }
