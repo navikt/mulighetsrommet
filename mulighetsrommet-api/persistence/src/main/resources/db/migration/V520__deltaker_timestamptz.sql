e1df3c69e0841021cb3bc69344effc7c916e131b
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-07-22 17:51:58 +0200

    drop view i forkant av kolonne-endringer

--- mulighetsrommet-api/persistence/src/main/resources/db/migration/V520__deltaker_timestamptz.sql
+++ mulighetsrommet-api/persistence/src/main/resources/db/migration/V520__deltaker_timestamptz.sql
@@ -1,3 +1,5 @@
+drop view if exists view_deltaker;
+
 alter table deltaker
     alter column registrert_tidspunkt type timestamptz using registrert_tidspunkt at time zone 'Europe/Oslo',
     alter column endret_tidspunkt type timestamptz using endret_tidspunkt at time zone 'Europe/Oslo',
