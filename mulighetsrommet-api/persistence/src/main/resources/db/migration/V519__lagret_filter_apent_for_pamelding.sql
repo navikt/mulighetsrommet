d8fccca3ddb448b2872363990020626c828d0ed6
Benedicte Helen Gangstø <benedicte.gangstoe@proton.me>
2026-07-22 09:55:21 +0200

    Sett apentForPamelding til null dersom verdien er APENT_ELLER_STENGT

--- mulighetsrommet-api/persistence/src/main/resources/db/migration/V519__lagret_filter_apent_for_pamelding.sql
+++ mulighetsrommet-api/persistence/src/main/resources/db/migration/V519__lagret_filter_apent_for_pamelding.sql
@@ -5,7 +5,7 @@ set filter = jsonb_set(
         case filter ->> 'apentForPamelding'
             when 'APENT' then '["APENT"]'
             when 'STENGT' then '["STENGT"]'
-            when 'APENT_ELLER_STENGT' then '["APENT","STENGT"]'
+            when 'APENT_ELLER_STENGT' then 'null'
             end::jsonb)
 where type = 'GJENNOMFORING_MODIA'
   and filter ->> 'apentForPamelding' in (
