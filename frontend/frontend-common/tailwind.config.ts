8a5befeb2af60ad4cef3c5446dcb60ad569a2c77
Benedicte Helen Gangstø <benedicte.gangstoe@proton.me>
2026-07-24 14:12:52 +0200

    Fiks aksel tailwind config

---
+++ frontend/frontend-common/tailwind.config.ts
@@ -0,0 +1,9 @@
+import dsTailwind from "@navikt/ds-tailwind";
+export default {
+  content: ["./**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [dsTailwind],
+};
