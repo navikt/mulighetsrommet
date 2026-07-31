1406b15887e48b36a4568e7e6ffd88c056dbd4c2
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-07-31 14:09:24 +0200

    Fjern arrangør fra skjema for TiltakDokument
    Skal ikke kunne settes selv, men vises for amo rammeavtale som finnes fra før

--- frontend/tiltaksadministrasjon-flate/src/components/tiltak-dokument/TiltakDokumentForm.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/tiltak-dokument/TiltakDokumentForm.tsx
@@ -1,6 +1,6 @@
 import { Heading, Tabs, VStack } from "@navikt/ds-react";
 import { useFormContext } from "react-hook-form";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { TiltakDokumentFormValues } from "@/pages/tiltak-dokument/TiltakDokumentFormValues";
 import { FormTextField } from "@/components/skjema/FormTextField";
 import { FormTextarea } from "@/components/skjema/FormTextarea";
@@ -8,12 +8,10 @@ import { FormCombobox } from "@/components/skjema/FormCombobox";
 import { FormComboboxMulti } from "@/components/skjema/FormComboboxMulti";
 import { FormListInput } from "@/components/skjema/FormListInput";
 import { useNavAnsatte } from "@/api/ansatt/useNavAnsatte";
-import { useArrangorer } from "@/api/arrangor/useArrangorer";
-import { useArrangorKontaktpersoner } from "@/api/arrangor/useArrangorKontaktpersoner";
 import { useKontorstruktur } from "@/api/enhet/useKontorstruktur";
 import { useTiltakstyperForGjennomforinger } from "@/api/tiltakstyper/useTiltakstyperForGjennomforinger";
 import { useSokNavAnsatt } from "@/api/ansatt/useSokNavAnsatt";
-import { ArrangorKontaktpersonAnsvar, Rolle } from "@tiltaksadministrasjon/api-client";
+import { Rolle } from "@tiltaksadministrasjon/api-client";
 import {
   getLokaleUnderenheterAsSelectOptions,
   getAndreUnderenheterAsSelectOptions,
@@ -23,15 +21,11 @@ import { avtaletekster } from "@/components/ledetekster/avtaleLedetekster";
 import { gjennomforingTekster } from "@/components/ledetekster/gjennomforingLedetekster";
 import { LabelWithHelpText } from "@mr/frontend-common/components/label/LabelWithHelpText";
 import { Separator } from "@mr/frontend-common/components/datadriven/Metadata";
-import { useDebounce } from "@mr/frontend-common";
-import { ArrangorKontaktpersonerModal } from "@/components/arrangor/ArrangorKontaktpersonerModal";
-import { KontaktpersonButton } from "@/components/kontaktperson/KontaktpersonButton";
 import { FormSelect } from "../skjema/FormSelect";
 import { TwoColumnGrid } from "@/layouts/TwoColumGrid";
 
 export function TiltakDokumentForm() {
-  const { watch, setValue } = useFormContext<TiltakDokumentFormValues>();
-  const arrangorKontaktpersonerModalRef = useRef<HTMLDialogElement>(null);
+  const { watch } = useFormContext<TiltakDokumentFormValues>();
 
   const tiltakstyper = useTiltakstyperForGjennomforinger();
   const tiltakstypeOptions = tiltakstyper.map((t) => ({ value: t.id, label: t.navn }));
@@ -41,21 +35,6 @@ export function TiltakDokumentForm() {
     value: a.navIdent,
     label: `${a.fornavn} ${a.etternavn} - ${a.navIdent}`,
   }));
-
-  const [arrangorQuery, setArrangorQuery] = useState("");
-  const debouncedArrangorQuery = useDebounce(arrangorQuery, 300);
-  const { data: arrangorerResult } = useArrangorer(undefined, { sok: debouncedArrangorQuery });
-  const arrangorOptions =
-    arrangorerResult?.data.map((a) => ({
-      value: a.id,
-      label: `${a.navn} - ${a.organisasjonsnummer}`,
-    })) ?? [];
-
-  const arrangorId = watch("arrangorId");
-  const { data: arrangorKontaktpersoner } = useArrangorKontaktpersoner(arrangorId ?? "");
-  const arrangorKontaktpersonOptions = (arrangorKontaktpersoner ?? [])
-    .filter((kp) => kp.ansvarligFor.includes(ArrangorKontaktpersonAnsvar.GJENNOMFORING))
-    .map((kp) => ({ value: kp.id, label: kp.navn }));
 
   const { data: kontorstruktur } = useKontorstruktur();
   const regionOptions = kontorstruktur.map((k) => ({
@@ -105,53 +84,6 @@ export function TiltakDokumentForm() {
             placeholder="Administratorer"
             options={administratorOptions}
           />
-
-          <Separator />
-          <Heading size="small" level="3">
-            Arrangør (valgfritt)
-          </Heading>
-
-          <FormCombobox<TiltakDokumentFormValues>
-            name="arrangorId"
-            label={gjennomforingTekster.tiltaksarrangorUnderenhetLabel}
-            placeholder="Søk etter arrangør"
-            options={arrangorOptions}
-            onChange={setArrangorQuery}
-            filteredOptions={arrangorOptions}
-          />
-
-          <VStack>
-            <FormComboboxMulti<TiltakDokumentFormValues>
-              name="arrangorKontaktpersoner"
-              label={gjennomforingTekster.kontaktpersonerHosTiltaksarrangorLabel}
-              readOnly={!arrangorId}
-              placeholder="Velg kontaktpersoner"
-              options={arrangorKontaktpersonOptions}
-            />
-            <KontaktpersonButton
-              onClick={() => arrangorKontaktpersonerModalRef.current?.showModal()}
-              knappetekst="Opprett eller rediger kontaktpersoner"
-            />
-          </VStack>
-
-          {arrangorId && (
-            <ArrangorKontaktpersonerModal
-              arrangorId={arrangorId}
-              modalRef={arrangorKontaktpersonerModalRef}
-              onOpprettSuccess={(kontaktperson) => {
-                if (
-                  !kontaktperson.ansvarligFor.includes(ArrangorKontaktpersonAnsvar.GJENNOMFORING)
-                ) {
-                  return;
-                }
-                const kontaktpersoner = watch("arrangorKontaktpersoner");
-                setValue("arrangorKontaktpersoner", [
-                  ...kontaktpersoner.filter((k) => k !== kontaktperson.id),
-                  kontaktperson.id,
-                ]);
-              }}
-            />
-          )}
         </VStack>
       </Tabs.Panel>
 
