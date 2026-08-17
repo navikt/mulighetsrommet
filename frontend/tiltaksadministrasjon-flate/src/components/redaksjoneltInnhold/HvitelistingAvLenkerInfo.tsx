0c2b1ac63798e10c223c02e9f01929246df5b377
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-08-17 09:58:35 +0200

    Hvitelisting av eksterne lenker: Link til teamkatalogen

--- frontend/tiltaksadministrasjon-flate/src/components/redaksjoneltInnhold/HvitelistingAvLenkerInfo.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/redaksjoneltInnhold/HvitelistingAvLenkerInfo.tsx
@@ -1,19 +1,8 @@
-import { BodyShort, Box, CopyButton, HGrid, InfoCard, Link } from "@navikt/ds-react";
+import { BodyShort, InfoCard, Link } from "@navikt/ds-react";
 import { InformationSquareIcon } from "@navikt/aksel-icons";
 
-const PORTEN_MELDE_HVITELISTING_URL =
-  "https://jira.adeo.no/plugins/servlet/desk/portal/542/create/2441?summary=Bestille åpning av lenke for Fagsystemportalen%2FModia";
-
-const PORTEN_SKJEMA_VERDIER = [
-  {
-    field: "Tjeneste",
-    value: "Nettverk - Ukategorisert",
-  },
-  {
-    field: "Ansvarlig gruppe",
-    value: "Team Secops Services",
-  },
-];
+const TEAM_VALP_TEAMKATALOG_URL =
+  "https://teamkatalog.nav.no/team/aa730c95-b437-497b-b1ae-0ccf69a10997";
 
 export function HvitelistingAvLenkerInfo() {
   return (
@@ -27,22 +16,11 @@ export function HvitelistingAvLenkerInfo() {
           i sikkerhetsfilteret hos Nav.
         </BodyShort>
         <BodyShort>
-          Endringsønsket{" "}
-          <Link target="_blank" href={PORTEN_MELDE_HVITELISTING_URL}>
-            meldes inn via Porten
+          Kontakt{" "}
+          <Link target="_blank" href={TEAM_VALP_TEAMKATALOG_URL}>
+            Team Valp
           </Link>{" "}
-          med følgende:
-          <Box paddingBlock="space-4">
-            <HGrid width="50%" columns="1fr auto auto" gap="space-4" align="center">
-              {PORTEN_SKJEMA_VERDIER.map(({ field, value }) => (
-                <>
-                  <b>{field}:</b>
-                  <span>{value}</span>
-                  <CopyButton size="small" copyText={value} />
-                </>
-              ))}
-            </HGrid>
-          </Box>
+          om lenken(e) ikke fungerer via Modia.
         </BodyShort>
       </InfoCard.Content>
     </InfoCard>
