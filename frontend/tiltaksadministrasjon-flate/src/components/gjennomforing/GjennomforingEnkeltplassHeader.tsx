e46775290d708cd03ca84ef40c15e3e1368909cb
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-08-17 07:56:43 +0200

    Adjust GjennomforingEnkeltplassHeader

--- frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/GjennomforingEnkeltplassHeader.tsx
+++ frontend/tiltaksadministrasjon-flate/src/components/gjennomforing/GjennomforingEnkeltplassHeader.tsx
@@ -8,26 +8,44 @@ import { DataElementStatusTag } from "@mr/frontend-common";
 interface Props {
   gjennomforing: GjennomforingEnkeltplassDto;
   deltaker: DeltakerDto;
+  short?: boolean;
 }
 
-export function GjennomforingEnkeltplassHeader({ gjennomforing, deltaker }: Props) {
+export function GjennomforingEnkeltplassHeader({ gjennomforing, deltaker, short = false }: Props) {
   return (
     <VStack className="px-4 bg-ax-bg-default">
       <Separator />
       <HStack className="grid grid-flow-col auto-cols-[minmax(0,400px)] justify-start gap-4">
+        <MetadataVStack
+          label={
+            <HStack gap="space-4" wrap={false}>
+              <PersonIcon fontSize="1.5rem" />
+              Navn
+            </HStack>
+          }
+          value={deltaker.navn}
+        />
         <HStack>
-          <PersonIcon fontSize="1.5rem" />
-          <MetadataVStack label="Navn" value={deltaker.navn} />
-        </HStack>
-        <HStack>
-          <MetadataVStack label="Fødselsnummer" value={deltaker.norskIdent} />
-          <CopyButton size="small" copyText={deltaker.norskIdent ?? ""} />
+          <MetadataVStack
+            label="Fødselsnummer"
+            value={
+              <HStack gap="space-4" wrap={false}>
+                {deltaker.norskIdent}
+                <CopyButton size="small" copyText={deltaker.norskIdent ?? ""} />
+              </HStack>
+            }
+          />
         </HStack>
         <MetadataVStack label="Enhet" value={deltaker.oppfolgingEnhet?.navn} />
-        <MetadataVStack label="Arrangør" value={gjennomforing.arrangor.navn} />
-        <MetadataVStack label="Startdato" value={formaterDato(deltaker.startDato)} />
-        <MetadataVStack label="Sluttdato" value={formaterDato(deltaker.sluttDato)} />
-        <MetadataVStack label="Status" value={<DataElementStatusTag {...deltaker.status} />} />
+        {short && <MetadataVStack label="Veileder" value={deltaker.navVeilederNavn} />}
+        {!short && (
+          <>
+            <MetadataVStack label="Arrangør" value={gjennomforing.arrangor.navn} />
+            <MetadataVStack label="Startdato" value={formaterDato(deltaker.startDato)} />
+            <MetadataVStack label="Sluttdato" value={formaterDato(deltaker.sluttDato)} />
+            <MetadataVStack label="Status" value={<DataElementStatusTag {...deltaker.status} />} />
+          </>
+        )}
       </HStack>
       <Separator />
     </VStack>
