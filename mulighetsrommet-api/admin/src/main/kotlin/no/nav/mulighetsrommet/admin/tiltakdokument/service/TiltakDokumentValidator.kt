061fa09fa1fe69f83042aad27e2241807f9151c4
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-08-19 09:04:02 +0200

    det går å oppdatere tiltak dokument på gammel tiltakstype
    når den allerede eksisterer

--- mulighetsrommet-api/admin/src/main/kotlin/no/nav/mulighetsrommet/admin/tiltakdokument/service/TiltakDokumentValidator.kt
+++ mulighetsrommet-api/admin/src/main/kotlin/no/nav/mulighetsrommet/admin/tiltakdokument/service/TiltakDokumentValidator.kt
@@ -1,6 +1,7 @@
 package no.nav.mulighetsrommet.admin.tiltakdokument.service
 
 import arrow.core.Either
+import no.nav.mulighetsrommet.admin.tiltakdokument.TiltakDokumentDto
 import no.nav.mulighetsrommet.api.domain.tiltak.Tiltakstype
 import no.nav.mulighetsrommet.api.domain.tiltakdokument.TiltakDokument
 import no.nav.mulighetsrommet.api.domain.tiltakdokument.TiltakDokument.Kontaktperson
@@ -9,9 +10,13 @@ import no.nav.mulighetsrommet.model.Tiltakskode
 import no.nav.mulighetsrommet.validation.validation
 
 object TiltakDokumentValidator {
-    fun validate(request: TiltakDokumentRequest, tiltakstype: Tiltakstype): Either<List<FieldError>, TiltakDokument> = validation {
+    fun validate(
+        request: TiltakDokumentRequest,
+        tiltakstype: Tiltakstype,
+        previous: TiltakDokumentDto?,
+    ): Either<List<FieldError>, TiltakDokument> = validation {
         validate(
-            tiltakstype.tiltakskode !in listOf(
+            previous != null || tiltakstype.tiltakskode !in listOf(
                 Tiltakskode.ENKELTPLASS_ARBEIDSMARKEDSOPPLAERING,
                 Tiltakskode.ENKELTPLASS_FAG_OG_YRKESOPPLAERING,
             ),
