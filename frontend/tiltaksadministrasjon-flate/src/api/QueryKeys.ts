11baf1eccc6f59d6296ef48fbfc61f0e7ba359a2
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-08-31 10:24:01 +0200

    Legg til opphørsknapp

--- frontend/tiltaksadministrasjon-flate/src/api/QueryKeys.ts
+++ frontend/tiltaksadministrasjon-flate/src/api/QueryKeys.ts
@@ -87,6 +87,7 @@ export const QueryKeys = {
   opplaeringtilskudd: () => ["opplaeringtilskudd"],
   tilskuddBehandlinger: (gjennomforingId?: string) => ["tilskudd-behandling", gjennomforingId],
   tilskuddBehandling: (behandlingId: string) => ["tilskudd-behandling", behandlingId],
+  tilskudd: (tilskuddId: string) => ["tilskudd", tilskuddId],
   tiltakDokumenter: (filter?: object) => ["tiltak-dokumenter", filter] as const,
   tiltakDokument: (id?: string) => ["tiltak-dokument", id] as const,
   tiltakDokumentHandlinger: (id: string) => ["tiltak-dokument-handlinger", id] as const,
