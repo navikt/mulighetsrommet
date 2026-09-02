f6eaf145172e4d6a02feed0cf8ad322fd94148b5
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-09-02 11:44:41 +0200

    Oppdater migrasjoner - tilskudd revurdering relasjon

--- mulighetsrommet-api/persistence/src/main/resources/db/migration/V533__bruker_utbetaling_forbered_kompositt_pk.sql
+++ mulighetsrommet-api/persistence/src/main/resources/db/migration/V533__bruker_utbetaling_forbered_kompositt_pk.sql
@@ -6,13 +6,11 @@ alter table bruker_utbetaling
     alter column behandling_id type integer using behandling_id::integer;
 
 alter table tilskudd
-    add column bruker_utbetaling_behandling_id integer;
+    add column bruker_utbetaling_behandling_id integer,
+    drop constraint tilskudd_bruker_utbetaling_id_fkey;
 
 update tilskudd t
 set bruker_utbetaling_behandling_id = bu.behandling_id
 from bruker_utbetaling bu
 where t.bruker_utbetaling_id = bu.id
   and t.bruker_utbetaling_behandling_id is null;
-
-create index idx_tilskudd_bruker_utbetaling_id_behandling_id
-    on tilskudd (bruker_utbetaling_id, bruker_utbetaling_behandling_id);
--- mulighetsrommet-api/persistence/src/main/resources/db/migration/V534__bruker_utbetaling_kompositt_pk.sql
+++ mulighetsrommet-api/persistence/src/main/resources/db/migration/V534__bruker_utbetaling_kompositt_pk.sql
@@ -1,9 +1,6 @@
 -- Bytt primærnøkkel i bruker_utbetaling til kompositt (id, behandling_id)
 -- oppdater tilskudd FK til å referere til kompositt
 
-alter table tilskudd
-    drop constraint if exists tilskudd_bruker_utbetaling_id_fkey;
-
 alter table bruker_utbetaling
     drop constraint bruker_utbetaling_pkey;
 
