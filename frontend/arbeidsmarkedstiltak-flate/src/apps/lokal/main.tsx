d87550b60e5c556af2ea84a66d746ef49a0c64f0
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-09-10 09:25:33 +0200

    flytt LokalApp til egen fil for å fikse fast refresh-advarsel

--- frontend/arbeidsmarkedstiltak-flate/src/apps/lokal/main.tsx
+++ frontend/arbeidsmarkedstiltak-flate/src/apps/lokal/main.tsx
@@ -1,13 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { useState } from "react";
-import { APPLICATION_NAME, AppTheme } from "@/constants";
-import { ModiaContext } from "@/apps/modia/ModiaContext";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router";
-import { NavArbeidsmarkedstiltak } from "@/apps/nav/NavArbeidsmarkedstiltak";
-import { PreviewArbeidsmarkedstiltak } from "@/apps/nav/PreviewArbeidsmarkedstiltak";
-import { ModiaArbeidsmarkedstiltak } from "@/apps/modia/ModiaArbeidsmarkedstiltak";
-import { ReactQueryProvider } from "@/ReactQueryProvider";
-import { Switch } from "@navikt/ds-react";
+import { APPLICATION_NAME } from "@/constants";
+import { LokalApp } from "./LokalApp";
 import "../../index.css";
 
 if (import.meta.env.VITE_MULIGHETSROMMET_API_MOCK === "true") {
@@ -30,62 +23,4 @@ function render() {
     const root = createRoot(container);
     root.render(<LokalApp />);
   }
-}
-
-function LokalApp() {
-  const [theme, setTheme] = useState<AppTheme>("light");
-
-  return (
-    <div>
-      <Router>
-        <div className="p-2 flex justify-between items-center">
-          <ul>
-            <li>
-              <Link to="/arbeidsmarkedstiltak">Modia Arbeidsmarkedstiltak</Link>
-            </li>
-            <li>
-              <Link to="/nav">Nav Arbeidsmarkedstiltak</Link>
-            </li>
-            <li>
-              <Link to="/preview">Preview Arbeidsmarkedstiltak</Link>
-            </li>
-          </ul>
-          <Switch
-            checked={theme === "dark"}
-            onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
-          >
-            Mørk modus (for Modia)
-          </Switch>
-        </div>
-        <hr />
-
-        <Routes>
-          <Route
-            path="arbeidsmarkedstiltak/*"
-            element={
-              <ModiaContext key="modia" contextData={{ fnr: "12345678910", enhet: "0315" }}>
-                <ModiaArbeidsmarkedstiltak theme={theme} />
-              </ModiaContext>
-            }
-          />
-          <Route
-            path="nav/*"
-            element={
-              <ReactQueryProvider>
-                <NavArbeidsmarkedstiltak />
-              </ReactQueryProvider>
-            }
-          />
-          <Route
-            path="preview/*"
-            element={
-              <ReactQueryProvider>
-                <PreviewArbeidsmarkedstiltak />
-              </ReactQueryProvider>
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
-  );
 }
