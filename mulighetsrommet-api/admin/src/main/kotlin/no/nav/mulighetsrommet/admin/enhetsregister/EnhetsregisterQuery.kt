b05de535df7207b973a99bb802854a3dca805829
Sondre Lefsaker <sondre.lefsaker@nav.no>
2026-09-14 15:11:53 +0200

    inkluder lokalt registrerte arrangører i resultat fra EnhetsregisterQuery
    I tillegg til utenlandske arrangører så støtter vi nå å hente andre alle
    evt. egenregistrerte arrangører. Årsaken til at dette trengs er at vi
    per dags dato går mot produksjonsmiljøet til brreg også i Dev-miljøet
    (pga arv fra q2-miljøet til Arena), men for å kunne teste integrasjoner
    mot Altinn så trenger vi å kunne teste med bedrifter fra
    tt02-registeret. Vi har allerede et par slike test-arrangører registrert
    i dev, og denne endringen gjør at klienter (Tiltadm/Modia) får
    muligheten til å velge disse også.

--- mulighetsrommet-api/admin/src/main/kotlin/no/nav/mulighetsrommet/admin/enhetsregister/EnhetsregisterQuery.kt
+++ mulighetsrommet-api/admin/src/main/kotlin/no/nav/mulighetsrommet/admin/enhetsregister/EnhetsregisterQuery.kt
@@ -11,35 +11,80 @@ import no.nav.mulighetsrommet.model.Organisasjonsnummer
 
 /**
  * Enhetsregisteret er vår generaliserte modell av virksomheter, uavhengig av om de er hentet fra
- * Brreg (via [EnhetsregisterGateway]) eller er utenlandske/slettede virksomheter som i dag er
- * lagret direkte i `arrangor`-tabellen (siden Brreg som et 3.-parts register ikke har data om
- * utenlandske virksomheter). Denne klassen eier logikken for å kombinere de to kildene.
+ * Brreg (via [EnhetsregisterGateway]) eller er utenlandske/slettede/manuelt registrerte
+ * virksomheter som i dag er lagret direkte i `arrangor`-tabellen.
+ * Denne klassen eier logikken for å kombinere de to kildene.
  */
 class EnhetsregisterQuery(
     private val gateway: EnhetsregisterGateway,
     private val db: AdminDatabase,
 ) {
+    /**
+     * Henter hovedenheter fra enhetsregisteret basert på [sok], som kan være formatert som enten
+     * selskapsnavne eller organisasjonsnummer.
+     *
+     * Inkluderer også treff på arrangører fra eget register, så fremt disse ikke allerede blir
+     * returnert fra Enhetsregisteret.
+     */
     suspend fun sokHovedenheter(sok: String): Either<EnhetsregisterError, List<Virksomhet.Hovedenhet>> {
         if (sok.isBlank()) {
             return EnhetsregisterError.UgyldigSok().left()
         }
 
         return gateway.sokHovedenheter(sok).map { hovedenheter ->
-            val utenlandske = db.session {
-                queries.arrangor.getAll(sok = sok, typer = setOf(ArrangorType.UTENLANDSK)).items.map { it.toHovedenhet() }
+            val hovedenheterFraEgetRegister = db.session {
+                queries.arrangor.getAll(
+                    sok = sok,
+                    typer = setOf(ArrangorType.NORSK_HOVEDENHET, ArrangorType.UTENLANDSK),
+                    slettet = false,
+                )
             }
-            hovedenheter + utenlandske
+
+            val orgnrEnhetsregisteret = hovedenheter.map { it.organisasjonsnummer }.toSet()
+            hovedenheter + hovedenheterFraEgetRegister.items
+                .filter { it.organisasjonsnummer !in orgnrEnhetsregisteret }
+                .map { it.toHovedenhet() }
         }
     }
 
+    /**
+     * Henter hovedenheter fra enhetsregisteret basert på [sok], som kan være formatert som enten
+     * selskapsnavne eller organisasjonsnummer.
+     *
+     * Inkluderer også treff på arrangører fra eget register, så fremt disse ikke allerede blir
+     * returnert fra Enhetsregisteret.
+     */
     suspend fun sokUnderenheter(sok: String): Either<EnhetsregisterError, List<Virksomhet.Underenhet>> {
         if (sok.isBlank()) {
             return EnhetsregisterError.UgyldigSok().left()
         }
 
-        return gateway.sokUnderenheter(sok)
+        return gateway.sokUnderenheter(sok).map { underenheter ->
+            val underenheterFraEgetRegister = db.session {
+                queries.arrangor.getAll(
+                    sok = sok,
+                    typer = setOf(ArrangorType.NORSK_UNDERENHET, ArrangorType.UTENLANDSK),
+                    slettet = false,
+                )
+            }
+
+            val orgnrEnhetsregisteret = underenheter.map { it.organisasjonsnummer }.toSet()
+            underenheter + underenheterFraEgetRegister.items
+                .filter { it.organisasjonsnummer !in orgnrEnhetsregisteret }
+                .map { it.toUnderenhet() }
+        }
     }
 
+    /**
+     * Henter alle underenheter for gitt [orgnr], inkludert underenheter fra eget register.
+     *
+     * Om [orgnr] er en utenlandsk virksomhet blir denne returnert direkte for å støtte opprettelse
+     * av avtaler/gjennomføringer.
+     *
+     * Slettede (altså enheter slettet fra enhetsregisteret) blir også inkludert slik at disse kan
+     * vises i eksisterende avtaler/gjennomføringer (selv om det ikke er lov å opprette nye
+     * avtaler/gjennomføringer knyttet til disse arrangørene).
+     */
     suspend fun hentUnderenheterForHovedenhet(orgnr: Organisasjonsnummer): Either<EnhetsregisterError, List<Virksomhet.Underenhet>> {
         val arrangor = db.session { repository.arrangor.getByOrganisasjonsnummer(orgnr) }
         if (arrangor != null && arrangor is Arrangor.Utenlandsk) {
@@ -55,14 +100,14 @@ class EnhetsregisterQuery(
         }
 
         return gateway.hentUnderenheterForHovedenhet(orgnr).map { underenheter ->
-            val slettede = db.session {
-                queries.arrangor.getAll(overordnetEnhetOrgnr = orgnr, slettet = true).items.map {
-                    it.toUnderenhet()
-                }
+            val underenheterFraEgetRegister = db.session {
+                queries.arrangor.getAll(overordnetEnhetOrgnr = orgnr)
             }
-            // Kombinerer resultat med virksomheter som er slettet fra brreg for å støtte
-            // avtaler/gjennomføringer som henger etter
-            underenheter + slettede
+
+            val orgnrEnhetsregisteret = underenheter.map { it.organisasjonsnummer }.toSet()
+            underenheter + underenheterFraEgetRegister.items
+                .filter { it.organisasjonsnummer !in orgnrEnhetsregisteret }
+                .map { it.toUnderenhet() }
         }
     }
 }
