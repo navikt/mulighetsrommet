e790e815a2dc3ba15e3371b1cd51538adf59c670
ovin-nav <ovin.eilertsen.teige@nav.no>
2026-09-15 14:29:04 +0200

    Hent ut alle tilskudd for en gjennomforing

--- mulighetsrommet-api/server/src/main/kotlin/no/nav/mulighetsrommet/api/tilskuddbehandling/api/TilskuddRoutes.kt
+++ mulighetsrommet-api/server/src/main/kotlin/no/nav/mulighetsrommet/api/tilskuddbehandling/api/TilskuddRoutes.kt
@@ -2,6 +2,7 @@ package no.nav.mulighetsrommet.api.tilskuddbehandling.api
 
 import io.github.smiley4.ktoropenapi.get
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.http.content.default
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.route
@@ -11,6 +12,7 @@ import no.nav.mulighetsrommet.api.domain.navansatt.Rolle
 import no.nav.mulighetsrommet.api.navansatt.ktor.authorize
 import no.nav.mulighetsrommet.api.plugins.pathParameterUuid
 import no.nav.mulighetsrommet.api.tilskuddbehandling.db.Tilskudd
+import no.nav.mulighetsrommet.api.tilskuddbehandling.db.TilskuddKompakt
 import no.nav.mulighetsrommet.model.ProblemDetail
 import org.koin.ktor.ext.inject
 import java.util.*
@@ -18,8 +20,31 @@ import java.util.*
 fun Route.tilskuddRoutes() {
     val db: ApiDatabase by inject()
 
-    route("tilskudd") {
+    route("gjennomforing/{gjennomforingId}/tilskudd") {
         authorize(Rolle.SAKSBEHANDLER_OKONOMI) {
+            get( {
+                description = "Hent alle tilskudds for en gjennomføring"
+                tags = setOf("Tilskudd")
+                operationId = "getAllTilskuddKompakt"
+                request {
+                    pathParameterUuid("gjennomforingId")
+                }
+                response {
+                    code(HttpStatusCode.OK) {
+                        description = "Liste av tilskudd"
+                        body<List<TilskuddKompakt>>()
+                    }
+                    default {
+                        description = "Problem details"
+                        body<ProblemDetail>()
+                    }
+                }
+            }) {
+                val gjennomforingId = call.parameters.getOrFail<UUID>("gjennomforingId")
+                val result = db.session { queries.tilskudd.getAll(gjennomforingId) }
+                call.respond(result)
+            }
+
             get("/{tilskuddId}", {
                 description = "Hent tilskudds gitt id"
                 tags = setOf("Tilskudd")
