98f384c10179dada4559c56c93b92bbedd8061bb
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2026-09-22 15:04:23 +0200

    Fjern suspendTransaction/session funksjoner
    Fører tydeligvis til tråddeadlock

--- mulighetsrommet-api/admin/src/testFixtures/kotlin/no/nav/mulighetsrommet/admin/testing/TestAdminDatabase.kt
+++ mulighetsrommet-api/admin/src/testFixtures/kotlin/no/nav/mulighetsrommet/admin/testing/TestAdminDatabase.kt
@@ -11,8 +11,4 @@ class TestAdminDatabase(private val ctx: TestQueryContext = TestQueryContext()) 
     override fun <T> session(block: QueryContext.() -> T): T = block(ctx)
 
     override fun <T> transaction(block: QueryContext.() -> T): T = block(ctx)
-
-    override suspend fun <T> suspendSession(block: suspend QueryContext.() -> T): T = block(ctx)
-
-    override suspend fun <T> suspendTransaction(block: suspend QueryContext.() -> T): T = block(ctx)
 }
